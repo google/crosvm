b657603f10daf62fa6fe18d56adbe35f6caf8587
Chirantan Ekbote <chirantan@chromium.org>
2018-04-04 18:16:43 -0700

    p9: Add server implementation
    Implement all the server methods for the 9P protocol.
    
    BUG=chromium:703939
    TEST=bonnie++ -r 256
    
    Change-Id: I6b1b5fe4fea4d4941db42e5c1a364a54d0827054
    Signed-off-by: Chirantan Ekbote <chirantan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1013203
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- p9/src/lib.rs
+++ p9/src/lib.rs
@@ -2,13 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+extern crate libc;
+
 #[macro_use]
 extern crate wire_format_derive;
 
 mod protocol;
+mod server;
 
-#[derive(P9WireFormat)]
-struct Test {
-    a: u32,
-    b: u16,
-}
+pub use server::Server;
