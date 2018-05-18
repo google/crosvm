d8f10af6aeaff319f407578cdff1afd0324f5912
Manoj Gupta <manojgupta@google.com>
2018-05-18 11:33:19 -0700

    crosvm: Switch to cc crate.
    We do not want to add dependencies on GCC. Switch to cc crate
    instead of gcc to honor CC setting.
    
    CQ-DEPEND=CL:1066462
    
    BUG=chromium:814480
    TEST=emerge-{eve,kevin} crosvm works.
    
    Change-Id: I4e846b2080503e5617ed66e709f7af5263c98fba
    Reviewed-on: https://chromium-review.googlesource.com/1066461
    Commit-Ready: Manoj Gupta <manojgupta@chromium.org>
    Tested-by: Manoj Gupta <manojgupta@chromium.org>
    Reviewed-by: Zach Reizner <zachr@chromium.org>

--- sys_util/build.rs
+++ sys_util/build.rs
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-extern crate gcc;
+extern crate cc;
 
 fn main() {
-    gcc::Build::new()
+    cc::Build::new()
            .file("sock_ctrl_msg.c")
            .compile("sock_ctrl_msg");
 }
