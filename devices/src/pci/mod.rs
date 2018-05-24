9b871c2db3feaee1c311ba0c49aaf50637994f71
Dylan Reid <dgreid@chromium.org>
2018-05-24 19:19:22 +0000

    devices: Add pci_types
    Start PCI work by defining an enum to represent the four PCI interrupt
    lines.
    
    Change-Id: Ib95a4e4a03f0d6917ed2bed4b1afb97d18ff4f9e
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1072573
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>

---
+++ devices/src/pci/mod.rs
@@ -0,0 +1,20 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Implements pci devices and busses.
+
+/// PCI has four interrupt pins A->D.
+#[derive(Copy, Clone)]
+pub enum PciInterruptPin {
+    IntA,
+    IntB,
+    IntC,
+    IntD,
+}
+
+impl PciInterruptPin {
+    pub fn to_mask(self) -> u32 {
+        self as u32
+    }
+}
