c13a68eecb6a813a5c979ec52b09e795c1da7493
Jingkui Wang <jkwang@google.com>
2018-11-08 13:43:03 -0800

    kvm: add Debug to IoeventAddress
    BUG=None
    TEST=local build
    
    Change-Id: I5edede32f4289c1e369aa46311966cfcd0c48f2c
    Reviewed-on: https://chromium-review.googlesource.com/1327515
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Jingkui Wang <jkwang@google.com>
    Reviewed-by: David Tolnay <dtolnay@chromium.org>
    Reviewed-by: Zach Reizner <zachr@chromium.org>

--- kvm/src/lib.rs
+++ kvm/src/lib.rs
@@ -241,7 +241,7 @@ impl AsRawFd for Kvm {
 }
 
 /// An address either in programmable I/O space or in memory mapped I/O space.
-#[derive(Copy, Clone, MsgOnSocket)]
+#[derive(Copy, Clone, Debug, MsgOnSocket)]
 pub enum IoeventAddress {
     Pio(u64),
     Mmio(u64),
