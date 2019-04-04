2a2e2587b1799f265f5f1631228e9865fbf527ad
Zach Reizner <zachr@google.com>
2019-04-04 10:58:12 -0700

    rand_ish: prevent spaces in urandom_str and use uniform samples only
    TEST=cargo test -p rand_ish
    BUG=None
    
    Change-Id: I8e91be856c3bc9148dc373c7bc2cf6c8731cb0d3
    Reviewed-on: https://chromium-review.googlesource.com/1553563
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Tested-by: Zach Reizner <zachr@chromium.org>
    Reviewed-by: Daniel Prilik <prilik@google.com>
    Reviewed-by: David Tolnay <dtolnay@chromium.org>

--- rand_ish/src/lib.rs
+++ rand_ish/src/lib.rs
@@ -27,16 +27,49 @@ impl SimpleRng {
     }
 }
 
-/// Samples `/dev/urandom` and generates a random ASCII string of length `len`
-pub fn urandom_str(len: usize) -> io::Result<String> {
-    const ASCII_CHARS: &'static [u8] = b"
+// Uniformly samples the ASCII alphanumeric characters given a random variable. If an `Err` is
+// passed in, the error is returned as `Some(Err(...))`. If the the random variable can not be used
+// to uniformly sample, `None` is returned.
+fn uniform_sample_ascii_alphanumeric(
+    b: Result<u8, std::io::Error>,
+) -> Option<Result<char, std::io::Error>> {
+    const ASCII_CHARS: &'static [u8] = b"\
           ABCDEFGHIJKLMNOPQRSTUVWXYZ\
           abcdefghijklmnopqrstuvwxyz\
           0123456789";
+    let char_index = match b {
+        Ok(c) => c as usize,
+        Err(e) => return Some(Err(e)),
+    };
+    // Throw away numbers that would cause sampling bias.
+    if char_index >= ASCII_CHARS.len() * 4 {
+        None
+    } else {
+        Some(Ok(ASCII_CHARS[char_index % ASCII_CHARS.len()] as char))
+    }
+}
 
+/// Samples `/dev/urandom` and generates a random ASCII string of length `len`
+pub fn urandom_str(len: usize) -> io::Result<String> {
     File::open("/dev/urandom")?
         .bytes()
-        .map(|b| b.map(|b| ASCII_CHARS[b as usize % ASCII_CHARS.len()] as char))
+        .filter_map(uniform_sample_ascii_alphanumeric)
         .take(len)
         .collect::<io::Result<String>>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_100() {
+        for i in 0..100 {
+            let s = urandom_str(i).unwrap();
+            assert_eq!(s.len(), i);
+            assert!(s.is_ascii());
+            assert!(!s.contains(' '));
+            assert!(!s.contains(|c: char| !c.is_ascii_alphanumeric()));
+        }
+    }
 }
