d6945a09b8aa4ea03abac7a1adc0d2990ca8a5e7
Stephen Barber <smbarber@chromium.org>
2019-04-22 15:59:43 -0700

    crosvm: add license blurb to all files
    A few files were missing license blurbs at the top, so update them all
    to include them.
    
    BUG=none
    TEST=none
    
    Change-Id: Ida101be2e5c255b8cffeb15f5b93f63bfd1b130b
    Reviewed-on: https://chromium-review.googlesource.com/1577900
    Commit-Ready: Stephen Barber <smbarber@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- protos/src/plugin.rs
+++ protos/src/plugin.rs
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 pub use crate::generated::plugin::*;
 
 /// Converts protobuf representation of CpuId data into KVM format.
--- tpm2/src/lib.rs
+++ tpm2/src/lib.rs
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 use std::os::raw::{c_int, c_uint};
 use std::ptr;
 use std::slice;
