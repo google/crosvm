971589f7ec1af2904cd5a5b828e9008df9c0bba1
Zach Reizner <zachr@google.com>
2019-08-16 15:37:46 -0700

    docker: make using X11 gpu display easier with docker
    This change makes using docker a bit easier when displaying on the hosts
    X11 server. The biggest change was that build_crosvm.sh will install the
    hosts NVIDIA proprietary driver if needed.
    
    TEST=docker/build_crosvm.sh
         docker/crosvm_wrapper.sh
    BUG=None
    
    Change-Id: If64074715b0ad3a06727a4cc764d3ac06c4dfece
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/1759139
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Tested-by: Zach Reizner <zachr@chromium.org>
    Commit-Queue: Zach Reizner <zachr@chromium.org>

--- docker/build_crosvm.sh
+++ docker/build_crosvm.sh
@@ -6,11 +6,16 @@
 set -ex
 cd "${0%/*}"
 
+if command -v nvidia-smi >/dev/null 2>&1; then
+    export NVIDIA_VERSION="$(nvidia-smi -q | grep "Driver Version" | cut -d':' -f 2 | xargs)"
+fi
+
 src_root="$(realpath ..)"
 
 docker build -t crosvm \
     -f Dockerfile.crosvm \
     --build-arg UID \
     --build-arg GID \
+    --build-arg NVIDIA_VERSION \
     "$@" \
     "${src_root}"
--- docker/crosvm_wrapper.sh
+++ docker/crosvm_wrapper.sh
@@ -4,14 +4,19 @@
 # found in the LICENSE file.
 
 set -ex
+
+# grab the pwd before changing it to this script's directory
+pwd="${PWD}"
+
 cd "${0%/*}"
 
 exec docker run -it --rm \
     --privileged \
+    --ipc=host \
     -e DISPLAY=$DISPLAY -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
     -v /dev/log:/dev/log \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --volume "$PWD":/wd \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --volume "$pwd":/wd \
     --workdir /wd \
     crosvm \
     "$@"
