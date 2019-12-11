d65265c63665ee00c8df83c2613e7dbb7b32a3de
Dmitry Torokhov <dtor@chromium.org>
2019-12-11 13:36:08 -0800

    crosvm: fix removing comments when processing -file options
    As it turns out trim_end_matching() does not do what I expected (namely
    trim the string when it encounters matching substring) but rather
    expects to remove exactly matching suffix, so it actually fails to
    remove comments. Let's switch to using splitn() that does what I want.
    
    BUG=b:144454617
    TEST=Run Plugin VM
    
    Change-Id: Ibe8cd14d9d3c14ff99dd9abfa8f286e452d96913
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/1975095
    Tested-by: kokoro <noreply+kokoro@google.com>
    Tested-by: Dmitry Torokhov <dtor@chromium.org>
    Commit-Queue: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- src/main.rs
+++ src/main.rs
@@ -834,7 +834,7 @@ fn set_argument(cfg: &mut Config, name: &str, value: Option<&str>) -> argument::
             let reader = BufReader::new(file);
             for l in reader.lines() {
                 let line = l.unwrap();
-                let trimmed_line = line.trim_end_matches("#").trim();
+                let trimmed_line = line.splitn(2, '#').nth(0).unwrap().trim();
                 if !trimmed_line.is_empty() {
                     let mount = parse_plugin_mount_option(trimmed_line)?;
                     cfg.plugin_mounts.push(mount);
@@ -853,7 +853,7 @@ fn set_argument(cfg: &mut Config, name: &str, value: Option<&str>) -> argument::
             let reader = BufReader::new(file);
             for l in reader.lines() {
                 let line = l.unwrap();
-                let trimmed_line = line.trim_end_matches("#").trim();
+                let trimmed_line = line.splitn(2, '#').nth(0).unwrap().trim();
                 if !trimmed_line.is_empty() {
                     let map = parse_plugin_gid_map_option(trimmed_line)?;
                     cfg.plugin_gid_maps.push(map);
