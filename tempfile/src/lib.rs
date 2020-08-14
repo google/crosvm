62fb00d4bbdbec195f57e2fa9e0159b7976541c3
Daniel Verkamp <dverkamp@chromium.org>
2020-08-14 15:01:23 -0700

    tempfile: add tempfile() and NamedTempFile
    This simplifies the common case of creating a temporary file whose path
    does not need to be known, as well as a a temporary file with a known
    name.
    
    BUG=None
    TEST=cargo test -p tempfile
    
    Change-Id: I664b6097b5ae44ae3f1aa7f6c366271375a8df46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/2360458
    Tested-by: kokoro <noreply+kokoro@google.com>
    Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- tempfile/src/lib.rs
+++ tempfile/src/lib.rs
@@ -19,14 +19,28 @@
 //! }
 //! ```
 
-use libc::mkdtemp;
+use libc::{mkdtemp, mkstemp};
 use std::env;
 use std::ffi::CString;
-use std::fs;
+use std::fs::{self, File};
 use std::io::{Error, ErrorKind, Result};
 use std::mem::ManuallyDrop;
+use std::os::unix::io::FromRawFd;
 use std::path::{Path, PathBuf};
 use std::ptr;
+
+fn temp_path_template(prefix: &str) -> Result<CString> {
+    // mkdtemp()/mkstemp() require the template to end in 6 X chars, which will be replaced
+    // with random characters to make the path unique.
+    let path_template = env::temp_dir().join(format!("{}.XXXXXX", prefix));
+    match path_template.to_str() {
+        Some(s) => Ok(CString::new(s)?),
+        None => Err(Error::new(
+            ErrorKind::InvalidData,
+            "Path to string conversion failed",
+        )),
+    }
+}
 
 pub struct Builder {
     prefix: String,
@@ -53,18 +67,7 @@ impl Builder {
     /// temporary directory. The new directory will be removed when the returned
     /// handle of type `TempDir` is dropped.
     pub fn tempdir(&self) -> Result<TempDir> {
-        // mkdtemp() requires the template to end in 6 X chars, which will be replaced
-        // with random characters to make the path unique.
-        let path_template = env::temp_dir().join(format!("{}.XXXXXX", self.prefix));
-        let template = match path_template.to_str() {
-            Some(s) => CString::new(s)?,
-            None => {
-                return Err(Error::new(
-                    ErrorKind::InvalidData,
-                    "Path to string conversion failed",
-                ))
-            }
-        };
+        let template = temp_path_template(&self.prefix)?;
         let ptr = template.into_raw();
         // Safe because ownership of the buffer is handed off to mkdtemp() only
         // until it returns, and ownership is reclaimed by calling CString::from_raw()
@@ -81,6 +84,34 @@ impl Builder {
             path: PathBuf::from(path.to_str().map_err(|_| {
                 Error::new(ErrorKind::InvalidData, "Path to string conversion failed")
             })?),
+        })
+    }
+
+    /// Creates a new temporary file under libc's preferred system
+    /// temporary directory. The new file will be removed when the returned
+    /// handle of type `NamedTempFile` is dropped.
+    pub fn tempfile(&self) -> Result<NamedTempFile> {
+        let template = temp_path_template(&self.prefix)?;
+        let ptr = template.into_raw();
+        // Safe because ownership of the buffer is handed off to mkstemp() only
+        // until it returns, and ownership is reclaimed by calling CString::from_raw()
+        // on the same pointer returned by into_raw().
+        let (file, path) = unsafe {
+            let ret = mkstemp(ptr);
+            let path = CString::from_raw(ptr);
+            if ret < 0 {
+                return Err(Error::last_os_error());
+            }
+            (File::from_raw_fd(ret), path)
+        };
+
+        Ok(NamedTempFile {
+            path: TempPath {
+                path: PathBuf::from(path.to_str().map_err(|_| {
+                    Error::new(ErrorKind::InvalidData, "Path to string conversion failed")
+                })?),
+            },
+            file,
         })
     }
 }
@@ -135,9 +166,61 @@ impl Drop for TempDir {
     }
 }
 
+/// Temporary file with a known name.  The file will be removed when this object is dropped.
+pub struct NamedTempFile {
+    path: TempPath,
+    file: File,
+}
+
+impl NamedTempFile {
+    pub fn new() -> Result<Self> {
+        Builder::new().tempfile()
+    }
+
+    /// Accesses the temporary file's `Path`.
+    pub fn path(&self) -> &Path {
+        self.path.path.as_ref()
+    }
+
+    /// Accesses the temporary file's `File` object.
+    pub fn as_file(&self) -> &File {
+        &self.file
+    }
+
+    /// Accesses the temporary file's `File` object mutably.
+    pub fn as_file_mut(&mut self) -> &mut File {
+        &mut self.file
+    }
+
+    /// Convert this `TempFile` into an open `File` and unlink it from the filesystem.
+    pub fn into_file(self) -> File {
+        self.file
+    }
+}
+
+// Container for NamedTempFile's path so that it can be dropped separately from the File.
+struct TempPath {
+    path: PathBuf,
+}
+
+impl Drop for TempPath {
+    fn drop(&mut self) {
+        let _ = fs::remove_file(&self.path);
+    }
+}
+
+/// Create a new anonymous temporary file under the preferred system
+/// temporary directory. The new file will be removed when the returned
+/// `File` is dropped.
+pub fn tempfile() -> Result<File> {
+    Ok(NamedTempFile::new()?.into_file())
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::TempDir;
+    use std::io::{Read, Seek, SeekFrom, Write};
+
+    use crate::{tempfile, NamedTempFile, TempDir};
 
     #[test]
     fn create_dir() {
@@ -152,6 +235,31 @@ mod tests {
         let t = TempDir::new().unwrap();
         let path = t.path().to_owned();
         assert!(t.remove().is_ok());
+        assert!(!path.exists());
+    }
+
+    #[test]
+    fn create_file() {
+        let mut f = tempfile().expect("tempfile() failed");
+        f.write_all(&[0, 1, 2, 3]).unwrap();
+        f.seek(SeekFrom::Start(0)).unwrap();
+        let mut data = vec![0u8; 4];
+        f.read_exact(&mut data).unwrap();
+        assert_eq!(data, [0, 1, 2, 3]);
+    }
+
+    #[test]
+    fn create_named_file() {
+        let named_temp = NamedTempFile::new().unwrap();
+        let path = named_temp.path().to_owned();
+        assert!(path.exists());
+
+        // as_file() should not delete the file.
+        let _f = named_temp.as_file();
+        assert!(path.exists());
+
+        // Dropping the NamedTempFile should delete the file.
+        drop(named_temp);
         assert!(!path.exists());
     }
 }
