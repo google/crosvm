de498d14f4a941ec26b02a11d482596838a6d0e8
Daniel Verkamp <dverkamp@chromium.org>
2021-01-14 11:00:15 -0800

    fuse: supply a no-op Mapper for fuzzing
    The fuzz_server interface was updated to require a Mapper; however,
    fs_server fuzzer was not updated to provide one, and it does not seem
    necessary since the Mapper calls don't affect control flow in the
    server.  Instead, just provide a no-op Mapper inside fuzz_server.
    
    Fixes fs_server_fuzzer build.
    
    BUG=chromium:1166742
    TEST=FEATURES=test emerge-amd64-generic crosvm # --profile=asan
    
    Change-Id: I6f2d2c7156056976a2e5dd1d7b5a29a231608150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/2629970
    Tested-by: kokoro <noreply+kokoro@google.com>
    Tested-by: Manoj Gupta <manojgupta@chromium.org>
    Reviewed-by: Manoj Gupta <manojgupta@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-by: Chirantan Ekbote <chirantan@chromium.org>

--- fuse/src/fuzzing.rs
+++ fuse/src/fuzzing.rs
@@ -5,6 +5,9 @@
 use crate::filesystem::{DirEntry, DirectoryIterator, FileSystem, ZeroCopyReader, ZeroCopyWriter};
 use crate::server::{Mapper, Reader, Server, Writer};
 
+use std::io;
+use std::os::unix::io::AsRawFd;
+
 // Use a file system that does nothing since we are fuzzing the server implementation.
 struct NullFs;
 impl FileSystem for NullFs {
@@ -20,13 +23,28 @@ impl DirectoryIterator for NullIter {
     }
 }
 
-/// Fuzz the server implementation.
-pub fn fuzz_server<R: Reader + ZeroCopyReader, W: Writer + ZeroCopyWriter, M: Mapper>(
-    r: R,
-    w: W,
-    m: M,
-) {
-    let server = Server::new(NullFs);
+struct NullMapper;
+impl Mapper for NullMapper {
+    fn map(
+        &self,
+        _mem_offset: u64,
+        _size: usize,
+        _fd: &dyn AsRawFd,
+        _file_offset: u64,
+        _prot: u32,
+    ) -> io::Result<()> {
+        Err(io::Error::from_raw_os_error(libc::EOPNOTSUPP))
+    }
 
-    let _ = server.handle_message(r, w, m);
+    fn unmap(&self, _offset: u64, _size: u64) -> io::Result<()> {
+        Err(io::Error::from_raw_os_error(libc::EOPNOTSUPP))
+    }
+}
+
+/// Fuzz the server implementation.
+pub fn fuzz_server<R: Reader + ZeroCopyReader, W: Writer + ZeroCopyWriter>(r: R, w: W) {
+    let server = Server::new(NullFs);
+    let mapper = NullMapper {};
+
+    let _ = server.handle_message(r, w, &mapper);
 }
