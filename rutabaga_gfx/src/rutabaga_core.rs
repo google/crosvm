84b0978847e8d609630f2c19673de58b5d24f79e
Jason Macnak <natsu@google.com>
2021-03-04 10:16:39 -0800

    rutabaga_gfx: Handle rutabaga gfx update
    Having both renderers running at the same time can work,
    but not without changes.  For now, allowing building both
    at the same time.
    
    BUG=b:181869105, b:182174209
    Test: launch_cvd --gpu_mode=gfxstream
    Test: launch_cvd --gpu_mode=virgl_renderer
    
    Change-Id: Ib8a3fd4927401b9f7ebc4a4425a3ddba6621a3e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3092405
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Commit-Queue: Gurchetan Singh <gurchetansingh@chromium.org>

--- rutabaga_gfx/src/rutabaga_core.rs
+++ rutabaga_gfx/src/rutabaga_core.rs
@@ -719,7 +719,7 @@ impl RutabagaBuilder {
             rutabaga_components.insert(RutabagaComponentType::Rutabaga2D, rutabaga_2d);
         } else {
             #[cfg(feature = "virgl_renderer")]
-            {
+            if self.default_component == RutabagaComponentType::VirglRenderer {
                 let virglrenderer_flags = self
                     .virglrenderer_flags
                     .ok_or(RutabagaError::InvalidRutabagaBuild)?;
@@ -742,7 +742,7 @@ impl RutabagaBuilder {
             }
 
             #[cfg(feature = "gfxstream")]
-            {
+            if self.default_component == RutabagaComponentType::Gfxstream {
                 let display_width = self
                     .display_width
                     .ok_or(RutabagaError::InvalidRutabagaBuild)?;
