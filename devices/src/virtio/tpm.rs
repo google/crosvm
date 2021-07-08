52c97d41bd26572018f93964e108101cb1e3092a
Chirantan Ekbote <chirantan@chromium.org>
2021-07-08 19:59:49 +0900

    tpm: Process all descriptors when notified
    In order to enable the VIRTIO_RING_F_EVENT_IDX feature, devices need to
    keep processing the queue until it is empty as the guest may not send
    any more notifications until the device has processed all pending
    messages.
    
    Change the process_queue method to process all messages in the queue.
    
    BUG=none
    TEST=????
    
    Change-Id: I7df56111ad99bc7511c685ecafc051aef077b34e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3026688
    Tested-by: kokoro <noreply+kokoro@google.com>
    Commit-Queue: Chirantan Ekbote <chirantan@chromium.org>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>

--- devices/src/virtio/tpm.rs
+++ devices/src/virtio/tpm.rs
@@ -81,23 +81,23 @@ impl Device {
 
 impl Worker {
     fn process_queue(&mut self) -> NeedsInterrupt {
-        let avail_desc = match self.queue.pop(&self.mem) {
-            Some(avail_desc) => avail_desc,
-            None => return NeedsInterrupt::No,
-        };
+        let mut needs_interrupt = NeedsInterrupt::No;
+        while let Some(avail_desc) = self.queue.pop(&self.mem) {
+            let index = avail_desc.index;
 
-        let index = avail_desc.index;
+            let len = match self.device.perform_work(&self.mem, avail_desc) {
+                Ok(len) => len,
+                Err(err) => {
+                    error!("{}", err);
+                    0
+                }
+            };
 
-        let len = match self.device.perform_work(&self.mem, avail_desc) {
-            Ok(len) => len,
-            Err(err) => {
-                error!("{}", err);
-                0
-            }
-        };
+            self.queue.add_used(&self.mem, index, len);
+            needs_interrupt = NeedsInterrupt::Yes;
+        }
 
-        self.queue.add_used(&self.mem, index, len);
-        NeedsInterrupt::Yes
+        needs_interrupt
     }
 
     fn run(mut self) {
