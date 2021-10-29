ef5b1e711bd87e95c1e0d2ea83c5f85b825d89fa
Keiichi Watanabe <keiichiw@chromium.org>
2021-10-30 03:29:31 +0900

    vmm_vhost: vhost_user: Split socket listener code into separate file
    More SocketListener code to socket.rs so that we can easily add a new
    file for a new VFIO backend. No behavior will be changed by this CL.
    
    BUG=b:194137301
    TEST=cargo test --all-features
    
    Change-Id: I8d987f85a63ba666619882559e5cceddd91814bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3268268
    Tested-by: kokoro <noreply+kokoro@google.com>
    Commit-Queue: Keiichi Watanabe <keiichiw@chromium.org>
    Reviewed-by: Chirantan Ekbote <chirantan@chromium.org>

--- third_party/vmm_vhost/src/vhost_user/connection.rs
+++ third_party/vmm_vhost/src/vhost_user/connection.rs
@@ -1,15 +1,18 @@
 // Copyright (C) 2019 Alibaba Cloud Computing. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-//! Structs for Unix Domain Socket listener and endpoint.
+//! Common data structures for listener and endpoint.
+
+mod socket;
+
+pub use self::socket::SocketListener;
 
 use std::fs::File;
-use std::io::ErrorKind;
 use std::marker::PhantomData;
 use std::mem;
 use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
-use std::os::unix::net::{UnixListener, UnixStream};
-use std::path::{Path, PathBuf};
+use std::os::unix::net::UnixStream;
+use std::path::Path;
 
 use data_model::DataInit;
 use libc::{c_void, iovec};
@@ -25,78 +28,6 @@ pub trait Listener: Sized {
 
     /// Change blocking status on the listener.
     fn set_nonblocking(&self, block: bool) -> Result<()>;
-}
-
-/// Unix domain socket listener for accepting incoming connections.
-pub struct SocketListener {
-    fd: UnixListener,
-    path: PathBuf,
-}
-
-impl SocketListener {
-    /// Create a unix domain socket listener.
-    ///
-    /// # Return:
-    /// * - the new Listener object on success.
-    /// * - SocketError: failed to create listener socket.
-    pub fn new<P: AsRef<Path>>(path: P, unlink: bool) -> Result<Self> {
-        if unlink {
-            let _ = std::fs::remove_file(&path);
-        }
-        let fd = UnixListener::bind(&path).map_err(Error::SocketError)?;
-        Ok(SocketListener {
-            fd,
-            path: path.as_ref().to_owned(),
-        })
-    }
-}
-
-impl Listener for SocketListener {
-    /// Accept an incoming connection.
-    ///
-    /// # Return:
-    /// * - Some(UnixStream): new UnixStream object if new incoming connection is available.
-    /// * - None: no incoming connection available.
-    /// * - SocketError: errors from accept().
-    fn accept(&self) -> Result<Option<UnixStream>> {
-        loop {
-            match self.fd.accept() {
-                Ok((socket, _addr)) => return Ok(Some(socket)),
-                Err(e) => {
-                    match e.kind() {
-                        // No incoming connection available.
-                        ErrorKind::WouldBlock => return Ok(None),
-                        // New connection closed by peer.
-                        ErrorKind::ConnectionAborted => return Ok(None),
-                        // Interrupted by signals, retry
-                        ErrorKind::Interrupted => continue,
-                        _ => return Err(Error::SocketError(e)),
-                    }
-                }
-            }
-        }
-    }
-
-    /// Change blocking status on the listener.
-    ///
-    /// # Return:
-    /// * - () on success.
-    /// * - SocketError: failure from set_nonblocking().
-    fn set_nonblocking(&self, block: bool) -> Result<()> {
-        self.fd.set_nonblocking(block).map_err(Error::SocketError)
-    }
-}
-
-impl AsRawFd for SocketListener {
-    fn as_raw_fd(&self) -> RawFd {
-        self.fd.as_raw_fd()
-    }
-}
-
-impl Drop for SocketListener {
-    fn drop(&mut self) {
-        let _ = std::fs::remove_file(&self.path);
-    }
 }
 
 /// Unix domain socket endpoint for vhost-user connection.
@@ -611,12 +542,6 @@ fn advance_slices(bufs: &mut &mut [&[u8]], mut count: usize) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::{Read, Seek, SeekFrom, Write};
-    use tempfile::{tempfile, Builder, TempDir};
-
-    fn temp_dir() -> TempDir {
-        Builder::new().prefix("/tmp/vhost_test").tempdir().unwrap()
-    }
 
     #[test]
     fn test_advance_slices() {
@@ -628,245 +553,5 @@ mod tests {
         advance_slices(&mut bufs, 10);
         assert_eq!(bufs[0], [2; 14].as_ref());
         assert_eq!(bufs[1], [3; 8].as_ref());
-    }
-
-    #[test]
-    fn create_listener() {
-        let dir = temp_dir();
-        let mut path = dir.path().to_owned();
-        path.push("sock");
-        let listener = SocketListener::new(&path, true).unwrap();
-
-        assert!(listener.as_raw_fd() > 0);
-    }
-
-    #[test]
-    fn accept_connection() {
-        let dir = temp_dir();
-        let mut path = dir.path().to_owned();
-        path.push("sock");
-        let listener = SocketListener::new(&path, true).unwrap();
-        listener.set_nonblocking(true).unwrap();
-
-        // accept on a fd without incoming connection
-        let conn = listener.accept().unwrap();
-        assert!(conn.is_none());
-    }
-
-    #[test]
-    fn send_data() {
-        let dir = temp_dir();
-        let mut path = dir.path().to_owned();
-        path.push("sock");
-        let listener = SocketListener::new(&path, true).unwrap();
-        listener.set_nonblocking(true).unwrap();
-        let mut master = Endpoint::<MasterReq>::connect(&path).unwrap();
-        let sock = listener.accept().unwrap().unwrap();
-        let mut slave = Endpoint::<MasterReq>::from_stream(sock);
-
-        let buf1 = vec![0x1, 0x2, 0x3, 0x4];
-        let mut len = master.send_slice(&buf1[..], None).unwrap();
-        assert_eq!(len, 4);
-        let (bytes, buf2, _) = slave.recv_into_buf(0x1000).unwrap();
-        assert_eq!(bytes, 4);
-        assert_eq!(&buf1[..], &buf2[..bytes]);
-
-        len = master.send_slice(&buf1[..], None).unwrap();
-        assert_eq!(len, 4);
-        let (bytes, buf2, _) = slave.recv_into_buf(0x2).unwrap();
-        assert_eq!(bytes, 2);
-        assert_eq!(&buf1[..2], &buf2[..]);
-        let (bytes, buf2, _) = slave.recv_into_buf(0x2).unwrap();
-        assert_eq!(bytes, 2);
-        assert_eq!(&buf1[2..], &buf2[..]);
-    }
-
-    #[test]
-    fn send_fd() {
-        let dir = temp_dir();
-        let mut path = dir.path().to_owned();
-        path.push("sock");
-        let listener = SocketListener::new(&path, true).unwrap();
-        listener.set_nonblocking(true).unwrap();
-        let mut master = Endpoint::<MasterReq>::connect(&path).unwrap();
-        let sock = listener.accept().unwrap().unwrap();
-        let mut slave = Endpoint::<MasterReq>::from_stream(sock);
-
-        let mut fd = tempfile().unwrap();
-        write!(fd, "test").unwrap();
-
-        // Normal case for sending/receiving file descriptors
-        let buf1 = vec![0x1, 0x2, 0x3, 0x4];
-        let len = master
-            .send_slice(&buf1[..], Some(&[fd.as_raw_fd()]))
-            .unwrap();
-        assert_eq!(len, 4);
-
-        let (bytes, buf2, files) = slave.recv_into_buf(4).unwrap();
-        assert_eq!(bytes, 4);
-        assert_eq!(&buf1[..], &buf2[..]);
-        assert!(files.is_some());
-        let files = files.unwrap();
-        {
-            assert_eq!(files.len(), 1);
-            let mut file = &files[0];
-            let mut content = String::new();
-            file.seek(SeekFrom::Start(0)).unwrap();
-            file.read_to_string(&mut content).unwrap();
-            assert_eq!(content, "test");
-        }
-
-        // Following communication pattern should work:
-        // Sending side: data(header, body) with fds
-        // Receiving side: data(header) with fds, data(body)
-        let len = master
-            .send_slice(
-                &buf1[..],
-                Some(&[fd.as_raw_fd(), fd.as_raw_fd(), fd.as_raw_fd()]),
-            )
-            .unwrap();
-        assert_eq!(len, 4);
-
-        let (bytes, buf2, files) = slave.recv_into_buf(0x2).unwrap();
-        assert_eq!(bytes, 2);
-        assert_eq!(&buf1[..2], &buf2[..]);
-        assert!(files.is_some());
-        let files = files.unwrap();
-        {
-            assert_eq!(files.len(), 3);
-            let mut file = &files[1];
-            let mut content = String::new();
-            file.seek(SeekFrom::Start(0)).unwrap();
-            file.read_to_string(&mut content).unwrap();
-            assert_eq!(content, "test");
-        }
-        let (bytes, buf2, files) = slave.recv_into_buf(0x2).unwrap();
-        assert_eq!(bytes, 2);
-        assert_eq!(&buf1[2..], &buf2[..]);
-        assert!(files.is_none());
-
-        // Following communication pattern should not work:
-        // Sending side: data(header, body) with fds
-        // Receiving side: data(header), data(body) with fds
-        let len = master
-            .send_slice(
-                &buf1[..],
-                Some(&[fd.as_raw_fd(), fd.as_raw_fd(), fd.as_raw_fd()]),
-            )
-            .unwrap();
-        assert_eq!(len, 4);
-
-        let (bytes, buf4) = slave.recv_data(2).unwrap();
-        assert_eq!(bytes, 2);
-        assert_eq!(&buf1[..2], &buf4[..]);
-        let (bytes, buf2, files) = slave.recv_into_buf(0x2).unwrap();
-        assert_eq!(bytes, 2);
-        assert_eq!(&buf1[2..], &buf2[..]);
-        assert!(files.is_none());
-
-        // Following communication pattern should work:
-        // Sending side: data, data with fds
-        // Receiving side: data, data with fds
-        let len = master.send_slice(&buf1[..], None).unwrap();
-        assert_eq!(len, 4);
-        let len = master
-            .send_slice(
-                &buf1[..],
-                Some(&[fd.as_raw_fd(), fd.as_raw_fd(), fd.as_raw_fd()]),
-            )
-            .unwrap();
-        assert_eq!(len, 4);
-
-        let (bytes, buf2, files) = slave.recv_into_buf(0x4).unwrap();
-        assert_eq!(bytes, 4);
-        assert_eq!(&buf1[..], &buf2[..]);
-        assert!(files.is_none());
-
-        let (bytes, buf2, files) = slave.recv_into_buf(0x2).unwrap();
-        assert_eq!(bytes, 2);
-        assert_eq!(&buf1[..2], &buf2[..]);
-        assert!(files.is_some());
-        let files = files.unwrap();
-        {
-            assert_eq!(files.len(), 3);
-            let mut file = &files[1];
-            let mut content = String::new();
-            file.seek(SeekFrom::Start(0)).unwrap();
-            file.read_to_string(&mut content).unwrap();
-            assert_eq!(content, "test");
-        }
-        let (bytes, buf2, files) = slave.recv_into_buf(0x2).unwrap();
-        assert_eq!(bytes, 2);
-        assert_eq!(&buf1[2..], &buf2[..]);
-        assert!(files.is_none());
-
-        // Following communication pattern should not work:
-        // Sending side: data1, data2 with fds
-        // Receiving side: data + partial of data2, left of data2 with fds
-        let len = master.send_slice(&buf1[..], None).unwrap();
-        assert_eq!(len, 4);
-        let len = master
-            .send_slice(
-                &buf1[..],
-                Some(&[fd.as_raw_fd(), fd.as_raw_fd(), fd.as_raw_fd()]),
-            )
-            .unwrap();
-        assert_eq!(len, 4);
-
-        let (bytes, _) = slave.recv_data(5).unwrap();
-        assert_eq!(bytes, 5);
-
-        let (bytes, _, files) = slave.recv_into_buf(0x4).unwrap();
-        assert_eq!(bytes, 3);
-        assert!(files.is_none());
-
-        // If the target fd array is too small, extra file descriptors will get lost.
-        let len = master
-            .send_slice(
-                &buf1[..],
-                Some(&[fd.as_raw_fd(), fd.as_raw_fd(), fd.as_raw_fd()]),
-            )
-            .unwrap();
-        assert_eq!(len, 4);
-
-        let (bytes, _, files) = slave.recv_into_buf(0x4).unwrap();
-        assert_eq!(bytes, 4);
-        assert!(files.is_some());
-    }
-
-    #[test]
-    fn send_recv() {
-        let dir = temp_dir();
-        let mut path = dir.path().to_owned();
-        path.push("sock");
-        let listener = SocketListener::new(&path, true).unwrap();
-        listener.set_nonblocking(true).unwrap();
-        let mut master = Endpoint::<MasterReq>::connect(&path).unwrap();
-        let sock = listener.accept().unwrap().unwrap();
-        let mut slave = Endpoint::<MasterReq>::from_stream(sock);
-
-        let mut hdr1 =
-            VhostUserMsgHeader::new(MasterReq::GET_FEATURES, 0, mem::size_of::<u64>() as u32);
-        hdr1.set_need_reply(true);
-        let features1 = 0x1u64;
-        master.send_message(&hdr1, &features1, None).unwrap();
-
-        let mut features2 = 0u64;
-        let slice = unsafe {
-            std::slice::from_raw_parts_mut(
-                (&mut features2 as *mut u64) as *mut u8,
-                mem::size_of::<u64>(),
-            )
-        };
-        let (hdr2, bytes, files) = slave.recv_body_into_buf(slice).unwrap();
-        assert_eq!(hdr1, hdr2);
-        assert_eq!(bytes, 8);
-        assert_eq!(features1, features2);
-        assert!(files.is_none());
-
-        master.send_header(&hdr1, None).unwrap();
-        let (hdr2, files) = slave.recv_header().unwrap();
-        assert_eq!(hdr1, hdr2);
-        assert!(files.is_none());
     }
 }
