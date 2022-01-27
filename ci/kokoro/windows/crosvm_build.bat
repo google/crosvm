71e74b0a643b37baedf4fab5eeb7cf2acaab7ede
Vikram Auradkar <auradkar@google.com>
2022-01-27 12:30:10 -0800

    Add kokoro build config file and helper script
    Build config sets timeout to 40 minutes.
    helper script relies on choco to install rustup, which is used
    to install rust toolchain.
    
    Bug: 213603713
    Test: Tested the toolchain setup part of the script in a
          codelab repo.
    Change-Id: Ibf139bbc4ff6fe6c811652767dd885584414dda6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3421515
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Brandon Amiosci <brandonamiosci@google.com>
    Commit-Queue: Vikram Auradkar <auradkar@google.com>

---
+++ ci/kokoro/windows/crosvm_build.bat
@@ -0,0 +1,43 @@
+:: Copyright 2022 The Chromium OS Authors. All rights reserved.
+:: Use of this source code is governed by a BSD-style license that can be
+:: found in the LICENSE file.
+
+:: Make environment changes (cd, env vars, etc.) local, so they don't affect the calling batch file
+setlocal
+
+:: Code under repo is checked out to %KOKORO_ARTIFACTS_DIR%\git.
+:: The final directory name in this path is determined by the scm name specified
+:: in the job configuration
+cd %KOKORO_ARTIFACTS_DIR%\git\crosvm
+
+:: Pin rustup to a known/tested version.
+set rustup_version=1.24.3
+
+:: Install rust toolchain through rustup.
+echo [%TIME%] installing rustup %rustup_version%
+choco install -y rustup.install --version=%rustup_version%
+
+:: Reload path for installed rustup binary
+call RefreshEnv.cmd
+
+set /p rust_version=<rust-toolchain
+echo [%TIME%] installing rust %rust_version%-x86_64-pc-windows-msvc
+rustup toolchain install %rust_version%-x86_64-pc-windows-msvc
+
+rustup component add clippy
+rustup component add rustfmt
+cargo install bindgen
+
+:: Reload path for installed rust toolchain.
+call RefreshEnv.cmd
+
+:: Log the version of the Rust toolchain
+echo [%TIME%] Using Rust toolchain version:
+cargo --version
+rustc --version
+
+echo [%TIME%] Calling crosvm\build_test.py
+py ./tools/windows/build_test.py --copy True --job_type kokoro --skip_file_name .windows_build_test_skip
+if %ERRORLEVEL% neq 0 ( exit /b %ERRORLEVEL% )
+
+exit /b %ERRORLEVEL%
