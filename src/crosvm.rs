google/crosvm

commit e464a7a6c0e701ed85a0edf6b7fa8da05b7b1ef1

gpu: prepare to start render server separately from GPU device
Separating the render server from virtio GPU code will allow us to start
it when starting plugin-based VMs.

BUG=None
TEST=./tools/presubmit

Change-Id: If31780effddfd2b3f21b6b6cbc0c9630931a3c9b
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3418733
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
Tested-by: kokoro <noreply+kokoro@google.com>
Commit-Queue: Dmitry Torokhov <dtor@chromium.org>

src/crosvm.rs
@@ -37,6 +37,7 @@ use devices::IommuDevType;
 use devices::StubPciParameters;
 use hypervisor::ProtectionType;
 use libc::{getegid, geteuid};
+use platform::GpuRenderServerParameters;
 use vm_control::BatteryType;
 
 static KVM_PATH: &str = "/dev/kvm";
@@ -374,6 +375,8 @@ pub struct Config {
     pub seccomp_log_failures: bool,
     #[cfg(feature = "gpu")]
     pub gpu_parameters: Option<GpuParameters>,
+    #[cfg(feature = "gpu")]
+    pub gpu_render_server_parameters: Option<GpuRenderServerParameters>,
     pub software_tpm: bool,
     pub display_window_keyboard: bool,
     pub display_window_mouse: bool,
@@ -474,6 +477,8 @@ impl Default for Config {
             cid: None,
             #[cfg(feature = "gpu")]
             gpu_parameters: None,
+            #[cfg(feature = "gpu")]
+            gpu_render_server_parameters: None,
             software_tpm: false,
             wayland_socket_paths: BTreeMap::new(),
             x_display: None,

devices/src/virtio/gpu/mod.rs
@@ -83,17 +83,9 @@ impl Default for GpuDisplayParameters {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct GpuRenderServerParameters {
-    pub path: PathBuf,
-    pub cache_path: Option<String>,
-    pub cache_size: Option<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
 #[serde(default)]
 pub struct GpuParameters {
     pub displays: Vec<GpuDisplayParameters>,
-    pub render_server: Option<GpuRenderServerParameters>,
     pub renderer_use_egl: bool,
     pub renderer_use_gles: bool,
     pub renderer_use_glx: bool,
@@ -120,7 +112,6 @@ impl Default for GpuParameters {
     fn default() -> Self {
         GpuParameters {
             displays: vec![],
-            render_server: None,
             renderer_use_egl: true,
             renderer_use_gles: true,
             renderer_use_glx: false,
@@ -1032,7 +1023,7 @@ impl Gpu {
             .use_surfaceless(gpu_parameters.renderer_use_surfaceless)
             .use_external_blob(external_blob)
             .use_venus(gpu_parameters.use_vulkan)
-            .use_render_server(gpu_parameters.render_server.is_some());
+            .use_render_server(render_server_fd.is_some());
         let gfxstream_flags = GfxstreamFlags::new()
             .use_egl(gpu_parameters.renderer_use_egl)
             .use_gles(gpu_parameters.renderer_use_gles)

src/linux/gpu.rs
@@ -9,7 +9,6 @@ use std::env;
 use std::path::PathBuf;
 
 use devices::virtio::vhost::user::vmm::Gpu as VhostUserGpu;
-use devices::virtio::GpuRenderServerParameters;
 
 use crate::VhostUserOption;
 
@@ -234,7 +233,14 @@ pub fn create_gpu_device(
     })
 }
 
-pub fn get_gpu_render_server_environment(cache_info: &GpuCacheInfo) -> Result<Vec<String>> {
+#[derive(Debug)]
+pub struct GpuRenderServerParameters {
+    pub path: PathBuf,
+    pub cache_path: Option<String>,
+    pub cache_size: Option<String>,
+}
+
+fn get_gpu_render_server_environment(cache_info: &GpuCacheInfo) -> Result<Vec<String>> {
     let mut env = Vec::new();
 
     let mut cache_env_keys = HashSet::with_capacity(cache_info.environment.len());
@@ -255,14 +261,6 @@ pub fn get_gpu_render_server_environment(cache_info: &GpuCacheInfo) -> Result<Ve
     }
 
     Ok(env)
-}
-
-pub struct ScopedMinijail(pub Minijail);
-
-impl Drop for ScopedMinijail {
-    fn drop(&mut self) {
-        let _ = self.0.kill();
-    }
 }
 
 pub fn start_gpu_render_server(

src/linux/jail_helpers.rs
@@ -22,6 +22,14 @@ pub(super) struct SandboxConfig<'a> {
     pub(super) remount_mode: Option<c_ulong>,
 }
 
+pub(super) struct ScopedMinijail(pub Minijail);
+
+impl Drop for ScopedMinijail {
+    fn drop(&mut self) {
+        let _ = self.0.kill();
+    }
+}
+
 pub(super) fn create_base_minijail(
     root: &Path,
     r_limit: Option<u64>,

src/linux/mod.rs
@@ -79,6 +79,8 @@ mod vcpu;
 #[cfg(feature = "gpu")]
 mod gpu;
 #[cfg(feature = "gpu")]
+pub use gpu::GpuRenderServerParameters;
+#[cfg(feature = "gpu")]
 use gpu::*;
 
 // gpu_device_tube is not used when GPU support is disabled.
@@ -1013,17 +1015,13 @@ where
 
     #[cfg(feature = "gpu")]
     // Hold on to the render server jail so it keeps running until we exit run_vm()
-    let mut _render_server_jail = None;
-    #[cfg(feature = "gpu")]
-    let mut render_server_fd = None;
-    #[cfg(feature = "gpu")]
-    if let Some(gpu_parameters) = &cfg.gpu_parameters {
-        if let Some(ref render_server_parameters) = gpu_parameters.render_server {
-            let (jail, fd) = start_gpu_render_server(&cfg, render_server_parameters)?;
-            _render_server_jail = Some(ScopedMinijail(jail));
-            render_server_fd = Some(fd);
-        }
-    }
+    let (_render_server_jail, render_server_fd) =
+        if let Some(parameters) = &cfg.gpu_render_server_parameters {
+            let (jail, fd) = start_gpu_render_server(&cfg, parameters)?;
+            (Some(ScopedMinijail(jail)), Some(fd))
+        } else {
+            (None, None)
+        };
 
     let init_balloon_size = components
         .memory_size

src/main.rs
@@ -20,6 +20,8 @@ use std::time::Duration;
 
 use arch::{set_default_serial_parameters, Pstore, VcpuAffinity};
 use base::{debug, error, getpid, info, kill_process_group, pagesize, reap_child, syslog, warn};
+#[cfg(all(feature = "gpu", feature = "virgl_renderer_next"))]
+use crosvm::platform::GpuRenderServerParameters;
 #[cfg(feature = "direct")]
 use crosvm::DirectIoOption;
 use crosvm::{
@@ -29,8 +31,6 @@ use crosvm::{
     VhostUserWlOption, VhostVsockDeviceParameter, DISK_ID_LEN,
 };
 use devices::serial_device::{SerialHardware, SerialParameters, SerialType};
-#[cfg(all(feature = "gpu", feature = "virgl_renderer_next"))]
-use devices::virtio::gpu::GpuRenderServerParameters;
 #[cfg(feature = "audio_cras")]
 use devices::virtio::snd::cras_backend::Error as CrasSndError;
 #[cfg(feature = "audio_cras")]
@@ -554,10 +554,7 @@ fn parse_gpu_display_options(
 }
 
 #[cfg(all(feature = "gpu", feature = "virgl_renderer_next"))]
-fn parse_gpu_render_server_options(
-    s: Option<&str>,
-    gpu_params: &mut GpuParameters,
-) -> argument::Result<()> {
+fn parse_gpu_render_server_options(s: Option<&str>) -> argument::Result<GpuRenderServerParameters> {
     let mut path: Option<PathBuf> = None;
     let mut cache_path = None;
     let mut cache_size = None;
@@ -593,12 +590,11 @@ fn parse_gpu_render_server_options(
     }
 
     if let Some(p) = path {
-        gpu_params.render_server = Some(GpuRenderServerParameters {
+        Ok(GpuRenderServerParameters {
             path: p,
             cache_path,
             cache_size,
-        });
-        Ok(())
+        })
     } else {
         Err(argument::Error::InvalidValue {
             value: s.unwrap_or("").to_string(),
@@ -1962,8 +1958,7 @@ fn set_argument(cfg: &mut Config, name: &str, value: Option<&str>) -> argument::
         }
         #[cfg(all(feature = "gpu", feature = "virgl_renderer_next"))]
         "gpu-render-server" => {
-            let gpu_parameters = cfg.gpu_parameters.get_or_insert_with(Default::default);
-            parse_gpu_render_server_options(value, gpu_parameters)?;
+            cfg.gpu_render_server_parameters = Some(parse_gpu_render_server_options(value)?);
         }
         "software-tpm" => {
             cfg.software_tpm = true;

commit 0615347ede9dbd17fe87e71fee2eb0b0d1bd09b9

crosvm: guard GPU import with the gpu feature flag
Fixes build when the gpu feature is disabled.

BUG=None
TEST=cargo build --no-default-features

Fixes: e464a7a6c0e7 ("gpu: prepare to start render server separately from GPU device")
Change-Id: I61185328c92ec1582b4f8bee8bad321207504ff5
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3438145
Reviewed-by: Dennis Kempin <denniskempin@google.com>
Tested-by: kokoro <noreply+kokoro@google.com>
Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

src/crosvm.rs
@@ -37,6 +37,7 @@ use devices::IommuDevType;
 use devices::StubPciParameters;
 use hypervisor::ProtectionType;
 use libc::{getegid, geteuid};
+#[cfg(feature = "gpu")]
 use platform::GpuRenderServerParameters;
 use vm_control::BatteryType;
 