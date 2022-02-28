google/crosvm

commit 45f1a419d4291b44a433652672ca9cf3f5a6f7e1

Make crates depend on base instead of sys_util
As we make progress on upstreaming sys_util, we do not want more
dependencies on sys_util added. This check helps in that cause.

With this change only files that are inside common can depend directly
on sys_util.

Test: python3 ./tools/impl/check_code_hygiene.py common/sys_util_core
Bug: b:213149154

Change-Id: I57a8cb9189d3263a4031338b302cb27da799f4de
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3473344
Tested-by: kokoro <noreply+kokoro@google.com>
Reviewed-by: Dennis Kempin <denniskempin@google.com>
Auto-Submit: Vikram Auradkar <auradkar@google.com>
Commit-Queue: Vikram Auradkar <auradkar@google.com>

devices/src/virtio/video/resource.rs
@@ -8,9 +8,9 @@ use std::convert::TryInto;
 use std::fmt;
 
 use base::{
-    self, FromRawDescriptor, IntoRawDescriptor, MemoryMappingArena, MmapError, SafeDescriptor,
+    self, FromRawDescriptor, IntoRawDescriptor, MemoryMapping, MemoryMappingArena, MmapError,
+    SafeDescriptor,
 };
-use sys_util::MemoryMapping;
 use vm_memory::{GuestAddress, GuestMemory, GuestMemoryError};
 
 use thiserror::Error as ThisError;
@@ -316,7 +316,7 @@ impl GuestResource {
 #[cfg(test)]
 mod tests {
     use base::{MappedRegion, SafeDescriptor};
-    use sys_util::{MemoryMapping, SharedMemory};
+    use base::{MemoryMapping, SharedMemory};
 
     use super::*;
 

devices/Cargo.toml
@@ -57,7 +57,6 @@ serde = { version = "1", features = [ "derive" ] }
 serde_json = "1"
 smallvec = "1.6.1"
 sync = { path = "../common/sync" }
-sys_util = { path = "../common/sys_util" }
 system_api = { version = "*", optional = true }
 thiserror = "1.0.20"
 tpm2 = { path = "../tpm2", optional = true }

devices/src/pci/ac97_bus_master.rs
@@ -568,7 +568,7 @@ impl Ac97BusMaster {
             StreamDirection::Playback => [0, 0],
         };
 
-        // Create a `sys_util::SharedMemory` object from a descriptor backing `self.mem`.
+        // Create a `base::SharedMemory` object from a descriptor backing `self.mem`.
         // This creation is expected to succeed because we can assume that `self.mem` was created
         // from a `SharedMemory` object and its type was generalized to `dyn AsRawDescriptor`.
         let desc: &dyn AsRawDescriptor = self

devices/src/virtio/fs/passthrough.rs
@@ -21,7 +21,7 @@ use std::{
 };
 
 use base::{
-    error, ioctl_ior_nr, ioctl_iow_nr, ioctl_iowr_nr, ioctl_with_mut_ptr, ioctl_with_ptr,
+    error, ioctl_ior_nr, ioctl_iow_nr, ioctl_iowr_nr, ioctl_with_mut_ptr, ioctl_with_ptr, syscall,
     AsRawDescriptor, FileFlags, FromRawDescriptor, RawDescriptor,
 };
 use data_model::DataInit;
@@ -33,7 +33,6 @@ use fuse::filesystem::{
 use fuse::sys::WRITE_KILL_PRIV;
 use fuse::Mapper;
 use sync::Mutex;
-use sys_util::syscall;
 
 #[cfg(feature = "chromeos")]
 use {
@@ -1037,7 +1036,7 @@ impl PassthroughFs {
                 let mut proto: SetMediaRWDataFileProjectIdRequest = Message::new();
                 proto.project_id = in_attr.fsx_projid;
                 // Safe because data is a valid file descriptor.
-                let fd = unsafe { dbus::arg::OwnedFd::new(sys_util::clone_descriptor(&*data)?) };
+                let fd = unsafe { dbus::arg::OwnedFd::new(base::clone_descriptor(&*data)?) };
                 match proxy.set_media_rwdata_file_project_id(fd, proto.write_to_bytes().unwrap()) {
                     Ok(r) => {
                         let r = protobuf::parse_from_bytes::<SetMediaRWDataFileProjectIdReply>(&r)

devices/src/virtio/fs/worker.rs
@@ -8,10 +8,9 @@ use std::io;
 use std::os::unix::io::AsRawFd;
 use std::sync::Arc;
 
-use base::{error, Event, PollToken, SafeDescriptor, Tube, WaitContext};
+use base::{error, syscall, Event, PollToken, SafeDescriptor, Tube, WaitContext};
 use fuse::filesystem::{FileSystem, ZeroCopyReader, ZeroCopyWriter};
 use sync::Mutex;
-use sys_util::syscall;
 use vm_control::{FsMappingRequest, VmResponse};
 use vm_memory::GuestMemory;
 

devices/src/virtio/iommu.rs
@@ -279,7 +279,7 @@ impl Worker {
 
             match vfio_map_result {
                 Ok(()) => (),
-                Err(e) => match sys_util::Error::last() {
+                Err(e) => match base::Error::last() {
                     err if err.errno() == libc::EEXIST => {
                         // If a mapping already exists in the requested range,
                         // the device SHOULD reject the request and set status

devices/src/virtio/snd/cras_backend/mod.rs
@@ -14,7 +14,9 @@ use std::thread;
 
 use anyhow::Context;
 use audio_streams::{SampleFormat, StreamSource};
-use base::{error, warn, Error as SysError, Event, RawDescriptor};
+use base::{
+    error, set_rt_prio_limit, set_rt_round_robin, warn, Error as SysError, Event, RawDescriptor,
+};
 use cros_async::sync::Mutex as AsyncMutex;
 use cros_async::{AsyncError, EventAsync, Executor};
 use data_model::DataInit;
@@ -24,7 +26,6 @@ use futures::channel::{
 };
 use futures::{pin_mut, select, Future, FutureExt, TryFutureExt};
 use libcras::{BoxError, CrasClient, CrasClientType, CrasSocketType};
-use sys_util::{set_rt_prio_limit, set_rt_round_robin};
 use thiserror::Error as ThisError;
 use vm_memory::GuestMemory;
 

devices/src/virtio/snd/vios_backend/shm_streams.rs
@@ -17,7 +17,9 @@ use audio_streams::shm_streams::{
 };
 use audio_streams::{BoxError, SampleFormat, StreamDirection, StreamEffect};
 
-use base::{error, MemoryMapping, MemoryMappingBuilder, SharedMemory, SharedMemoryUnix};
+use base::{
+    error, Error as SysError, MemoryMapping, MemoryMappingBuilder, SharedMemory, SharedMemoryUnix,
+};
 use data_model::VolatileMemory;
 use sync::Mutex;
 
@@ -26,8 +28,6 @@ use std::os::unix::io::{FromRawFd, RawFd};
 use std::path::Path;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
-
-use sys_util::Error as SysError;
 
 use super::shm_vios::{Error, Result};
 

commit abfe4b5a53ffd10d8de1e9dd23d37079dc206d15

devices: virtio: video: fix build with base MemoryMapping
The base version of MemoryMapping does not have the from_fd_offset
function. Use base::MemoryMappingBuilder and MemoryMappingBuilderUnix to
get the same functionality without depending directly on sys_util.

Fixes the build with virtio video features enabled.

BUG=b:213149154
TEST=emerge-hatch crosvm

Fixes: 45f1a419d429 ("Make crates depend on base instead of sys_util")
Change-Id: I8924d2334ae3d3ef489114b17071143871d1424d
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3490743
Reviewed-by: Vikram Auradkar <auradkar@google.com>
Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
Tested-by: kokoro <noreply+kokoro@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

devices/src/virtio/video/resource.rs
@@ -8,8 +8,8 @@ use std::convert::TryInto;
 use std::fmt;
 
 use base::{
-    self, FromRawDescriptor, IntoRawDescriptor, MemoryMapping, MemoryMappingArena, MmapError,
-    SafeDescriptor,
+    self, FromRawDescriptor, IntoRawDescriptor, MemoryMappingArena, MemoryMappingBuilder,
+    MemoryMappingBuilderUnix, MmapError, SafeDescriptor,
 };
 use vm_memory::{GuestAddress, GuestMemory, GuestMemoryError};
 
@@ -135,7 +135,11 @@ impl BufferHandle for VirtioObjectHandle {
     }
 
     fn get_mapping(&self, offset: usize, size: usize) -> Result<MemoryMappingArena, MmapError> {
-        MemoryMapping::from_fd_offset(&self.desc, size, offset as u64).map(MemoryMappingArena::from)
+        MemoryMappingBuilder::new(size)
+            .from_descriptor(&self.desc)
+            .offset(offset as u64)
+            .build()
+            .map(MemoryMappingArena::from)
     }
 }
 