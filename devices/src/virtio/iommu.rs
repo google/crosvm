google/crosvm

commit 055b81b29557aec9542a9396cc217b239c84c1d8

VIRTIO_F_ACCESS_PLATFORM support in virtio-iommu
With VIRTIO_F_ACCESS_PLATFORM, virtqueues and desc chains of a
virtual device will receive IO virtual addresses. Mappings from
IOVA to GPA will be sent to virtio-iommu. `IpcMemoryMapper` is used
by the virtio device to get the translated addresses from the
virtio-iommu in another process.

Memory blocks with contiguous IOVA but discontinguous GPAs are
supported.

The only way to enable this new path is to hardcode the flag in
the features of each device. The flag should be automatically
for every virtio-vhost-user device in the future. It seems
questionable to add a per device command line option just for
testing.

BUG=b:215307964
TEST=cargo test/block device with VIRTIO_F_ACCESS_PLATFORM enabled

Change-Id: I9a89acf4f38d52816adad34c0dbff043677bebac
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3347309
Reviewed-by: David Stevens <stevensd@chromium.org>
Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
Tested-by: kokoro <noreply+kokoro@google.com>
Commit-Queue: Woody Chow <woodychow@google.com>

devices/src/virtio/iommu.rs
@@ -2,22 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use super::{
-    async_utils, copy_config, DescriptorChain, DescriptorError, Interrupt, Queue, Reader,
-    SignalableInterrupt, VirtioDevice, Writer, TYPE_IOMMU,
-};
-use crate::pci::PciAddress;
-use crate::vfio::{VfioContainer, VfioError};
-use acpi_tables::sdt::SDT;
-use anyhow::Context;
-use base::{
-    error, pagesize, warn, AsRawDescriptor, Error as SysError, Event, RawDescriptor,
-    Result as SysResult,
-};
-use cros_async::{AsyncError, EventAsync, Executor};
-use data_model::{DataInit, Le64};
-use futures::{select, FutureExt};
-use remain::sorted;
 use std::cell::RefCell;
 use std::collections::BTreeMap;
 use std::io::{self, Write};
@@ -25,15 +9,35 @@ use std::mem::size_of;
 use std::rc::Rc;
 use std::sync::Arc;
 use std::{result, thread};
+
+use acpi_tables::sdt::SDT;
+use anyhow::Context;
+use base::{
+    error, pagesize, warn, AsRawDescriptor, AsyncTube, Error as SysError, Event, RawDescriptor,
+    Result as SysResult, Tube, TubeError,
+};
+use cros_async::{AsyncError, EventAsync, Executor};
+use data_model::{DataInit, Le64};
+use futures::{select, FutureExt};
+use remain::sorted;
 use sync::Mutex;
 use thiserror::Error;
 use vm_memory::{GuestAddress, GuestMemory, GuestMemoryError};
 
+use crate::pci::PciAddress;
+use crate::virtio::{
+    async_utils, copy_config, DescriptorChain, DescriptorError, Interrupt, Queue, Reader,
+    SignalableInterrupt, VirtioDevice, Writer, TYPE_IOMMU,
+};
+
 pub mod protocol;
 use crate::virtio::iommu::protocol::*;
 pub mod ipc_memory_mapper;
+use crate::virtio::iommu::ipc_memory_mapper::*;
 pub mod memory_mapper;
+pub mod memory_util;
 pub mod vfio_wrapper;
+use crate::virtio::iommu::memory_mapper::{Error as MemoryMapperError, *};
 
 const QUEUE_SIZE: u16 = 256;
 const NUM_QUEUES: usize = 2;
@@ -109,14 +113,16 @@ pub enum IommuError {
     GuestMemoryRead(io::Error),
     #[error("failed to write to guest address: {0}")]
     GuestMemoryWrite(io::Error),
+    #[error("memory mapper failed: {0}")]
+    MemoryMapper(MemoryMapperError),
     #[error("Failed to read descriptor asynchronously: {0}")]
     ReadAsyncDesc(AsyncError),
     #[error("failed to read from virtio queue Event: {0}")]
     ReadQueueEvent(SysError),
+    #[error("tube error: {0}")]
+    Tube(TubeError),
     #[error("unexpected descriptor error")]
     UnexpectedDescriptor,
-    #[error("failed on VFIO ioctl call: {0}")]
-    VfioContainerError(VfioError),
     #[error("failed to wait for events: {0}")]
     WaitError(SysError),
     #[error("write buffer length too small")]
@@ -126,8 +132,6 @@ pub enum IommuError {
 struct Worker {
     mem: GuestMemory,
     page_mask: u64,
-    // contains all pass-through endpoints that attach to the IOMMU device
-    endpoints: BTreeMap<u32, Arc<Mutex<VfioContainer>>>,
     // All PCI endpoints that attach to certain IOMMU domain
     // key: endpoint PCI address
     // value: attached domain ID
@@ -135,14 +139,14 @@ struct Worker {
     // All attached domains
     // key: domain ID
     // value: reference counter and VfioContainer
-    domain_map: BTreeMap<u32, (u32, Arc<Mutex<VfioContainer>>)>,
+    domain_map: BTreeMap<u32, (u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>)>,
 }
 
 impl Worker {
     // Remove the endpoint from the endpoint_map and
     // decrement the reference counter (or remove the entry if the ref count is 1)
     // from domain_map
-    fn dettach_endpoint(&mut self, endpoint: u32) {
+    fn detach_endpoint(&mut self, endpoint: u32) {
         // The endpoint has attached to an IOMMU domain
         if let Some(attached_domain) = self.endpoint_map.get(&endpoint) {
             // Remove the entry or update the domain reference count
@@ -179,6 +183,7 @@ impl Worker {
         &mut self,
         reader: &mut Reader,
         tail: &mut virtio_iommu_req_tail,
+        endpoints: &Rc<RefCell<BTreeMap<u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>>>>,
     ) -> Result<usize> {
         let req: virtio_iommu_req_attach =
             reader.read_obj().map_err(IommuError::GuestMemoryRead)?;
@@ -196,7 +201,7 @@ impl Worker {
         // VIRTIO_IOMMU_S_NOENT.
         let domain: u32 = req.domain.into();
         let endpoint: u32 = req.endpoint.into();
-        if !self.endpoints.contains_key(&endpoint) {
+        if !endpoints.borrow().contains_key(&endpoint) {
             tail.status = VIRTIO_IOMMU_S_NOENT;
             return Ok(0);
         }
@@ -205,10 +210,10 @@ impl Worker {
         // to another domain, then the device SHOULD first detach it
         // from that domain and attach it to the one identified by domain.
         if self.endpoint_map.contains_key(&endpoint) {
-            self.dettach_endpoint(endpoint);
+            self.detach_endpoint(endpoint);
         }
 
-        if let Some(vfio_container) = self.endpoints.get(&endpoint) {
+        if let Some(vfio_container) = endpoints.borrow_mut().get(&endpoint) {
             let new_ref = match self.domain_map.get(&domain) {
                 None => 1,
                 Some(val) => val.0 + 1,
@@ -259,35 +264,30 @@ impl Worker {
         // without the VIRTIO_IOMMU_MAP_F_WRITE flag.
         let write_en = u32::from(req.flags) & VIRTIO_IOMMU_MAP_F_WRITE != 0;
 
-        if let Some(vfio_container) = self.domain_map.get(&domain) {
+        if let Some(mapper) = self.domain_map.get(&domain) {
             let size = u64::from(req.virt_end) - u64::from(req.virt_start) + 1u64;
-            let host_addr = self
-                .mem
-                .get_host_address_range(GuestAddress(u64::from(req.phys_start)), size as usize)
-                .map_err(IommuError::GetHostAddress)?;
 
-            // Safe because both guest and host address are guaranteed by
-            // get_host_address_range() to be valid
-            let vfio_map_result = unsafe {
-                vfio_container.1.lock().vfio_dma_map(
-                    req.virt_start.into(),
-                    size,
-                    host_addr as u64,
-                    write_en,
-                )
-            };
+            let vfio_map_result = mapper.1.lock().add_map(MappingInfo {
+                iova: req.virt_start.into(),
+                gpa: GuestAddress(req.phys_start.into()),
+                size,
+                perm: match write_en {
+                    true => Permission::RW,
+                    false => Permission::Read,
+                },
+            });
 
             match vfio_map_result {
                 Ok(()) => (),
-                Err(e) => match base::Error::last() {
-                    err if err.errno() == libc::EEXIST => {
+                Err(e) => match e {
+                    MemoryMapperError::IovaRegionOverlap => {
                         // If a mapping already exists in the requested range,
                         // the device SHOULD reject the request and set status
                         // to VIRTIO_IOMMU_S_INVAL.
                         tail.status = VIRTIO_IOMMU_S_INVAL;
                         return Ok(0);
                     }
-                    _ => return Err(IommuError::VfioContainerError(e)),
+                    _ => return Err(IommuError::MemoryMapper(e)),
                 },
             }
         }
@@ -303,13 +303,13 @@ impl Worker {
         let req: virtio_iommu_req_unmap = reader.read_obj().map_err(IommuError::GuestMemoryRead)?;
 
         let domain: u32 = req.domain.into();
-        if let Some(vfio_container) = self.domain_map.get(&domain) {
+        if let Some(mapper) = self.domain_map.get(&domain) {
             let size = u64::from(req.virt_end) - u64::from(req.virt_start) + 1;
-            vfio_container
+            mapper
                 .1
                 .lock()
-                .vfio_dma_unmap(u64::from(req.virt_start), size)
-                .map_err(IommuError::VfioContainerError)?;
+                .remove_map(u64::from(req.virt_start), size)
+                .map_err(IommuError::MemoryMapper)?;
         } else {
             // If domain does not exist, the device SHOULD set the
             // request status to VIRTIO_IOMMU_S_NOENT
@@ -325,6 +325,7 @@ impl Worker {
         reader: &mut Reader,
         writer: &mut Writer,
         tail: &mut virtio_iommu_req_tail,
+        endpoints: &Rc<RefCell<BTreeMap<u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>>>>,
     ) -> Result<usize> {
         let req: virtio_iommu_req_probe = reader.read_obj().map_err(IommuError::GuestMemoryRead)?;
         let endpoint: u32 = req.endpoint.into();
@@ -332,7 +333,7 @@ impl Worker {
         // If the endpoint identified by endpoint doesn’t exist,
         // then the device SHOULD reject the request and set status
         // to VIRTIO_IOMMU_S_NOENT.
-        if !self.endpoints.contains_key(&endpoint) {
+        if !endpoints.borrow().contains_key(&endpoint) {
             tail.status = VIRTIO_IOMMU_S_NOENT;
         }
 
@@ -381,7 +382,11 @@ impl Worker {
         Ok(properties_size)
     }
 
-    fn execute_request(&mut self, avail_desc: &DescriptorChain) -> Result<usize> {
+    fn execute_request(
+        &mut self,
+        avail_desc: &DescriptorChain,
+        endpoints: &Rc<RefCell<BTreeMap<u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>>>>,
+    ) -> Result<usize> {
         let mut reader =
             Reader::new(self.mem.clone(), avail_desc.clone()).map_err(IommuError::CreateReader)?;
         let mut writer =
@@ -401,7 +406,9 @@ impl Worker {
         };
 
         let reply_len = match req_head.type_ {
-            VIRTIO_IOMMU_T_ATTACH => self.process_attach_request(&mut reader, &mut tail)?,
+            VIRTIO_IOMMU_T_ATTACH => {
+                self.process_attach_request(&mut reader, &mut tail, endpoints)?
+            }
             VIRTIO_IOMMU_T_DETACH => {
                 // A few reasons why we don't support VIRTIO_IOMMU_T_DETACH for now:
                 //
@@ -423,7 +430,7 @@ impl Worker {
             VIRTIO_IOMMU_T_UNMAP => self.process_dma_unmap_request(&mut reader, &mut tail)?,
             #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
             VIRTIO_IOMMU_T_PROBE => {
-                self.process_probe_request(&mut reader, &mut writer, &mut tail)?
+                self.process_probe_request(&mut reader, &mut writer, &mut tail, endpoints)?
             }
             _ => return Err(IommuError::UnexpectedDescriptor),
         };
@@ -439,6 +446,7 @@ impl Worker {
         mut queue: Queue,
         mut queue_event: EventAsync,
         interrupt: &I,
+        endpoints: &Rc<RefCell<BTreeMap<u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>>>>,
     ) -> Result<()> {
         loop {
             let avail_desc = queue
@@ -447,10 +455,10 @@ impl Worker {
                 .map_err(IommuError::ReadAsyncDesc)?;
             let desc_index = avail_desc.index;
 
-            let len = match self.execute_request(&avail_desc) {
+            let len = match self.execute_request(&avail_desc, endpoints) {
                 Ok(len) => len as u32,
                 Err(e) => {
-                    error!("{}", e);
+                    error!("execute_request failed: {}", e);
 
                     // If a request type is not recognized, the device SHOULD NOT write
                     // the buffer and SHOULD set the used length to zero
@@ -469,6 +477,9 @@ impl Worker {
         queue_evts: Vec<Event>,
         kill_evt: Event,
         interrupt: Interrupt,
+        endpoints: BTreeMap<u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>>,
+        translate_response_senders: Option<BTreeMap<u32, Tube>>,
+        translate_request_rx: Option<Tube>,
     ) -> Result<()> {
         let ex = Executor::new().expect("Failed to create an executor");
 
@@ -481,15 +492,37 @@ impl Worker {
 
         let (req_queue, req_evt) = (queues.remove(0), evts_async.remove(0));
 
-        let f_request = self.request_queue(req_queue, req_evt, interrupt_ref);
+        let endpoints: Rc<RefCell<BTreeMap<u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>>>> =
+            Rc::new(RefCell::new(endpoints));
+
         let f_resample = async_utils::handle_irq_resample(&ex, interrupt.clone());
         let f_kill = async_utils::await_and_exit(&ex, kill_evt);
 
+        let request_tube = translate_request_rx.map(|t| {
+            t.into_async_tube(&ex)
+                .expect("Failed to create async tube for rx")
+        });
+        let response_tubes = translate_response_senders.map(|m| {
+            m.into_iter()
+                .map(|x| {
+                    (
+                        x.0,
+                        x.1.into_async_tube(&ex)
+                            .expect("Failed to create async tube"),
+                    )
+                })
+                .collect()
+        });
+
+        let f_handle_translate_request =
+            handle_translate_request(&endpoints, request_tube, response_tubes);
+        let f_request = self.request_queue(req_queue, req_evt, interrupt_ref, &endpoints);
         let done = async {
             select! {
                 res = f_request.fuse() => res.context("error in handling request queue"),
                 res = f_resample.fuse() => res.context("error in handle_irq_resample"),
                 res = f_kill.fuse() => res.context("error in await_and_exit"),
+                res = f_handle_translate_request.fuse() => res.context("error in handle_translate_request"),
             }
         };
         match ex.run_until(done) {
@@ -502,27 +535,71 @@ impl Worker {
     }
 }
 
+async fn handle_translate_request(
+    endpoints: &Rc<RefCell<BTreeMap<u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>>>>,
+    request_tube: Option<AsyncTube>,
+    response_tubes: Option<BTreeMap<u32, AsyncTube>>,
+) -> Result<()> {
+    let request_tube = match request_tube {
+        Some(r) => r,
+        None => {
+            let () = futures::future::pending().await;
+            return Ok(());
+        }
+    };
+    let response_tubes = response_tubes.unwrap();
+    loop {
+        let TranslateRequest {
+            endpoint_id,
+            iova,
+            size,
+        } = request_tube.next().await.map_err(|e| IommuError::Tube(e))?;
+        if let Some(mapper) = endpoints.borrow_mut().get(&endpoint_id) {
+            response_tubes
+                .get(&endpoint_id)
+                .unwrap()
+                .send(
+                    &mapper
+                        .lock()
+                        .translate(iova, size)
+                        .map_err(|e| {
+                            error!("Failed to handle TranslateRequest: {}", e);
+                            e
+                        })
+                        .ok(),
+                )
+                .map_err(IommuError::Tube)?;
+        } else {
+            error!("endpoint_id {} not found", endpoint_id)
+        }
+    }
+}
+
 /// Virtio device for IOMMU memory management.
 pub struct Iommu {
     kill_evt: Option<Event>,
     worker_thread: Option<thread::JoinHandle<Worker>>,
     config: virtio_iommu_config,
     avail_features: u64,
-    endpoints: BTreeMap<u32, Arc<Mutex<VfioContainer>>>,
+    endpoints: BTreeMap<u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>>,
+    translate_response_senders: Option<BTreeMap<u32, Tube>>,
+    translate_request_rx: Option<Tube>,
 }
 
 impl Iommu {
     /// Create a new virtio IOMMU device.
     pub fn new(
         base_features: u64,
-        endpoints: BTreeMap<u32, Arc<Mutex<VfioContainer>>>,
+        endpoints: BTreeMap<u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>>,
         phys_max_addr: u64,
+        translate_response_senders: Option<BTreeMap<u32, Tube>>,
+        translate_request_rx: Option<Tube>,
     ) -> SysResult<Iommu> {
         let mut page_size_mask = !0_u64;
         for (_, container) in endpoints.iter() {
             page_size_mask &= container
                 .lock()
-                .vfio_get_iommu_page_size_mask()
+                .get_mask()
                 .map_err(|_e| SysError::new(libc::EIO))?;
         }
 
@@ -558,6 +635,8 @@ impl Iommu {
             config,
             avail_features,
             endpoints,
+            translate_response_senders,
+            translate_request_rx,
         })
     }
 }
@@ -578,9 +657,18 @@ impl VirtioDevice for Iommu {
     fn keep_rds(&self) -> Vec<RawDescriptor> {
         let mut rds = Vec::new();
 
-        for (_, vfio) in self.endpoints.iter() {
-            rds.push(vfio.lock().as_raw_descriptor());
+        for (_, mapper) in self.endpoints.iter() {
+            rds.append(&mut mapper.lock().as_raw_descriptors());
         }
+        if let Some(senders) = &self.translate_response_senders {
+            for (_, tube) in senders.iter() {
+                rds.push(tube.as_raw_descriptor());
+            }
+        }
+        if let Some(rx) = &self.translate_request_rx {
+            rds.push(rx.as_raw_descriptor());
+        }
+
         rds
     }
 
@@ -626,17 +714,28 @@ impl VirtioDevice for Iommu {
         // granularity of IOMMU mappings
         let page_mask = (1u64 << u64::from(self.config.page_size_mask).trailing_zeros()) - 1;
         let eps = self.endpoints.clone();
+
+        let translate_response_senders = self.translate_response_senders.take();
+        let translate_request_rx = self.translate_request_rx.take();
+
         let worker_result = thread::Builder::new()
             .name("virtio_iommu".to_string())
             .spawn(move || {
                 let mut worker = Worker {
                     mem,
                     page_mask,
-                    endpoints: eps,
                     endpoint_map: BTreeMap::new(),
                     domain_map: BTreeMap::new(),
                 };
-                let result = worker.run(queues, queue_evts, kill_evt, interrupt);
+                let result = worker.run(
+                    queues,
+                    queue_evts,
+                    kill_evt,
+                    interrupt,
+                    eps,
+                    translate_response_senders,
+                    translate_request_rx,
+                );
                 if let Err(e) = result {
                     error!("virtio-iommu worker thread exited with error: {}", e);
                 }

devices/src/virtio/iommu/memory_util.rs
@@ -1,0 +1,144 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Utilities to access GuestMemory with IO virtual addresses and iommu
+
+use std::convert::TryInto;
+use std::sync::Arc;
+use sync::Mutex;
+
+use anyhow::{bail, Context};
+use data_model::DataInit;
+use vm_memory::{GuestAddress, GuestMemory};
+
+use crate::virtio::iommu::IpcMemoryMapper;
+use crate::virtio::memory_mapper::{Permission, Translate};
+
+/// A wrapper that works with gpa, or iova and an iommu.
+pub fn is_valid_wrapper<T: Translate>(
+    mem: &GuestMemory,
+    iommu: &Option<T>,
+    addr: GuestAddress,
+    size: u64,
+) -> anyhow::Result<bool> {
+    if let Some(iommu) = iommu {
+        is_valid(mem, iommu, addr.offset(), size)
+    } else {
+        Ok(addr
+            .checked_add(size as u64)
+            .map_or(false, |v| mem.address_in_range(v)))
+    }
+}
+
+/// Translates `iova` into gpa regions (or 1 gpa region when it is contiguous), and check if the
+/// gpa regions are all valid in `mem`.
+pub fn is_valid<T: Translate>(
+    mem: &GuestMemory,
+    iommu: &T,
+    iova: u64,
+    size: u64,
+) -> anyhow::Result<bool> {
+    match iommu.translate(iova, size) {
+        Ok(regions) => {
+            for r in regions {
+                if !mem.address_in_range(r.gpa)
+                    || mem
+                        .checked_offset(
+                            r.gpa,
+                            r.len.try_into().context("u64 doesn't fit in usize")?,
+                        )
+                        .is_none()
+                {
+                    return Ok(false);
+                }
+            }
+        }
+        Err(e) => bail!("failed to translate iova to gpa: {}", e),
+    }
+    Ok(true)
+}
+
+/// A wrapper that works with gpa, or iova and an iommu.
+pub fn read_obj_from_addr_wrapper<T: DataInit>(
+    mem: &GuestMemory,
+    iommu: &Option<Arc<Mutex<IpcMemoryMapper>>>,
+    addr: GuestAddress,
+) -> anyhow::Result<T> {
+    if let Some(iommu) = iommu {
+        read_obj_from_addr::<T>(mem, &iommu.lock(), addr.offset())
+    } else {
+        mem.read_obj_from_addr::<T>(addr)
+            .context("read_obj_from_addr failed")
+    }
+}
+
+/// A version of `GuestMemory::read_obj_from_addr` that works with iova and iommu.
+pub fn read_obj_from_addr<T: DataInit>(
+    mem: &GuestMemory,
+    iommu: &IpcMemoryMapper,
+    iova: u64,
+) -> anyhow::Result<T> {
+    let regions = iommu
+        .translate(
+            iova,
+            std::mem::size_of::<T>()
+                .try_into()
+                .context("u64 doesn't fit in usize")?,
+        )
+        .context("failed to translate iova to gpa")?;
+    let mut buf = vec![0u8; std::mem::size_of::<T>()];
+    let mut addr: usize = 0;
+    for r in regions {
+        if (r.perm as u8 & Permission::Read as u8) == 0 {
+            bail!("gpa is not readable");
+        }
+        mem.read_at_addr(&mut buf[addr..(addr + r.len as usize)], r.gpa)
+            .context("failed to read from gpa")?;
+        addr += r.len as usize;
+    }
+    Ok(*T::from_slice(&buf).context("failed to construct obj")?)
+}
+
+/// A wrapper that works with gpa, or iova and an iommu.
+pub fn write_obj_at_addr_wrapper<T: DataInit>(
+    mem: &GuestMemory,
+    iommu: &Option<Arc<Mutex<IpcMemoryMapper>>>,
+    val: T,
+    addr: GuestAddress,
+) -> anyhow::Result<()> {
+    if let Some(iommu) = iommu {
+        write_obj_at_addr(mem, &iommu.lock(), val, addr.offset())
+    } else {
+        mem.write_obj_at_addr(val, addr)
+            .context("write_obj_at_addr failed")
+    }
+}
+
+/// A version of `GuestMemory::write_obj_at_addr` that works with iova and iommu.
+pub fn write_obj_at_addr<T: DataInit>(
+    mem: &GuestMemory,
+    iommu: &IpcMemoryMapper,
+    val: T,
+    iova: u64,
+) -> anyhow::Result<()> {
+    let regions = iommu
+        .translate(
+            iova,
+            std::mem::size_of::<T>()
+                .try_into()
+                .context("u64 doesn't fit in usize")?,
+        )
+        .context("failed to translate iova to gpa")?;
+    let buf = val.as_slice();
+    let mut addr: usize = 0;
+    for r in regions {
+        if (r.perm as u8 & Permission::Read as u8) == 0 {
+            bail!("gpa is not writable");
+        }
+        mem.write_at_addr(&buf[addr..(addr + (r.len as usize))], r.gpa)
+            .context("failed to write to gpa")?;
+        addr += r.len as usize;
+    }
+    Ok(())
+}

commit ad9e98dbe7f19d54454b2cef16c42d4ceb616b2b

devices: virtio: fix iommu clippy warnings
The compiler was warning about a missing `dyn` on the impl block for
PciDevice, which only contained supports_iommu():

  warning: trait objects without an explicit `dyn` are deprecated

We could add the `dyn` to fix it, but it seems cleaner to just move the
definition into the VirtioPciDevice impl's existing supports_iommu
function.

Additionally fix up a few clippy warnings about open-coded instances of
map and unnecessary references.

BUG=None
TEST=cargo build # completes without warnings
TEST=tools/presubmit

Fixes: 055b81b29557 ("VIRTIO_F_ACCESS_PLATFORM support in virtio-iommu")
Change-Id: I7c923d1663d4f8c958a3c79619a06ab04e1e00ae
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3498843
Reviewed-by: Anton Romanov <romanton@google.com>
Tested-by: kokoro <noreply+kokoro@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

devices/src/virtio/iommu.rs
@@ -553,7 +553,7 @@ async fn handle_translate_request(
             endpoint_id,
             iova,
             size,
-        } = request_tube.next().await.map_err(|e| IommuError::Tube(e))?;
+        } = request_tube.next().await.map_err(IommuError::Tube)?;
         if let Some(mapper) = endpoints.borrow_mut().get(&endpoint_id) {
             response_tubes
                 .get(&endpoint_id)

devices/src/virtio/iommu/memory_util.rs
@@ -42,14 +42,7 @@ pub fn is_valid<T: Translate>(
     match iommu.translate(iova, size) {
         Ok(regions) => {
             for r in regions {
-                if !mem.address_in_range(r.gpa)
-                    || mem
-                        .checked_offset(
-                            r.gpa,
-                            r.len.try_into().context("u64 doesn't fit in usize")?,
-                        )
-                        .is_none()
-                {
+                if !mem.address_in_range(r.gpa) || mem.checked_offset(r.gpa, r.len).is_none() {
                     return Ok(false);
                 }
             }

devices/src/virtio/queue.rs
@@ -209,11 +209,7 @@ impl DescriptorChain {
         if self.has_next() {
             // Once we see a write-only descriptor, all subsequent descriptors must be write-only.
             let required_flags = self.flags & VIRTQ_DESC_F_WRITE;
-            let iommu = if let Some(iommu) = &self.iommu {
-                Some(Arc::clone(&iommu))
-            } else {
-                None
-            };
+            let iommu = self.iommu.as_ref().map(Arc::clone);
             match DescriptorChain::checked_new(
                 &self.mem,
                 self.desc_table,
@@ -358,7 +354,7 @@ impl Queue {
             (avail_ring, avail_ring_size, "available ring"),
             (used_ring, used_ring_size, "used ring"),
         ] {
-            match is_valid_wrapper(&mem, &iommu, addr, size as u64) {
+            match is_valid_wrapper(mem, &iommu, addr, size as u64) {
                 Ok(valid) => {
                     if !valid {
                         error!(
@@ -489,11 +485,7 @@ impl Queue {
         let descriptor_index: u16 =
             read_obj_from_addr_wrapper(mem, &self.iommu, desc_idx_addr).unwrap();
 
-        let iommu = if let Some(iommu) = &self.iommu {
-            Some(Arc::clone(&iommu))
-        } else {
-            None
-        };
+        let iommu = self.iommu.as_ref().map(Arc::clone);
         DescriptorChain::checked_new(mem, self.desc_table, queue_size, descriptor_index, 0, iommu)
             .map_err(|e| {
                 error!("{:#}", e);

devices/src/virtio/virtio_device.rs
@@ -121,9 +121,3 @@ pub trait VirtioDevice: Send {
         None
     }
 }
-
-impl VirtioDevice {
-    pub fn supports_iommu(&self) -> bool {
-        (self.features() & (1 << VIRTIO_F_ACCESS_PLATFORM)) != 0
-    }
-}

devices/src/virtio/virtio_pci_device.rs
@@ -480,7 +480,7 @@ impl VirtioPciDevice {
 
 impl PciDevice for VirtioPciDevice {
     fn supports_iommu(&self) -> bool {
-        self.device.supports_iommu()
+        (self.device.features() & (1 << VIRTIO_F_ACCESS_PLATFORM)) != 0
     }
 
     fn debug_label(&self) -> String {
@@ -802,7 +802,7 @@ impl PciDevice for VirtioPciDevice {
         if !self.device_activated && self.is_driver_ready() {
             if let Some(iommu) = &self.iommu {
                 for q in &mut self.queues {
-                    q.set_iommu(Arc::clone(&iommu));
+                    q.set_iommu(Arc::clone(iommu));
                 }
             }
 
@@ -833,7 +833,7 @@ impl PciDevice for VirtioPciDevice {
     }
 
     fn set_iommu(&mut self, iommu: IpcMemoryMapper) -> anyhow::Result<()> {
-        assert!(self.device.supports_iommu());
+        assert!(self.supports_iommu());
         self.iommu = Some(Arc::new(Mutex::new(iommu)));
         Ok(())
     }

src/linux/device_helpers.rs
@@ -1115,7 +1115,7 @@ pub fn create_vfio_device(
             iommu_endpoints.insert(
                 endpoint_addr.to_u32(),
                 Arc::new(Mutex::new(Box::new(VfioWrapper::new(
-                    vfio_container.clone(),
+                    vfio_container,
                     vm.get_memory().clone(),
                 )))),
             );
@@ -1194,7 +1194,7 @@ pub fn setup_virtio_access_platform(
                     response_tx,
                 } = create_ipc_mapper(endpoint_id, request_tx.try_clone()?);
                 translate_response_senders
-                    .get_or_insert_with(|| BTreeMap::new())
+                    .get_or_insert_with(BTreeMap::new)
                     .insert(endpoint_id, response_tx);
                 iommu_attached_endpoints.insert(endpoint_id, mapper);
                 pci_dev.set_iommu(ipc_mapper)?;