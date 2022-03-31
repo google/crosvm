bc712f12dc52a2734729a097bab1622a398952cc
Daniel Verkamp <dverkamp@chromium.org>
2022-03-31 15:46:17 -0700

    devices: virtio: block: add missing copyright headers
    BUG=None
    TEST=None
    
    Change-Id: Ic4be21c0a2eed2f149a9eee8e6ac2f83c0cf9eb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3564229
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

--- devices/src/virtio/block/sys/mod.rs
+++ devices/src/virtio/block/sys/mod.rs
@@ -1,3 +1,7 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 cfg_if::cfg_if! {
     if #[cfg(unix)] {
         mod unix;
--- devices/src/virtio/block/sys/windows.rs
+++ devices/src/virtio/block/sys/windows.rs
@@ -1,3 +1,7 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 pub fn system_block_avail_features() -> u64 {
     0
 }
