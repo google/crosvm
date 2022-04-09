213f9fe8a79934f4e62572164e74eaa2ba82fbe0
Daniel Almeida <daniel.almeida@collabora.corp-partner.google.com>
2022-04-09 16:17:34 -0300

    Add a VAAPI wrapper crate
    In light of the upcoming VAAPI video decoder backend, add a VAAPI
    wrapper crate that exposes a safe Rust API for a subset of the VAAPI C
    code. This crate will be called from the VAAPI video decoder backend in
    order to decode frames.
    
    BUG=b:214478588
    TEST=cargo build --features "video-decoder,vaapi"
    TEST=`cargo test -- --include-ignored` in `media/libva` passes on a
    device with Intel GPU and libva installed.
    
    Change-Id: I4afa96c49d045251827b97bd78faeba57575aedc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3422779
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>

---
+++ media/libva/src/bindings.rs
@@ -0,0 +1,10 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! This module implements the bindgen C FFI bindings for use within this crate
+
+#[allow(missing_docs)]
+pub mod va;
+
+pub use va::*;
