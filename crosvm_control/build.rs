25d7911e8dd7719020cf9557e2806a1ca2b73e63
Keiichi Watanabe <keiichiw@chromium.org>
2022-04-14 17:15:23 +0900

    crosvm_control: Minor style fix
    BUG=none
    TEST=cargo chekc
    
    Change-Id: I24fa7eced9053eee535778153390f4039044e47f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3581447
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Commit-Queue: Kameron Lutes <kalutes@chromium.org>

--- crosvm_control/build.rs
+++ crosvm_control/build.rs
@@ -1,4 +1,6 @@
-extern crate cbindgen;
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 use std::env;
 use std::path::PathBuf;
