dc6fc84d412cc57983081e3a0cda89b199989f96
Vikram Auradkar <auradkar@google.com>
2022-04-21 03:32:57 +0000

    base: define Timer type
    Alias TimerFd to Timer for now. Next patch should get rid of the alias.
    
    A series of patches move unix specific code into src/sys/unix/timer.rs,
    retain platform independent code in src/timer.rs and then introduces
    windows timer.rs.
    
    BUG=b:213153157
    TEST=presubmit
    
    Change-Id: I155936fb14c2435a121d8ef602e208ffb88ebf3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3600759
    Tested-by: kokoro <noreply+kokoro@google.com>
    Commit-Queue: Vikram Auradkar <auradkar@google.com>
    Reviewed-by: Dennis Kempin <denniskempin@google.com>

--- base/src/sys/unix/timer.rs
+++ base/src/sys/unix/timer.rs
@@ -7,15 +7,14 @@ use std::{
     mem,
     os::unix::io::{AsRawFd, FromRawFd, IntoRawFd, RawFd},
     ptr,
-    sync::Arc,
     time::Duration,
 };
-use sync::Mutex;
 
 use libc::{self, clock_getres, timerfd_create, timerfd_settime, CLOCK_MONOTONIC, TFD_CLOEXEC};
 
-use super::{errno_result, EventFd, FakeClock, Result};
-use crate::IntoRawDescriptor;
+use crate::AsRawDescriptor;
+
+use super::{errno_result, Result};
 
 /// A safe wrapper around a Linux timerfd (man 2 timerfd_create).
 pub struct TimerFd(File);
@@ -43,7 +42,7 @@ impl TimerFd {
     /// Sets the timer to expire after `dur`.  If `interval` is not `None` it represents
     /// the period for repeated expirations after the initial expiration.  Otherwise
     /// the timer will expire just once.  Cancels any existing duration and repeating interval.
-    pub fn reset(&self, dur: Duration, interval: Option<Duration>) -> Result<()> {
+    pub fn reset(&mut self, dur: Duration, interval: Option<Duration>) -> Result<()> {
         // Safe because we are zero-initializing a struct with only primitive member fields.
         let mut spec: libc::itimerspec = unsafe { mem::zeroed() };
         spec.it_value.tv_sec = dur.as_secs() as libc::time_t;
@@ -126,6 +125,12 @@ impl AsRawFd for TimerFd {
     }
 }
 
+impl AsRawDescriptor for TimerFd {
+    fn as_raw_descriptor(&self) -> crate::RawDescriptor {
+        self.0.as_raw_descriptor()
+    }
+}
+
 impl FromRawFd for TimerFd {
     unsafe fn from_raw_fd(fd: RawFd) -> Self {
         TimerFd(File::from_raw_fd(fd))
@@ -135,96 +140,6 @@ impl FromRawFd for TimerFd {
 impl IntoRawFd for TimerFd {
     fn into_raw_fd(self) -> RawFd {
         self.0.into_raw_fd()
-    }
-}
-
-/// FakeTimerFd: For use in tests.
-pub struct FakeTimerFd {
-    clock: Arc<Mutex<FakeClock>>,
-    deadline_ns: Option<u64>,
-    interval: Option<Duration>,
-    fd: EventFd,
-}
-
-impl FakeTimerFd {
-    /// Creates a new fake timerfd.  The timer is initally disarmed and must be armed by calling
-    /// `reset`.
-    pub fn new(clock: Arc<Mutex<FakeClock>>) -> Self {
-        FakeTimerFd {
-            clock,
-            deadline_ns: None,
-            interval: None,
-            fd: EventFd::new().unwrap(),
-        }
-    }
-
-    fn duration_to_nanos(d: Duration) -> u64 {
-        d.as_secs() * 1_000_000_000 + u64::from(d.subsec_nanos())
-    }
-
-    /// Sets the timer to expire after `dur`.  If `interval` is not `None` it represents
-    /// the period for repeated expirations after the initial expiration.  Otherwise
-    /// the timer will expire just once.  Cancels any existing duration and repeating interval.
-    pub fn reset(&mut self, dur: Duration, interval: Option<Duration>) -> Result<()> {
-        let mut guard = self.clock.lock();
-        let deadline = guard.nanos() + FakeTimerFd::duration_to_nanos(dur);
-        self.deadline_ns = Some(deadline);
-        self.interval = interval;
-        guard.add_event_fd(deadline, self.fd.try_clone()?);
-        Ok(())
-    }
-
-    /// Waits until the timer expires.  The return value represents the number of times the timer
-    /// has expired since the last time `wait` was called.  If the timer has not yet expired once
-    /// this call will block until it does.
-    pub fn wait(&mut self) -> Result<u64> {
-        loop {
-            self.fd.read()?;
-            if let Some(deadline_ns) = &mut self.deadline_ns {
-                let mut guard = self.clock.lock();
-                let now = guard.nanos();
-                if now >= *deadline_ns {
-                    let mut expirys = 0;
-                    if let Some(interval) = self.interval {
-                        let interval_ns = FakeTimerFd::duration_to_nanos(interval);
-                        if interval_ns > 0 {
-                            expirys += (now - *deadline_ns) / interval_ns;
-                            *deadline_ns += (expirys + 1) * interval_ns;
-                            guard.add_event_fd(*deadline_ns, self.fd.try_clone()?);
-                        }
-                    }
-                    return Ok(expirys + 1);
-                }
-            }
-        }
-    }
-
-    /// Disarms the timer.
-    pub fn clear(&mut self) -> Result<()> {
-        self.deadline_ns = None;
-        self.interval = None;
-        Ok(())
-    }
-
-    /// Returns the resolution of timers on the host.
-    pub fn resolution() -> Result<Duration> {
-        Ok(Duration::from_nanos(1))
-    }
-
-    pub fn try_clone(&self) -> std::result::Result<TimerFd, std::io::Error> {
-        unimplemented!()
-    }
-}
-
-impl AsRawFd for FakeTimerFd {
-    fn as_raw_fd(&self) -> RawFd {
-        self.fd.as_raw_fd()
-    }
-}
-
-impl IntoRawFd for FakeTimerFd {
-    fn into_raw_fd(self) -> RawFd {
-        self.fd.into_raw_descriptor()
     }
 }
 
@@ -238,7 +153,7 @@ mod tests {
 
     #[test]
     fn one_shot() {
-        let tfd = TimerFd::new().expect("failed to create timerfd");
+        let mut tfd = TimerFd::new().expect("failed to create timerfd");
 
         let dur = Duration::from_millis(200);
         let now = Instant::now();
@@ -252,7 +167,7 @@ mod tests {
 
     #[test]
     fn repeating() {
-        let tfd = TimerFd::new().expect("failed to create timerfd");
+        let mut tfd = TimerFd::new().expect("failed to create timerfd");
 
         let dur = Duration::from_millis(200);
         let interval = Duration::from_millis(100);
@@ -262,40 +177,5 @@ mod tests {
 
         let count = tfd.wait().expect("unable to wait for timer");
         assert!(count >= 5, "count = {}", count);
-    }
-
-    #[test]
-    fn fake_one_shot() {
-        let clock = Arc::new(Mutex::new(FakeClock::new()));
-        let mut tfd = FakeTimerFd::new(clock.clone());
-
-        let dur = Duration::from_nanos(200);
-        tfd.reset(dur, None).expect("failed to arm timer");
-
-        clock.lock().add_ns(200);
-
-        let count = tfd.wait().expect("unable to wait for timer");
-
-        assert_eq!(count, 1);
-    }
-
-    #[test]
-    fn fake_repeating() {
-        let clock = Arc::new(Mutex::new(FakeClock::new()));
-        let mut tfd = FakeTimerFd::new(clock.clone());
-
-        let dur = Duration::from_nanos(200);
-        let interval = Duration::from_nanos(100);
-        tfd.reset(dur, Some(interval)).expect("failed to arm timer");
-
-        clock.lock().add_ns(300);
-
-        let mut count = tfd.wait().expect("unable to wait for timer");
-        // An expiration from the initial expiry and from 1 repeat.
-        assert_eq!(count, 2);
-
-        clock.lock().add_ns(300);
-        count = tfd.wait().expect("unable to wait for timer");
-        assert_eq!(count, 3);
     }
 }
--- base/src/timer.rs
+++ base/src/timer.rs
@@ -2,77 +2,136 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use crate::descriptor::{AsRawDescriptor, FromRawDescriptor, IntoRawDescriptor};
-use crate::{FakeClock, RawDescriptor, Result};
+use super::EventFd;
+use crate::descriptor::AsRawDescriptor;
+use crate::{FakeClock, Result};
 
-use crate::platform::{FakeTimerFd, TimerFd};
+use crate::platform::TimerFd;
 use std::{sync::Arc, time::Duration};
 use sync::Mutex;
 
-#[cfg(unix)]
-use std::os::unix::io::{AsRawFd, FromRawFd, IntoRawFd};
+pub type Timer = TimerFd;
 
-/// See [TimerFd](crate::platform::TimerFd) for struct- and method-level
-/// documentation.
-pub struct Timer(pub TimerFd);
-impl Timer {
-    pub fn new() -> Result<Timer> {
-        TimerFd::new().map(Timer)
-    }
+/// FakeTimer: For use in tests.
+pub struct FakeTimer {
+    clock: Arc<Mutex<FakeClock>>,
+    deadline_ns: Option<u64>,
+    interval: Option<Duration>,
+    fd: EventFd,
 }
 
-/// See [FakeTimerFd](crate::platform::FakeTimerFd) for struct- and method-level
-/// documentation.
-pub struct FakeTimer(FakeTimerFd);
 impl FakeTimer {
+    /// Creates a new fake timerfd.  The timer is initally disarmed and must be armed by calling
+    /// `reset`.
     pub fn new(clock: Arc<Mutex<FakeClock>>) -> Self {
-        FakeTimer(FakeTimerFd::new(clock))
+        FakeTimer {
+            clock,
+            deadline_ns: None,
+            interval: None,
+            fd: EventFd::new().unwrap(),
+        }
+    }
+
+    fn duration_to_nanos(d: Duration) -> u64 {
+        d.as_secs() * 1_000_000_000 + u64::from(d.subsec_nanos())
+    }
+
+    /// Sets the timer to expire after `dur`.  If `interval` is not `None` it represents
+    /// the period for repeated expirations after the initial expiration.  Otherwise
+    /// the timer will expire just once.  Cancels any existing duration and repeating interval.
+    pub fn reset(&mut self, dur: Duration, interval: Option<Duration>) -> Result<()> {
+        let mut guard = self.clock.lock();
+        let deadline = guard.nanos() + FakeTimer::duration_to_nanos(dur);
+        self.deadline_ns = Some(deadline);
+        self.interval = interval;
+        guard.add_event_fd(deadline, self.fd.try_clone()?);
+        Ok(())
+    }
+
+    /// Waits until the timer expires.  The return value represents the number of times the timer
+    /// has expired since the last time `wait` was called.  If the timer has not yet expired once
+    /// this call will block until it does.
+    pub fn wait(&mut self) -> Result<u64> {
+        loop {
+            self.fd.read()?;
+            if let Some(deadline_ns) = &mut self.deadline_ns {
+                let mut guard = self.clock.lock();
+                let now = guard.nanos();
+                if now >= *deadline_ns {
+                    let mut expirys = 0;
+                    if let Some(interval) = self.interval {
+                        let interval_ns = FakeTimer::duration_to_nanos(interval);
+                        if interval_ns > 0 {
+                            expirys += (now - *deadline_ns) / interval_ns;
+                            *deadline_ns += (expirys + 1) * interval_ns;
+                            guard.add_event_fd(*deadline_ns, self.fd.try_clone()?);
+                        }
+                    }
+                    return Ok(expirys + 1);
+                }
+            }
+        }
+    }
+
+    /// Disarms the timer.
+    pub fn clear(&mut self) -> Result<()> {
+        self.deadline_ns = None;
+        self.interval = None;
+        Ok(())
+    }
+
+    /// Returns the resolution of timers on the host.
+    pub fn resolution() -> Result<Duration> {
+        Ok(Duration::from_nanos(1))
+    }
+
+    pub fn try_clone(&self) -> std::result::Result<TimerFd, std::io::Error> {
+        unimplemented!()
     }
 }
 
-macro_rules! build_timer {
-    ($timer:ident, $inner:ident) => {
-        impl $timer {
-            pub fn reset(&mut self, dur: Duration, interval: Option<Duration>) -> Result<()> {
-                self.0.reset(dur, interval)
-            }
-
-            pub fn wait(&mut self) -> Result<()> {
-                self.0.wait().map(|_| ())
-            }
-
-            pub fn clear(&mut self) -> Result<()> {
-                self.0.clear()
-            }
-
-            pub fn resolution() -> Result<Duration> {
-                $inner::resolution()
-            }
-
-            pub fn try_clone(&self) -> std::result::Result<Timer, std::io::Error> {
-                Ok(Timer(self.0.try_clone()?))
-            }
-        }
-
-        impl AsRawDescriptor for $timer {
-            fn as_raw_descriptor(&self) -> RawDescriptor {
-                self.0.as_raw_fd()
-            }
-        }
-
-        impl IntoRawDescriptor for $timer {
-            fn into_raw_descriptor(self) -> RawDescriptor {
-                self.0.into_raw_fd()
-            }
-        }
-    };
+impl AsRawDescriptor for FakeTimer {
+    fn as_raw_descriptor(&self) -> crate::RawDescriptor {
+        self.fd.as_raw_descriptor()
+    }
 }
 
-build_timer!(Timer, TimerFd);
-build_timer!(FakeTimer, FakeTimerFd);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::time::Duration;
+    #[test]
+    fn fake_one_shot() {
+        let clock = Arc::new(Mutex::new(FakeClock::new()));
+        let mut tfd = FakeTimer::new(clock.clone());
 
-impl FromRawDescriptor for Timer {
-    unsafe fn from_raw_descriptor(descriptor: RawDescriptor) -> Self {
-        Timer(TimerFd::from_raw_fd(descriptor))
+        let dur = Duration::from_nanos(200);
+        tfd.reset(dur, None).expect("failed to arm timer");
+
+        clock.lock().add_ns(200);
+
+        let count = tfd.wait().expect("unable to wait for timer");
+
+        assert_eq!(count, 1);
+    }
+
+    #[test]
+    fn fake_repeating() {
+        let clock = Arc::new(Mutex::new(FakeClock::new()));
+        let mut tfd = FakeTimer::new(clock.clone());
+
+        let dur = Duration::from_nanos(200);
+        let interval = Duration::from_nanos(100);
+        tfd.reset(dur, Some(interval)).expect("failed to arm timer");
+
+        clock.lock().add_ns(300);
+
+        let mut count = tfd.wait().expect("unable to wait for timer");
+        // An expiration from the initial expiry and from 1 repeat.
+        assert_eq!(count, 2);
+
+        clock.lock().add_ns(300);
+        count = tfd.wait().expect("unable to wait for timer");
+        assert_eq!(count, 3);
     }
 }
