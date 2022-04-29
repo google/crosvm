be1cc96549ecee1ceb85dcd8bfc9533cbb2cadb3
Vikram Auradkar <auradkar@google.com>
2022-04-29 17:14:54 +0000

    Make the mod not-arm only
    BUG=b:213153157
    TEST=presubmit
    
    Change-Id: Ia351a3254c4bc261f2abd5380e5e678156353900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3614614
    Commit-Queue: Vikram Auradkar <auradkar@google.com>
    Auto-Submit: Vikram Auradkar <auradkar@google.com>
    Reviewed-by: Anton Romanov <romanton@google.com>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- common/cros_asyncv2/src/unix/io_driver/cmsg.rs
+++ common/cros_asyncv2/src/unix/io_driver/cmsg.rs
@@ -122,13 +122,13 @@ pub fn take_fds_from_message(msg: &libc::msghdr, fds: &mut [RawFd]) -> anyhow::R
 }
 
 #[cfg(test)]
+#[cfg(not(target_arch = "arm"))]
 mod tests {
     use std::ptr;
 
     use super::*;
 
     #[test]
-    #[cfg(not(target_arch = "arm"))]
     fn test_add_fds_to_message() {
         let buf = [0xEAu8, 0xDD, 0xAA, 0xCC];
         let mut iov = libc::iovec {
@@ -169,7 +169,6 @@ mod tests {
     }
 
     #[test]
-    #[cfg(not(target_arch = "arm"))]
     fn test_take_fds_from_message() {
         let buf = [0xEAu8, 0xDD, 0xAA, 0xCC];
         let mut iov = libc::iovec {
