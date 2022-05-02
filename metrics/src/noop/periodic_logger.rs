4c2017c7578dc892b9c019d5d53150cd07cdc386
Michael Hoyle <mikehoyle@google.com>
2022-05-02 20:34:10 +0000

    metrics: Add metrics crate.
    This crate is effectively a collection of stubs in this repo,
    but will allow a downstream repo to implement those stubs.
    
    BUG=b:213152497
    FIXED=b:213152497
    TEST=build + kokoro
    
    Change-Id: I3c544644ec3d7917f9ebb2e8621042c35b556db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3622799
    Reviewed-by: Noah Gold <nkgold@google.com>
    Commit-Queue: Michael Hoyle <mikehoyle@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

---
+++ metrics/src/noop/periodic_logger.rs
@@ -0,0 +1,22 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use crate::MetricEventType;
+use std::result::Result;
+use std::time::Duration;
+
+/// A logging struct meant for use in tracking and periodically
+/// logging a single metric. The metric is aggregated over the
+/// designated time period. Intended for use with high-frequency metrics.
+pub struct PeriodicLogger;
+
+impl PeriodicLogger {
+    pub fn new(_event: MetricEventType, _period: Duration) -> Result<PeriodicLogger, String> {
+        Ok(PeriodicLogger)
+    }
+
+    /// Indicate the event has occurred with the given
+    /// value to be aggregated over the given time period.
+    pub fn log(&self, _value: i64) {}
+}
---
+++ metrics/src/noop/request_handler.rs
@@ -0,0 +1,15 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use crate::controller::MetricsRequestHandler;
+use crate::metrics_requests::MetricsRequest;
+
+pub struct NoopMetricsRequestHandler;
+impl MetricsRequestHandler for NoopMetricsRequestHandler {
+    fn new() -> Self {
+        NoopMetricsRequestHandler
+    }
+    fn handle_request(&self, _req: MetricsRequest) {}
+    fn shutdown(&self) {}
+}
