1f43c67a122d09c2604ed62226a3d0c369760574
Dennis Kempin <denniskempin@google.com>
2022-05-04 23:07:55 +0000

    infra: Also support gitlies input to support use as post-submit
    BUG=1300370
    TEST=./recipes.py test run
    
    Change-Id: I5db70526f728aebd9dd253da0d85056a340c7c22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3627054
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Reviewed-by: Yuanjun Huang <yuanjunh@google.com>

--- infra/recipes/verify_cl.py
+++ infra/recipes/verify_cl.py
@@ -11,25 +11,20 @@ DEPS = [
 
 def RunSteps(api):
     # TODO(denniskempin): Consider using git directly for simpler config
-    cl = api.buildbucket.build.input.gerrit_changes[0]
-
-    gs_suffix = "-review.googlesource.com"
-    host = cl.host
-    if host.endswith(gs_suffix):
-        host = "%s.googlesource.com" % host[: -len(gs_suffix)]
+    gitilies = api.buildbucket.build.input.gitiles_commit
+    if gitilies.host:
+        url = "http://%s/%s" % (gitilies.host, gitilies.project)
+    else:
+        cl = api.buildbucket.build.input.gerrit_changes[0]
+        gs_suffix = "-review.googlesource.com"
+        host = cl.host
+        if host.endswith(gs_suffix):
+            host = "%s.googlesource.com" % host[: -len(gs_suffix)]
+        url = "http://%s/%s" % (host, cl.project)
 
     gclient_config = api.gclient.make_config()
     s = gclient_config.solutions.add()
-    s.url = "https://%s/%s" % (host, cl.project)
-
-    # name is the subfolder under api.path['cache'].join('builder') the
-    # repo will be checked out at. For simplicity, I've picked `src`.
-    #
-    # Note that gclient (and by extension, bot_update), allow for complicated
-    # multi-repo layouts and dependency inclusion (via "DEPS" files). There are
-    # other repo management tools and techniques available, but this one is what
-    # chrome currently uses and is tuned to have reasonable performance by default
-    # via caches.
+    s.url = url
     s.name = "src"
     gclient_config.got_revision_mapping[s.name] = "got_revision"
 
@@ -46,11 +41,21 @@ def RunSteps(api):
 
 def GenTests(api):
     yield api.test(
-        "basic",
+        "basic try",
         # These are just to make the JSON expectation file data look closer to
         # reality. Project and git_repo will be filled in "for real" by the LUCI
         # Change Verifier service when it creates your build.
         api.buildbucket.try_build(
+            project="crosvm",
+            git_repo="https://chromium.googlesource.com/crosvm/crosvm",
+        ),
+    )
+    yield api.test(
+        "basic ci",
+        # These are just to make the JSON expectation file data look closer to
+        # reality. Project and git_repo will be filled in "for real" by the LUCI
+        # Change Verifier service when it creates your build.
+        api.buildbucket.ci_build(
             project="crosvm",
             git_repo="https://chromium.googlesource.com/crosvm/crosvm",
         ),
