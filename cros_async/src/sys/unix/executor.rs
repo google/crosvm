google/crosvm

commit d368c1520aae762a388bfc4b186d5495cb184cd5

devices: vhost-user: fs: remove global executor variable
This variable can be passed as part of the backend structure.

BUG=None
TEST=cargo build

Change-Id: Ic2f5987110acb54bd5486bd63924d052bded17cc
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3451755
Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
Tested-by: kokoro <noreply+kokoro@google.com>
Commit-Queue: Alexandre Courbot <acourbot@chromium.org>

devices/src/virtio/vhost/user/device/fs.rs
@@ -17,7 +17,6 @@ use fuse::Server;
 use futures::future::{AbortHandle, Abortable};
 use hypervisor::ProtectionType;
 use minijail::{self, Minijail};
-use once_cell::sync::OnceCell;
 use sync::Mutex;
 use vm_memory::GuestMemory;
 use vmm_vhost::message::{VhostUserProtocolFeatures, VhostUserVirtioFeatures};
@@ -29,8 +28,6 @@ use crate::virtio::fs::{process_fs_queue, virtio_fs_config, FS_MAX_TAG_LEN};
 use crate::virtio::vhost::user::device::handler::{
     DeviceRequestHandler, Doorbell, VhostUserBackend,
 };
-
-static FS_EXECUTOR: OnceCell<Executor> = OnceCell::new();
 
 async fn handle_fs_queue(
     mut queue: virtio::Queue,
@@ -114,6 +111,7 @@ fn jail_and_fork(
 }
 
 struct FsBackend {
+    ex: Executor,
     server: Arc<fuse::Server<PassthroughFs>>,
     tag: [u8; FS_MAX_TAG_LEN],
     avail_features: u64,
@@ -124,7 +122,7 @@ struct FsBackend {
 }
 
 impl FsBackend {
-    pub fn new(tag: &str) -> anyhow::Result<Self> {
+    pub fn new(ex: &Executor, tag: &str) -> anyhow::Result<Self> {
         if tag.len() > FS_MAX_TAG_LEN {
             bail!(
                 "fs tag is too long: {} (max supported: {})",
@@ -147,6 +145,7 @@ impl FsBackend {
         let server = Arc::new(Server::new(fs));
 
         Ok(FsBackend {
+            ex: ex.clone(),
             server,
             tag: fs_tag,
             avail_features,
@@ -226,29 +225,27 @@ impl VhostUserBackend for FsBackend {
             handle.abort();
         }
 
-        // Safe because the executor is initialized in main() below.
-        let ex = FS_EXECUTOR.get().expect("Executor not initialized");
-
         // Enable any virtqueue features that were negotiated (like VIRTIO_RING_F_EVENT_IDX).
         queue.ack_features(self.acked_features);
 
-        let kick_evt =
-            EventAsync::new(kick_evt.0, ex).context("failed to create EventAsync for kick_evt")?;
+        let kick_evt = EventAsync::new(kick_evt.0, &self.ex)
+            .context("failed to create EventAsync for kick_evt")?;
         let (handle, registration) = AbortHandle::new_pair();
         let (_, fs_device_tube) = Tube::pair()?;
 
-        ex.spawn_local(Abortable::new(
-            handle_fs_queue(
-                queue,
-                mem,
-                doorbell,
-                kick_evt,
-                self.server.clone(),
-                Arc::new(Mutex::new(fs_device_tube)),
-            ),
-            registration,
-        ))
-        .detach();
+        self.ex
+            .spawn_local(Abortable::new(
+                handle_fs_queue(
+                    queue,
+                    mem,
+                    doorbell,
+                    kick_evt,
+                    self.server.clone(),
+                    Arc::new(Mutex::new(fs_device_tube)),
+                ),
+                registration,
+            ))
+            .detach();
 
         self.workers[idx] = Some(handle);
         Ok(())
@@ -293,7 +290,8 @@ pub fn run_fs_device(program_name: &str, args: &[&str]) -> anyhow::Result<()> {
 
     base::syslog::init().context("Failed to initialize syslog")?;
 
-    let fs_device = FsBackend::new(&opts.tag)?;
+    let ex = Executor::new().context("Failed to create executor")?;
+    let fs_device = FsBackend::new(&ex, &opts.tag)?;
 
     // Create and bind unix socket
     let listener = UnixListener::bind(opts.socket).map(UnlinkUnixListener)?;
@@ -330,11 +328,6 @@ pub fn run_fs_device(program_name: &str, args: &[&str]) -> anyhow::Result<()> {
             bail!(io::Error::last_os_error());
         }
     }
-
-    // Child, we can continue by spawning the executor and set up the device
-    let ex = Executor::new().context("Failed to create executor")?;
-
-    let _ = FS_EXECUTOR.set(ex.clone());
 
     // run_until() returns an Result<Result<..>> which the ? operator lets us flatten.
     ex.run_until(handler.run_with_listener(listener, &ex))?

commit 9ab4f29e7a0f94c7a23a24da0ffc5233018a6319

devices: vhost-user: fs: keep executor fd inside jail
A previous commit moved the creation of the executor for the vhost-user
device processes before the minijail jail_and_fork() call, and the file
descriptor of the executor was not marked for preservation when entering
the jail, so running the vhost-user-fs device would result in errors
like this:

  [WARNING:cros_async/src/sys/unix/uring_executor.rs:343] Failed to
  submit NOP for waking up executor: Failed to enter io uring: 9

This is because the io_uring context file descriptor was no longer
valid, so uring system calls would return 9 (EBADF).

Add AsRawDescriptors implementations to UringExecutor and FdExecutor so
we can preserve the fd across the minijail fork.

Other vhost-user devices were not affected because they don't enter a
jail; the fs device requires this so it can do uid/gid remapping.

BUG=b:231396155
TEST=crosvm device fs --socket /tmp/vhost.sock --tag test --shared-dir /

Fixes: d368c1520aae ("devices: vhost-user: fs: remove global executor variable")
Change-Id: I38cf02034bbc28de2582e2ea92bdc13b1d6055d2
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3628292
Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
Tested-by: kokoro <noreply+kokoro@google.com>
Commit-Queue: Lepton Wu <lepton@chromium.org>

cros_async/src/sys/unix/executor.rs
@@ -6,6 +6,8 @@ use std::future::Future;
 
 use async_task::Task;
 
+use base::{AsRawDescriptors, RawDescriptor};
+
 use super::{
     poll_source::Error as PollError, uring_executor::use_uring, FdExecutor, PollSource,
     URingExecutor, UringSource,
@@ -349,6 +351,15 @@ impl Executor {
         match self {
             Executor::Uring(ex) => Ok(ex.run_until(f)?),
             Executor::Fd(ex) => Ok(ex.run_until(f).map_err(PollError::Executor)?),
+        }
+    }
+}
+
+impl AsRawDescriptors for Executor {
+    fn as_raw_descriptors(&self) -> Vec<RawDescriptor> {
+        match self {
+            Executor::Uring(ex) => ex.as_raw_descriptors(),
+            Executor::Fd(ex) => ex.as_raw_descriptors(),
         }
     }
 }

cros_async/src/sys/unix/fd_executor.rs
@@ -23,7 +23,10 @@ use std::{
 };
 
 use async_task::Task;
-use base::{add_fd_flags, warn, AsRawDescriptor, EpollContext, EpollEvents, Event, WatchingEvents};
+use base::{
+    add_fd_flags, warn, AsRawDescriptor, AsRawDescriptors, EpollContext, EpollEvents, Event,
+    RawDescriptor, WatchingEvents,
+};
 use futures::task::noop_waker;
 use pin_utils::pin_mut;
 use remain::sorted;
@@ -271,10 +274,17 @@ struct RawExecutor {
     blocking_pool: BlockingPool,
     state: AtomicI32,
     notify: Event,
+    // Descriptor of the original event that was cloned to create notify.
+    // This is only needed for the AsRawDescriptors implementation.
+    notify_dup: RawDescriptor,
 }
 
 impl RawExecutor {
-    fn new(notify: Event) -> Result<Self> {
+    fn new(notify: &Event) -> Result<Self> {
+        // Save the original descriptor before cloning. This descriptor will be used when creating
+        // the notify task, so we need to preserve it for AsRawDescriptors.
+        let notify_dup = notify.as_raw_descriptor();
+        let notify = notify.try_clone().map_err(Error::CloneEvent)?;
         Ok(RawExecutor {
             queue: RunnableQueue::new(),
             poll_ctx: EpollContext::new().map_err(Error::CreatingContext)?,
@@ -282,6 +292,7 @@ impl RawExecutor {
             blocking_pool: Default::default(),
             state: AtomicI32::new(PROCESSING),
             notify,
+            notify_dup,
         })
     }
 
@@ -445,6 +456,16 @@ impl RawExecutor {
     }
 }
 
+impl AsRawDescriptors for RawExecutor {
+    fn as_raw_descriptors(&self) -> Vec<RawDescriptor> {
+        vec![
+            self.poll_ctx.as_raw_descriptor(),
+            self.notify.as_raw_descriptor(),
+            self.notify_dup,
+        ]
+    }
+}
+
 impl WeakWake for RawExecutor {
     fn wake_by_ref(weak_self: &Weak<Self>) {
         if let Some(arc_self) = weak_self.upgrade() {
@@ -494,11 +515,7 @@ pub struct FdExecutor {
 impl FdExecutor {
     pub fn new() -> Result<FdExecutor> {
         let notify = Event::new().map_err(Error::CreateEvent)?;
-        let raw = notify
-            .try_clone()
-            .map_err(Error::CloneEvent)
-            .and_then(RawExecutor::new)
-            .map(Arc::new)?;
+        let raw = RawExecutor::new(&notify).map(Arc::new)?;
 
         raw.spawn(notify_task(notify, Arc::downgrade(&raw)))
             .detach();
@@ -550,6 +567,12 @@ impl FdExecutor {
             source: f,
             ex: Arc::downgrade(&self.raw),
         })
+    }
+}
+
+impl AsRawDescriptors for FdExecutor {
+    fn as_raw_descriptors(&self) -> Vec<RawDescriptor> {
+        self.raw.as_raw_descriptors()
     }
 }
 

cros_async/src/sys/unix/uring_executor.rs
@@ -73,7 +73,7 @@ use std::{
 };
 
 use async_task::Task;
-use base::{warn, AsRawDescriptor, WatchingEvents};
+use base::{warn, AsRawDescriptor, RawDescriptor, WatchingEvents};
 use futures::task::noop_waker;
 use io_uring::URingContext;
 use once_cell::sync::Lazy;
@@ -726,6 +726,12 @@ impl RawExecutor {
     }
 }
 
+impl AsRawDescriptor for RawExecutor {
+    fn as_raw_descriptor(&self) -> RawDescriptor {
+        self.ctx.as_raw_descriptor()
+    }
+}
+
 impl WeakWake for RawExecutor {
     fn wake_by_ref(weak_self: &Weak<Self>) {
         if let Some(arc_self) = weak_self.upgrade() {
@@ -847,6 +853,12 @@ impl URingExecutor {
             tag: self.raw.register_source(Arc::new(duped_fd)),
             ex: Arc::downgrade(&self.raw),
         })
+    }
+}
+
+impl AsRawDescriptor for URingExecutor {
+    fn as_raw_descriptor(&self) -> RawDescriptor {
+        self.raw.as_raw_descriptor()
     }
 }
 

devices/src/virtio/vhost/user/device/fs.rs
@@ -10,7 +10,10 @@ use std::sync::Arc;
 
 use anyhow::{anyhow, bail, Context};
 use argh::FromArgs;
-use base::{error, get_max_open_files, warn, Event, RawDescriptor, Tube, UnlinkUnixListener};
+use base::{
+    error, get_max_open_files, warn, AsRawDescriptors, Event, RawDescriptor, Tube,
+    UnlinkUnixListener,
+};
 use cros_async::{EventAsync, Executor};
 use data_model::{DataInit, Le32};
 use fuse::Server;
@@ -142,10 +145,13 @@ impl FsBackend {
         let mut keep_rds: Vec<RawDescriptor> = [0, 1, 2].to_vec();
         keep_rds.append(&mut fs.keep_rds());
 
+        let ex = ex.clone();
+        keep_rds.extend(ex.as_raw_descriptors());
+
         let server = Arc::new(Server::new(fs));
 
         Ok(FsBackend {
-            ex: ex.clone(),
+            ex,
             server,
             tag: fs_tag,
             avail_features,