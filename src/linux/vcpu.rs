google/crosvm

commit 06c00d580d9c1a206f74c1b5b6e5fe13b55a14ba

hypervisor: Trait changes for windows hypervisors support
No functional changes.

Add trait changes to allow upstreaming hypervisor implementations. This
change differentiates between the hypervisor related data structures
from the VM specific actions to take. The format chosen is using a FnMut
that gets passed to hypervisor handler which contains the VM specific
actions.

BUG=b:213150327
TEST=Compiled crosvm.

Change-Id: I05fde5ff3d6b5f42684c7bfe8a135b6805c9dc06
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3606975
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
Tested-by: kokoro <noreply+kokoro@google.com>
Commit-Queue: Vaibhav Nagarnaik <vnagarnaik@google.com>

src/linux/vcpu.rs
@@ -14,8 +14,8 @@ use libc::{self, c_int};
 
 use anyhow::{Context, Result};
 use base::*;
-use devices::{self, IrqChip, VcpuRunState};
-use hypervisor::{Vcpu, VcpuExit, VcpuRunHandle};
+use devices::{self, Bus, IrqChip, VcpuRunState};
+use hypervisor::{IoOperation, IoParams, Vcpu, VcpuExit, VcpuRunHandle};
 use vm_control::*;
 #[cfg(all(target_arch = "x86_64", feature = "gdb"))]
 use vm_memory::GuestMemory;
@@ -55,6 +55,32 @@ pub fn setup_vcpu_signal_handler<T: Vcpu>(use_hypervisor_signals: bool) -> Resul
         }
     }
     Ok(())
+}
+
+fn bus_io_handler(bus: &Bus) -> impl FnMut(IoParams) -> Option<[u8; 8]> + '_ {
+    |IoParams {
+         address,
+         mut size,
+         operation: direction,
+     }| match direction {
+        IoOperation::Read => {
+            let mut data = [0u8; 8];
+            if size > data.len() {
+                error!("unsupported Read size of {} bytes", size);
+                size = data.len();
+            }
+            bus.read(address, &mut data[..size]).then(|| data)
+        }
+        IoOperation::Write { data } => {
+            if size > data.len() {
+                error!("unsupported Write size of {} bytes", size);
+                size = data.len()
+            }
+            let data = &data[..size];
+            bus.write(address, data);
+            None
+        }
+    }
 }
 
 // Sets up a vcpu and converts it into a runnable vcpu.
@@ -262,13 +288,13 @@ fn handle_s2idle_request(privileged_vm: bool) {
 fn vcpu_loop<V>(
     mut run_mode: VmRunMode,
     cpu_id: usize,
-    vcpu: V,
+    mut vcpu: V,
     vcpu_run_handle: VcpuRunHandle,
     irq_chip: Box<dyn IrqChipArch + 'static>,
     run_rt: bool,
     delay_rt: bool,
-    io_bus: devices::Bus,
-    mmio_bus: devices::Bus,
+    io_bus: Bus,
+    mmio_bus: Bus,
     requires_pvclock_ctrl: bool,
     from_main_tube: mpsc::Receiver<VcpuControl>,
     use_hypervisor_signals: bool,
@@ -392,58 +418,24 @@ where
 
         if !interrupted_by_signal {
             match vcpu.run(&vcpu_run_handle) {
-                Ok(VcpuExit::IoIn { port, mut size }) => {
-                    let mut data = [0; 8];
-                    if size > data.len() {
-                        error!(
-                            "unsupported IoIn size of {} bytes at port {:#x}",
-                            size, port
-                        );
-                        size = data.len();
-                    }
-                    io_bus.read(port as u64, &mut data[..size]);
-                    if let Err(e) = vcpu.set_data(&data[..size]) {
-                        error!(
-                            "failed to set return data for IoIn at port {:#x}: {}",
-                            port, e
-                        );
+                Ok(VcpuExit::Io) => {
+                    if let Err(e) = vcpu.handle_io(&mut bus_io_handler(&io_bus)) {
+                        error!("failed to handle io: {}", e)
                     }
                 }
-                Ok(VcpuExit::IoOut {
-                    port,
-                    mut size,
-                    data,
-                }) => {
-                    if size > data.len() {
-                        error!(
-                            "unsupported IoOut size of {} bytes at port {:#x}",
-                            size, port
-                        );
-                        size = data.len();
+                Ok(VcpuExit::Mmio) => {
+                    if let Err(e) = vcpu.handle_mmio(&mut bus_io_handler(&mmio_bus)) {
+                        error!("failed to handle mmio: {}", e);
                     }
-                    io_bus.write(port as u64, &data[..size]);
-                }
-                Ok(VcpuExit::MmioRead { address, size }) => {
-                    let mut data = [0; 8];
-                    mmio_bus.read(address, &mut data[..size]);
-                    // Setting data for mmio can not fail.
-                    let _ = vcpu.set_data(&data[..size]);
-                }
-                Ok(VcpuExit::MmioWrite {
-                    address,
-                    size,
-                    data,
-                }) => {
-                    mmio_bus.write(address, &data[..size]);
                 }
                 Ok(VcpuExit::RdMsr { index }) => {
                     if let Some(data) = msr_handlers.read(index) {
-                        let _ = vcpu.set_data(&data.to_ne_bytes());
+                        let _ = vcpu.handle_rdmsr(data);
                     }
                 }
                 Ok(VcpuExit::WrMsr { index, data }) => {
                     if msr_handlers.write(index, data).is_some() {
-                        let _ = vcpu.set_data(&[]);
+                        vcpu.handle_wrmsr();
                     }
                 }
                 Ok(VcpuExit::IoapicEoi { vector }) => {
@@ -538,8 +530,8 @@ pub fn run_vcpu<V>(
     no_smt: bool,
     start_barrier: Arc<Barrier>,
     has_bios: bool,
-    mut io_bus: devices::Bus,
-    mut mmio_bus: devices::Bus,
+    mut io_bus: Bus,
+    mut mmio_bus: Bus,
     exit_evt: Event,
     reset_evt: Event,
     crash_evt: Event,

hypervisor/src/kvm/mod.rs
@@ -15,7 +15,7 @@ use x86_64::*;
 use std::cell::RefCell;
 use std::cmp::{min, Reverse};
 use std::collections::{BTreeMap, BinaryHeap};
-use std::convert::{TryFrom, TryInto};
+use std::convert::TryFrom;
 use std::ffi::CString;
 use std::mem::{size_of, ManuallyDrop};
 use std::os::raw::{c_int, c_ulong, c_void};
@@ -41,8 +41,9 @@ use sync::Mutex;
 use vm_memory::{GuestAddress, GuestMemory};
 
 use crate::{
-    ClockState, Datamatch, DeviceKind, Hypervisor, HypervisorCap, IoEventAddress, IrqRoute,
-    IrqSource, MPState, MemSlot, ProtectionType, Vcpu, VcpuExit, VcpuRunHandle, Vm, VmCap,
+    ClockState, Datamatch, DeviceKind, HypervHypercall, Hypervisor, HypervisorCap, IoEventAddress,
+    IoOperation, IoParams, IrqRoute, IrqSource, MPState, MemSlot, ProtectionType, Vcpu, VcpuExit,
+    VcpuRunHandle, Vm, VmCap,
 };
 
 // Wrapper around KVM_SET_USER_MEMORY_REGION ioctl, which creates, modifies, or deletes a mapping
@@ -780,88 +781,6 @@ impl Vcpu for KvmVcpu {
         f
     }
 
-    #[allow(clippy::cast_ptr_alignment)]
-    fn set_data(&self, data: &[u8]) -> Result<()> {
-        // Safe because we know we mapped enough memory to hold the kvm_run struct because the
-        // kernel told us how large it was. The pointer is page aligned so casting to a different
-        // type is well defined, hence the clippy allow attribute.
-        let run = unsafe { &mut *(self.run_mmap.as_ptr() as *mut kvm_run) };
-        match run.exit_reason {
-            KVM_EXIT_IO => {
-                let run_start = run as *mut kvm_run as *mut u8;
-                // Safe because the exit_reason (which comes from the kernel) told us which
-                // union field to use.
-                let io = unsafe { run.__bindgen_anon_1.io };
-                if io.direction as u32 != KVM_EXIT_IO_IN {
-                    return Err(Error::new(EINVAL));
-                }
-                let data_size = (io.count as usize) * (io.size as usize);
-                if data_size != data.len() {
-                    return Err(Error::new(EINVAL));
-                }
-                // The data_offset is defined by the kernel to be some number of bytes into the
-                // kvm_run structure, which we have fully mmap'd.
-                unsafe {
-                    let data_ptr = run_start.offset(io.data_offset as isize);
-                    copy_nonoverlapping(data.as_ptr(), data_ptr, data_size);
-                }
-                Ok(())
-            }
-            KVM_EXIT_MMIO => {
-                // Safe because the exit_reason (which comes from the kernel) told us which
-                // union field to use.
-                let mmio = unsafe { &mut run.__bindgen_anon_1.mmio };
-                if mmio.is_write != 0 {
-                    return Err(Error::new(EINVAL));
-                }
-                let len = mmio.len as usize;
-                if len != data.len() {
-                    return Err(Error::new(EINVAL));
-                }
-                mmio.data[..len].copy_from_slice(data);
-                Ok(())
-            }
-            KVM_EXIT_HYPERV => {
-                // Safe because the exit_reason (which comes from the kernel) told us which
-                // union field to use.
-                let hyperv = unsafe { &mut run.__bindgen_anon_1.hyperv };
-                if hyperv.type_ != KVM_EXIT_HYPERV_HCALL {
-                    return Err(Error::new(EINVAL));
-                }
-                let hcall = unsafe { &mut hyperv.u.hcall };
-                match data.try_into() {
-                    Ok(data) => {
-                        hcall.result = u64::from_ne_bytes(data);
-                    }
-                    _ => return Err(Error::new(EINVAL)),
-                }
-                Ok(())
-            }
-            KVM_EXIT_X86_RDMSR => {
-                // Safe because the exit_reason (which comes from the kernel) told us which
-                // union field to use.
-                let msr = unsafe { &mut run.__bindgen_anon_1.msr };
-
-                match data.try_into() {
-                    Ok(data) => {
-                        msr.data = u64::from_ne_bytes(data);
-                        msr.error = 0;
-                    }
-                    _ => return Err(Error::new(EINVAL)),
-                }
-                Ok(())
-            }
-            KVM_EXIT_X86_WRMSR => {
-                // Safe because the exit_reason (which comes from the kernel) told us which
-                // union field to use.
-                let msr = unsafe { &mut run.__bindgen_anon_1.msr };
-                msr.error = 0;
-                Ok(())
-            }
-            _ => Err(Error::new(EINVAL)),
-        }
-    }
-
     fn pvclock_ctrl(&self) -> Result<()> {
         self.pvclock_ctrl_arch()
     }
@@ -917,7 +836,7 @@ impl Vcpu for KvmVcpu {
     #[allow(clippy::cast_ptr_alignment)]
     // The pointer is page aligned so casting to a different type is well defined, hence the clippy
     // allow attribute.
-    fn run(&self, run_handle: &VcpuRunHandle) -> Result<VcpuExit> {
+    fn run(&mut self, run_handle: &VcpuRunHandle) -> Result<VcpuExit> {
         // Acquire is used to ensure this check is ordered after the `compare_exchange` in `run`.
         if self
             .vcpu_run_handle_fingerprint
@@ -937,74 +856,15 @@ impl Vcpu for KvmVcpu {
         // kernel told us how large it was.
         let run = unsafe { &mut *(self.run_mmap.as_ptr() as *mut kvm_run) };
         match run.exit_reason {
-            KVM_EXIT_IO => {
-                // Safe because the exit_reason (which comes from the kernel) told us which
-                // union field to use.
-                let io = unsafe { run.__bindgen_anon_1.io };
-                let port = io.port;
-                let size = (io.count as usize) * (io.size as usize);
-                match io.direction as u32 {
-                    KVM_EXIT_IO_IN => Ok(VcpuExit::IoIn { port, size }),
-                    KVM_EXIT_IO_OUT => {
-                        let mut data = [0; 8];
-                        let run_start = run as *const kvm_run as *const u8;
-                        // The data_offset is defined by the kernel to be some number of bytes
-                        // into the kvm_run structure, which we have fully mmap'd.
-                        unsafe {
-                            let data_ptr = run_start.offset(io.data_offset as isize);
-                            copy_nonoverlapping(data_ptr, data.as_mut_ptr(), min(size, data.len()));
-                        }
-                        Ok(VcpuExit::IoOut { port, size, data })
-                    }
-                    _ => Err(Error::new(EINVAL)),
-                }
-            }
-            KVM_EXIT_MMIO => {
-                // Safe because the exit_reason (which comes from the kernel) told us which
-                // union field to use.
-                let mmio = unsafe { &run.__bindgen_anon_1.mmio };
-                let address = mmio.phys_addr;
-                let size = min(mmio.len as usize, mmio.data.len());
-                if mmio.is_write != 0 {
-                    Ok(VcpuExit::MmioWrite {
-                        address,
-                        size,
-                        data: mmio.data,
-                    })
-                } else {
-                    Ok(VcpuExit::MmioRead { address, size })
-                }
-            }
+            KVM_EXIT_IO => Ok(VcpuExit::Io),
+            KVM_EXIT_MMIO => Ok(VcpuExit::Mmio),
             KVM_EXIT_IOAPIC_EOI => {
                 // Safe because the exit_reason (which comes from the kernel) told us which
                 // union field to use.
                 let vector = unsafe { run.__bindgen_anon_1.eoi.vector };
                 Ok(VcpuExit::IoapicEoi { vector })
             }
-            KVM_EXIT_HYPERV => {
-                // Safe because the exit_reason (which comes from the kernel) told us which
-                // union field to use.
-                let hyperv = unsafe { &run.__bindgen_anon_1.hyperv };
-                match hyperv.type_ as u32 {
-                    KVM_EXIT_HYPERV_SYNIC => {
-                        let synic = unsafe { &hyperv.u.synic };
-                        Ok(VcpuExit::HypervSynic {
-                            msr: synic.msr,
-                            control: synic.control,
-                            evt_page: synic.evt_page,
-                            msg_page: synic.msg_page,
-                        })
-                    }
-                    KVM_EXIT_HYPERV_HCALL => {
-                        let hcall = unsafe { &hyperv.u.hcall };
-                        Ok(VcpuExit::HypervHcall {
-                            input: hcall.input,
-                            params: hcall.params,
-                        })
-                    }
-                    _ => Err(Error::new(EINVAL)),
-                }
-            }
+            KVM_EXIT_HYPERV => Ok(VcpuExit::HypervHypercall),
             KVM_EXIT_UNKNOWN => Ok(VcpuExit::Unknown),
             KVM_EXIT_EXCEPTION => Ok(VcpuExit::Exception),
             KVM_EXIT_HYPERCALL => Ok(VcpuExit::Hypercall),
@@ -1078,6 +938,147 @@ impl Vcpu for KvmVcpu {
             }
             r => panic!("unknown kvm exit reason: {}", r),
         }
+    }
+
+    fn handle_mmio(&self, handle_fn: &mut dyn FnMut(IoParams) -> Option<[u8; 8]>) -> Result<()> {
+        // Safe because we know we mapped enough memory to hold the kvm_run struct because the
+        // kernel told us how large it was. The pointer is page aligned so casting to a different
+        // type is well defined, hence the clippy allow attribute.
+        let run = unsafe { &mut *(self.run_mmap.as_ptr() as *mut kvm_run) };
+        // Verify that the handler is called in the right context.
+        assert!(run.exit_reason == KVM_EXIT_MMIO);
+        // Safe because the exit_reason (which comes from the kernel) told us which
+        // union field to use.
+        let mmio = unsafe { &mut run.__bindgen_anon_1.mmio };
+        let address = mmio.phys_addr;
+        let size = min(mmio.len as usize, mmio.data.len());
+        if mmio.is_write != 0 {
+            handle_fn(IoParams {
+                address,
+                size,
+                operation: IoOperation::Write { data: mmio.data },
+            });
+            Ok(())
+        } else if let Some(data) = handle_fn(IoParams {
+            address,
+            size,
+            operation: IoOperation::Read,
+        }) {
+            mmio.data[..size].copy_from_slice(&data[..size]);
+            Ok(())
+        } else {
+            Err(Error::new(EINVAL))
+        }
+    }
+
+    fn handle_io(&self, handle_fn: &mut dyn FnMut(IoParams) -> Option<[u8; 8]>) -> Result<()> {
+        // Safe because we know we mapped enough memory to hold the kvm_run struct because the
+        // kernel told us how large it was. The pointer is page aligned so casting to a different
+        // type is well defined, hence the clippy allow attribute.
+        let run = unsafe { &mut *(self.run_mmap.as_ptr() as *mut kvm_run) };
+        // Verify that the handler is called in the right context.
+        assert!(run.exit_reason == KVM_EXIT_IO);
+        let run_start = run as *mut kvm_run as *mut u8;
+        // Safe because the exit_reason (which comes from the kernel) told us which
+        // union field to use.
+        let io = unsafe { run.__bindgen_anon_1.io };
+        let size = (io.count as usize) * (io.size as usize);
+        match io.direction as u32 {
+            KVM_EXIT_IO_IN => {
+                if let Some(data) = handle_fn(IoParams {
+                    address: io.port.into(),
+                    size,
+                    operation: IoOperation::Read,
+                }) {
+                    // The data_offset is defined by the kernel to be some number of bytes
+                    // into the kvm_run structure, which we have fully mmap'd.
+                    unsafe {
+                        let data_ptr = run_start.offset(io.data_offset as isize);
+                        copy_nonoverlapping(data.as_ptr(), data_ptr, size);
+                    }
+                    Ok(())
+                } else {
+                    Err(Error::new(EINVAL))
+                }
+            }
+            KVM_EXIT_IO_OUT => {
+                let mut data = [0; 8];
+                // The data_offset is defined by the kernel to be some number of bytes
+                // into the kvm_run structure, which we have fully mmap'd.
+                unsafe {
+                    let data_ptr = run_start.offset(io.data_offset as isize);
+                    copy_nonoverlapping(data_ptr, data.as_mut_ptr(), min(size, data.len()));
+                }
+                handle_fn(IoParams {
+                    address: io.port.into(),
+                    size,
+                    operation: IoOperation::Write { data },
+                });
+                Ok(())
+            }
+            _ => Err(Error::new(EINVAL)),
+        }
+    }
+
+    fn handle_hyperv_hypercall(
+        &self,
+        handle_fn: &mut dyn FnMut(HypervHypercall) -> u64,
+    ) -> Result<()> {
+        // Safe because we know we mapped enough memory to hold the kvm_run struct because the
+        // kernel told us how large it was.
+        let run = unsafe { &mut *(self.run_mmap.as_ptr() as *mut kvm_run) };
+        // Verify that the handler is called in the right context.
+        assert!(run.exit_reason == KVM_EXIT_HYPERV_HCALL);
+        // Safe because the exit_reason (which comes from the kernel) told us which
+        // union field to use.
+        let hyperv = unsafe { &mut run.__bindgen_anon_1.hyperv };
+        match hyperv.type_ as u32 {
+            KVM_EXIT_HYPERV_SYNIC => {
+                let synic = unsafe { &hyperv.u.synic };
+                handle_fn(HypervHypercall::HypervSynic {
+                    msr: synic.msr,
+                    control: synic.control,
+                    evt_page: synic.evt_page,
+                    msg_page: synic.msg_page,
+                });
+                Ok(())
+            }
+            KVM_EXIT_HYPERV_HCALL => {
+                let hcall = unsafe { &mut hyperv.u.hcall };
+                hcall.result = handle_fn(HypervHypercall::HypervHcall {
+                    input: hcall.input,
+                    params: hcall.params,
+                });
+                Ok(())
+            }
+            _ => Err(Error::new(EINVAL)),
+        }
+    }
+
+    fn handle_rdmsr(&self, data: u64) -> Result<()> {
+        // Safe because we know we mapped enough memory to hold the kvm_run struct because the
+        // kernel told us how large it was.
+        let run = unsafe { &mut *(self.run_mmap.as_ptr() as *mut kvm_run) };
+        // Verify that the handler is called in the right context.
+        assert!(run.exit_reason == KVM_EXIT_X86_RDMSR);
+        // Safe because the exit_reason (which comes from the kernel) told us which
+        // union field to use.
+        let msr = unsafe { &mut run.__bindgen_anon_1.msr };
+        msr.data = data;
+        msr.error = 0;
+        Ok(())
+    }
+
+    fn handle_wrmsr(&self) {
+        // Safe because we know we mapped enough memory to hold the kvm_run struct because the
+        // kernel told us how large it was.
+        let run = unsafe { &mut *(self.run_mmap.as_ptr() as *mut kvm_run) };
+        // Verify that the handler is called in the right context.
+        assert!(run.exit_reason == KVM_EXIT_X86_WRMSR);
+        // Safe because the exit_reason (which comes from the kernel) told us which
+        // union field to use.
+        let msr = unsafe { &mut run.__bindgen_anon_1.msr };
+        msr.error = 0;
     }
 }
 

hypervisor/src/lib.rs
@@ -211,6 +211,26 @@ impl Drop for VcpuRunHandle {
     }
 }
 
+/// Operation for Io and Mmio
+#[derive(Copy, Clone, Debug)]
+pub enum IoOperation {
+    Read,
+    Write {
+        /// Data to be written.
+        ///
+        /// For 64 bit architecture, Mmio and Io only work with at most 8 bytes of data.
+        data: [u8; 8],
+    },
+}
+
+/// Parameters describing an MMIO or PIO from the guest.
+#[derive(Copy, Clone, Debug)]
+pub struct IoParams {
+    pub address: u64,
+    pub size: usize,
+    pub operation: IoOperation,
+}
+
 /// A virtual CPU holding a virtualized hardware thread's state, such as registers and interrupt
 /// state, which may be used to execute virtual machines.
 ///
@@ -239,7 +259,7 @@ pub trait Vcpu: downcast_rs::DowncastSync {
     /// Note that the state of the VCPU and associated VM must be setup first for this to do
     /// anything useful. The given `run_handle` must be the same as the one returned by
     /// `take_run_handle` for this `Vcpu`.
-    fn run(&self, run_handle: &VcpuRunHandle) -> Result<VcpuExit>;
+    fn run(&mut self, run_handle: &VcpuRunHandle) -> Result<VcpuExit>;
 
     /// Returns the vcpu id.
     fn id(&self) -> usize;
@@ -256,11 +276,48 @@ pub trait Vcpu: downcast_rs::DowncastSync {
     /// signal-safe way when called.
     fn set_local_immediate_exit_fn(&self) -> extern "C" fn();
 
-    /// Sets the data received by a mmio read, ioport in, or hypercall instruction.
+    /// Handles an incoming MMIO request from the guest.
     ///
-    /// This function should be called after `Vcpu::run` returns an `VcpuExit::IoIn`,
-    /// `VcpuExit::MmioRead`, or 'VcpuExit::HypervHcall`.
-    fn set_data(&self, data: &[u8]) -> Result<()>;
+    /// This function should be called after `Vcpu::run` returns `VcpuExit::Mmio`, and in the same
+    /// thread as run().
+    ///
+    /// Once called, it will determine whether a MMIO read or MMIO write was the reason for the MMIO
+    /// exit, call `handle_fn` with the respective IoParams to perform the MMIO read or write, and
+    /// set the return data in the vcpu so that the vcpu can resume running.
+    fn handle_mmio(&self, handle_fn: &mut dyn FnMut(IoParams) -> Option<[u8; 8]>) -> Result<()>;
+
+    /// Handles an incoming PIO from the guest.
+    ///
+    /// This function should be called after `Vcpu::run` returns `VcpuExit::Io`, and in the same
+    /// thread as run().
+    ///
+    /// Once called, it will determine whether an input or output was the reason for the Io exit,
+    /// call `handle_fn` with the respective IoParams to perform the input/output operation, and set
+    /// the return data in the vcpu so that the vcpu can resume running.
+    fn handle_io(&self, handle_fn: &mut dyn FnMut(IoParams) -> Option<[u8; 8]>) -> Result<()>;
+
+    /// Handles the HYPERV_HYPERCALL exit from a vcpu.
+    ///
+    /// This function should be called after `Vcpu::run` returns `VcpuExit::HypervHcall`, and in the
+    /// same thread as run.
+    ///
+    /// Once called, it will parse the appropriate input parameters to the provided function to
+    /// handle the hyperv call, and then set the return data into the vcpu so it can resume running.
+    fn handle_hyperv_hypercall(&self, func: &mut dyn FnMut(HypervHypercall) -> u64) -> Result<()>;
+
+    /// Handles a RDMSR exit from the guest.
+    ///
+    /// This function should be called after `Vcpu::run` returns `VcpuExit::RdMsr`,
+    /// and in the same thread as run.
+    ///
+    /// It will put `data` into the guest buffer and return.
+    fn handle_rdmsr(&self, data: u64) -> Result<()>;
+
+    /// Handles a WRMSR exit from the guest by removing any error indication for the operation.
+    ///
+    /// This function should be called after `Vcpu::run` returns `VcpuExit::WrMsr`,
+    /// and in the same thread as run.
+    fn handle_wrmsr(&self);
 
     /// Signals to the hypervisor that this guest is being paused by userspace.  Only works on Vms
     /// that support `VmCap::PvClockSuspend`.
@@ -303,47 +360,16 @@ pub enum Datamatch {
 /// A reason why a VCPU exited. One of these returns every time `Vcpu::run` is called.
 #[derive(Debug)]
 pub enum VcpuExit {
-    /// An out port instruction was run on the given port with the given data.
-    IoOut {
-        port: u16,
-        size: usize,
-        data: [u8; 8],
-    },
-    /// An in port instruction was run on the given port.
-    ///
-    /// The data that the instruction receives should be set with `set_data` before `Vcpu::run` is
-    /// called again.
-    IoIn {
-        port: u16,
-        size: usize,
-    },
-    /// A read instruction was run against the given MMIO address.
-    ///
-    /// The data that the instruction receives should be set with `set_data` before `Vcpu::run` is
-    /// called again.
-    MmioRead {
-        address: u64,
-        size: usize,
-    },
-    /// A write instruction was run against the given MMIO address with the given data.
-    MmioWrite {
-        address: u64,
-        size: usize,
-        data: [u8; 8],
-    },
+    /// An io instruction needs to be emulated.
+    /// vcpu handle_io should be called to handle the io operation
+    Io,
+    /// A mmio instruction needs to be emulated.
+    /// vcpu handle_mmio should be called to handle the mmio operation
+    Mmio,
     IoapicEoi {
         vector: u8,
     },
-    HypervSynic {
-        msr: u32,
-        control: u64,
-        evt_page: u64,
-        msg_page: u64,
-    },
-    HypervHcall {
-        input: u64,
-        params: [u64; 2],
-    },
+    HypervHypercall,
     Unknown,
     Exception,
     Hypercall,
@@ -378,6 +404,21 @@ pub enum VcpuExit {
     WrMsr {
         index: u32,
         data: u64,
+    },
+}
+
+/// A hypercall with parameters being made from the guest.
+#[derive(Debug)]
+pub enum HypervHypercall {
+    HypervSynic {
+        msr: u32,
+        control: u64,
+        evt_page: u64,
+        msg_page: u64,
+    },
+    HypervHcall {
+        input: u64,
+        params: [u64; 2],
     },
 }
 

commit 13e29506329e537efa8aad39ee9419c1b128cac0

vcpu: restore behavior of reading unpopulated Bus regions
When handling a MMIO or IO read vcpu exit, we don't want to report an
error if there is nothing on the Bus at that location. Instead, we
should just return all zeroes. This matches the previous behavior before
https://crrev.com/c/3606975 and removes an error message during
integration_tests:boot.

BUG=b:213150327
TEST=tools/dev_container tools/presubmit --all

Fixes: 06c00d580d9c ("hypervisor: Trait changes for windows hypervisors support")
Change-Id: I6c955ece9184f253b1173e46465cd9a6dafe25bf
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3631029
Reviewed-by: Vaibhav Nagarnaik <vnagarnaik@google.com>
Reviewed-by: Noah Gold <nkgold@google.com>
Tested-by: kokoro <noreply+kokoro@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

src/linux/vcpu.rs
@@ -69,7 +69,10 @@ fn bus_io_handler(bus: &Bus) -> impl FnMut(IoParams) -> Option<[u8; 8]> + '_ {
                 error!("unsupported Read size of {} bytes", size);
                 size = data.len();
             }
-            bus.read(address, &mut data[..size]).then(|| data)
+            // Ignore the return value of `read()`. If no device exists on the bus at the given
+            // location, return the initial value of data, which is all zeroes.
+            let _ = bus.read(address, &mut data[..size]);
+            Some(data)
         }
         IoOperation::Write { data } => {
             if size > data.len() {