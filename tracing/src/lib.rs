13313d403ac27761463732ba9cb883b93f56aa53
Vikram Auradkar <auradkar@google.com>
2022-06-16 00:19:18 +0000

    tracing: Add noop tracing
    BUG=b:213154559
    TEST=presubmit
    Change-Id: Idfe23544c8381e664d913c4a80742f4e25a30006
    
    Change-Id: I2bb0b59098ae183c45e92fcf15e1bcfe834df204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3707622
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Commit-Queue: Vikram Auradkar <auradkar@google.com>

---
+++ tracing/src/lib.rs
@@ -0,0 +1,8 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// A crate that provides noop tracing.
+mod noop;
+
+pub use noop::*;
