google/crosvm

commit 21445b1b8378b72f9dfe7a1773499c09763e5367

base: Upstream unix net set_nonblocking fn
Bug: b:231641496
Upstream-Crate: base/src/sys/unix
Change-Id: I62b07f2fc3f7a02c1b72da735ac41d6dc73416f9
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3708762
Reviewed-by: Noah Gold <nkgold@google.com>
Tested-by: kokoro <noreply+kokoro@google.com>
Reviewed-by: Vikram Auradkar <auradkar@google.com>
Commit-Queue: Clarissa Garvey <clarissagarvey@chromium.org>

base/Cargo.toml
@@ -27,6 +27,9 @@ tempfile = "3"
 thiserror = "1.0.20"
 uuid = { version = "0.8.2", features = ["v4"] }
 
+[target.'cfg(unix)'.dependencies]
+cvt = "*"
+
 [target.'cfg(windows)'.dependencies]
 lazy_static = "*"
 rand = "*"

base/src/sys/unix/net.rs
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use cvt::cvt;
 use std::{
     cmp::Ordering,
     convert::TryFrom,
@@ -594,6 +595,12 @@ impl UnixSeqpacket {
     pub fn set_write_timeout(&self, timeout: Option<Duration>) -> io::Result<()> {
         self.set_timeout(timeout, libc::SO_SNDTIMEO)
     }
+
+    /// Sets the blocking mode for this socket.
+    pub fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()> {
+        let mut nonblocking = nonblocking as libc::c_int;
+        cvt(unsafe { libc::ioctl(self.fd, libc::FIONBIO, &mut nonblocking) }).map(drop)
+    }
 }
 
 impl Drop for UnixSeqpacket {
@@ -786,6 +793,12 @@ impl UnixSeqpacketListener {
                 .collect(),
         );
         Ok(path_os_str.into())
+    }
+
+    /// Sets the blocking mode for this socket.
+    pub fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()> {
+        let mut nonblocking = nonblocking as libc::c_int;
+        cvt(unsafe { libc::ioctl(self.fd, libc::FIONBIO, &mut nonblocking) }).map(drop)
     }
 }
 

commit b33353cc46b5cb9c4d627e48db0b12e75e3786c6

base: remove use of cvt crate
cvt isn't used anywhere else in the base crate, and it isn't included in
the ChromeOS crosvm ebuild, so this breaks the CrOS build. Just check
the result and return an error the same way we do elsewhere in base.

BUG=b:231641496
TEST=cargo build
TEST=emerge-hatch crosvm

Fixes: 21445b1b8378 ("base: Upstream unix net set_nonblocking fn")
Change-Id: Iba8beee21810210e50e0c715a1b84de29278625a
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3716851
Tested-by: kokoro <noreply+kokoro@google.com>
Reviewed-by: Dennis Kempin <denniskempin@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

base/Cargo.toml
@@ -27,9 +27,6 @@ tempfile = "3"
 thiserror = "1.0.20"
 uuid = { version = "0.8.2", features = ["v4"] }
 
-[target.'cfg(unix)'.dependencies]
-cvt = "*"
-
 [target.'cfg(windows)'.dependencies]
 lazy_static = "*"
 rand = "*"

base/src/sys/unix/net.rs
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use cvt::cvt;
 use std::{
     cmp::Ordering,
     convert::TryFrom,
@@ -599,7 +598,12 @@ impl UnixSeqpacket {
     /// Sets the blocking mode for this socket.
     pub fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()> {
         let mut nonblocking = nonblocking as libc::c_int;
-        cvt(unsafe { libc::ioctl(self.fd, libc::FIONBIO, &mut nonblocking) }).map(drop)
+        let ret = unsafe { libc::ioctl(self.fd, libc::FIONBIO, &mut nonblocking) };
+        if ret < 0 {
+            Err(io::Error::last_os_error())
+        } else {
+            Ok(())
+        }
     }
 }
 
@@ -798,7 +802,12 @@ impl UnixSeqpacketListener {
     /// Sets the blocking mode for this socket.
     pub fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()> {
         let mut nonblocking = nonblocking as libc::c_int;
-        cvt(unsafe { libc::ioctl(self.fd, libc::FIONBIO, &mut nonblocking) }).map(drop)
+        let ret = unsafe { libc::ioctl(self.fd, libc::FIONBIO, &mut nonblocking) };
+        if ret < 0 {
+            Err(io::Error::last_os_error())
+        } else {
+            Ok(())
+        }
     }
 }
 