google/crosvm

commit 28c318fa20fa4e94a827602fbd6337ce4e4b1f43

Fix ChromeOS build
Fix a crosvm-direct build breakage and some audio_cras build breakages
and clippy warnings.

BUG=None
TEST=emerge-brya-manatee crosvm

Change-Id: I480f0b32b531110d5397c171446ab0dccc7d8808
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/3802785
Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
Commit-Queue: David Stevens <stevensd@chromium.org>
Tested-by: David Stevens <stevensd@chromium.org>

devices/src/pci/ac97/sys/unix.rs
@@ -4,8 +4,14 @@
 
 use audio_streams::shm_streams::NullShmStreamSource;
 use audio_streams::shm_streams::ShmStreamSource;
-use base::error;
-use serde::{Deserialize, Serialize};
+#[cfg(feature = "audio_cras")]
+use libcras::CrasClient;
+#[cfg(feature = "audio_cras")]
+use libcras::CrasClientType;
+#[cfg(feature = "audio_cras")]
+use libcras::CrasSocketType;
+use serde::Deserialize;
+use serde::Serialize;
 use vm_memory::GuestMemory;
 
 use crate::pci::ac97::Ac97Dev;
@@ -26,10 +32,10 @@ pub enum Ac97Backend {
 impl Ac97Dev {
     pub(in crate::pci::ac97) fn initialize_backend(
         ac97_backend: &Ac97Backend,
-        mem: GuestMemory,
+        #[allow(unused_variables)] mem: GuestMemory,
         #[allow(unused_variables)] param: &Ac97Parameters,
     ) -> Result<Self> {
-        match ac97_backend {
+        match *ac97_backend {
             #[cfg(feature = "audio_cras")]
             Ac97Backend::Cras => Self::create_cras_audio_device(param, mem.clone()).or_else(|e| {
                 error!(
@@ -38,10 +44,6 @@ impl Ac97Dev {
                 );
                 Ok(Self::create_null_audio_device(mem))
             }),
-            _ => {
-                error!("Invalid ac97_backend. Fallback to null audio device");
-                Ok(Self::create_null_audio_device(mem))
-            }
         }
     }
 
@@ -71,10 +73,9 @@ impl Ac97Dev {
     /// Return the minijail policy file path for the current Ac97Dev.
     pub fn minijail_policy(&self) -> &'static str {
         match &self.backend {
-            crate::pci::ac97::Ac97Backend::System(backend) => match backend {
+            crate::pci::ac97::Ac97Backend::System(backend) => match *backend {
                 #[cfg(feature = "audio_cras")]
                 Ac97Backend::Cras => "cras_audio_device",
-                _ => "null_audio_device",
             },
             crate::pci::ac97::Ac97Backend::NULL => "null_audio_device",
         }

devices/src/pci/ac97.rs
@@ -13,8 +13,6 @@ use base::RawDescriptor;
 #[cfg(windows)]
 use base::Tube;
 #[cfg(feature = "audio_cras")]
-use libcras::CrasClient;
-#[cfg(feature = "audio_cras")]
 use libcras::CrasClientType;
 #[cfg(feature = "audio_cras")]
 use libcras::CrasSocketType;

src/crosvm/config.rs
@@ -323,7 +323,7 @@ impl TouchDeviceOption {
     }
 
     /// If the user specifies the size, use it. Otherwise, use the default values.
-    #[cfg(feature = "gpu")]
+    #[cfg(any(unix, feature = "gpu"))]
     pub fn get_size(&self) -> (u32, u32) {
         (
             self.width.unwrap_or(self.default_width),

commit 44146d272ce19eccd33e13fb8b5c7c9dc42fd840

devices: ac97: fix missing import with audio_cras
The `error!` macro is still used when the audio_cras feature is enabled.
Add its import back.

BUG=None
TEST=emerge-trogdor crosvm

Change-Id: I06430fece32fc203bf0e9005f29e346dad88eea4
Fixes: 28c318fa20fa ("Fix ChromeOS build")
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/3798972
Tested-by: Daniel Verkamp <dverkamp@chromium.org>
Auto-Submit: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
Commit-Queue: Junichi Uekawa <uekawa@chromium.org>

devices/src/pci/ac97/sys/unix.rs
@@ -5,6 +5,8 @@
 use audio_streams::shm_streams::NullShmStreamSource;
 use audio_streams::shm_streams::ShmStreamSource;
 #[cfg(feature = "audio_cras")]
+use base::error;
+#[cfg(feature = "audio_cras")]
 use libcras::CrasClient;
 #[cfg(feature = "audio_cras")]
 use libcras::CrasClientType;