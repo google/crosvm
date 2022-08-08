669af0d4d7444d9073e4cb91ebb2fa1b5ca9581e
Pierre-Clément Tosi <ptosi@google.com>
2022-08-08 13:13:44 +0100

    VcpuRegAArch64: Parametrize X0-X30 as X(u8)
    Use a single variant to represent a core GPR instead of having one
    variant per register. This simplifies manipulating VcpuRegAArch64 at the
    expense of needing the instantiating code to ensure that the <u8> is
    properly set (i.e. is < 31). This is currently not an issue as the
    variant is only instantiated with constant values.
    
    BUG=b:222222882
    BUG=chromium:1141812
    TEST=tools/dev_container ./tools/run_tests
    
    Change-Id: I0043c101e88794a1661fadbc22b79fc68356270d
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/3822688
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
    Auto-Submit: Pierre-Clément Tosi <ptosi@google.com>
    Tested-by: Pierre-Clément Tosi <ptosi@google.com>

--- aarch64/src/lib.rs
+++ aarch64/src/lib.rs
@@ -754,13 +754,13 @@ impl AArch64 {
             };
             match protected_vm {
                 ProtectionType::Protected => {
-                    vcpu.set_one_reg(VcpuRegAArch64::X1, entry_addr.offset())
+                    vcpu.set_one_reg(VcpuRegAArch64::X(1), entry_addr.offset())
                         .map_err(Error::SetReg)?;
                 }
                 ProtectionType::UnprotectedWithFirmware => {
                     vcpu.set_one_reg(VcpuRegAArch64::Pc, AARCH64_PROTECTED_VM_FW_START)
                         .map_err(Error::SetReg)?;
-                    vcpu.set_one_reg(VcpuRegAArch64::X1, entry_addr.offset())
+                    vcpu.set_one_reg(VcpuRegAArch64::X(1), entry_addr.offset())
                         .map_err(Error::SetReg)?;
                 }
                 ProtectionType::Unprotected | ProtectionType::ProtectedWithoutFirmware => {
@@ -772,7 +772,7 @@ impl AArch64 {
             /* X0 -- fdt address */
             let mem_size = guest_mem.memory_size();
             let fdt_addr = (AARCH64_PHYS_MEM_START + fdt_offset(mem_size, has_bios)) as u64;
-            vcpu.set_one_reg(VcpuRegAArch64::X0, fdt_addr)
+            vcpu.set_one_reg(VcpuRegAArch64::X(0), fdt_addr)
                 .map_err(Error::SetReg)?;
 
             /* X2 -- image size */
@@ -780,7 +780,7 @@ impl AArch64 {
                 protected_vm,
                 ProtectionType::Protected | ProtectionType::UnprotectedWithFirmware
             ) {
-                vcpu.set_one_reg(VcpuRegAArch64::X2, image_size as u64)
+                vcpu.set_one_reg(VcpuRegAArch64::X(2), image_size as u64)
                     .map_err(Error::SetReg)?;
             }
         }
