39b125eb726ebe6910b82198b351a7624ee26a53
Dennis Kempin <denniskempin@google.com>
2022-08-18 21:15:26 +0000

    Revert "test_runner: Support multiple profiles per test"
    This reverts commit 9ec4afc0ae23e739da343e4f661ac849ac2807cd.
    
    Reason for revert: Breaks post-submit
    
    https://ci.chromium.org/ui/p/crosvm/builders/ci/linux_x86_64/b8805461167965542065/overview
    
    Original change's description:
    > test_runner: Support multiple profiles per test
    >
    > Tests may spawn multiple processes (e.g. for running crosvm in
    > integration_tests). We want those to generate a separate profile
    > file to collect them as well.
    >
    > This should add coverage from integration tests into the coverage
    > data.
    >
    > BUG=b:239255082
    > TEST=./tools/run_tests --generate-lcov coverage.lcov
    >
    > Change-Id: Ic6c6b0801b676c96c4692069c1cd6111edea6fc4
    > Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/3840311
    > Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    > Commit-Queue: Dennis Kempin <denniskempin@google.com>
    > Tested-by: Dennis Kempin <denniskempin@google.com>
    
    Bug: b:239255082
    Change-Id: I958792f86a5d5bc24910c057c61242fe405db20d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/3840169
    Tested-by: Dennis Kempin <denniskempin@google.com>
    Commit-Queue: Dennis Kempin <denniskempin@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Dennis Kempin <denniskempin@google.com>

--- tools/impl/test_runner.py
+++ tools/impl/test_runner.py
@@ -71,14 +71,14 @@ class ExecutableResults(object):
         success: bool,
         test_log: str,
         previous_attempts: List["ExecutableResults"],
-        profile_files: List[Path],
+        profile_file: Optional[Path],
     ):
         self.name = name
         self.binary_file = binary_file
         self.success = success
         self.test_log = test_log
         self.previous_attempts = previous_attempts
-        self.profile_files = profile_files
+        self.profile_file = profile_file
 
 
 class Executable(NamedTuple):
@@ -250,7 +250,7 @@ def build_all_binaries(target: TestTarget, crosvm_direct: bool, instrument_cover
 
     print("Building crosvm workspace")
     features = BUILD_FEATURES[str(target.build_triple)]
-    extra_args: List[str] = []
+    extra_args = []
     if crosvm_direct:
         features += ",direct"
         extra_args.append("--no-default-features")
@@ -316,22 +316,24 @@ def execute_test(target: TestTarget, attempts: int, collect_coverage: bool, exec
             print(f"Running test {executable.name} on {target}... (attempt {i}/{attempts})")
 
         try:
+            profile_file = None
+            if collect_coverage:
+                profile_file = binary_path.with_suffix(".profraw")
+
             # Pipe stdout/err to be printed in the main process if needed.
             test_process = test_target.exec_file_on_target(
                 target,
                 binary_path,
                 args=args,
                 timeout=get_test_timeout(target, executable),
-                generate_profile=True,
+                profile_file=profile_file,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT,
             )
-            profile_files: List[Path] = []
-            if collect_coverage:
-                profile_files = [*test_target.list_profile_files(binary_path)]
-                if not profile_files:
-                    print()
-                    print(f"Warning: Running {binary_path} did not produce a profile file.")
+            if profile_file and not profile_file.exists():
+                print()
+                print(f"Warning: Running {binary_path} did not produce profile file.")
+                profile_file = None
 
             result = ExecutableResults(
                 executable.name,
@@ -339,7 +341,7 @@ def execute_test(target: TestTarget, attempts: int, collect_coverage: bool, exec
                 test_process.returncode == 0,
                 test_process.stdout,
                 previous_attempts,
-                profile_files,
+                profile_file,
             )
         except subprocess.TimeoutExpired as e:
             # Append a note about the timeout to the stdout of the process.
@@ -350,7 +352,7 @@ def execute_test(target: TestTarget, attempts: int, collect_coverage: bool, exec
                 False,
                 e.stdout.decode("utf-8") + msg,
                 previous_attempts,
-                [],
+                None,
             )
         if result.success:
             break
@@ -423,7 +425,7 @@ def find_crosvm_binary(executables: List[Executable]):
 def generate_lcov(results: List[ExecutableResults], lcov_file: str):
     print("Merging profiles")
     merged_file = testvm.cargo_target_dir() / "merged.profraw"
-    profiles = [str(p) for r in results if r.profile_files for p in r.profile_files]
+    profiles = [str(r.profile_file) for r in results if r.profile_file]
     subprocess.check_call(["rust-profdata", "merge", "-sparse", *profiles, "-o", str(merged_file)])
 
     print("Exporting report")
@@ -432,7 +434,7 @@ def generate_lcov(results: List[ExecutableResults], lcov_file: str):
             "rust-cov",
             "export",
             "--format=lcov",
-            "--ignore-filename-regex='/registry/'",
+            "--ignore-filename-regex='/.cargo/registry'",
             f"--instr-profile={merged_file}",
             *(f"--object={r.binary_file}" for r in results),
         ],
