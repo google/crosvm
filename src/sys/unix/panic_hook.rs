e27dde0caa87619495f8c4b87a101166876acd3d
Daniel Verkamp <dverkamp@chromium.org>
2022-09-12 18:09:00 -0700

    crosvm: unix: install memfd panic hook
    Add a call to the install_memfd_handler() function so that panics
    produce a specially named memfd to store the panic information for
    retrieval by the crash reporter.
    
    A new panic-memfd feature (enabled by default with --features=chromeos)
    is added to control this functionality.
    
    BUG=b:234093439
    TEST=emerge-hatch crosvm
    
    Change-Id: Ibc92b3970609d097371b0019d6f1c8d00a3e5f15
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/3892142
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Paramjit Oberoi <psoberoi@google.com>
    Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

--- src/sys/unix/panic_hook.rs
+++ src/sys/unix/panic_hook.rs
@@ -112,4 +112,8 @@ pub fn set_panic_hook() {
         // Abort to trigger the crash reporter so that a minidump is generated.
         abort();
     }));
+
+    // Install the memfd handler last so it will run before the panic hook above that calls abort().
+    #[cfg(feature = "panic-memfd")]
+    base::sys::unix::panic_handler::install_memfd_handler();
 }
