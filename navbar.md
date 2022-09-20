16c3d76f5fcae24041c13d1f45fdf13260f2f9d8
Dennis Kempin <denniskempin@google.com>
2022-09-20 20:30:00 +0000

    Cleanup: Remove old, deprecated and no longer used files
    Most of these are artifacts of previous developer tools or infra.
    
    BUG=None
    TEST=None
    
    Change-Id: Iff379ec786e85c3ea2e89ae3c6100fa2de9422d3
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/3905069
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Commit-Queue: Dennis Kempin <denniskempin@google.com>

--- navbar.md
+++
@@ -1,6 +0,0 @@
-# crosvm
-
-- [Home](/README.md)
-- [Contribution guide](/CONTRIBUTING.md)
-- [Architecture](/ARCHITECTURE.md)
-- [Book of crosvm](https://crosvm.dev/book/)
--- setup_cros_cargo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright 2021 The ChromiumOS Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-echo "./setup_cros_cargo.sh is deprecated. " \
-    "Please use: ./tools/chromeos/setup_cargo"
