839577a79665920e42c45157ce8e7e506f798a89
Alexandre Courbot <acourbot@chromium.org>
2022-10-12 12:18:50 +0900

    media: libvda: allow to build without linking to libvda
    libvda is only available on ChromeOS, and being unable to link to it
    with regular builds reduces our build coverage.
    
    Add a "libvda-stub" feature that, if enabled, results in dummy C stubs
    being build for all the exported libvda functions. This allows builds
    with the "libvda" feature to pass, although of course the resulting
    video device would immediately crash and thus should not be used.
    
    BUG=b:244619291
    TEST=`cargo build --features="video-decoder,video-encoder,libvda-stub"`
    completes.
    TEST=`cargo build --features="video-decoder,video-encoder,libvda"`
    reports link errors against libvda.
    TEST=`cargo build --features all-x86_64` builds libvda and completes
    without error.
    
    Change-Id: I9bb60f6caf670081d67c91275727f3888272d64b
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/3947844
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>

--- media/libvda/src/decode/mod.rs
+++ media/libvda/src/decode/mod.rs
@@ -12,3 +12,95 @@ pub use event::*;
 pub use format::*;
 pub use session::*;
 pub use vda_instance::*;
+
+/// libvda only exists on ChromeOS, so we cannot link against it in a regular environment, which
+/// limits our build coverage. These stubs are built if the "chromeos" feature is not specified,
+/// which allows build to complete successfully, although the video device will just badly crash if
+/// it is ever used.
+#[cfg(feature = "libvda-stub")]
+mod native_stubs {
+    use super::bindings::*;
+
+    #[no_mangle]
+    extern "C" fn initialize(_impl_type: vda_impl_type_t) -> *mut ::std::os::raw::c_void {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn deinitialize(_impl_: *mut ::std::os::raw::c_void) {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn get_vda_capabilities(
+        _impl_: *mut ::std::os::raw::c_void,
+    ) -> *const vda_capabilities_t {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn init_decode_session(
+        _impl_: *mut ::std::os::raw::c_void,
+        _profile: vda_profile_t,
+    ) -> *mut vda_session_info_t {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn close_decode_session(
+        _impl_: *mut ::std::os::raw::c_void,
+        _session_info: *mut vda_session_info_t,
+    ) {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn vda_decode(
+        _ctx: *mut ::std::os::raw::c_void,
+        _bitstream_id: i32,
+        _fd: ::std::os::raw::c_int,
+        _offset: u32,
+        _bytes_used: u32,
+    ) -> vda_result_t {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn vda_set_output_buffer_count(
+        _ctx: *mut ::std::os::raw::c_void,
+        _num_output_buffers: usize,
+    ) -> vda_result_t {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn vda_use_output_buffer(
+        _ctx: *mut ::std::os::raw::c_void,
+        _picture_buffer_id: i32,
+        _format: vda_pixel_format_t,
+        _fd: ::std::os::raw::c_int,
+        _num_planes: usize,
+        _planes: *mut video_frame_plane_t,
+        _modifier: u64,
+    ) -> vda_result_t {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn vda_reuse_output_buffer(
+        _ctx: *mut ::std::os::raw::c_void,
+        _picture_buffer_id: i32,
+    ) -> vda_result_t {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn vda_flush(_ctx: *mut ::std::os::raw::c_void) -> vda_result_t {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn vda_reset(_ctx: *mut ::std::os::raw::c_void) -> vda_result_t {
+        unimplemented!()
+    }
+}
--- media/libvda/src/encode/mod.rs
+++ media/libvda/src/encode/mod.rs
@@ -12,3 +12,83 @@ pub use event::*;
 pub use format::*;
 pub use session::*;
 pub use vea_instance::*;
+
+/// libvda only exists on ChromeOS, so we cannot link against it in a regular environment, which
+/// limits our build coverage. These stubs are built if the "chromeos" feature is not specified,
+/// which allows build to complete successfully, although the video device will just badly crash if
+/// it is ever used.
+#[cfg(feature = "libvda-stub")]
+mod native_stubs {
+    use super::bindings::*;
+
+    #[no_mangle]
+    extern "C" fn initialize_encode(_type_: vea_impl_type_t) -> *mut ::std::os::raw::c_void {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn deinitialize_encode(_impl_: *mut ::std::os::raw::c_void) {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn get_vea_capabilities(
+        _impl_: *mut ::std::os::raw::c_void,
+    ) -> *const vea_capabilities_t {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn init_encode_session(
+        _impl_: *mut ::std::os::raw::c_void,
+        _config: *mut vea_config_t,
+    ) -> *mut vea_session_info_t {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn close_encode_session(
+        _impl_: *mut ::std::os::raw::c_void,
+        _session_info: *mut vea_session_info_t,
+    ) {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn vea_encode(
+        _ctx: *mut ::std::os::raw::c_void,
+        _input_buffer_id: vea_input_buffer_id_t,
+        _fd: ::std::os::raw::c_int,
+        _num_planes: usize,
+        _planes: *mut video_frame_plane_t,
+        _timestamp: i64,
+        _force_keyframe: u8,
+    ) -> ::std::os::raw::c_int {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn vea_use_output_buffer(
+        _ctx: *mut ::std::os::raw::c_void,
+        _output_buffer_id: vea_output_buffer_id_t,
+        _fd: ::std::os::raw::c_int,
+        _offset: u32,
+        _size: u32,
+    ) -> ::std::os::raw::c_int {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn vea_request_encoding_params_change(
+        _ctx: *mut ::std::os::raw::c_void,
+        _bitrate: vea_bitrate_t,
+        _framerate: u32,
+    ) -> ::std::os::raw::c_int {
+        unimplemented!()
+    }
+
+    #[no_mangle]
+    extern "C" fn vea_flush(_ctx: *mut ::std::os::raw::c_void) -> ::std::os::raw::c_int {
+        unimplemented!()
+    }
+}
