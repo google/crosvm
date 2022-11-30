eae9062eae0d75625a77917dffaad2731f42c41d
Idan Raiter <idanr@google.com>
2022-11-30 12:51:39 -0800

    rutabaga_gfx: Add external blob flag to gfxstream
    Adds the external blob flag, allowing blobs to be shared across
    processes.
    
    BUG=b:243061269
    TEST=downstream / presubmit
    
    Change-Id: Id993788829ed502d3c149340c654deb1f807ad6c
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4068289
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Reviewed-by: Kaiyi Li <kaiyili@google.com>
    Commit-Queue: Idan Raiter <idanr@google.com>

--- rutabaga_gfx/src/rutabaga_core.rs
+++ rutabaga_gfx/src/rutabaga_core.rs
@@ -849,6 +849,7 @@ impl RutabagaBuilder {
 
     /// Sets use external blob in virglrenderer.
     pub fn set_use_external_blob(mut self, v: bool) -> RutabagaBuilder {
+        self.gfxstream_flags = self.gfxstream_flags.use_external_blob(v);
         self.virglrenderer_flags = self.virglrenderer_flags.use_external_blob(v);
         self
     }
