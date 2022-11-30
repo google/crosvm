39fdc374fde9c2e5fd2c41811aa7e9587e01e7a9
Daniel Verkamp <dverkamp@chromium.org>
2022-11-29 17:37:35 -0800

    vmm_vhost: remove unnecessary clippy allow
    This lint has been renamed (identity_conversion -> useless_conversion),
    but the original reason for allowing the lint is gone now, so we can
    just remove the line entirely.
    
    BUG=b:260784028
    TEST=tools/clippy
    
    Change-Id: If673848eaaa989970d5f18d2e0b3dac3c55abd23
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4064708
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

--- third_party/vmm_vhost/src/message.rs
+++ third_party/vmm_vhost/src/message.rs
@@ -667,7 +667,6 @@ impl VhostUserVringAddr {
     }
 
     /// Create a new instance from `VringConfigData`.
-    #[cfg_attr(feature = "cargo-clippy", allow(clippy::identity_conversion))]
     pub fn from_config_data(index: u32, config_data: &VringConfigData) -> Self {
         let log_addr = config_data.log_addr.unwrap_or(0);
         VhostUserVringAddr {
