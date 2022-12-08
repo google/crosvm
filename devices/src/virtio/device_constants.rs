85ed7ed02b37dd148688ed095978d9ba303a1a21
Bartłomiej Grzesik <bag@semihalf.com>
2022-12-08 08:48:16 +0000

    virtio: video: Pretend to support guest pages for VDA
    This CL add a hack for b/230794283 that changes supported features of
    virtio video devices backed by VDA. The device will pretend to support
    VIRTIO_VIDEO_F_RESOURCE_GUEST_PAGES
    
    BUG=b:230794283
    TEST=android.media.cts.DecoderTest
    
    Change-Id: Id91450a289142f3f5526b3776d65386a50a85892
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4085056
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Commit-Queue: Bartłomiej Grzesik <bgrzesik@google.com>

--- devices/src/virtio/device_constants.rs
+++ devices/src/virtio/device_constants.rs
@@ -214,7 +214,12 @@ pub mod video {
 
     /// The same set of virtio features is supported by the vda decoder and encoder.
     pub fn vda_supported_virtio_features() -> u64 {
-        1u64 << VIRTIO_VIDEO_F_RESOURCE_NON_CONTIG | 1u64 << VIRTIO_VIDEO_F_RESOURCE_VIRTIO_OBJECT
+        // VDA does not support decoding using guest pages. However this hack
+        // is required for ARCVM.
+        // TODO(b/261818131): remove VIRTIO_VIDEO_F_RESOURCE_GUEST_PAGES.
+        1u64 << VIRTIO_VIDEO_F_RESOURCE_GUEST_PAGES
+            | 1u64 << VIRTIO_VIDEO_F_RESOURCE_NON_CONTIG
+            | 1u64 << VIRTIO_VIDEO_F_RESOURCE_VIRTIO_OBJECT
     }
 
     /// Union of the supported features of all decoder and encoder backends.
