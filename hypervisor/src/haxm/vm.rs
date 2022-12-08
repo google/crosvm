google/crosvm

commit 59f6611a74b306a4493e6d3193f2b882949b02fd

Add bus lock detect support in CROSVM
A bus lock is acquired through either split locked access to writeback
(WB) memory or any locked access to non-WB memory. It is typically >1000
cycles slower than an atomic operation within a cache and can also
disrupts performance on other cores.

Virtual Machines can exploit bus locks to degrade the performance of
system. To address this kind of performance DOS attack coming from the
VMs, bus lock VM exit is introduced in KVM and it can report the bus
locks detected in guest. If enabled in KVM, it would exit to the
userspace to let the user enforce throttling policies once bus locks
acquired in VMs.

The availability of bus lock VM exit can be detected through the
KVM_CAP_X86_BUS_LOCK_EXIT. The returned bitmap contains the potential
policies supported by KVM. The field KVM_BUS_LOCK_DETECTION_EXIT in
bitmap is the only supported strategy at present. It indicates that KVM
will exit to userspace to handle the bus locks.

This patch adds a ratelimit on the bus locks acquired in guest as a
mitigation policy.

Introduce a new field "bus_lock_ratelimit" to record the limited speed
of bus locks in the target VM. The user can specify it through
"bus-lock-ratelimit". In current implementation,
the default value of the speed is 0 per second, which means no
restrictions on the bus locks.

As for ratelimit on detected bus locks, simply set the ratelimit
interval to 1s and restrict the quota of bus lock occurence to the value
of "bus_lock_ratelimit". A potential alternative is to introduce the
time slice as a property which can help the user achieve more precise
control.

Change-Id: I60708cfd056062281d6384a2a3aa9acfe0326af2
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/3969352
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Zhenyu Z Wang <zhenyuw@linux.intel.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

hypervisor/src/haxm/vm.rs
@@ -212,6 +212,10 @@ impl Vm for HaxmVm {
         false
     }
 
+    fn enable_capability(&self, _c: VmCap, _flags: u32) -> Result<bool> {
+        Ok(false)
+    }
+
     fn get_memory(&self) -> &GuestMemory {
         &self.guest_mem
     }

hypervisor/src/lib.rs
@@ -63,6 +63,9 @@ pub trait Vm: Send {
     /// reflects the usable capabilities.
     fn check_capability(&self, c: VmCap) -> bool;
 
+    /// Enable the VM capabilities.
+    fn enable_capability(&self, capability: VmCap, flags: u32) -> Result<bool>;
+
     /// Get the guest physical address size in bits.
     fn get_guest_phys_addr_bits(&self) -> u8;
 
@@ -458,6 +461,8 @@ pub enum VcpuExit {
     ApicSmiTrap,
     /// vcpu stopped due to an apic trap
     ApicInitSipiTrap,
+    /// vcpu stoppted due to bus lock
+    BusLock,
 }
 
 /// A hypercall with parameters being made from the guest.

hypervisor/src/caps.rs
@@ -39,4 +39,7 @@ pub enum VmCap {
     Protected,
     /// VM completes initialization of CPUID at creation time, not required after.
     EarlyInitCpuid,
+    /// VM can detect the bus lock
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    BusLockDetect,
 }

hypervisor/src/kvm/mod.rs
@@ -138,7 +138,7 @@ pub struct Kvm {
     kvm: SafeDescriptor,
 }
 
-type KvmCap = kvm::Cap;
+pub type KvmCap = kvm::Cap;
 
 impl Kvm {
     pub fn new_with_path(device_path: &Path) -> Result<Kvm> {
@@ -439,7 +439,18 @@ impl KvmVm {
     pub fn check_raw_capability(&self, capability: KvmCap) -> bool {
         // Safe because we know that our file is a KVM fd, and if the cap is invalid KVM assumes
         // it's an unavailable extension and returns 0.
-        unsafe { ioctl_with_val(self, KVM_CHECK_EXTENSION(), capability as c_ulong) == 1 }
+        let ret = unsafe { ioctl_with_val(self, KVM_CHECK_EXTENSION(), capability as c_ulong) };
+        match capability {
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            KvmCap::BusLockDetect => {
+                if ret > 0 {
+                    ret as u32 & KVM_BUS_LOCK_DETECTION_EXIT == KVM_BUS_LOCK_DETECTION_EXIT
+                } else {
+                    false
+                }
+            }
+            _ => ret == 1,
+        }
     }
 
     // Currently only used on aarch64, but works on any architecture.
@@ -494,6 +505,21 @@ impl Vm for KvmVm {
             VmCap::PvClockSuspend => self.check_raw_capability(KvmCap::KvmclockCtrl),
             VmCap::Protected => self.check_raw_capability(KvmCap::ArmProtectedVm),
             VmCap::EarlyInitCpuid => false,
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            VmCap::BusLockDetect => self.check_raw_capability(KvmCap::BusLockDetect),
+        }
+    }
+
+    fn enable_capability(&self, c: VmCap, _flags: u32) -> Result<bool> {
+        match c {
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            VmCap::BusLockDetect => {
+                let args = [KVM_BUS_LOCK_DETECTION_EXIT as u64, 0, 0, 0];
+                Ok(unsafe {
+                    self.enable_raw_capability(KvmCap::BusLockDetect, _flags, &args) == Ok(())
+                })
+            }
+            _ => Ok(false),
         }
     }
 
@@ -992,6 +1018,7 @@ impl Vcpu for KvmVcpu {
                 let data = msr.data;
                 Ok(VcpuExit::WrMsr { index, data })
             }
+            KVM_EXIT_X86_BUS_LOCK => Ok(VcpuExit::BusLock),
             r => panic!("unknown kvm exit reason: {}", r),
         }
     }

kvm/src/cap.rs
@@ -123,4 +123,6 @@ pub enum Cap {
     IoapicNumPins = KVM_CAP_IOAPIC_NUM_PINS,
     ArmProtectedVm = KVM_CAP_ARM_PROTECTED_VM,
     ArmMte = KVM_CAP_ARM_MTE,
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    BusLockDetect = KVM_CAP_X86_BUS_LOCK_EXIT,
 }

src/crosvm.rs
@@ -13,4 +13,6 @@ pub mod gdb;
 mod gpu_config;
 #[cfg(feature = "plugin")]
 pub mod plugin;
+#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_os = "linux"))]
+pub mod ratelimit;
 pub mod sys;

src/crosvm/cmdline.rs
@@ -856,6 +856,13 @@ pub struct RunCommand {
     ///         precedence over the global --async-executor option.
     block: Vec<DiskOptionWithId>,
 
+    /// ratelimit enforced on detected bus locks in guest.
+    /// The default value of the bus_lock_ratelimit is 0 per second,
+    /// which means no limitation on the guest's bus locks.
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    #[argh(option)]
+    pub bus_lock_ratelimit: Option<u64>,
+
     #[cfg(feature = "config-file")]
     #[argh(option, arg_name = "CONFIG_FILE", from_str_fn(load_config_file))]
     // TODO(b/218223240) We only allow one configuration file because accurate merging is not
@@ -2190,6 +2197,11 @@ impl TryFrom<RunCommand> for super::config::Config {
         cfg.android_fstab = cmd.android_fstab;
 
         cfg.async_executor = cmd.async_executor;
+
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        if let Some(p) = cmd.bus_lock_ratelimit {
+            cfg.bus_lock_ratelimit = p;
+        }
 
         cfg.params.extend(cmd.params);
 

src/crosvm/config.rs
@@ -1043,6 +1043,8 @@ pub struct Config {
     pub block_vhost_user_tube: Vec<Tube>,
     #[cfg(windows)]
     pub broker_shutdown_event: Option<Event>,
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    pub bus_lock_ratelimit: u64,
     pub cid: Option<u64>,
     #[cfg(unix)]
     pub coiommu_param: Option<devices::CoIommuParameters>,
@@ -1254,6 +1256,8 @@ impl Default for Config {
             block_vhost_user_tube: Vec::new(),
             #[cfg(windows)]
             broker_shutdown_event: None,
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            bus_lock_ratelimit: 0,
             cid: None,
             #[cfg(unix)]
             coiommu_param: None,

commit 6acfd6a98e4b6369a8d201bdad04347155562fab

hypervisor: add default enable_capability impl
Since the newly-added Vm::enable_capability() function is only required
for hypervisors which support VmCap::BusLockDetect, we can add a default
implementation of the function in the trait for other hypervisors. This
replaces the version from HaxmVm and also implements it for WhpxVm,
which is not enabled by default.

Additionally, handle the VmCap::BusLockDetect case in the WpxVm
check_capability() function to fix compilation.

BUG=None
TEST=Build for Windows with whpx feature enabled

Fixes: 59f6611a74b3 ("Add bus lock detect support in CROSVM")
Change-Id: I8a4ed7c14a224439dbba79d44134219658786e8f
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4087466
Reviewed-by: Clarissa Garvey <clarissagarvey@chromium.org>
Reviewed-by: Dennis Kempin <denniskempin@google.com>
Reviewed-by: Yiming Zhang <yiming.zhang@intel.corp-partner.google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

hypervisor/src/haxm/vm.rs
@@ -212,10 +212,6 @@ impl Vm for HaxmVm {
         false
     }
 
-    fn enable_capability(&self, _c: VmCap, _flags: u32) -> Result<bool> {
-        Ok(false)
-    }
-
     fn get_memory(&self) -> &GuestMemory {
         &self.guest_mem
     }

hypervisor/src/lib.rs
@@ -64,7 +64,9 @@ pub trait Vm: Send {
     fn check_capability(&self, c: VmCap) -> bool;
 
     /// Enable the VM capabilities.
-    fn enable_capability(&self, capability: VmCap, flags: u32) -> Result<bool>;
+    fn enable_capability(&self, _capability: VmCap, _flags: u32) -> Result<bool> {
+        Err(std::io::Error::from(std::io::ErrorKind::Unsupported).into())
+    }
 
     /// Get the guest physical address size in bits.
     fn get_guest_phys_addr_bits(&self) -> u8;

hypervisor/src/whpx/vm.rs
@@ -408,6 +408,8 @@ impl Vm for WhpxVm {
             VmCap::Protected => false,
             // whpx initializes cpuid early during VM creation.
             VmCap::EarlyInitCpuid => true,
+            #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+            VmCap::BusLockDetect => false,
         }
     }
 