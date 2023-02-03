d03c99fc0c64fe22f200badd80fb7b2d42a05207
Junichi Uekawa <dancer@debian.org>
2023-02-03 10:53:53 +0900

    crosvm: Uprev uuid > 1
    Uprev to version available in Debian.
    
    This seem to end up doing uprev to 1.3 now.
    
    BUG=b:265082456
    BUG=b:229895468
    TEST=build
    
    Change-Id: I550778acb675c9034b9cfcea77f4ae847e2d2ea1
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4364559
    Commit-Queue: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
    Auto-Submit: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: Frederick Mayle <fmayle@google.com>

--- base/src/lib.rs
+++ base/src/lib.rs
@@ -180,7 +180,7 @@ pub trait EnabledHighResTimer {}
 pub fn generate_uuid() -> String {
     let mut buf = Uuid::encode_buffer();
     Uuid::new_v4()
-        .to_hyphenated()
+        .as_hyphenated()
         .encode_lower(&mut buf)
         .to_owned()
 }
