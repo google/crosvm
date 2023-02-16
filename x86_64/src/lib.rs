google/crosvm

commit 5bedfa3408690bf60c555a82c57a291898248b7e

x86_64: add SETUP_RNG_SEED setup_data entry
This provides the kernel with an early source of entropy for its random
number generator.

The kernel supports for this was added in commit 68b8e9713c8e
("x86/setup: Use rng seeds from setup_data"):
<https://lore.kernel.org/all/20220710172921.51545-1-Jason@zx2c4.com/>

Earlier kernels will ignore the unknown setup_data entry.

BUG=b:265031416
TEST=Boot x86-64 Linux bzImage with added printk in rng seed init

Change-Id: I627aa8663bd61b04ca311d4d095e1c121fda8543
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/3855344
Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
Reviewed-by: Dennis Kempin <denniskempin@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Alexandre Courbot <acourbot@chromium.org>

x86_64/src/lib.rs
@@ -9,6 +9,7 @@
 mod fdt;
 
 const SETUP_DTB: u32 = 2;
+const SETUP_RNG_SEED: u32 = 9;
 const X86_64_FDT_MAX_SIZE: u64 = 0x20_0000;
 
 #[allow(dead_code)]
@@ -127,6 +128,8 @@ use hypervisor::VmX86_64;
 #[cfg(unix)]
 use minijail::Minijail;
 use once_cell::sync::OnceCell;
+use rand::rngs::OsRng;
+use rand::RngCore;
 use remain::sorted;
 use resources::AddressRange;
 use resources::SystemAllocator;
@@ -304,6 +307,7 @@ unsafe impl data_model::DataInit for setup_data_hdr {}
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum SetupDataType {
     Dtb = SETUP_DTB,
+    RngSeed = SETUP_RNG_SEED,
 }
 
 /// A single entry to be inserted in the bootparam `setup_data` linked list.
@@ -568,6 +572,16 @@ fn write_setup_data(
     }
 
     Ok(setup_data_list_head)
+}
+
+/// Generate a SETUP_RNG_SEED SetupData with random seed data.
+fn setup_data_rng_seed() -> SetupData {
+    let mut data = vec![0u8; 256];
+    OsRng.fill_bytes(&mut data);
+    SetupData {
+        data,
+        type_: SetupDataType::RngSeed,
+    }
 }
 
 /// Add an e820 region to the e820 map.
@@ -1557,6 +1571,7 @@ impl X8664arch {
         if let Some(android_fstab) = android_fstab {
             setup_data.push(fdt::create_fdt(android_fstab).map_err(Error::CreateFdt)?);
         }
+        setup_data.push(setup_data_rng_seed());
 
         let setup_data = write_setup_data(mem, &mut free_addr, &setup_data)?;
 

x86_64/Cargo.toml
@@ -23,6 +23,7 @@ kernel_cmdline = { path = "../kernel_cmdline" }
 kernel_loader = { path = "../kernel_loader" }
 libc = "*"
 once_cell = "1.7.2"
+rand = "0.8"
 remain = "*"
 resources = { path = "../resources" }
 sync = { path = "../common/sync" }

commit e5aec1e53d9ffa77f724fe863b2bdfbeabb26b85

x86_64: place setup_data immediately after cmdline
This moves the setup_data out of the way of the bzImage decompression
stub, so it won't get overwritten.

Fixes a regression with some kernel configurations introduced in commit
5bedfa340869 ("x86_64: add SETUP_RNG_SEED setup_data entry"), which
unconditionally adds a setup_data entry.

BUG=b:268397895
TEST=boot x86-64 bzImage in crosvm that previously failed
TEST=cargo test -p x86_64

Change-Id: Ie5efc19d607aad16c1ca80bcdd2fb62933dd73e1
Fixes: 5bedfa340869 ("x86_64: add SETUP_RNG_SEED setup_data entry")
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4233967
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Dmitrii Osipenko <dmitry.osipenko@collabora.corp-partner.google.com>
Reviewed-by: Zihan Chen <zihanchen@google.com>

x86_64/src/lib.rs
@@ -331,6 +331,8 @@ const TSS_ADDR: u64 = 0xfffb_d000;
 pub const KERNEL_START_OFFSET: u64 = 0x20_0000;
 const CMDLINE_OFFSET: u64 = 0x2_0000;
 const CMDLINE_MAX_SIZE: u64 = 0x800; // including terminating zero
+const SETUP_DATA_START: u64 = CMDLINE_OFFSET + CMDLINE_MAX_SIZE;
+const SETUP_DATA_END: u64 = ACPI_HI_RSDP_WINDOW_BASE;
 const X86_64_SERIAL_1_3_IRQ: u32 = 4;
 const X86_64_SERIAL_2_4_IRQ: u32 = 3;
 // X86_64_SCI_IRQ is used to fill the ACPI FACP table.
@@ -510,15 +512,14 @@ fn configure_system(
 /// Returns the guest address of the first entry in the setup_data list, if any.
 fn write_setup_data(
     guest_mem: &GuestMemory,
-    free_addr: &mut u64,
+    setup_data_start: GuestAddress,
+    setup_data_end: GuestAddress,
     setup_data: &[SetupData],
 ) -> Result<Option<GuestAddress>> {
     let mut setup_data_list_head = None;
 
-    // Place the first setup_data at the first 64-bit aligned offset following free_addr.
-    let mut setup_data_addr = GuestAddress(*free_addr)
-        .align(8)
-        .ok_or(Error::SetupDataTooLarge)?;
+    // Place the first setup_data at the first 64-bit aligned offset following setup_data_start.
+    let mut setup_data_addr = setup_data_start.align(8).ok_or(Error::SetupDataTooLarge)?;
 
     let mut entry_iter = setup_data.iter().peekable();
     while let Some(entry) = entry_iter.next() {
@@ -528,9 +529,13 @@ fn write_setup_data(
 
         // Ensure the entry (header plus data) fits into guest memory.
         let entry_size = (mem::size_of::<setup_data_hdr>() + entry.data.len()) as u64;
-        setup_data_addr
+        let entry_end = setup_data_addr
             .checked_add(entry_size)
             .ok_or(Error::SetupDataTooLarge)?;
+
+        if entry_end >= setup_data_end {
+            return Err(Error::SetupDataTooLarge);
+        }
 
         let next_setup_data_addr = if entry_iter.peek().is_some() {
             // Place the next setup_data at a 64-bit aligned address.
@@ -563,7 +568,6 @@ fn write_setup_data(
             )
             .map_err(Error::WritingSetupData)?;
 
-        *free_addr = setup_data_addr.offset() + entry_size;
         setup_data_addr = next_setup_data_addr;
     }
 
@@ -1589,10 +1593,6 @@ impl X8664arch {
         kernel_loader::load_cmdline(mem, GuestAddress(CMDLINE_OFFSET), cmdline)
             .map_err(Error::LoadCmdline)?;
 
-        // Track the first free address after the kernel - this is where extra
-        // data like the device tree blob and initrd will be loaded.
-        let mut free_addr = kernel_end;
-
         let mut setup_data = Vec::<SetupData>::new();
         if let Some(android_fstab) = android_fstab {
             setup_data.push(
@@ -1601,7 +1601,12 @@ impl X8664arch {
         }
         setup_data.push(setup_data_rng_seed());
 
-        let setup_data = write_setup_data(mem, &mut free_addr, &setup_data)?;
+        let setup_data = write_setup_data(
+            mem,
+            GuestAddress(SETUP_DATA_START),
+            GuestAddress(SETUP_DATA_END),
+            &setup_data,
+        )?;
 
         let initrd = match initrd_file {
             Some(mut initrd_file) => {
@@ -1619,7 +1624,7 @@ impl X8664arch {
                 let (initrd_start, initrd_size) = arch::load_image_high(
                     mem,
                     &mut initrd_file,
-                    GuestAddress(free_addr),
+                    GuestAddress(kernel_end),
                     GuestAddress(initrd_addr_max),
                     base::pagesize() as u64,
                 )
@@ -2315,18 +2320,19 @@ mod tests {
     fn write_setup_data_empty() {
         let mem = GuestMemory::new(&[(GuestAddress(0), 0x2_0000)]).unwrap();
         let setup_data = [];
-        let mut free_addr = 0x1000;
-        let setup_data_addr =
-            write_setup_data(&mem, &mut free_addr, &setup_data).expect("write_setup_data");
+        let setup_data_addr = write_setup_data(
+            &mem,
+            GuestAddress(0x1000),
+            GuestAddress(0x2000),
+            &setup_data,
+        )
+        .expect("write_setup_data");
         assert_eq!(setup_data_addr, None);
-        assert_eq!(free_addr, 0x1000);
     }
 
     #[test]
     fn write_setup_data_two_of_them() {
         let mem = GuestMemory::new(&[(GuestAddress(0), 0x2_0000)]).unwrap();
-
-        let mut free_addr = 0x1000;
 
         let entry1_addr = GuestAddress(0x1000);
         let entry1_next_addr = entry1_addr;
@@ -2341,9 +2347,6 @@ mod tests {
         let entry2_len_addr = entry2_addr.checked_add(12).unwrap();
         let entry2_data_addr = entry2_addr.checked_add(16).unwrap();
         let entry2_data = [0xAAu8; 9];
-        let entry2_size = (size_of::<setup_data_hdr>() + entry2_data.len()) as u64;
-
-        let next_free_addr = entry2_addr.checked_add(entry2_size).unwrap();
 
         let setup_data = [
             SetupData {
@@ -2356,10 +2359,14 @@ mod tests {
             },
         ];
 
-        let setup_data_head_addr =
-            write_setup_data(&mem, &mut free_addr, &setup_data).expect("write_setup_data");
+        let setup_data_head_addr = write_setup_data(
+            &mem,
+            GuestAddress(0x1000),
+            GuestAddress(0x2000),
+            &setup_data,
+        )
+        .expect("write_setup_data");
         assert_eq!(setup_data_head_addr, Some(entry1_addr));
-        assert_eq!(free_addr, next_free_addr.offset());
 
         assert_eq!(
             mem.read_obj_from_addr::<u64>(entry1_next_addr).unwrap(),