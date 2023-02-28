41cecab05d805c0739bcd7339a93d5b4f3640248
Junichi Uekawa <dancer@debian.org>
2023-02-28 12:19:35 +0900

    crosvm: Remove cros_asyncv2
    We don't have the necessary dependency in Debian.
    
    BUG=b:265082456
    TEST=build
    
    Change-Id: Ia6a38b0d9b5ce4e509a88f2b8e0790b605b47175
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4364560
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
    Reviewed-by: Frederick Mayle <fmayle@google.com>
    Commit-Queue: Junichi Uekawa <uekawa@chromium.org>

--- common/cros_asyncv2/src/blocking.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 The ChromiumOS Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-mod block_on;
-mod pool;
-
-pub use block_on::*;
-pub use pool::*;
