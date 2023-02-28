b67eaca4684f05bdc172fd3b9633283fea2f84b6
Dennis Kempin <denniskempin@google.com>
2023-02-28 11:01:18 -0800

    Use custom test harness for tests using fork
    forking a process with multiple threads will lead to
    undefined behavior. Unfortunately, newer versions of
    libtest will spawn multiple threads even when running
    a with --test-threads=1.
    
    This change implements a custom, test harness using
    libtest-mimic. It mimics the libtest CLI but enforces
    a single threaded test process.
    
    BUG=b:270167741
    BUG=b:268496046
    TEST=tools/run_tests --dut=host
    
    Change-Id: Icef6a1b65bab7f5cd5021c01fbd94487fa0ca5fb
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4295157
    Reviewed-by: George Burgess <gbiv@chromium.org>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Commit-Queue: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Frederick Mayle <fmayle@google.com>

--- base/tests/process.rs
+++ base/tests/process.rs
@@ -2,161 +2,137 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#![cfg(unix)]
+#[cfg(unix)]
+mod test {
+    use std::thread;
+    use std::time::Duration;
 
-use std::env::current_exe;
-use std::process::Command;
-use std::thread;
-use std::time::Duration;
+    use base::getpid;
+    use base::unix::process::fork_process;
+    use base::AsRawDescriptor;
+    use base::Tube;
+    use minijail::Minijail;
 
-use base::getpid;
-use base::unix::process::fork_process;
-use base::AsRawDescriptor;
-use base::Tube;
-use minijail::Minijail;
+    pub fn pid_diff() {
+        let (tube, fork_tube) = Tube::pair().expect("failed to create tube");
+        let jail = Minijail::new().unwrap();
+        let keep_rds = vec![fork_tube.as_raw_descriptor()];
 
-/// Tests using fork_process will fail if run inside a multithreaded process. By default, the
-/// cargo test harness will use multiple threads to execute tests.
-/// To make sure these tests work, we will re-execute the test binary in single threaded mode
-/// to execute just the specified test.
-fn call_test_in_child_process(name: &str) {
-    let result = Command::new(current_exe().unwrap())
-        .args([
-            "--test-threads=1",
-            "--nocapture",
-            "--ignored",
-            "--exact",
-            name,
-        ])
-        .status()
-        .unwrap();
-    if !result.success() {
-        panic!("Test {name} failed in child process.");
+        let pid = getpid();
+        let child = fork_process(jail, keep_rds, None, || {
+            // checks that this is a genuine fork with a new PID
+            if pid != getpid() {
+                fork_tube.send(&1).unwrap()
+            } else {
+                fork_tube.send(&2).unwrap()
+            }
+        })
+        .expect("failed to fork");
+
+        assert_eq!(tube.recv::<u32>().unwrap(), 1);
+        child.wait().unwrap();
+    }
+
+    pub fn thread_name() {
+        let (tube, fork_tube) = Tube::pair().expect("failed to create tube");
+        let jail = Minijail::new().unwrap();
+        let keep_rds = vec![fork_tube.as_raw_descriptor()];
+        let thread_name = String::from("thread_name");
+
+        let child = fork_process(jail, keep_rds, Some(thread_name.clone()), || {
+            fork_tube.send::<u32>(&1).unwrap();
+            thread::sleep(Duration::from_secs(10));
+        })
+        .expect("failed to fork");
+
+        // wait the forked process running.
+        tube.recv::<u32>().unwrap();
+        let thread_comm =
+            std::fs::read_to_string(format!("/proc/{0}/task/{0}/comm", child.pid)).unwrap();
+
+        assert_eq!(thread_comm, thread_name + "\n");
+
+        unsafe { libc::kill(child.pid, libc::SIGKILL) };
+        child.wait().unwrap();
+    }
+
+    pub fn thread_name_trimmed() {
+        let (tube, fork_tube) = Tube::pair().expect("failed to create tube");
+        let jail = Minijail::new().unwrap();
+        let keep_rds = vec![fork_tube.as_raw_descriptor()];
+        let thread_name = String::from("12345678901234567890");
+
+        let child = fork_process(jail, keep_rds, Some(thread_name), || {
+            fork_tube.send::<u32>(&1).unwrap();
+            thread::sleep(Duration::from_secs(10));
+        })
+        .expect("failed to fork");
+
+        // wait the forked process running.
+        tube.recv::<u32>().unwrap();
+        let thread_comm =
+            std::fs::read_to_string(format!("/proc/{0}/task/{0}/comm", child.pid)).unwrap();
+
+        assert_eq!(thread_comm, "123456789012345\n");
+
+        unsafe { libc::kill(child.pid, libc::SIGKILL) };
+        child.wait().unwrap();
+    }
+
+    pub fn wait_for_success() {
+        let jail = Minijail::new().unwrap();
+        let child = fork_process(jail, vec![], None, || {
+            // exit successfully
+        })
+        .expect("failed to fork");
+
+        assert_eq!(child.wait().unwrap(), 0);
+    }
+
+    pub fn wait_for_panic() {
+        let jail = Minijail::new().unwrap();
+        let child = fork_process(jail, vec![], None, || {
+            panic!("fails");
+        })
+        .expect("failed to fork");
+
+        assert_eq!(child.wait().unwrap(), 101);
     }
 }
 
-#[test]
-#[ignore = "Disabled pending a fix to b/268496046"]
-fn pid_diff() {
-    call_test_in_child_process("pid_diff_impl");
-}
+fn main() {
+    let args = libtest_mimic::Arguments {
+        // Force single-threaded execution to allow safe use of libc::fork in these tests.
+        test_threads: Some(1),
+        ..libtest_mimic::Arguments::from_args()
+    };
 
-#[test]
-#[ignore = "Only to be called by pid_diff"]
-fn pid_diff_impl() {
-    let (tube, fork_tube) = Tube::pair().expect("failed to create tube");
-    let jail = Minijail::new().unwrap();
-    let keep_rds = vec![fork_tube.as_raw_descriptor()];
-
-    let pid = getpid();
-    let child = fork_process(jail, keep_rds, None, || {
-        // checks that this is a genuine fork with a new PID
-        if pid != getpid() {
-            fork_tube.send(&1).unwrap()
-        } else {
-            fork_tube.send(&2).unwrap()
-        }
-    })
-    .expect("failed to fork");
-
-    assert_eq!(tube.recv::<u32>().unwrap(), 1);
-    child.wait().unwrap();
-}
-
-#[test]
-#[ignore = "Disabled pending a fix to b/268496046"]
-fn thread_name() {
-    call_test_in_child_process("thread_name_impl");
-}
-
-#[test]
-#[ignore = "Only to be called by thread_name"]
-fn thread_name_impl() {
-    let (tube, fork_tube) = Tube::pair().expect("failed to create tube");
-    let jail = Minijail::new().unwrap();
-    let keep_rds = vec![fork_tube.as_raw_descriptor()];
-    let thread_name = String::from("thread_name");
-
-    let child = fork_process(jail, keep_rds, Some(thread_name.clone()), || {
-        fork_tube.send::<u32>(&1).unwrap();
-        thread::sleep(Duration::from_secs(10));
-    })
-    .expect("failed to fork");
-
-    // wait the forked process running.
-    tube.recv::<u32>().unwrap();
-    let thread_comm =
-        std::fs::read_to_string(format!("/proc/{0}/task/{0}/comm", child.pid)).unwrap();
-
-    assert_eq!(thread_comm, thread_name + "\n");
-
-    unsafe { libc::kill(child.pid, libc::SIGKILL) };
-    child.wait().unwrap();
-}
-
-#[test]
-#[ignore = "Disabled pending a fix to b/268496046"]
-fn thread_name_trimmed() {
-    call_test_in_child_process("thread_name_trimmed_impl");
-}
-
-#[test]
-#[ignore = "Only to be called by thread_name_trimmed"]
-fn thread_name_trimmed_impl() {
-    let (tube, fork_tube) = Tube::pair().expect("failed to create tube");
-    let jail = Minijail::new().unwrap();
-    let keep_rds = vec![fork_tube.as_raw_descriptor()];
-    let thread_name = String::from("12345678901234567890");
-
-    let child = fork_process(jail, keep_rds, Some(thread_name), || {
-        fork_tube.send::<u32>(&1).unwrap();
-        thread::sleep(Duration::from_secs(10));
-    })
-    .expect("failed to fork");
-
-    // wait the forked process running.
-    tube.recv::<u32>().unwrap();
-    let thread_comm =
-        std::fs::read_to_string(format!("/proc/{0}/task/{0}/comm", child.pid)).unwrap();
-
-    assert_eq!(thread_comm, "123456789012345\n");
-
-    unsafe { libc::kill(child.pid, libc::SIGKILL) };
-    child.wait().unwrap();
-}
-
-#[test]
-#[ignore = "Disabled pending a fix to b/268496046"]
-fn wait_for_success() {
-    call_test_in_child_process("wait_for_success_impl");
-}
-
-#[test]
-#[ignore = "Only to be called by wait_for_success"]
-fn wait_for_success_impl() {
-    let jail = Minijail::new().unwrap();
-    let child = fork_process(jail, vec![], None, || {
-        // exit successfully
-    })
-    .expect("failed to fork");
-
-    assert_eq!(child.wait().unwrap(), 0);
-}
-
-#[test]
-#[ignore = "Disabled pending a fix to b/268496046"]
-fn wait_for_panic() {
-    call_test_in_child_process("wait_for_panic_impl");
-}
-
-#[test]
-#[ignore = "Only to be called by wait_for_panic"]
-fn wait_for_panic_impl() {
-    let jail = Minijail::new().unwrap();
-    let child = fork_process(jail, vec![], None, || {
-        panic!("fails");
-    })
-    .expect("failed to fork");
-
-    assert_eq!(child.wait().unwrap(), 101);
+    let tests = vec![
+        #[cfg(unix)]
+        libtest_mimic::Trial::test("pid_diff", move || {
+            test::pid_diff();
+            Ok(())
+        }),
+        #[cfg(unix)]
+        libtest_mimic::Trial::test("thread_name", move || {
+            test::thread_name();
+            Ok(())
+        }),
+        #[cfg(unix)]
+        libtest_mimic::Trial::test("thread_name_trimmed", move || {
+            test::thread_name_trimmed();
+            Ok(())
+        }),
+        #[cfg(unix)]
+        libtest_mimic::Trial::test("wait_for_success", move || {
+            test::wait_for_success();
+            Ok(())
+        }),
+        #[cfg(unix)]
+        libtest_mimic::Trial::test("wait_for_panic", move || {
+            test::wait_for_panic();
+            Ok(())
+        }),
+    ];
+    libtest_mimic::run(&args, tests).exit();
 }
--- swap/tests/main.rs
+++ swap/tests/main.rs
@@ -4,81 +4,103 @@
 
 //! Integration tests for vmm-swap feature
 
-#![cfg(unix)]
-
+#[cfg(unix)]
 mod common;
 
-use base::pagesize;
-use base::sys::wait_for_pid;
-use base::AsRawDescriptor;
-use base::FromRawDescriptor;
-use base::IntoRawDescriptor;
-use base::SafeDescriptor;
-use base::Tube;
-use common::*;
-use swap::userfaultfd::register_regions;
-use swap::userfaultfd::unregister_regions;
-use swap::userfaultfd::Userfaultfd;
+#[cfg(unix)]
+mod test {
+    use super::common::*;
+    use base::pagesize;
+    use base::sys::wait_for_pid;
+    use base::AsRawDescriptor;
+    use base::FromRawDescriptor;
+    use base::IntoRawDescriptor;
+    use base::SafeDescriptor;
+    use base::Tube;
+    use swap::userfaultfd::register_regions;
+    use swap::userfaultfd::unregister_regions;
+    use swap::userfaultfd::Userfaultfd;
 
-#[test]
-fn register_region_skip_obsolete_process() {
-    let shm = create_shared_memory("test", 3 * pagesize());
-    let uffd = create_uffd_for_test();
-    let base_addr = shm.base_addr();
-    let regions = [base_addr..(base_addr + 3 * pagesize())];
-    let (tube_main, tube_child) = Tube::pair().unwrap();
-    let pid = unsafe { libc::fork() };
-    if pid == 0 {
-        // child process
+    pub fn register_region_skip_obsolete_process() {
+        let shm = create_shared_memory("test", 3 * pagesize());
         let uffd = create_uffd_for_test();
-        tube_child
-            .send(&unsafe { SafeDescriptor::from_raw_descriptor(uffd.as_raw_descriptor()) })
-            .unwrap();
-        std::process::exit(0);
+        let base_addr = shm.base_addr();
+        let regions = [base_addr..(base_addr + 3 * pagesize())];
+        let (tube_main, tube_child) = Tube::pair().unwrap();
+        let pid = unsafe { libc::fork() };
+        if pid == 0 {
+            // child process
+            let uffd = create_uffd_for_test();
+            tube_child
+                .send(&unsafe { SafeDescriptor::from_raw_descriptor(uffd.as_raw_descriptor()) })
+                .unwrap();
+            std::process::exit(0);
+        }
+        let uffd_descriptor = tube_main
+            .recv::<SafeDescriptor>()
+            .unwrap()
+            .into_raw_descriptor();
+        wait_for_pid(pid, 0).unwrap();
+        let uffd_child = unsafe { Userfaultfd::from_raw_descriptor(uffd_descriptor) };
+
+        let result = unsafe { register_regions(&regions, &[uffd, uffd_child]) };
+
+        // no error from ENOMEM
+        assert_eq!(result.is_ok(), true);
     }
-    let uffd_descriptor = tube_main
-        .recv::<SafeDescriptor>()
-        .unwrap()
-        .into_raw_descriptor();
-    wait_for_pid(pid, 0).unwrap();
-    let uffd_child = unsafe { Userfaultfd::from_raw_descriptor(uffd_descriptor) };
 
-    let result = unsafe { register_regions(&regions, &[uffd, uffd_child]) };
+    pub fn unregister_region_skip_obsolete_process() {
+        let shm = create_shared_memory("test", 3 * pagesize());
+        let uffd = create_uffd_for_test();
+        let base_addr = shm.base_addr();
+        let regions = [base_addr..(base_addr + 3 * pagesize())];
+        let (tube_main, tube_child) = Tube::pair().unwrap();
+        let pid = unsafe { libc::fork() };
+        if pid == 0 {
+            // child process
+            let uffd = create_uffd_for_test();
+            tube_child
+                .send(&unsafe { SafeDescriptor::from_raw_descriptor(uffd.as_raw_descriptor()) })
+                .unwrap();
+            tube_child.recv::<u8>().unwrap();
+            std::process::exit(0);
+        }
+        let uffd_descriptor = tube_main
+            .recv::<SafeDescriptor>()
+            .unwrap()
+            .into_raw_descriptor();
+        let uffd_child = unsafe { Userfaultfd::from_raw_descriptor(uffd_descriptor) };
+        let uffds = [uffd, uffd_child];
 
-    // no error from ENOMEM
-    assert_eq!(result.is_ok(), true);
+        unsafe { register_regions(&regions, &uffds) }.unwrap();
+        tube_main.send(&0_u8).unwrap();
+        // wait until the child process die and the uffd_child become obsolete.
+        wait_for_pid(pid, 0).unwrap();
+        let result = unregister_regions(&regions, &uffds);
+
+        // no error from ENOMEM
+        assert_eq!(result.is_ok(), true);
+    }
 }
 
-#[test]
-fn unregister_region_skip_obsolete_process() {
-    let shm = create_shared_memory("test", 3 * pagesize());
-    let uffd = create_uffd_for_test();
-    let base_addr = shm.base_addr();
-    let regions = [base_addr..(base_addr + 3 * pagesize())];
-    let (tube_main, tube_child) = Tube::pair().unwrap();
-    let pid = unsafe { libc::fork() };
-    if pid == 0 {
-        // child process
-        let uffd = create_uffd_for_test();
-        tube_child
-            .send(&unsafe { SafeDescriptor::from_raw_descriptor(uffd.as_raw_descriptor()) })
-            .unwrap();
-        tube_child.recv::<u8>().unwrap();
-        std::process::exit(0);
-    }
-    let uffd_descriptor = tube_main
-        .recv::<SafeDescriptor>()
-        .unwrap()
-        .into_raw_descriptor();
-    let uffd_child = unsafe { Userfaultfd::from_raw_descriptor(uffd_descriptor) };
-    let uffds = [uffd, uffd_child];
+fn main() {
+    let args = libtest_mimic::Arguments {
+        // Force single-threaded execution to allow safe use of libc::fork in these tests.
+        test_threads: Some(1),
+        ..libtest_mimic::Arguments::from_args()
+    };
 
-    unsafe { register_regions(&regions, &uffds) }.unwrap();
-    tube_main.send(&0_u8).unwrap();
-    // wait until the child process die and the uffd_child become obsolete.
-    wait_for_pid(pid, 0).unwrap();
-    let result = unregister_regions(&regions, &uffds);
-
-    // no error from ENOMEM
-    assert_eq!(result.is_ok(), true);
+    let tests = vec![
+        #[cfg(unix)]
+        libtest_mimic::Trial::test("register_region_skip_obsolete_process", move || {
+            test::register_region_skip_obsolete_process();
+            Ok(())
+        }),
+        #[cfg(unix)]
+        libtest_mimic::Trial::test("unregister_region_skip_obsolete_process", move || {
+            test::unregister_region_skip_obsolete_process();
+            Ok(())
+        }),
+    ];
+    libtest_mimic::run(&args, tests).exit();
 }
