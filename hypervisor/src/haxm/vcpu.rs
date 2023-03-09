7e848f215c65e84c28167e723724c3e6e4cf3647
Vaibhav Nagarnaik <vnagarnaik@google.com>
2023-03-09 19:01:00 +0000

    haxm: fix future incompatibility warning
    use of `unaligned_references` will be blocked soon and spits out a
    warning during compiling. fix it in haxm.
    
    Test: Compiled and ran with haxm enabled.
    Signed-off-by: Vaibhav Nagarnaik <vnagarnaik@google.com>
    Change-Id: Ia887cff1ce01e6c14a9cf4978bf49835e996dd5a
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4324279
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>

--- hypervisor/src/haxm/vcpu.rs
+++ hypervisor/src/haxm/vcpu.rs
@@ -233,18 +233,11 @@ impl Vcpu for HaxmVcpu {
                     operation: IoOperation::Read,
                 }) {
                     // Safe because we know this is an mmio read, so we need to put data into the
-                    // "value" field of the hax_fastmmio. The "value" field is a u64, so casting
-                    // as a &mut [u8] of len 8 is safe.
-                    // TODO(b/250085118): remove this clippy allow as it's allowing potentially
-                    // problematic behavior.
-                    #[allow(unaligned_references)]
-                    let buffer = unsafe {
-                        std::slice::from_raw_parts_mut(
-                            &mut (*mmio).__bindgen_anon_1.value as *mut u64 as *mut u8,
-                            8,
-                        )
-                    };
-                    buffer[..size].copy_from_slice(&data[..size]);
+                    // "value" field of the hax_fastmmio.
+                    let data = u64::from_ne_bytes(data);
+                    unsafe {
+                        (*mmio).__bindgen_anon_1.value = data;
+                    }
                 }
                 Ok(())
             }
