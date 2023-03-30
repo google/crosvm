901b23e83c6373b74512c66ce6553514d18d3c16
Gurchetan Singh <gurchetansingh@google.com>
2023-03-29 17:04:47 -0700

    rutabaga_gfx: fix target os
    Typo.
    
    BUG=b:273555494
    TEST=compile
    
    Change-Id: I99020d97537b2dfec0ea7fcb2243b0347fa0a257
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4382402
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Commit-Queue: Gurchetan Singh <gurchetansingh@chromium.org>
    Reviewed-by: Frederick Mayle <fmayle@google.com>

--- rutabaga_gfx/src/rutabaga_os/sys/mod.rs
+++ rutabaga_gfx/src/rutabaga_os/sys/mod.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#[cfg(any(arget_os = "android", target_os = "linux"))]
+#[cfg(any(target_os = "android", target_os = "linux"))]
 pub mod unix;
 
 #[cfg(target_os = "fuchsia")]
