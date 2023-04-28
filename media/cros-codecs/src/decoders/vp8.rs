82ce97e6ac66a59e56174454e2d079741463fcb7
Alexandre Courbot <acourbot@chromium.org>
2023-04-28 11:24:44 +0900

    media: remove local libva and cros-codecs crates
    Now that we have switched to the external versions of these crates,
    remove our old local copies.
    
    BUG=b:262824148
    TEST=presubmit
    
    Change-Id: I993e56831e0f5e895c036c8b40e8181f4577ae27
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4518094
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>

--- media/cros-codecs/src/decoders/vp8.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 The ChromiumOS Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-pub mod backends;
-mod bool_decoder;
-pub mod decoder;
-pub mod parser;
-mod probs;
--- media/cros-codecs/src/utils.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 The ChromiumOS Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#[cfg(test)]
-pub(crate) mod dummy;
-#[cfg(feature = "vaapi")]
-pub mod vaapi;
