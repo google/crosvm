2c0a9c21a96e2dcff965550a63cfb651bcf1d122
Frederick Mayle <fmayle@google.com>
2023-05-17 17:34:11 -0700

    vmm_vhost: delete unused trait VhostBackendMut
    Change-Id: Idc2591ca1785f8c769225864112d0e30ee642209
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4553723
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>

--- third_party/vmm_vhost/src/backend.rs
+++ third_party/vmm_vhost/src/backend.rs
@@ -9,9 +9,6 @@
 
 //! Common traits and structs for vhost-user backend drivers.
 
-use std::cell::RefCell;
-use std::sync::RwLock;
-
 use base::Event;
 use base::RawDescriptor;
 use base::INVALID_DESCRIPTOR;
@@ -183,282 +180,35 @@ pub trait VhostBackend: std::marker::Sized {
     fn restore(&self, data_bytes: &[u8], queue_evts: Option<Vec<Event>>) -> Result<()>;
 }
 
-/// An interface for setting up vhost-based backend drivers.
-///
-/// Vhost devices are subset of virtio devices, which improve virtio device's performance by
-/// delegating data plane operations to dedicated IO service processes. Vhost devices use the
-/// same virtqueue layout as virtio devices to allow vhost devices to be mapped directly to
-/// virtio devices.
-///
-/// The purpose of vhost is to implement a subset of a virtio device's functionality outside the
-/// VMM process. Typically fast paths for IO operations are delegated to the dedicated IO service
-/// processes, and slow path for device configuration are still handled by the VMM process. It may
-/// also be used to control access permissions of virtio backend devices.
-pub trait VhostBackendMut: std::marker::Sized {
-    /// Get a bitmask of supported virtio/vhost features.
-    fn get_features(&mut self) -> Result<u64>;
-
-    /// Inform the vhost subsystem which features to enable.
-    /// This should be a subset of supported features from get_features().
-    ///
-    /// # Arguments
-    /// * `features` - Bitmask of features to set.
-    fn set_features(&mut self, features: u64) -> Result<()>;
-
-    /// Set the current process as the owner of the vhost backend.
-    /// This must be run before any other vhost commands.
-    fn set_owner(&mut self) -> Result<()>;
-
-    /// Used to be sent to request disabling all rings
-    /// This is no longer used.
-    fn reset_owner(&mut self) -> Result<()>;
-
-    /// Set the guest memory mappings for vhost to use.
-    fn set_mem_table(&mut self, regions: &[VhostUserMemoryRegionInfo]) -> Result<()>;
-
-    /// Set base address for page modification logging.
-    fn set_log_base(&mut self, base: u64, fd: Option<RawDescriptor>) -> Result<()>;
-
-    /// Specify an event file descriptor to signal on log write.
-    fn set_log_fd(&mut self, fd: RawDescriptor) -> Result<()>;
-
-    /// Set the number of descriptors in the vring.
-    ///
-    /// # Arguments
-    /// * `queue_index` - Index of the queue to set descriptor count for.
-    /// * `num` - Number of descriptors in the queue.
-    fn set_vring_num(&mut self, queue_index: usize, num: u16) -> Result<()>;
-
-    /// Set the addresses for a given vring.
-    ///
-    /// # Arguments
-    /// * `queue_index` - Index of the queue to set addresses for.
-    /// * `config_data` - Configuration data for a vring.
-    fn set_vring_addr(&mut self, queue_index: usize, config_data: &VringConfigData) -> Result<()>;
-
-    /// Set the first index to look for available descriptors.
-    ///
-    /// # Arguments
-    /// * `queue_index` - Index of the queue to modify.
-    /// * `num` - Index where available descriptors start.
-    fn set_vring_base(&mut self, queue_index: usize, base: u16) -> Result<()>;
-
-    /// Get the available vring base offset.
-    fn get_vring_base(&mut self, queue_index: usize) -> Result<u32>;
-
-    /// Set the event to trigger when buffers have been used by the host.
-    ///
-    /// # Arguments
-    /// * `queue_index` - Index of the queue to modify.
-    /// * `event` - Event to trigger.
-    fn set_vring_call(&mut self, queue_index: usize, event: &Event) -> Result<()>;
-
-    /// Set the event that will be signaled by the guest when buffers are
-    /// available for the host to process.
-    ///
-    /// # Arguments
-    /// * `queue_index` - Index of the queue to modify.
-    /// * `event` - Event that will be signaled from guest.
-    fn set_vring_kick(&mut self, queue_index: usize, event: &Event) -> Result<()>;
-
-    /// Set the event that will be signaled by the guest when error happens.
-    ///
-    /// # Arguments
-    /// * `queue_index` - Index of the queue to modify.
-    /// * `event` - Event that will be signaled from guest.
-    fn set_vring_err(&mut self, queue_index: usize, event: &Event) -> Result<()>;
-
-    /// Put the device to sleep.
-    fn sleep(&mut self) -> Result<()>;
-
-    /// Wake the device up.
-    fn wake(&mut self) -> Result<()>;
-
-    /// Snapshot the device and receive serialized state of the device.
-    fn snapshot(&mut self) -> Result<Vec<u8>>;
-
-    /// Restore the device.
-    fn restore(&mut self, data_bytes: &[u8], queue_evts: Option<Vec<Event>>) -> Result<()>;
-}
-
-impl<T: VhostBackendMut> VhostBackend for RwLock<T> {
-    fn get_features(&self) -> Result<u64> {
-        self.write().unwrap().get_features()
-    }
-
-    fn set_features(&self, features: u64) -> Result<()> {
-        self.write().unwrap().set_features(features)
-    }
-
-    fn set_owner(&self) -> Result<()> {
-        self.write().unwrap().set_owner()
-    }
-
-    fn reset_owner(&self) -> Result<()> {
-        self.write().unwrap().reset_owner()
-    }
-
-    fn set_mem_table(&self, regions: &[VhostUserMemoryRegionInfo]) -> Result<()> {
-        self.write().unwrap().set_mem_table(regions)
-    }
-
-    fn set_log_base(&self, base: u64, fd: Option<RawDescriptor>) -> Result<()> {
-        self.write().unwrap().set_log_base(base, fd)
-    }
-
-    fn set_log_fd(&self, fd: RawDescriptor) -> Result<()> {
-        self.write().unwrap().set_log_fd(fd)
-    }
-
-    fn set_vring_num(&self, queue_index: usize, num: u16) -> Result<()> {
-        self.write().unwrap().set_vring_num(queue_index, num)
-    }
-
-    fn set_vring_addr(&self, queue_index: usize, config_data: &VringConfigData) -> Result<()> {
-        self.write()
-            .unwrap()
-            .set_vring_addr(queue_index, config_data)
-    }
-
-    fn set_vring_base(&self, queue_index: usize, base: u16) -> Result<()> {
-        self.write().unwrap().set_vring_base(queue_index, base)
-    }
-
-    fn get_vring_base(&self, queue_index: usize) -> Result<u32> {
-        self.write().unwrap().get_vring_base(queue_index)
-    }
-
-    fn set_vring_call(&self, queue_index: usize, event: &Event) -> Result<()> {
-        self.write().unwrap().set_vring_call(queue_index, event)
-    }
-
-    fn set_vring_kick(&self, queue_index: usize, event: &Event) -> Result<()> {
-        self.write().unwrap().set_vring_kick(queue_index, event)
-    }
-
-    fn set_vring_err(&self, queue_index: usize, event: &Event) -> Result<()> {
-        self.write().unwrap().set_vring_err(queue_index, event)
-    }
-
-    fn sleep(&self) -> Result<()> {
-        self.write().unwrap().sleep()
-    }
-
-    fn wake(&self) -> Result<()> {
-        self.write().unwrap().wake()
-    }
-
-    fn snapshot(&self) -> Result<Vec<u8>> {
-        self.write().unwrap().snapshot()
-    }
-
-    fn restore(&self, data_bytes: &[u8], queue_evts: Option<Vec<Event>>) -> Result<()> {
-        self.write().unwrap().restore(data_bytes, queue_evts)
-    }
-}
-
-impl<T: VhostBackendMut> VhostBackend for RefCell<T> {
-    fn get_features(&self) -> Result<u64> {
-        self.borrow_mut().get_features()
-    }
-
-    fn set_features(&self, features: u64) -> Result<()> {
-        self.borrow_mut().set_features(features)
-    }
-
-    fn set_owner(&self) -> Result<()> {
-        self.borrow_mut().set_owner()
-    }
-
-    fn reset_owner(&self) -> Result<()> {
-        self.borrow_mut().reset_owner()
-    }
-
-    fn set_mem_table(&self, regions: &[VhostUserMemoryRegionInfo]) -> Result<()> {
-        self.borrow_mut().set_mem_table(regions)
-    }
-
-    fn set_log_base(&self, base: u64, fd: Option<RawDescriptor>) -> Result<()> {
-        self.borrow_mut().set_log_base(base, fd)
-    }
-
-    fn set_log_fd(&self, fd: RawDescriptor) -> Result<()> {
-        self.borrow_mut().set_log_fd(fd)
-    }
-
-    fn set_vring_num(&self, queue_index: usize, num: u16) -> Result<()> {
-        self.borrow_mut().set_vring_num(queue_index, num)
-    }
-
-    fn set_vring_addr(&self, queue_index: usize, config_data: &VringConfigData) -> Result<()> {
-        self.borrow_mut().set_vring_addr(queue_index, config_data)
-    }
-
-    fn set_vring_base(&self, queue_index: usize, base: u16) -> Result<()> {
-        self.borrow_mut().set_vring_base(queue_index, base)
-    }
-
-    fn get_vring_base(&self, queue_index: usize) -> Result<u32> {
-        self.borrow_mut().get_vring_base(queue_index)
-    }
-
-    fn set_vring_call(&self, queue_index: usize, event: &Event) -> Result<()> {
-        self.borrow_mut().set_vring_call(queue_index, event)
-    }
-
-    fn set_vring_kick(&self, queue_index: usize, event: &Event) -> Result<()> {
-        self.borrow_mut().set_vring_kick(queue_index, event)
-    }
-
-    fn set_vring_err(&self, queue_index: usize, event: &Event) -> Result<()> {
-        self.borrow_mut().set_vring_err(queue_index, event)
-    }
-
-    fn sleep(&self) -> Result<()> {
-        self.borrow_mut().sleep()
-    }
-
-    fn wake(&self) -> Result<()> {
-        self.borrow_mut().wake()
-    }
-
-    fn snapshot(&self) -> Result<Vec<u8>> {
-        self.borrow_mut().snapshot()
-    }
-
-    fn restore(&self, data_bytes: &[u8], queue_evts: Option<Vec<Event>>) -> Result<()> {
-        self.borrow_mut().restore(data_bytes, queue_evts)
-    }
-}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     struct MockBackend {}
 
-    impl VhostBackendMut for MockBackend {
-        fn get_features(&mut self) -> Result<u64> {
+    impl VhostBackend for MockBackend {
+        fn get_features(&self) -> Result<u64> {
             Ok(0x1)
         }
 
-        fn set_features(&mut self, features: u64) -> Result<()> {
+        fn set_features(&self, features: u64) -> Result<()> {
             assert_eq!(features, 0x1);
             Ok(())
         }
 
-        fn set_owner(&mut self) -> Result<()> {
+        fn set_owner(&self) -> Result<()> {
             Ok(())
         }
 
-        fn reset_owner(&mut self) -> Result<()> {
+        fn reset_owner(&self) -> Result<()> {
             Ok(())
         }
 
-        fn set_mem_table(&mut self, _regions: &[VhostUserMemoryRegionInfo]) -> Result<()> {
+        fn set_mem_table(&self, _regions: &[VhostUserMemoryRegionInfo]) -> Result<()> {
             Ok(())
         }
 
-        fn set_log_base(&mut self, base: u64, fd: Option<RawDescriptor>) -> Result<()> {
+        fn set_log_base(&self, base: u64, fd: Option<RawDescriptor>) -> Result<()> {
             assert_eq!(base, 0x100);
             #[allow(clippy::unnecessary_cast)]
             let rd = 100 as RawDescriptor;
@@ -466,74 +216,70 @@ mod tests {
             Ok(())
         }
 
-        fn set_log_fd(&mut self, fd: RawDescriptor) -> Result<()> {
+        fn set_log_fd(&self, fd: RawDescriptor) -> Result<()> {
             #[allow(clippy::unnecessary_cast)]
             let rd = 100 as RawDescriptor;
             assert_eq!(fd, rd);
             Ok(())
         }
 
-        fn set_vring_num(&mut self, queue_index: usize, num: u16) -> Result<()> {
+        fn set_vring_num(&self, queue_index: usize, num: u16) -> Result<()> {
             assert_eq!(queue_index, 1);
             assert_eq!(num, 256);
             Ok(())
         }
 
-        fn set_vring_addr(
-            &mut self,
-            queue_index: usize,
-            _config_data: &VringConfigData,
-        ) -> Result<()> {
+        fn set_vring_addr(&self, queue_index: usize, _config_data: &VringConfigData) -> Result<()> {
             assert_eq!(queue_index, 1);
             Ok(())
         }
 
-        fn set_vring_base(&mut self, queue_index: usize, base: u16) -> Result<()> {
+        fn set_vring_base(&self, queue_index: usize, base: u16) -> Result<()> {
             assert_eq!(queue_index, 1);
             assert_eq!(base, 2);
             Ok(())
         }
 
-        fn get_vring_base(&mut self, queue_index: usize) -> Result<u32> {
+        fn get_vring_base(&self, queue_index: usize) -> Result<u32> {
             assert_eq!(queue_index, 1);
             Ok(2)
         }
 
-        fn set_vring_call(&mut self, queue_index: usize, _event: &Event) -> Result<()> {
+        fn set_vring_call(&self, queue_index: usize, _event: &Event) -> Result<()> {
             assert_eq!(queue_index, 1);
             Ok(())
         }
 
-        fn set_vring_kick(&mut self, queue_index: usize, _event: &Event) -> Result<()> {
+        fn set_vring_kick(&self, queue_index: usize, _event: &Event) -> Result<()> {
             assert_eq!(queue_index, 1);
             Ok(())
         }
 
-        fn set_vring_err(&mut self, queue_index: usize, _event: &Event) -> Result<()> {
+        fn set_vring_err(&self, queue_index: usize, _event: &Event) -> Result<()> {
             assert_eq!(queue_index, 1);
             Ok(())
         }
 
-        fn sleep(&mut self) -> Result<()> {
+        fn sleep(&self) -> Result<()> {
             Ok(())
         }
 
-        fn wake(&mut self) -> Result<()> {
+        fn wake(&self) -> Result<()> {
             Ok(())
         }
 
-        fn snapshot(&mut self) -> Result<Vec<u8>> {
+        fn snapshot(&self) -> Result<Vec<u8>> {
             Ok(Vec::new())
         }
 
-        fn restore(&mut self, _data_bytes: &[u8], _queue_evts: Option<Vec<Event>>) -> Result<()> {
+        fn restore(&self, _data_bytes: &[u8], _queue_evts: Option<Vec<Event>>) -> Result<()> {
             Ok(())
         }
     }
 
     #[test]
-    fn test_vring_backend_mut() {
-        let b = RwLock::new(MockBackend {});
+    fn test_vring_backend_mock() {
+        let b = MockBackend {};
 
         assert_eq!(b.get_features().unwrap(), 0x1);
         b.set_features(0x1).unwrap();
