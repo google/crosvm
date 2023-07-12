3343aec3b10d849a660cfaff8f8b21ed4be317a3
Braden Kell <bradenkell@google.com>
2023-07-12 20:51:31 +0000

    rutabaga_gfx: Add riscv64 cfg to fix test targets
    This change fixes compiler errors that occur when building crosvm test
    targets for riscv64 Android.
    
    BUG=b:286142463
    TEST=Copied patch to Android, built crosvm_test_src_main
    
    Change-Id: I9729b084a0784c23133248752fdf41f8b8b3cee4
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4682168
    Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>

--- rutabaga_gfx/src/virgl_renderer.rs
+++ rutabaga_gfx/src/virgl_renderer.rs
@@ -161,7 +161,12 @@ extern "C" fn debug_callback(fmt: *const ::std::os::raw::c_char, ap: stdio::va_l
 
     let printed_len = unsafe {
         let ptr = v.as_mut_ptr() as *mut ::std::os::raw::c_char;
-        #[cfg(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64"))]
+        #[cfg(any(
+            target_arch = "x86",
+            target_arch = "x86_64",
+            target_arch = "aarch64",
+            target_arch = "riscv64"
+        ))]
         let size = BUF_LEN as ::std::os::raw::c_ulong;
         #[cfg(target_arch = "arm")]
         let size = BUF_LEN as ::std::os::raw::c_uint;
