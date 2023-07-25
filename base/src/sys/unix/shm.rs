google/crosvm

commit 5b7cdc1eb03974b4e49fb63a73dcc4f2067c4f5c

base: use MFD_NOEXEC_SEAL if available when creating memfds
The new MFD_NOEXEC_SEAL flag allows creating memfds that don't have the
execute file mode. Add a function to detect whether this feature is
available on the currently running kernel so we can make the guest
memory memfd a non-executable file if the feature is available.

BUG=b:282221246
TEST=run crosvm on a ChromeOS kernel with MFD_NOEXEC patch
TEST=stat -L /proc/<crosvm pid>/fd/<crosvm memfd>; observe no x
TEST=run crosvm on an older vanilla Linux kernel without MFD_NOEXEC

Change-Id: I5d60cfe4b90eb23f79ecbc04d6bde9e1f46eeb8e
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4528584
Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Jeff Xu <jeffxu@google.com>

base/src/sys/unix/shm.rs
@@ -15,6 +15,7 @@ use libc::c_char;
 use libc::c_int;
 use libc::c_long;
 use libc::c_uint;
+use libc::close;
 use libc::fcntl;
 use libc::ftruncate64;
 use libc::off64_t;
@@ -29,12 +30,14 @@ use libc::F_SEAL_SEAL;
 use libc::F_SEAL_SHRINK;
 use libc::F_SEAL_WRITE;
 use libc::MFD_ALLOW_SEALING;
+use once_cell::sync::Lazy;
 use serde::Deserialize;
 use serde::Serialize;
 
 use super::errno_result;
 use super::Error;
 use super::Result;
+use crate::trace;
 use crate::AsRawDescriptor;
 use crate::FromRawDescriptor;
 use crate::IntoRawDescriptor;
@@ -52,6 +55,7 @@ pub struct SharedMemory {
 
 // from <sys/memfd.h>
 const MFD_CLOEXEC: c_uint = 0x0001;
+const MFD_NOEXEC_SEAL: c_uint = 0x0008;
 
 unsafe fn memfd_create(name: *const c_char, flags: c_uint) -> c_int {
     syscall(SYS_memfd_create as c_long, name, flags) as c_int
@@ -137,18 +141,50 @@ impl MemfdSeals {
     }
 }
 
+static MFD_NOEXEC_SEAL_SUPPORTED: Lazy<bool> = Lazy::new(|| {
+    // SAFETY: We pass a valid zero-terminated C string and check the result.
+    let fd = unsafe {
+        // The memfd name used here does not need to be unique, since duplicates are allowed and
+        // will not cause failures.
+        memfd_create(
+            b"MFD_NOEXEC_SEAL_test\0".as_ptr() as *const c_char,
+            MFD_CLOEXEC | MFD_ALLOW_SEALING | MFD_NOEXEC_SEAL,
+        )
+    };
+    if fd < 0 {
+        trace!("MFD_NOEXEC_SEAL is not supported");
+        false
+    } else {
+        trace!("MFD_NOEXEC_SEAL is supported");
+        // SAFETY: We know `fd` is a valid file descriptor owned by us.
+        unsafe {
+            close(fd);
+        }
+        true
+    }
+});
+
 impl SharedMemory {
-    /// Creates a new shared memory file descriptor with zero size.
+    /// Creates a new shared memory file descriptor with the specified `size` in bytes.
     ///
-    /// If a name is given, it will appear in `/proc/self/fd/<shm fd>` for the purposes of
-    /// debugging. The name does not need to be unique.
+    /// `name` will appear in `/proc/self/fd/<shm fd>` for the purposes of debugging. The name does
+    /// not need to be unique.
     ///
     /// The file descriptor is opened with the close on exec flag and allows memfd sealing.
+    ///
+    /// If the `MFD_NOEXEC_SEAL` flag is supported, the resulting file will also be created with a
+    /// non-executable file mode (in other words, it cannot be passed to the `exec` family of system
+    /// calls).
     pub fn new(debug_name: &CStr, size: u64) -> Result<SharedMemory> {
+        let mut flags = MFD_CLOEXEC | MFD_ALLOW_SEALING;
+        if *MFD_NOEXEC_SEAL_SUPPORTED {
+            flags |= MFD_NOEXEC_SEAL;
+        }
+
         let shm_name = debug_name.as_ptr() as *const c_char;
         // The following are safe because we give a valid C string and check the
         // results of the memfd_create call.
-        let fd = unsafe { memfd_create(shm_name, MFD_CLOEXEC | MFD_ALLOW_SEALING) };
+        let fd = unsafe { memfd_create(shm_name, flags) };
         if fd < 0 {
             return errno_result();
         }

commit 2b67ccffeb1503e26d2ced29309056fab63fe2b0

base: unix: adjust shm test for SEAL_EXEC
Due to MFD_NOEXEC_SEAL being applied by SharedMemory::new() on newer
kernels, the set of seals may not be 0 after shm creation. Tweak the
test to validate that only the F_SEAL_SEAL flag is clear before we
intentionally set it so the test can pass on both old and new kernels.

BUG=b:282221246
TEST=tools/dev_container tools/presubmit # on Linux 6.3.11 kernel

Fixes: 5b7cdc1eb039 ("base: use MFD_NOEXEC_SEAL if available when creating memfds")
Change-Id: I60e3886bf0401f037b3ca03ba1b43186adcec5bb
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4718767
Reviewed-by: Dennis Kempin <denniskempin@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

base/src/sys/unix/shm.rs
@@ -414,7 +414,7 @@ mod tests {
     fn new_sealed() {
         let mut shm = create_test_shmem();
         let mut seals = shm.get_seals().expect("failed to get seals");
-        assert_eq!(seals.bitmask(), 0);
+        assert!(!seals.seal_seal());
         seals.set_seal_seal();
         shm.add_seals(seals).expect("failed to add seals");
         seals = shm.get_seals().expect("failed to get seals");