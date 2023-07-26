94dc10b57cfb142e7996d17a66e2e29a824f0d9d
maciek swiech <drmasquatch@google.com>
2023-07-26 18:22:52 +0000

    Revert "balloon: rename WSS to WS"
    This reverts commit 1eb1106537ed005aac1fea9c57b5e2893058c005.
    
    Reason for revert: headers generated by cbindgen do not compile as expected downstream
    
    Original change's description:
    > balloon: rename WSS to WS
    >
    > as the working set feature approaches acceptance at the spec-level, we
    > are keeping up with nomenclature changes.
    >
    > BUG=b/288432539
    > TEST=cargo build, cargo build --no-default-features, cargo build --features registered_events
    >
    > Change-Id: I323a2c5402cb8008f8c912b0849e8e3501f9e1f3
    > Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4702340
    > Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    > Commit-Queue: maciek swiech <drmasquatch@google.com>
    
    BUG=b/288432539
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    
    Change-Id: I46459b4afd428c67436a754b3e62a104a680c68f
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4722468
    Commit-Queue: maciek swiech <drmasquatch@google.com>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>

--- src/crosvm/sys/unix/device_helpers.rs
+++ src/crosvm/sys/unix/device_helpers.rs
@@ -755,7 +755,7 @@ pub fn create_balloon_device(
     init_balloon_size: u64,
     enabled_features: u64,
     #[cfg(feature = "registered_events")] registered_evt_q: Option<SendTube>,
-    ws_num_bins: u8,
+    wss_num_bins: u8,
 ) -> DeviceResult {
     let dev = virtio::Balloon::new(
         virtio::base_features(protection_type),
@@ -766,7 +766,7 @@ pub fn create_balloon_device(
         enabled_features,
         #[cfg(feature = "registered_events")]
         registered_evt_q,
-        ws_num_bins,
+        wss_num_bins,
     )
     .context("failed to create balloon")?;
 
