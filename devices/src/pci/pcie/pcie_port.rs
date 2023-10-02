2355fd832c7c88cba51dee14c605182d8d4fabb1
David Stevens <stevensd@chromium.org>
2023-10-02 19:10:55 +0900

    devices: pcie: Split up PciePort
    Split the logic specific to handling reads/writes to the PCIe capability
    from PciePort into a separate struct. This is preparation for a change
    to how capabilities are handled.
    
    BUG=b:301865576
    TEST=./tools/dev_container ./tools/presubmit all
    
    Change-Id: I7d5b92ec6e38c32c2b04d2db1b0c4bc6029f5b0e
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4904600
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Commit-Queue: David Stevens <stevensd@chromium.org>

--- devices/src/pci/pcie/pcie_port.rs
+++ devices/src/pci/pcie/pcie_port.rs
@@ -102,20 +102,17 @@ pub struct PciePort {
     pcie_host: Option<PcieHostPort>,
     pcie_cap_reg_idx: Option<usize>,
     pm_cap_reg_idx: Option<usize>,
-    msi_config: Option<Arc<Mutex<MsiConfig>>>,
+    pcie_config: PcieConfig,
     pm_config: PmConfig,
 
-    slot_control: Option<u16>,
-    slot_status: u16,
+    msi_config: Option<Arc<Mutex<MsiConfig>>>,
 
     // For PcieRootPort, root_cap point to itself
     // For PcieDownstreamPort or PciDownstreamPort, root_cap point to PcieRootPort its behind.
     root_cap: Arc<Mutex<PcieRootCap>>,
     is_root_port: bool,
 
-    hp_interrupt_pending: bool,
     prepare_hotplug: bool,
-    removed_downstream_valid: bool,
 }
 
 impl PciePort {
@@ -155,21 +152,13 @@ impl PciePort {
             pcie_cap_reg_idx: None,
             pm_cap_reg_idx: None,
             msi_config: None,
+            pcie_config: PcieConfig::new(root_cap.clone(), slot_implemented, is_root_port),
             pm_config: PmConfig::new(),
-
-            slot_control: if slot_implemented {
-                Some(PCIE_SLTCTL_PIC_OFF | PCIE_SLTCTL_AIC_OFF)
-            } else {
-                None
-            },
-            slot_status: 0,
 
             root_cap,
             is_root_port,
 
-            hp_interrupt_pending: false,
             prepare_hotplug: false,
-            removed_downstream_valid: false,
         }
     }
 
@@ -202,21 +191,13 @@ impl PciePort {
             pcie_cap_reg_idx: None,
             pm_cap_reg_idx: None,
             msi_config: None,
+            pcie_config: PcieConfig::new(root_cap.clone(), slot_implemented, is_root_port),
             pm_config: PmConfig::new(),
-
-            slot_control: if slot_implemented {
-                Some(PCIE_SLTCTL_PIC_OFF | PCIE_SLTCTL_AIC_OFF)
-            } else {
-                None
-            },
-            slot_status: 0,
 
             root_cap,
             is_root_port,
 
-            hp_interrupt_pending: false,
             prepare_hotplug: false,
-            removed_downstream_valid: false,
         })
     }
 
@@ -268,6 +249,176 @@ impl PciePort {
             }
         }
         self.pci_address.ok_or(PciDeviceError::PciAllocationFailed)
+    }
+
+    pub fn read_config(&self, reg_idx: usize, data: &mut u32) {
+        if let Some(pcie_cap_reg_idx) = self.pcie_cap_reg_idx {
+            if reg_idx >= pcie_cap_reg_idx && reg_idx < pcie_cap_reg_idx + (PCIE_CAP_LEN / 4) {
+                let offset = (reg_idx - pcie_cap_reg_idx) * 4;
+                self.pcie_config.read_pcie_cap(offset, data);
+            }
+        }
+        if let Some(pm_cap_reg_idx) = self.pm_cap_reg_idx {
+            if reg_idx == pm_cap_reg_idx + PM_CAP_CONTROL_STATE_OFFSET {
+                self.pm_config.read(data);
+            }
+        }
+        if let Some(host) = &self.pcie_host {
+            host.read_config(reg_idx, data);
+        }
+    }
+
+    pub fn write_config(&mut self, reg_idx: usize, offset: u64, data: &[u8]) {
+        if let Some(pcie_cap_reg_idx) = self.pcie_cap_reg_idx {
+            if reg_idx >= pcie_cap_reg_idx && reg_idx < pcie_cap_reg_idx + (PCIE_CAP_LEN / 4) {
+                let delta = ((reg_idx - pcie_cap_reg_idx) * 4) + offset as usize;
+                self.pcie_config.write_pcie_cap(delta, data);
+            }
+        }
+        if let Some(pm_cap_reg_idx) = self.pm_cap_reg_idx {
+            if reg_idx == pm_cap_reg_idx + PM_CAP_CONTROL_STATE_OFFSET {
+                let old_status = self.pm_config.get_power_status();
+                self.pm_config.write(offset, data);
+                let new_status = self.pm_config.get_power_status();
+                if old_status == PciDevicePower::D3
+                    && new_status == PciDevicePower::D0
+                    && self.prepare_hotplug
+                {
+                    if let Some(host) = self.pcie_host.as_mut() {
+                        host.hotplug_probe();
+                        self.prepare_hotplug = false;
+                    }
+                }
+            }
+        }
+        if let Some(host) = self.pcie_host.as_mut() {
+            host.write_config(reg_idx, offset, data);
+        }
+    }
+
+    pub fn set_capability_reg_idx(&mut self, id: PciCapabilityID, reg_idx: usize) {
+        match id {
+            PciCapabilityID::PciExpress => self.pcie_cap_reg_idx = Some(reg_idx),
+            PciCapabilityID::PowerManagement => self.pm_cap_reg_idx = Some(reg_idx),
+            _ => (),
+        }
+    }
+
+    pub fn get_bus_range(&self) -> Option<PciBridgeBusRange> {
+        Some(self.bus_range)
+    }
+
+    pub fn get_bridge_window_size(&self) -> (u64, u64) {
+        if let Some(host) = &self.pcie_host {
+            host.get_bridge_window_size()
+        } else {
+            (PCIE_BR_MEM_SIZE, PCIE_BR_PREF_MEM_SIZE)
+        }
+    }
+
+    pub fn clone_interrupt(&mut self, msi_config: Arc<Mutex<MsiConfig>>) {
+        if self.is_root_port {
+            self.root_cap.lock().clone_interrupt(msi_config.clone());
+        }
+        self.pcie_config.msi_config = Some(msi_config.clone());
+        self.msi_config = Some(msi_config);
+    }
+
+    pub fn hotplug_implemented(&self) -> bool {
+        self.pcie_config.slot_control.is_some()
+    }
+
+    pub fn inject_pme(&mut self, requester_id: u16) {
+        let mut r = self.root_cap.lock();
+        if (r.status & PCIE_ROOTSTA_PME_STATUS) != 0 {
+            r.status |= PCIE_ROOTSTA_PME_PENDING;
+            r.pme_pending_requester_id = Some(requester_id);
+        } else {
+            r.status &= !PCIE_ROOTSTA_PME_REQ_ID_MASK;
+            r.status |= requester_id as u32;
+            r.pme_pending_requester_id = None;
+            r.status |= PCIE_ROOTSTA_PME_STATUS;
+            r.trigger_pme_interrupt();
+        }
+    }
+
+    pub fn trigger_hp_or_pme_interrupt(&mut self) {
+        if self.pm_config.should_trigger_pme() {
+            self.pcie_config.hp_interrupt_pending = true;
+            self.inject_pme(self.pci_address.unwrap().pme_requester_id());
+        } else {
+            self.pcie_config.trigger_hp_interrupt();
+        }
+    }
+
+    pub fn is_host(&self) -> bool {
+        self.pcie_host.is_some()
+    }
+
+    pub fn hot_unplug(&mut self) {
+        if let Some(host) = self.pcie_host.as_mut() {
+            host.hot_unplug()
+        }
+    }
+
+    pub fn is_match(&self, host_addr: PciAddress) -> Option<u8> {
+        if host_addr.bus == self.bus_range.secondary || self.pcie_host.is_none() {
+            Some(self.bus_range.secondary)
+        } else {
+            None
+        }
+    }
+
+    pub fn removed_downstream_valid(&self) -> bool {
+        self.pcie_config.removed_downstream_valid
+    }
+
+    pub fn set_slot_status(&mut self, flag: u16) {
+        self.pcie_config.set_slot_status(flag)
+    }
+
+    pub fn should_trigger_pme(&mut self) -> bool {
+        self.pm_config.should_trigger_pme()
+    }
+
+    pub fn prepare_hotplug(&mut self) {
+        self.prepare_hotplug = true;
+    }
+}
+
+pub struct PcieConfig {
+    msi_config: Option<Arc<Mutex<MsiConfig>>>,
+
+    slot_control: Option<u16>,
+    slot_status: u16,
+
+    // For PcieRootPort, root_cap point to itself
+    // For PcieDownstreamPort or PciDownstreamPort, root_cap point to PcieRootPort its behind.
+    root_cap: Arc<Mutex<PcieRootCap>>,
+    is_root_port: bool,
+
+    hp_interrupt_pending: bool,
+    removed_downstream_valid: bool,
+}
+
+impl PcieConfig {
+    fn new(root_cap: Arc<Mutex<PcieRootCap>>, slot_implemented: bool, is_root_port: bool) -> Self {
+        PcieConfig {
+            msi_config: None,
+
+            slot_control: if slot_implemented {
+                Some(PCIE_SLTCTL_PIC_OFF | PCIE_SLTCTL_AIC_OFF)
+            } else {
+                None
+            },
+            slot_status: 0,
+
+            root_cap,
+            is_root_port,
+
+            hp_interrupt_pending: false,
+            removed_downstream_valid: false,
+        }
     }
 
     fn read_pcie_cap(&self, offset: usize, data: &mut u32) {
@@ -393,87 +544,11 @@ impl PciePort {
         }
     }
 
-    pub fn read_config(&self, reg_idx: usize, data: &mut u32) {
-        if let Some(pcie_cap_reg_idx) = self.pcie_cap_reg_idx {
-            if reg_idx >= pcie_cap_reg_idx && reg_idx < pcie_cap_reg_idx + (PCIE_CAP_LEN / 4) {
-                let offset = (reg_idx - pcie_cap_reg_idx) * 4;
-                self.read_pcie_cap(offset, data);
-            }
-        }
-        if let Some(pm_cap_reg_idx) = self.pm_cap_reg_idx {
-            if reg_idx == pm_cap_reg_idx + PM_CAP_CONTROL_STATE_OFFSET {
-                self.pm_config.read(data);
-            }
-        }
-        if let Some(host) = &self.pcie_host {
-            host.read_config(reg_idx, data);
-        }
-    }
-
-    pub fn write_config(&mut self, reg_idx: usize, offset: u64, data: &[u8]) {
-        if let Some(pcie_cap_reg_idx) = self.pcie_cap_reg_idx {
-            if reg_idx >= pcie_cap_reg_idx && reg_idx < pcie_cap_reg_idx + (PCIE_CAP_LEN / 4) {
-                let delta = ((reg_idx - pcie_cap_reg_idx) * 4) + offset as usize;
-                self.write_pcie_cap(delta, data);
-            }
-        }
-        if let Some(pm_cap_reg_idx) = self.pm_cap_reg_idx {
-            if reg_idx == pm_cap_reg_idx + PM_CAP_CONTROL_STATE_OFFSET {
-                let old_status = self.pm_config.get_power_status();
-                self.pm_config.write(offset, data);
-                let new_status = self.pm_config.get_power_status();
-                if old_status == PciDevicePower::D3
-                    && new_status == PciDevicePower::D0
-                    && self.prepare_hotplug
-                {
-                    if let Some(host) = self.pcie_host.as_mut() {
-                        host.hotplug_probe();
-                        self.prepare_hotplug = false;
-                    }
-                }
-            }
-        }
-        if let Some(host) = self.pcie_host.as_mut() {
-            host.write_config(reg_idx, offset, data);
-        }
-    }
-
-    pub fn set_capability_reg_idx(&mut self, id: PciCapabilityID, reg_idx: usize) {
-        match id {
-            PciCapabilityID::PciExpress => self.pcie_cap_reg_idx = Some(reg_idx),
-            PciCapabilityID::PowerManagement => self.pm_cap_reg_idx = Some(reg_idx),
-            _ => (),
-        }
-    }
-
-    pub fn get_bus_range(&self) -> Option<PciBridgeBusRange> {
-        Some(self.bus_range)
-    }
-
-    pub fn get_bridge_window_size(&self) -> (u64, u64) {
-        if let Some(host) = &self.pcie_host {
-            host.get_bridge_window_size()
-        } else {
-            (PCIE_BR_MEM_SIZE, PCIE_BR_PREF_MEM_SIZE)
-        }
-    }
-
     fn get_slot_control(&self) -> u16 {
         if let Some(slot_control) = self.slot_control {
             return slot_control;
         }
         0
-    }
-
-    pub fn clone_interrupt(&mut self, msi_config: Arc<Mutex<MsiConfig>>) {
-        if self.is_root_port {
-            self.root_cap.lock().clone_interrupt(msi_config.clone());
-        }
-        self.msi_config = Some(msi_config);
-    }
-
-    pub fn hotplug_implemented(&self) -> bool {
-        self.slot_control.is_some()
     }
 
     fn trigger_cc_interrupt(&self) {
@@ -494,60 +569,7 @@ impl PciePort {
         }
     }
 
-    pub fn inject_pme(&mut self, requester_id: u16) {
-        let mut r = self.root_cap.lock();
-        if (r.status & PCIE_ROOTSTA_PME_STATUS) != 0 {
-            r.status |= PCIE_ROOTSTA_PME_PENDING;
-            r.pme_pending_requester_id = Some(requester_id);
-        } else {
-            r.status &= !PCIE_ROOTSTA_PME_REQ_ID_MASK;
-            r.status |= requester_id as u32;
-            r.pme_pending_requester_id = None;
-            r.status |= PCIE_ROOTSTA_PME_STATUS;
-            r.trigger_pme_interrupt();
-        }
-    }
-
-    pub fn trigger_hp_or_pme_interrupt(&mut self) {
-        if self.pm_config.should_trigger_pme() {
-            self.hp_interrupt_pending = true;
-            self.inject_pme(self.pci_address.unwrap().pme_requester_id());
-        } else {
-            self.trigger_hp_interrupt();
-        }
-    }
-
-    pub fn is_host(&self) -> bool {
-        self.pcie_host.is_some()
-    }
-
-    pub fn hot_unplug(&mut self) {
-        if let Some(host) = self.pcie_host.as_mut() {
-            host.hot_unplug()
-        }
-    }
-
-    pub fn is_match(&self, host_addr: PciAddress) -> Option<u8> {
-        if host_addr.bus == self.bus_range.secondary || self.pcie_host.is_none() {
-            Some(self.bus_range.secondary)
-        } else {
-            None
-        }
-    }
-
-    pub fn removed_downstream_valid(&self) -> bool {
-        self.removed_downstream_valid
-    }
-
-    pub fn set_slot_status(&mut self, flag: u16) {
+    fn set_slot_status(&mut self, flag: u16) {
         self.slot_status |= flag;
-    }
-
-    pub fn should_trigger_pme(&mut self) -> bool {
-        self.pm_config.should_trigger_pme()
-    }
-
-    pub fn prepare_hotplug(&mut self) {
-        self.prepare_hotplug = true;
     }
 }
