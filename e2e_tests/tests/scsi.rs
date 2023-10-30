572481fd082684a230b6d8222ce26d7f37aa0cbe
Joe Hattori <hattorij@google.com>
2023-10-30 09:11:34 +0000

    jail: seccomp: Add seccomp policy file of scsi device for arm and aarch64
    Currently x86_64 is the only architecture with scsi seccomp policy
    file. This commit introduces the policy for aarch64 and arm devices.
    From this commit, vm.Fio.scsi_* tast tests should pass.
    
    BUG=b:300042376
    TEST=tools/presubmit
    TEST=./tools/run_tests2 --dut=vm -E 'test(test_scsi_mount)'
    TEST=tast run $DUT vm.Fio.scsi_*
    
    Change-Id: Ia3071d6aebe2bd427ea75a448e6208185eecbb04
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4987210
    Reviewed-by: Takaya Saeki <takayas@chromium.org>
    Commit-Queue: Joe Hattori <hattorij@google.com>
    Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>

--- e2e_tests/tests/scsi.rs
+++ e2e_tests/tests/scsi.rs
@@ -8,19 +8,28 @@ use fixture::utils::prepare_disk_img;
 use fixture::vm::Config;
 use fixture::vm::TestVm;
 
-#[test]
-fn mount_scsi_device() -> anyhow::Result<()> {
+fn mount_scsi_device(config: Config) -> anyhow::Result<()> {
     let disk = prepare_disk_img();
     let scsi_disk = disk.path().to_str().unwrap();
     println!("scsi-disk={scsi_disk}");
 
-    let config = Config::new()
-        .disable_sandbox()
-        .extra_args(vec!["--scsi-block".to_string(), scsi_disk.to_string()]);
+    let config = config.extra_args(vec!["--scsi-block".to_string(), scsi_disk.to_string()]);
     let mut vm = TestVm::new(config).unwrap();
     vm.exec_in_guest("mount -t ext4 /dev/sda /mnt")?;
     vm.exec_in_guest("echo 42 > /mnt/tmp")?;
     vm.exec_in_guest("sync -d /mnt/tmp")?;
     assert_eq!(vm.exec_in_guest("cat /mnt/tmp")?.stdout.trim(), "42");
     Ok(())
+}
+
+#[test]
+fn test_scsi_mount() {
+    let config = Config::new();
+    mount_scsi_device(config).unwrap();
+}
+
+#[test]
+fn test_scsi_mount_disable_sandbox() {
+    let config = Config::new().disable_sandbox();
+    mount_scsi_device(config).unwrap();
 }
