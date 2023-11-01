a97ada3cddaaa19f40451d7e058ec0af3b6841e9
A. Cody Schuffelen <schuffelen@google.com>
2023-11-01 11:11:46 -0700

    Restore `unix`  on audio_streams
    Test: cargo build -p audio_streams --target=aarch64-apple-darwin
    Change-Id: I50666bedc333d6ed0906da629b80b559e49584c9
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/4997934
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Commit-Queue: Cody Schuffelen <schuffelen@google.com>

--- common/audio_streams/src/audio_streams.rs
+++ common/audio_streams/src/audio_streams.rs
@@ -48,7 +48,7 @@ use std::fmt::Display;
 use std::io;
 use std::io::Read;
 use std::io::Write;
-#[cfg(any(target_os = "android", target_os = "linux"))]
+#[cfg(unix)]
 use std::os::unix::io::RawFd as RawDescriptor;
 #[cfg(windows)]
 use std::os::windows::io::RawHandle as RawDescriptor;
