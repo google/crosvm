c9c8daa8b1d0c345884c8de6372a2e321253bd1c
Frederick Mayle <fmayle@google.com>
2023-11-30 13:51:19 -0800

    vmm_vhost: simplify Slave
    Now that the trait methods accept `&mut self`, there is no need for an
    internal mutex.
    
    Change-Id: Idb626317100e9d5ec421a114d6c93a74c3c7ba54
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5076927
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>

--- third_party/vmm_vhost/src/slave_proxy.rs
+++ third_party/vmm_vhost/src/slave_proxy.rs
@@ -1,10 +1,8 @@
 // Copyright (C) 2020 Alibaba Cloud. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::io;
 use std::mem;
-use std::sync::Mutex;
-use std::sync::MutexGuard;
+use std::string::ToString;
 
 use base::AsRawDescriptor;
 use base::RawDescriptor;
@@ -19,7 +17,15 @@ use crate::SlaveReq;
 use crate::SystemStream;
 use crate::VhostUserMasterReqHandler;
 
-struct SlaveInternal {
+/// Request proxy to send slave requests to the master through the slave communication channel.
+///
+/// The [Slave] acts as a message proxy to forward slave requests to the master through the
+/// vhost-user slave communication channel. The forwarded messages will be handled by the
+/// [MasterReqHandler] server.
+///
+/// [Slave]: struct.Slave.html
+/// [MasterReqHandler]: struct.MasterReqHandler.html
+pub struct Slave {
     sock: Connection<SlaveReq>,
 
     // Protocol feature VHOST_USER_PROTOCOL_F_REPLY_ACK has been negotiated.
@@ -29,13 +35,27 @@ struct SlaveInternal {
     error: Option<i32>,
 }
 
-impl SlaveInternal {
+impl Slave {
+    /// Constructs a new slave proxy from the given connection.
+    pub fn new(ep: Connection<SlaveReq>) -> Self {
+        Slave {
+            sock: ep,
+            reply_ack_negotiated: false,
+            error: None,
+        }
+    }
+
+    /// Create a new instance from a `SystemStream` object.
+    pub fn from_stream(sock: SystemStream) -> Self {
+        Self::new(Connection::from(sock))
+    }
+
     fn send_message<T>(
         &mut self,
         request: SlaveReq,
         msg: &T,
         fds: Option<&[RawDescriptor]>,
-    ) -> Result<u64>
+    ) -> HandlerResult<u64>
     where
         T: AsBytes,
     {
@@ -44,9 +64,12 @@ impl SlaveInternal {
         if self.reply_ack_negotiated {
             hdr.set_need_reply(true);
         }
-        self.sock.send_message(&hdr, msg, fds)?;
+        self.sock
+            .send_message(&hdr, msg, fds)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
 
         self.wait_for_reply(&hdr)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))
     }
 
     fn wait_for_reply(&mut self, hdr: &VhostUserMsgHeader<SlaveReq>) -> Result<u64> {
@@ -69,70 +92,19 @@ impl SlaveInternal {
 
         Ok(body.value)
     }
-}
-
-/// Request proxy to send slave requests to the master through the slave communication channel.
-///
-/// The [Slave] acts as a message proxy to forward slave requests to the master through the
-/// vhost-user slave communication channel. The forwarded messages will be handled by the
-/// [MasterReqHandler] server.
-///
-/// [Slave]: struct.Slave.html
-/// [MasterReqHandler]: struct.MasterReqHandler.html
-pub struct Slave {
-    // underlying socket for communication
-    // TODO: Switch methods to accept `&mut self` and remove this mutex, then inline
-    // `SlaveInternal`.
-    node: Mutex<SlaveInternal>,
-}
-
-impl Slave {
-    /// Constructs a new slave proxy from the given connection.
-    pub fn new(ep: Connection<SlaveReq>) -> Self {
-        Slave {
-            node: Mutex::new(SlaveInternal {
-                sock: ep,
-                reply_ack_negotiated: false,
-                error: None,
-            }),
-        }
-    }
-
-    fn node(&self) -> MutexGuard<SlaveInternal> {
-        self.node.lock().unwrap()
-    }
-
-    fn send_message<T>(
-        &self,
-        request: SlaveReq,
-        msg: &T,
-        fds: Option<&[RawDescriptor]>,
-    ) -> io::Result<u64>
-    where
-        T: AsBytes,
-    {
-        self.node()
-            .send_message(request, msg, fds)
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{}", e)))
-    }
-
-    /// Create a new instance from a `SystemStream` object.
-    pub fn from_stream(sock: SystemStream) -> Self {
-        Self::new(Connection::from(sock))
-    }
 
     /// Set the negotiation state of the `VHOST_USER_PROTOCOL_F_REPLY_ACK` protocol feature.
     ///
     /// When the `VHOST_USER_PROTOCOL_F_REPLY_ACK` protocol feature has been negotiated,
     /// the "REPLY_ACK" flag will be set in the message header for every slave to master request
     /// message.
-    pub fn set_reply_ack_flag(&self, enable: bool) {
-        self.node().reply_ack_negotiated = enable;
+    pub fn set_reply_ack_flag(&mut self, enable: bool) {
+        self.reply_ack_negotiated = enable;
     }
 
     /// Mark connection as failed with specified error code.
-    pub fn set_failed(&self, error: i32) {
-        self.node().error = Some(error);
+    pub fn set_failed(&mut self, error: i32) {
+        self.error = Some(error);
     }
 }
 
@@ -193,11 +165,11 @@ mod tests {
     #[test]
     fn test_slave_req_set_failed() {
         let (p1, _p2) = SystemStream::pair().unwrap();
-        let fs_cache = Slave::from_stream(p1);
+        let mut fs_cache = Slave::from_stream(p1);
 
-        assert!(fs_cache.node().error.is_none());
+        assert!(fs_cache.error.is_none());
         fs_cache.set_failed(libc::EAGAIN);
-        assert_eq!(fs_cache.node().error, Some(libc::EAGAIN));
+        assert_eq!(fs_cache.error, Some(libc::EAGAIN));
     }
 
     #[test]
