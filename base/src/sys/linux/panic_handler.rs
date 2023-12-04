f2b7cd9a2d2e91d8348e6c03352a3c877bdb5209
Daniel Verkamp <dverkamp@chromium.org>
2023-12-04 13:39:02 -0800

    base: linux: manually format panic information
    Rust 1.73 changes the default panic information formatting in a way that
    breaks the ChromeOS memfd panic handler collection system. Update the
    panic hook so that it uses the same formatting as libchromeos-rs, which
    matches the pattern expected by the crash-reporter collector.
    
    This version differs slightly from the one used in libchromeos-rs in
    that it does not try to retrieve the message from PanicInfo::message().
    This function is only available in nightly, and it also should only be
    used for panics originating from core (rather than std), which doesn't
    apply to our use case.
    
    BUG=b:309651697
    TEST=manually add panic and verify message formatting
    
    Change-Id: Id3e890f8d7db0c9d44c05e55b236f943ec17e148
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5078851
    Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
    Reviewed-by: Dennis Kempin <denniskempin@google.com>

--- base/src/sys/linux/panic_handler.rs
+++ base/src/sys/linux/panic_handler.rs
@@ -4,6 +4,7 @@
 
 //! A panic handler for better crash signatures for rust apps.
 
+use std::fmt::Write;
 use std::fs::File;
 use std::io;
 use std::mem;
@@ -15,13 +16,38 @@ use crate::SharedMemory;
 
 const PANIC_MEMFD_NAME: &str = "RUST_PANIC_SIG";
 
+// TODO(b/309651697): This was written to be compatible with existing PanicInfo formatting, but it
+// should probably be made more stable.
+fn format_panic_info(panic_info: &panic::PanicInfo<'_>) -> String {
+    // 128 is arbitrary, but should be enough to cover most cases.
+    let mut result = String::with_capacity(128);
+    result += "panicked at '";
+    result += panic_info
+        .payload()
+        .downcast_ref::<&'static str>()
+        .unwrap_or(&"<unknown>");
+    result += ", ";
+
+    // At the time of writing, `PanicInfo::location()` cannot return `None`.
+    match panic_info.location() {
+        Some(location) => {
+            let _ = write!(&mut result, "{}", location);
+        }
+        None => {
+            result += "no location info";
+        }
+    }
+
+    result
+}
+
 /// Inserts a panic handler that writes the panic info to a memfd called
 /// "RUST_PANIC_SIG" before calling the original panic handler. This
 /// makes it possible for external crash handlers to recover the panic info.
 pub fn install_memfd_handler() {
     let hook = panic::take_hook();
     panic::set_hook(Box::new(move |p| {
-        let panic_info = format!("{}\n", &p);
+        let panic_info = format_panic_info(p);
         let panic_bytes = panic_info.as_bytes();
         // On failure, ignore the error and call the original handler.
         if let Ok(panic_memfd) = SharedMemory::new(PANIC_MEMFD_NAME, panic_bytes.len() as u64) {
