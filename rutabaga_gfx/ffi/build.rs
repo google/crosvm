86e0bdd7e600af1f06c51c88fcadfa9fa06cc0fa
Alex Bennée <alex.bennee@linaro.org>
2024-01-02 15:38:18 +0000

    rutabaga_gfx: allow .pc {prefix} to be relocated
    While /usr is the usual place to put headers we should follow the lead
    of the PREFIX set at install time, especially before this is widely
    packaged. Therefor replace the static .pc file with one generated by
    build.rs.
    
    TEST=make prefix=$PREFIX install
    
    Change-Id: I905422df37f89e31f3942af38a91ebc51f39e479
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5159307
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Commit-Queue: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Dennis Kempin <denniskempin@google.com>

--- rutabaga_gfx/ffi/build.rs
+++ rutabaga_gfx/ffi/build.rs
@@ -2,8 +2,45 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use std::env;
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    // Override prefix from environment variable (with a default)
+    let prefix = format!(
+        r##"prefix={prefix}"##,
+        prefix = env::var("PREFIX").unwrap_or_else(|_| "/usr".to_string())
+    );
+
+    // Generate .pc file contents
+    let pc_contents = format!(
+        "{}{}",
+        prefix,
+        r###"
+exec_prefix=${prefix}
+includedir=${prefix}/include
+libdir=${exec_prefix}/lib
+
+Name: rutabaga_gfx_ffi
+Description: C FFI bindings to Rutabaga VGI
+Version: 0.1.2
+Cflags: -I${includedir}
+Libs: -L${libdir} -lrutabaga_gfx_ffi
+"###,
+    );
+
+    // Write the .pc file to the output directory
+    let mut pc_file = File::create(format!(
+        "target/{}/rutabaga_gfx_ffi.pc",
+        env::var("PROFILE").unwrap()
+    ))
+    .unwrap();
+    pc_file.write_all(pc_contents.as_bytes()).unwrap();
+
+    println!("cargo:rerun-if-changed=build.rs"); // Rebuild if build.rs changes
 
     if target_os.contains("linux") || target_os.contains("nto") {
         println!("cargo:rustc-cdylib-link-arg=-Wl,-soname,librutabaga_gfx_ffi.so.0");
