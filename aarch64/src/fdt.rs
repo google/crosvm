5eeefb830b32e9ee3dc5a20842a49c534ad89630
David Dai <davidai@google.com>
2024-01-17 14:47:55 -0800

    aarch64: fdt: Remove compatible string for virt cpufreq
    Remove "qemu,virtual-cpufreq" compatible string.
    
    Bug: None
    Test: ./tools/presubmit
    
    Change-Id: I34a4896ba2cf085a50b330e0748dd5fc98f98bdb
    Signed-off-by: David Dai <davidai@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5207894
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>

--- aarch64/src/fdt.rs
+++ aarch64/src/fdt.rs
@@ -242,11 +242,10 @@ fn create_timer_node(fdt: &mut Fdt, num_cpus: u32) -> Result<()> {
 }
 
 fn create_virt_cpufreq_node(fdt: &mut Fdt, num_cpus: u64) -> Result<()> {
-    let compatible = "qemu,virtual-cpufreq";
+    // TODO: b/320770346: add compatible string
     let vcf_node = fdt.root_mut().subnode_mut("cpufreq")?;
     let reg = [AARCH64_VIRTFREQ_BASE, AARCH64_VIRTFREQ_SIZE * num_cpus];
 
-    vcf_node.set_prop("compatible", compatible)?;
     vcf_node.set_prop("reg", &reg)?;
     Ok(())
 }
