google/crosvm

commit 53c8fb14bf28a3bf7d52af74dd8bcb6912006824

devices: apply new noncoherent attribute for GPU mem's cache info
On Intel, new noncoherent DMA mem flag can be set to allow KVM to
recognize guest cache setting for EPT, which allows WC attribute to be
applied to ensure guest memory access is synchronized in noncoherent DMA.

Unlike previous POC for new device attribute, this uses new added flag
for mem slot.

BUG=b:316337317
TEST=cargo build and run GPU workload

Change-Id: If16d61031bdb2cf0252a57e99d4b6725a89dd38a
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5171312
Reviewed-by: Frederick Mayle <fmayle@google.com>
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
Commit-Queue: Stanisław Kardach <skardach@google.com>

hypervisor/src/whpx/vm.rs
@@ -54,6 +54,7 @@ use crate::DestinationMode;
 use crate::DeviceKind;
 use crate::IoEventAddress;
 use crate::LapicState;
+use crate::MemCacheType;
 use crate::MemSlot;
 use crate::TriggerMode;
 use crate::VcpuX86_64;
@@ -529,6 +530,7 @@ impl Vm for WhpxVm {
         mem: Box<dyn MappedRegion>,
         read_only: bool,
         log_dirty_pages: bool,
+        _cache: MemCacheType,
     ) -> Result<MemSlot> {
         let size = mem.size() as u64;
         let end_addr = guest_addr.checked_add(size).ok_or(Error::new(EOVERFLOW))?;
@@ -1002,8 +1004,14 @@ mod tests {
             .from_shared_memory(&shm)
             .build()
             .unwrap();
-        vm.add_memory_region(GuestAddress(0x1000), Box::new(mem), true, false)
-            .unwrap();
+        vm.add_memory_region(
+            GuestAddress(0x1000),
+            Box::new(mem),
+            true,
+            false,
+            MemCacheType::CacheCoherent,
+        )
+        .unwrap();
     }
 
     #[test]
@@ -1023,7 +1031,13 @@ mod tests {
             .unwrap();
         let mem_ptr = mem.as_ptr();
         let slot = vm
-            .add_memory_region(GuestAddress(0x1000), Box::new(mem), false, false)
+            .add_memory_region(
+                GuestAddress(0x1000),
+                Box::new(mem),
+                false,
+                false,
+                MemCacheType::CacheCoherent,
+            )
             .unwrap();
         let removed_mem = vm.remove_memory_region(slot).unwrap();
         assert_eq!(removed_mem.size(), mem_size);
@@ -1058,7 +1072,13 @@ mod tests {
             .build()
             .unwrap();
         assert!(vm
-            .add_memory_region(GuestAddress(0x2000), Box::new(mem), false, false)
+            .add_memory_region(
+                GuestAddress(0x2000),
+                Box::new(mem),
+                false,
+                false,
+                MemCacheType::CacheCoherent
+            )
             .is_err());
     }
 
@@ -1078,7 +1098,13 @@ mod tests {
             .build()
             .unwrap();
         let slot = vm
-            .add_memory_region(GuestAddress(0x10000), Box::new(mem), false, false)
+            .add_memory_region(
+                GuestAddress(0x10000),
+                Box::new(mem),
+                false,
+                false,
+                MemCacheType::CacheCoherent,
+            )
             .unwrap();
         vm.msync_memory_region(slot, mem_size - 1, 0).unwrap();
         vm.msync_memory_region(slot, 0, mem_size).unwrap();

hypervisor/src/haxm/vm.rs
@@ -45,6 +45,7 @@ use crate::Datamatch;
 use crate::DeviceKind;
 use crate::Hypervisor;
 use crate::IoEventAddress;
+use crate::MemCacheType;
 use crate::MemSlot;
 use crate::VcpuX86_64;
 use crate::Vm;
@@ -224,6 +225,7 @@ impl Vm for HaxmVm {
             VmCap::EarlyInitCpuid => false,
             VmCap::BusLockDetect => false,
             VmCap::ReadOnlyMemoryRegion => false,
+            VmCap::MemNoncoherentDma => false,
         }
     }
 
@@ -237,6 +239,7 @@ impl Vm for HaxmVm {
         mem: Box<dyn MappedRegion>,
         read_only: bool,
         _log_dirty_pages: bool,
+        _cache: MemCacheType,
     ) -> Result<MemSlot> {
         let size = mem.size() as u64;
         let end_addr = guest_addr.checked_add(size).ok_or(Error::new(EOVERFLOW))?;
@@ -623,7 +626,13 @@ mod tests {
             .unwrap();
         let mem_ptr = mem.as_ptr();
         let slot = vm
-            .add_memory_region(GuestAddress(0x1000), Box::new(mem), false, false)
+            .add_memory_region(
+                GuestAddress(0x1000),
+                Box::new(mem),
+                false,
+                false,
+                MemCacheType::CacheCoherent,
+            )
             .unwrap();
         let removed_mem = vm.remove_memory_region(slot).unwrap();
         assert_eq!(removed_mem.size(), mem_size);

aarch64/src/lib.rs
@@ -50,6 +50,7 @@ use hypervisor::CpuConfigAArch64;
 use hypervisor::DeviceKind;
 use hypervisor::Hypervisor;
 use hypervisor::HypervisorCap;
+use hypervisor::MemCacheType;
 use hypervisor::ProtectionType;
 use hypervisor::VcpuAArch64;
 use hypervisor::VcpuFeature;
@@ -522,6 +523,7 @@ impl arch::LinuxArch for AArch64 {
                 Box::new(pvtime_mem),
                 false,
                 false,
+                MemCacheType::CacheCoherent,
             )
             .map_err(Error::MapPvtimeError)?;
         }

arch/src/lib.rs
@@ -61,6 +61,7 @@ pub use fdt::DtbOverlay;
 #[cfg(feature = "gdb")]
 use gdbstub::arch::Arch;
 use hypervisor::IoEventAddress;
+use hypervisor::MemCacheType;
 use hypervisor::Vm;
 #[cfg(windows)]
 use jail::FakeMinijailStub as Minijail;
@@ -1160,6 +1161,7 @@ pub fn generate_pci_root(
                     Box::new(mmap),
                     false,
                     false,
+                    MemCacheType::CacheCoherent,
                 );
             }
         }

arch/src/pstore.rs
@@ -8,6 +8,7 @@ use anyhow::bail;
 use anyhow::Context;
 use anyhow::Result;
 use base::MemoryMappingBuilder;
+use hypervisor::MemCacheType;
 use hypervisor::Vm;
 use resources::AddressRange;
 use vm_memory::GuestAddress;
@@ -52,6 +53,7 @@ pub fn create_memory_region(
         Box::new(memory_mapping),
         false,
         false,
+        MemCacheType::CacheCoherent,
     )
     .context("failed to add pstore region")?;
 

devices/src/pci/coiommu.rs
@@ -49,6 +49,7 @@ use base::TubeError;
 use base::WaitContext;
 use base::WorkerThread;
 use hypervisor::Datamatch;
+use hypervisor::MemCacheType;
 use resources::Alloc;
 use resources::AllocOptions;
 use resources::SystemAllocator;
@@ -1139,6 +1140,7 @@ impl CoIommuDev {
                 },
                 VmMemoryDestination::GuestPhysicalAddress(gpa),
                 prot,
+                MemCacheType::CacheCoherent,
             )
             .context("register_mmap register_memory failed")?;
         Ok(())

devices/src/pci/pci_root.rs
@@ -19,6 +19,7 @@ use base::RawDescriptor;
 use base::SendTube;
 use base::SharedMemory;
 use base::VmEventType;
+use hypervisor::MemCacheType;
 use hypervisor::Vm;
 use resources::SystemAllocator;
 use serde::Deserialize;
@@ -591,8 +592,14 @@ impl<T: Vm> PciMmioMapper for T {
             .protection(Protection::read())
             .build()
             .context("failed to map shmem")?;
-        self.add_memory_region(addr, Box::new(mapping), true, false)
-            .context("failed to create vm mapping")
+        self.add_memory_region(
+            addr,
+            Box::new(mapping),
+            true,
+            false,
+            MemCacheType::CacheCoherent,
+        )
+        .context("failed to create vm mapping")
     }
 }
 

devices/src/pci/vfio_pci.rs
@@ -28,6 +28,7 @@ use base::RawDescriptor;
 use base::Tube;
 use base::WaitContext;
 use base::WorkerThread;
+use hypervisor::MemCacheType;
 use resources::AddressRange;
 use resources::Alloc;
 use resources::AllocOptions;
@@ -1182,6 +1183,7 @@ impl VfioPciDevice {
                     },
                     VmMemoryDestination::GuestPhysicalAddress(guest_map_start),
                     Protection::read_write(),
+                    MemCacheType::CacheCoherent,
                 ) {
                     Ok(id) => {
                         mmaps_ids.push(id);

commit f6961898b21751e8b4063feb682df3f02f1acceb

hypervisor: whpx: handle new VmCap::MemNoncoherentDma
Fixes Windows build when --features=whpx is enabled.

Fixes: 53c8fb14bf28 ("devices: apply new noncoherent attribute for GPU mem's cache info")
Change-Id: Ic1e6a5607478f9ca384ee62a0a6e9ae3eda3b6f9
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5263195
Reviewed-by: Noah Gold <nkgold@google.com>
Commit-Queue: Noah Gold <nkgold@google.com>
Auto-Submit: Daniel Verkamp <dverkamp@chromium.org>

hypervisor/src/whpx/vm.rs
@@ -517,6 +517,7 @@ impl Vm for WhpxVm {
             #[cfg(target_arch = "x86_64")]
             VmCap::BusLockDetect => false,
             VmCap::ReadOnlyMemoryRegion => true,
+            VmCap::MemNoncoherentDma => false,
         }
     }
 