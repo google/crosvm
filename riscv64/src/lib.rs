google/crosvm

commit 2dde104c1e9f74a6a8293a7c77b43d05f89506b4

arch: return created devices from add_serial_devices()
This will allow the architecture-specific code to iterate over devices
without needing to have hard-coded knowledge of the address space layout
or IRQ assignment.

BUG=b:331472317
TEST=tools/dev_container tools/presubmit

Change-Id: I59c29c4695cf12332de58fa32f9bf14b19852be9
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5399620
Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

riscv64/src/lib.rs
@@ -213,11 +213,12 @@ impl arch::LinuxArch for Riscv64 {
 
         let com_evt_1_3 = Event::new().map_err(Error::CreateEvent)?;
         let com_evt_2_4 = Event::new().map_err(Error::CreateEvent)?;
-        arch::add_serial_devices(
+        let serial_devices = arch::add_serial_devices(
             components.hv_cfg.protection_type,
             &mmio_bus,
-            &com_evt_1_3,
-            &com_evt_2_4,
+            // TODO: the IRQ numbers are bogus since the events aren't actually wired up
+            (0, &com_evt_1_3),
+            (0, &com_evt_2_4),
             serial_parameters,
             serial_jail,
             #[cfg(feature = "swap")]
@@ -284,7 +285,7 @@ impl arch::LinuxArch for Riscv64 {
             .insert(pci_bus, RISCV64_PCI_CFG_BASE, RISCV64_PCI_CFG_SIZE)
             .map_err(Error::RegisterPci)?;
 
-        get_serial_cmdline(&mut cmdline, serial_parameters, "mmio")
+        get_serial_cmdline(&mut cmdline, serial_parameters, "mmio", serial_devices)
             .map_err(Error::GetSerialCmdline)?;
         for param in components.extra_kernel_params {
             cmdline.insert_str(&param).map_err(Error::Cmdline)?;

aarch64/src/lib.rs
@@ -632,11 +632,11 @@ impl arch::LinuxArch for AArch64 {
 
         let com_evt_1_3 = devices::IrqEdgeEvent::new().map_err(Error::CreateEvent)?;
         let com_evt_2_4 = devices::IrqEdgeEvent::new().map_err(Error::CreateEvent)?;
-        arch::add_serial_devices(
+        let serial_devices = arch::add_serial_devices(
             components.hv_cfg.protection_type,
             &mmio_bus,
-            com_evt_1_3.get_trigger(),
-            com_evt_2_4.get_trigger(),
+            (AARCH64_SERIAL_1_3_IRQ, com_evt_1_3.get_trigger()),
+            (AARCH64_SERIAL_2_4_IRQ, com_evt_2_4.get_trigger()),
             serial_parameters,
             serial_jail,
             #[cfg(feature = "swap")]
@@ -695,7 +695,7 @@ impl arch::LinuxArch for AArch64 {
         }
 
         let mut cmdline = Self::get_base_linux_cmdline();
-        get_serial_cmdline(&mut cmdline, serial_parameters, "mmio")
+        get_serial_cmdline(&mut cmdline, serial_parameters, "mmio", &serial_devices)
             .map_err(Error::GetSerialCmdline)?;
         for param in components.extra_kernel_params {
             cmdline.insert_str(&param).map_err(Error::Cmdline)?;

x86_64/src/lib.rs
@@ -52,6 +52,7 @@ use acpi_tables::aml::Aml;
 use acpi_tables::sdt::SDT;
 use anyhow::Context;
 use arch::get_serial_cmdline;
+use arch::serial::SerialDeviceInfo;
 use arch::CpuSet;
 use arch::DtbOverlay;
 use arch::GetSerialCmdlineError;
@@ -872,7 +873,7 @@ impl arch::LinuxArch for X8664arch {
         } else {
             None
         };
-        Self::setup_serial_devices(
+        let serial_devices = Self::setup_serial_devices(
             components.hv_cfg.protection_type,
             irq_chip.as_irq_chip_mut(),
             &io_bus,
@@ -988,7 +989,7 @@ impl arch::LinuxArch for X8664arch {
 
         let mut cmdline = Self::get_base_linux_cmdline();
 
-        get_serial_cmdline(&mut cmdline, serial_parameters, "io")
+        get_serial_cmdline(&mut cmdline, serial_parameters, "io", &serial_devices)
             .map_err(Error::GetSerialCmdline)?;
 
         for param in components.extra_kernel_params {
@@ -2172,14 +2173,13 @@ impl X8664arch {
         ))
     }
 
-    /// Sets up the serial devices for this platform. Returns the serial port number and serial
-    /// device to be used for stdout
+    /// Sets up the serial devices for this platform. Returns a list of configured serial devices.
     ///
     /// # Arguments
     ///
     /// * - `irq_chip` the IrqChip object for registering irq events
     /// * - `io_bus` the I/O bus to add the devices to
-    /// * - `serial_parmaters` - definitions for how the serial devices should be configured
+    /// * - `serial_parameters` - definitions for how the serial devices should be configured
     pub fn setup_serial_devices(
         protection_type: ProtectionType,
         irq_chip: &mut dyn IrqChip,
@@ -2187,15 +2187,15 @@ impl X8664arch {
         serial_parameters: &BTreeMap<(SerialHardware, u8), SerialParameters>,
         serial_jail: Option<Minijail>,
         #[cfg(feature = "swap")] swap_controller: &mut Option<swap::SwapController>,
-    ) -> Result<()> {
+    ) -> Result<Vec<SerialDeviceInfo>> {
         let com_evt_1_3 = devices::IrqEdgeEvent::new().map_err(Error::CreateEvent)?;
         let com_evt_2_4 = devices::IrqEdgeEvent::new().map_err(Error::CreateEvent)?;
 
-        arch::add_serial_devices(
+        let serial_devices = arch::add_serial_devices(
             protection_type,
             io_bus,
-            com_evt_1_3.get_trigger(),
-            com_evt_2_4.get_trigger(),
+            (X86_64_SERIAL_1_3_IRQ, com_evt_1_3.get_trigger()),
+            (X86_64_SERIAL_2_4_IRQ, com_evt_2_4.get_trigger()),
             serial_parameters,
             serial_jail,
             #[cfg(feature = "swap")]
@@ -2215,7 +2215,7 @@ impl X8664arch {
             .register_edge_irq_event(X86_64_SERIAL_2_4_IRQ, &com_evt_2_4, source)
             .map_err(Error::RegisterIrqfd)?;
 
-        Ok(())
+        Ok(serial_devices)
     }
 
     fn setup_debugcon_devices(

arch/src/serial.rs
@@ -83,6 +83,19 @@ pub fn set_default_serial_parameters(
 /// Address for Serial ports in x86
 pub const SERIAL_ADDR: [u64; 4] = [0x3f8, 0x2f8, 0x3e8, 0x2e8];
 
+/// Information about a serial device (16550-style UART) created by `add_serial_devices()`.
+pub struct SerialDeviceInfo {
+    /// Address of the device on the bus.
+    /// This is the I/O bus on x86 machines and MMIO otherwise.
+    pub address: u64,
+
+    /// Size of the device's address space on the bus.
+    pub size: u64,
+
+    /// IRQ number of the device.
+    pub irq: u32,
+}
+
 /// Adds serial devices to the provided bus based on the serial parameters given.
 ///
 /// Only devices with hardware type `SerialHardware::Serial` are added by this function.
@@ -91,20 +104,21 @@ pub const SERIAL_ADDR: [u64; 4] = [0x3f8, 0x2f8, 0x3e8, 0x2e8];
 ///
 /// * `protection_type` - VM protection mode.
 /// * `io_bus` - Bus to add the devices to
-/// * `com_evt_1_3` - event for com1 and com3
-/// * `com_evt_1_4` - event for com2 and com4
+/// * `com_evt_1_3` - irq and event for com1 and com3
+/// * `com_evt_1_4` - irq and event for com2 and com4
 /// * `serial_parameters` - definitions of serial parameter configurations.
 /// * `serial_jail` - minijail object cloned for use with each serial device. All four of the
 ///   traditional PC-style serial ports (COM1-COM4) must be specified.
 pub fn add_serial_devices(
     protection_type: ProtectionType,
     io_bus: &Bus,
-    com_evt_1_3: &Event,
-    com_evt_2_4: &Event,
+    com_evt_1_3: (u32, &Event),
+    com_evt_2_4: (u32, &Event),
     serial_parameters: &BTreeMap<(SerialHardware, u8), SerialParameters>,
     #[cfg_attr(windows, allow(unused_variables))] serial_jail: Option<Minijail>,
     #[cfg(feature = "swap")] swap_controller: &mut Option<swap::SwapController>,
-) -> std::result::Result<(), DeviceRegistrationError> {
+) -> std::result::Result<Vec<SerialDeviceInfo>, DeviceRegistrationError> {
+    let mut devices = Vec::new();
     for com_num in 0..=3 {
         let com_evt = match com_num {
             0 => &com_evt_1_3,
@@ -113,6 +127,8 @@ pub fn add_serial_devices(
             3 => &com_evt_2_4,
             _ => &com_evt_1_3,
         };
+
+        let (irq, com_evt) = (com_evt.0, com_evt.1);
 
         let param = serial_parameters
             .get(&(SerialHardware::Serial, com_num + 1))
@@ -155,9 +171,10 @@ pub fn add_serial_devices(
         let address = SERIAL_ADDR[usize::from(com_num)];
         let size = 0x8; // 16550 UART uses 8 bytes of address space.
         io_bus.insert(com, address, size).unwrap();
+        devices.push(SerialDeviceInfo { address, size, irq })
     }
 
-    Ok(())
+    Ok(devices)
 }
 
 #[sorted]
@@ -179,6 +196,7 @@ pub fn get_serial_cmdline(
     cmdline: &mut kernel_cmdline::Cmdline,
     serial_parameters: &BTreeMap<(SerialHardware, u8), SerialParameters>,
     serial_io_type: &str,
+    serial_devices: &[SerialDeviceInfo],
 ) -> GetSerialCmdlineResult<()> {
     for serial_parameter in serial_parameters
         .iter()
@@ -207,11 +225,11 @@ pub fn get_serial_cmdline(
         .next()
     {
         Some((SerialHardware::Serial, num)) => {
-            if let Some(addr) = SERIAL_ADDR.get(*num as usize - 1) {
+            if let Some(serial_device) = serial_devices.get(*num as usize - 1) {
                 cmdline
                     .insert(
                         "earlycon",
-                        &format!("uart8250,{},0x{:x}", serial_io_type, addr),
+                        &format!("uart8250,{},0x{:x}", serial_io_type, serial_device.address),
                     )
                     .map_err(GetSerialCmdlineError::KernelCmdline)?;
             }
@@ -227,6 +245,7 @@ pub fn get_serial_cmdline(
 
 #[cfg(test)]
 mod tests {
+    use devices::BusType;
     use kernel_cmdline::Cmdline;
 
     use super::*;
@@ -235,9 +254,23 @@ mod tests {
     fn get_serial_cmdline_default() {
         let mut cmdline = Cmdline::new(4096);
         let mut serial_parameters = BTreeMap::new();
+        let io_bus = Bus::new(BusType::Io);
+        let evt1_3 = Event::new().unwrap();
+        let evt2_4 = Event::new().unwrap();
 
         set_default_serial_parameters(&mut serial_parameters, false);
-        get_serial_cmdline(&mut cmdline, &serial_parameters, "io")
+        let serial_devices = add_serial_devices(
+            ProtectionType::Unprotected,
+            &io_bus,
+            (4, &evt1_3),
+            (3, &evt2_4),
+            &serial_parameters,
+            None,
+            #[cfg(feature = "swap")]
+            &mut None,
+        )
+        .unwrap();
+        get_serial_cmdline(&mut cmdline, &serial_parameters, "io", &serial_devices)
             .expect("get_serial_cmdline failed");
 
         let cmdline_str = cmdline.as_str();
@@ -248,6 +281,9 @@ mod tests {
     fn get_serial_cmdline_virtio_console() {
         let mut cmdline = Cmdline::new(4096);
         let mut serial_parameters = BTreeMap::new();
+        let io_bus = Bus::new(BusType::Io);
+        let evt1_3 = Event::new().unwrap();
+        let evt2_4 = Event::new().unwrap();
 
         // Add a virtio-console device with console=true.
         serial_parameters.insert(
@@ -269,7 +305,18 @@ mod tests {
         );
 
         set_default_serial_parameters(&mut serial_parameters, false);
-        get_serial_cmdline(&mut cmdline, &serial_parameters, "io")
+        let serial_devices = add_serial_devices(
+            ProtectionType::Unprotected,
+            &io_bus,
+            (4, &evt1_3),
+            (3, &evt2_4),
+            &serial_parameters,
+            None,
+            #[cfg(feature = "swap")]
+            &mut None,
+        )
+        .unwrap();
+        get_serial_cmdline(&mut cmdline, &serial_parameters, "io", &serial_devices)
             .expect("get_serial_cmdline failed");
 
         let cmdline_str = cmdline.as_str();
@@ -280,6 +327,9 @@ mod tests {
     fn get_serial_cmdline_virtio_console_serial_earlycon() {
         let mut cmdline = Cmdline::new(4096);
         let mut serial_parameters = BTreeMap::new();
+        let io_bus = Bus::new(BusType::Io);
+        let evt1_3 = Event::new().unwrap();
+        let evt2_4 = Event::new().unwrap();
 
         // Add a virtio-console device with console=true.
         serial_parameters.insert(
@@ -320,7 +370,18 @@ mod tests {
         );
 
         set_default_serial_parameters(&mut serial_parameters, false);
-        get_serial_cmdline(&mut cmdline, &serial_parameters, "io")
+        let serial_devices = add_serial_devices(
+            ProtectionType::Unprotected,
+            &io_bus,
+            (4, &evt1_3),
+            (3, &evt2_4),
+            &serial_parameters,
+            None,
+            #[cfg(feature = "swap")]
+            &mut None,
+        )
+        .unwrap();
+        get_serial_cmdline(&mut cmdline, &serial_parameters, "io", &serial_devices)
             .expect("get_serial_cmdline failed");
 
         let cmdline_str = cmdline.as_str();
@@ -332,6 +393,9 @@ mod tests {
     fn get_serial_cmdline_virtio_console_invalid_earlycon() {
         let mut cmdline = Cmdline::new(4096);
         let mut serial_parameters = BTreeMap::new();
+        let io_bus = Bus::new(BusType::Io);
+        let evt1_3 = Event::new().unwrap();
+        let evt2_4 = Event::new().unwrap();
 
         // Try to add a virtio-console device with earlycon=true (unsupported).
         serial_parameters.insert(
@@ -353,7 +417,18 @@ mod tests {
         );
 
         set_default_serial_parameters(&mut serial_parameters, false);
-        get_serial_cmdline(&mut cmdline, &serial_parameters, "io")
+        let serial_devices = add_serial_devices(
+            ProtectionType::Unprotected,
+            &io_bus,
+            (4, &evt1_3),
+            (3, &evt2_4),
+            &serial_parameters,
+            None,
+            #[cfg(feature = "swap")]
+            &mut None,
+        )
+        .unwrap();
+        get_serial_cmdline(&mut cmdline, &serial_parameters, "io", &serial_devices)
             .expect_err("get_serial_cmdline succeeded");
     }
 }

commit 5b1089f652f0185d10582898788fe6a2c30e5596

riscv64: fix get_serial_cmdline() parameter type
The get_serial_cmdline() function expects the serial devices as a slice,
not a Vec.

The riscv64 version isn't compile-tested in upstream crosvm, so this was
missed as part of the original review.

BUG=b:332595867

Fixes: 2dde104c1e9f ("arch: return created devices from add_serial_devices()")
Change-Id: I75536649d56e075cfbb87ee775442932fd62b0c3
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5416573
Auto-Submit: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
Commit-Queue: Junichi Uekawa <uekawa@chromium.org>

riscv64/src/lib.rs
@@ -285,7 +285,7 @@ impl arch::LinuxArch for Riscv64 {
             .insert(pci_bus, RISCV64_PCI_CFG_BASE, RISCV64_PCI_CFG_SIZE)
             .map_err(Error::RegisterPci)?;
 
-        get_serial_cmdline(&mut cmdline, serial_parameters, "mmio", serial_devices)
+        get_serial_cmdline(&mut cmdline, serial_parameters, "mmio", &serial_devices)
             .map_err(Error::GetSerialCmdline)?;
         for param in components.extra_kernel_params {
             cmdline.insert_str(&param).map_err(Error::Cmdline)?;