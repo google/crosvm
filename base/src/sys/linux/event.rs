1503bd56406b288b6cae8f597e3be3132e57ac02
Frederick Mayle <fmayle@google.com>
2024-04-25 16:26:38 -0700

    base: linux: event: handle EINTR and short reads/writes
    I haven't seen these happen in practice, but the code was technically
    wrong, as pointed out in
    https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5507771/comment/5f1d7aff_5419cde5/
    
    BUG=b:338274203
    
    Change-Id: I3449abe47fb4679c37e0eb3f41bca8c32546b4d0
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5537837
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
    Commit-Queue: Frederick Mayle <fmayle@google.com>

--- base/src/sys/linux/event.rs
+++ base/src/sys/linux/event.rs
@@ -15,12 +15,14 @@ use serde::Deserialize;
 use serde::Serialize;
 
 use super::errno_result;
+use super::Error;
 use super::RawDescriptor;
 use super::Result;
 use crate::descriptor::AsRawDescriptor;
 use crate::descriptor::FromRawDescriptor;
 use crate::descriptor::IntoRawDescriptor;
 use crate::descriptor::SafeDescriptor;
+use crate::handle_eintr_errno;
 use crate::unix::duration_to_timespec;
 use crate::EventWaitResult;
 
@@ -75,15 +77,18 @@ impl PlatformEvent {
         // SAFETY:
         // This is safe because we made this fd and the pointer we pass can not overflow because we
         // give the syscall's size parameter properly.
-        let ret = unsafe {
+        let ret = handle_eintr_errno!(unsafe {
             write(
                 self.as_raw_descriptor(),
                 &v as *const u64 as *const c_void,
                 mem::size_of::<u64>(),
             )
-        };
-        if ret <= 0 {
+        });
+        if ret < 0 {
             return errno_result();
+        }
+        if ret as usize != mem::size_of::<u64>() {
+            return Err(Error::new(libc::EIO));
         }
         Ok(())
     }
@@ -94,15 +99,18 @@ impl PlatformEvent {
         // SAFETY:
         // This is safe because we made this fd and the pointer we pass can not overflow because
         // we give the syscall's size parameter properly.
-        let ret = unsafe {
+        let ret = handle_eintr_errno!(unsafe {
             read(
                 self.as_raw_descriptor(),
                 &mut buf as *mut u64 as *mut c_void,
                 mem::size_of::<u64>(),
             )
-        };
-        if ret <= 0 {
+        });
+        if ret < 0 {
             return errno_result();
+        }
+        if ret as usize != mem::size_of::<u64>() {
+            return Err(Error::new(libc::EIO));
         }
         Ok(buf)
     }
