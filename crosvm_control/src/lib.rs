google/crosvm

commit df8168ad254640aad7c5a3900e97814c0fde465e

Remove unused pub re-exports
Fixes new clippy warnings about dead code.

BUG=b:344974550
TEST=tools/clippy

Change-Id: If5dc935542db3f39e918845b5864c46dad4a902c
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5604661
Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

vm_control/src/client.rs
@@ -18,7 +18,6 @@ pub use crate::gpu::*;
 pub use crate::sys::handle_request;
 #[cfg(any(target_os = "android", target_os = "linux"))]
 pub use crate::sys::handle_request_with_timeout;
-pub use crate::*;
 
 #[sorted]
 #[derive(Error, Debug)]

base/src/sys/linux/mod.rs
@@ -60,7 +60,6 @@ use std::time::Duration;
 
 pub use acpi_event::*;
 pub use capabilities::drop_capabilities;
-pub use descriptor::*;
 pub use event::EventExt;
 pub(crate) use event::PlatformEvent;
 pub use file::find_next_data;
@@ -97,7 +96,6 @@ pub use signal::*;
 pub use signalfd::Error as SignalFdError;
 pub use signalfd::*;
 pub use terminal::*;
-pub use timer::*;
 pub(crate) use write_zeroes::file_punch_hole;
 pub(crate) use write_zeroes::file_write_zeroes_at;
 

devices/src/pci/mod.rs
@@ -27,10 +27,7 @@ use libc::EINVAL;
 use serde::Deserialize;
 use serde::Serialize;
 
-pub use self::acpi::DeviceVcfgRegister;
-pub use self::acpi::DsmMethod;
 pub use self::acpi::GpeScope;
-pub use self::acpi::PowerResourceMethod;
 #[cfg(any(target_os = "android", target_os = "linux"))]
 pub use self::coiommu::CoIommuDev;
 #[cfg(any(target_os = "android", target_os = "linux"))]
@@ -63,7 +60,6 @@ pub use self::pci_configuration::PciSerialBusSubClass;
 pub use self::pci_configuration::PciSimpleCommunicationControllerSubclass;
 pub use self::pci_configuration::PciSubclass;
 pub use self::pci_configuration::PciWirelessControllerSubclass;
-pub use self::pci_configuration::CAPABILITY_LIST_HEAD_OFFSET;
 pub use self::pci_device::BarRange;
 pub use self::pci_device::Error as PciDeviceError;
 pub use self::pci_device::PciBus;

devices/src/virtio/video/protocol.rs
@@ -32,11 +32,6 @@ use zerocopy::AsBytes;
 use zerocopy::FromBytes;
 use zerocopy::FromZeroes;
 
-pub use crate::virtio::device_constants::video::virtio_video_config;
-pub use crate::virtio::device_constants::video::VIRTIO_VIDEO_F_RESOURCE_GUEST_PAGES;
-pub use crate::virtio::device_constants::video::VIRTIO_VIDEO_F_RESOURCE_NON_CONTIG;
-pub use crate::virtio::device_constants::video::VIRTIO_VIDEO_F_RESOURCE_VIRTIO_OBJECT;
-
 pub const VIRTIO_VIDEO_MAX_PLANES: u32 = 8;
 pub const VIRTIO_VIDEO_FORMAT_RAW_MIN: virtio_video_format = 1;
 pub const VIRTIO_VIDEO_FORMAT_ARGB8888: virtio_video_format = 1;

rutabaga_gfx/src/rutabaga_os/descriptor.rs
@@ -35,15 +35,6 @@ pub trait AsRawDescriptor {
     fn as_raw_descriptor(&self) -> RawDescriptor;
 }
 
-/// A trait similar to `AsRawDescriptor` but supports an arbitrary number of descriptors.
-pub trait AsRawDescriptors {
-    /// Returns the underlying raw descriptors.
-    ///
-    /// Please refer to the documentation of [`AsRawDescriptor::as_raw_descriptor`] for limitations
-    /// and recommended use.
-    fn as_raw_descriptors(&self) -> Vec<RawDescriptor>;
-}
-
 pub trait FromRawDescriptor {
     /// # Safety
     /// Safe only if the caller ensures nothing has access to the descriptor after passing it to
@@ -54,15 +45,6 @@ pub trait FromRawDescriptor {
 impl AsRawDescriptor for SafeDescriptor {
     fn as_raw_descriptor(&self) -> RawDescriptor {
         self.descriptor
-    }
-}
-
-impl<T> AsRawDescriptors for T
-where
-    T: AsRawDescriptor,
-{
-    fn as_raw_descriptors(&self) -> Vec<RawDescriptor> {
-        vec![self.as_raw_descriptor()]
     }
 }
 

rutabaga_gfx/src/rutabaga_os/mod.rs
@@ -8,7 +8,6 @@ mod shm;
 pub mod sys;
 
 pub use descriptor::AsRawDescriptor;
-pub use descriptor::AsRawDescriptors;
 pub use descriptor::FromRawDescriptor;
 pub use descriptor::IntoRawDescriptor;
 pub use descriptor::SafeDescriptor;

rutabaga_gfx/src/rutabaga_os/sys/linux/mod.rs
@@ -7,5 +7,4 @@ pub mod memory_mapping;
 pub mod shm;
 
 pub use memory_mapping::MemoryMapping;
-pub use shm::round_up_to_page_size;
 pub use shm::SharedMemory;

commit 041266d12a110be7e37340c0f021e861968f1a0d

vm_control: fix client build without gpu feature
The vm_control client depends on crate imports that were implicitly
getting pulled in by a wildcard pub re-export. Remove the wildcard
import and import only the required types.

BUG=b:344974550
TEST=cargo build -p vm_control
TEST=cargo build -p vm_control --features=gpu
TEST=tools/clippy

Fixes: df8168ad2546 ("Remove unused pub re-exports")
Change-Id: I83461203482d0174b8ca85c1cb1aadeb9afd2097
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5623434
Reviewed-by: Dennis Kempin <denniskempin@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

crosvm_control/src/lib.rs
@@ -25,13 +25,24 @@ use std::time::Duration;
 use libc::c_char;
 use libc::ssize_t;
 pub use swap::SwapStatus;
-use vm_control::client::*;
+use vm_control::client::do_modify_battery;
+use vm_control::client::do_net_add;
+use vm_control::client::do_net_remove;
+use vm_control::client::do_security_key_attach;
+use vm_control::client::do_usb_attach;
+use vm_control::client::do_usb_detach;
+use vm_control::client::do_usb_list;
+use vm_control::client::handle_request;
+#[cfg(any(target_os = "android", target_os = "linux"))]
+use vm_control::client::handle_request_with_timeout;
+use vm_control::client::vms_request;
 use vm_control::BalloonControlCommand;
 use vm_control::BalloonStats;
 use vm_control::BalloonWS;
 use vm_control::DiskControlCommand;
 #[cfg(feature = "registered_events")]
 use vm_control::RegisteredEvent;
+use vm_control::SwapCommand;
 use vm_control::UsbControlAttachedDevice;
 use vm_control::UsbControlResult;
 use vm_control::VmRequest;

vm_control/src/client.rs
@@ -14,10 +14,27 @@ use remain::sorted;
 use thiserror::Error;
 
 #[cfg(feature = "gpu")]
-pub use crate::gpu::*;
+pub use crate::gpu::do_gpu_display_add;
+#[cfg(feature = "gpu")]
+pub use crate::gpu::do_gpu_display_list;
+#[cfg(feature = "gpu")]
+pub use crate::gpu::do_gpu_display_remove;
+#[cfg(feature = "gpu")]
+pub use crate::gpu::do_gpu_set_display_mouse_mode;
+#[cfg(feature = "gpu")]
+pub use crate::gpu::ModifyGpuResult;
 pub use crate::sys::handle_request;
 #[cfg(any(target_os = "android", target_os = "linux"))]
 pub use crate::sys::handle_request_with_timeout;
+use crate::BatControlCommand;
+use crate::BatControlResult;
+use crate::BatteryType;
+use crate::SwapCommand;
+use crate::UsbControlCommand;
+use crate::UsbControlResult;
+use crate::VmRequest;
+use crate::VmResponse;
+use crate::USB_CONTROL_MAX_PORTS;
 
 #[sorted]
 #[derive(Error, Debug)]
@@ -63,7 +80,8 @@ pub fn do_net_add<T: AsRef<Path> + std::fmt::Debug>(
     tap_name: &str,
     socket_path: T,
 ) -> AnyHowResult<u8> {
-    let request = VmRequest::HotPlugNetCommand(NetControlCommand::AddTap(tap_name.to_owned()));
+    let request =
+        VmRequest::HotPlugNetCommand(crate::NetControlCommand::AddTap(tap_name.to_owned()));
     let response = handle_request(&request, socket_path).map_err(|()| anyhow!("socket error: "))?;
     match response {
         VmResponse::PciHotPlugResponse { bus } => Ok(bus),
@@ -77,7 +95,7 @@ pub fn do_net_add<T: AsRef<Path> + std::fmt::Debug>(
     _tap_name: &str,
     _socket_path: T,
 ) -> AnyHowResult<u8> {
-    bail!("Unsupported: pci-hotplug feature disabled");
+    anyhow::bail!("Unsupported: pci-hotplug feature disabled");
 }
 
 #[cfg(feature = "pci-hotplug")]
@@ -86,7 +104,7 @@ pub fn do_net_remove<T: AsRef<Path> + std::fmt::Debug>(
     bus_num: u8,
     socket_path: T,
 ) -> AnyHowResult<()> {
-    let request = VmRequest::HotPlugNetCommand(NetControlCommand::RemoveTap(bus_num));
+    let request = VmRequest::HotPlugNetCommand(crate::NetControlCommand::RemoveTap(bus_num));
     let response = handle_request(&request, socket_path).map_err(|()| anyhow!("socket error: "))?;
     match response {
         VmResponse::Ok => Ok(()),
@@ -100,7 +118,7 @@ pub fn do_net_remove<T: AsRef<Path> + std::fmt::Debug>(
     _bus_num: u8,
     _socket_path: T,
 ) -> AnyHowResult<()> {
-    bail!("Unsupported: pci-hotplug feature disabled");
+    anyhow::bail!("Unsupported: pci-hotplug feature disabled");
 }
 
 pub fn do_usb_attach<T: AsRef<Path> + std::fmt::Debug>(