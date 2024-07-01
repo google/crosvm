google/crosvm

commit c2bad2ed99d16908503b6fc9578cfc37561ed832

Implement virtio-pvclock for arm64
BUG=b:333815324
TEST=tast run arc.Suspend.* (with https://crrev.com/c/5529202)

Co-authored-by: Suleiman Souhlal <suleiman@google.com>
Change-Id: I991192b42419d5aef488ffc625eb28ef4931124c
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5487336
Commit-Queue: Tatsuyuki Ishi <ishitatsuyuki@google.com>
Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>

devices/src/virtio/pvclock.rs
@@ -43,7 +43,8 @@
 //! However, it doesn't address the difference between CLOCK_BOOTTIME and CLOCK_MONOTONIC related
 //! to host's suspend/resume, as it is designed to maintain the CLOCK_REALTIME in sync mainly.
 
-use std::arch::x86_64::_rdtsc;
+#[cfg(target_arch = "aarch64")]
+use std::arch::asm;
 use std::collections::BTreeMap;
 use std::mem::replace;
 use std::mem::size_of;
@@ -109,6 +110,25 @@ const VIRTIO_PVCLOCK_S_OK: u8 = 0;
 const VIRTIO_PVCLOCK_S_IOERR: u8 = 1;
 
 const VIRTIO_PVCLOCK_CLOCKSOURCE_RATING: u32 = 450;
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+fn read_clock_counter() -> u64 {
+    // SAFETY: rdtsc is unprivileged and have no side effects.
+    unsafe { std::arch::x86_64::_rdtsc() }
+}
+
+#[cfg(target_arch = "aarch64")]
+fn read_clock_counter() -> u64 {
+    let mut x: u64;
+    // SAFETY: This instruction have no side effect apart from storing the current timestamp counter
+    //         into the specified register.
+    unsafe {
+        asm!("mrs {x}, cntvct_el0",
+            x = out(reg) x,
+        );
+    }
+    x
+}
 
 // The config structure being exposed to the guest to tell them how much suspend time should be
 // injected to the guest's CLOCK_BOOTTIME.
@@ -200,7 +220,7 @@ impl PvclockSharedData {
 
     pub fn set_tsc_frequency(&mut self, frequency: u64) -> Result<()> {
         // TSC values are converted to timestamps using the following algorithm:
-        //   delta = _rdtsc() - tsc_suspended_delta
+        //   delta = rdtsc() - tsc_suspended_delta
         //   if tsc_frequency_shift > 0:
         //     delta <<= tsc_frequency_shift
         //   else:
@@ -496,14 +516,11 @@ impl PvClockWorker {
         }
         self.suspend_time = Some(PvclockInstant {
             time: Utc::now(),
-            // SAFETY:
-            // Safe because _rdtsc takes no arguments, and we trust _rdtsc to not modify any other
-            // memory.
-            tsc_value: unsafe { _rdtsc() },
+            tsc_value: read_clock_counter(),
         });
     }
 
-    pub fn resume(&mut self) -> Result<()> {
+    pub fn resume(&mut self) -> Result<u64> {
         // First, increment the sequence lock by 1 before writing to the pvclock page.
         self.increment_pvclock_seqlock()?;
 
@@ -516,9 +533,9 @@ impl PvClockWorker {
         // the bit, the guest will unset it once the guest has handled the stoppage.
         // We get the result here because we want to call increment_pvclock_seqlock regardless of
         // the result of these calls.
-        let result = self
-            .set_suspended_time()
-            .and_then(|_| self.set_guest_stopped_bit());
+        let total_suspended_ticks = self.set_suspended_time()?;
+
+        self.set_guest_stopped_bit()?;
 
         // The guest makes sure there are memory barriers in between reads of the seqlock and other
         // fields, we should make sure there are memory barriers in between writes of seqlock and
@@ -528,7 +545,7 @@ impl PvClockWorker {
         // Do a final increment once changes are done.
         self.increment_pvclock_seqlock()?;
 
-        result
+        Ok(total_suspended_ticks)
     }
 
     fn get_suspended_duration(suspend_time: &PvclockInstant) -> Duration {
@@ -546,18 +563,15 @@ impl PvClockWorker {
         }
     }
 
-    fn set_suspended_time(&mut self) -> Result<()> {
+    fn set_suspended_time(&mut self) -> Result<u64> {
         let (this_suspend_duration, this_suspend_tsc_delta) =
             if let Some(suspend_time) = self.suspend_time.take() {
                 (
                     Self::get_suspended_duration(&suspend_time),
-                    // SAFETY:
-                    // Safe because _rdtsc takes no arguments, and we trust _rdtsc to not modify
-                    // any other memory.
                     // NB: This calculation may wrap around, as TSC can be reset to zero when
                     // the device has resumed from the "deep" suspend state (it may not happen for
                     // s2idle cases). It also happens when the tsc value itself wraps.
-                    unsafe { _rdtsc() }.wrapping_sub(suspend_time.tsc_value),
+                    read_clock_counter().wrapping_sub(suspend_time.tsc_value),
                 )
             } else {
                 return Err(Error::new(libc::ENOTSUP))
@@ -587,7 +601,7 @@ impl PvClockWorker {
         self.total_injected_ns
             .fetch_add(this_suspend_duration.as_nanos() as u64, Ordering::SeqCst);
 
-        Ok(())
+        Ok(self.total_suspend_tsc_delta)
     }
 
     fn increment_pvclock_seqlock(&mut self) -> Result<()> {
@@ -823,13 +837,20 @@ fn run_main_worker(
                             PvClockCommandResponse::Ok
                         }
                         PvClockCommand::Resume => {
-                            if let Err(e) = worker.resume() {
-                                error!("Failed to resume pvclock: {:#}", e);
-                                PvClockCommandResponse::Err(pvclock_response_error_from_anyhow(e))
-                            } else {
-                                // signal to the driver that the total_suspend_ns has changed
-                                interrupt.signal_config_changed();
-                                PvClockCommandResponse::Ok
+                            match worker.resume() {
+                                Ok(total_suspended_ticks) => {
+                                    // signal to the driver that the total_suspend_ns has changed
+                                    interrupt.signal_config_changed();
+                                    PvClockCommandResponse::Resumed {
+                                        total_suspended_ticks,
+                                    }
+                                }
+                                Err(e) => {
+                                    error!("Failed to resume pvclock: {:#}", e);
+                                    PvClockCommandResponse::Err(pvclock_response_error_from_anyhow(
+                                        e,
+                                    ))
+                                }
                             }
                         }
                     };

Cargo.toml
@@ -157,7 +157,7 @@ net = ["devices/net"]
 ## Enables PCI hotplug. Only available on Linux, and currently only for x86/x86-64.
 pci-hotplug = ["devices/pci-hotplug", "vm_control/pci-hotplug"]
 
-## Enables virtio-pvclock. Only available on Linux, and currently only for x86/x86-64.
+## Enables virtio-pvclock. Only available on Linux, and currently only for x86/x86-64 and aarch64.
 pvclock = ["devices/pvclock"]
 
 ## Enables the use of the qcow format for block devices.
@@ -349,6 +349,7 @@ all-default = [
     "noncoherent-dma",
     "pci-hotplug",
     "power-monitor-powerd",
+    "pvclock",
     "registered_events",
     "slirp",
     "swap",
@@ -382,7 +383,6 @@ all-x86_64 = [
     "android_display",
     "android_display_stub",
     "plugin",
-    "pvclock",
     "scudo"
 ]
 

hypervisor/src/aarch64.rs
@@ -182,6 +182,12 @@ pub trait VmAArch64: Vm {
         fdt_address: GuestAddress,
         fdt_size: usize,
     ) -> Result<()>;
+
+    /// Set an offset that describes a number of counter cycles that are subtracted from both
+    /// virtual and physical counter views.
+    fn set_counter_offset(&self, _offset: u64) -> Result<()> {
+        Err(Error::new(libc::ENOSYS))
+    }
 }
 
 /// A wrapper around creating and using a VCPU on aarch64.

hypervisor/src/kvm/aarch64.rs
@@ -221,6 +221,19 @@ impl VmAArch64 for KvmVm {
     ) -> Result<()> {
         Ok(())
     }
+
+    fn set_counter_offset(&self, offset: u64) -> Result<()> {
+        let off = kvm_arm_counter_offset {
+            counter_offset: offset,
+            reserved: 0,
+        };
+        // SAFETY: self.vm is a valid KVM fd
+        let ret = unsafe { ioctl_with_ref(&self.vm, KVM_ARM_SET_COUNTER_OFFSET(), &off) };
+        if ret != 0 {
+            return errno_result();
+        }
+        Ok(())
+    }
 }
 
 impl KvmVcpu {

jail/seccomp/aarch64/pvclock_device.policy
@@ -1,0 +1,8 @@
+# Copyright 2024 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+@include /usr/share/policy/crosvm/common_device.policy
+
+openat: return ENOENT
+prctl: arg0 == PR_SET_NAME

kvm_sys/src/lib.rs
@@ -74,6 +74,12 @@ pub mod aarch64 {
     ioctl_iow_nr!(KVM_ARM_SET_DEVICE_ADDR, KVMIO, 0xab, kvm_arm_device_addr);
     ioctl_iow_nr!(KVM_ARM_VCPU_INIT, KVMIO, 0xae, kvm_vcpu_init);
     ioctl_ior_nr!(KVM_ARM_PREFERRED_TARGET, KVMIO, 0xaf, kvm_vcpu_init);
+    ioctl_iow_nr!(
+        KVM_ARM_SET_COUNTER_OFFSET,
+        KVMIO,
+        0xb5,
+        kvm_arm_counter_offset
+    );
 }
 #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
 pub use crate::aarch64::*;

src/crosvm/sys/linux.rs
@@ -17,6 +17,8 @@ pub(crate) mod pci_hotplug_helpers;
 pub(crate) mod pci_hotplug_manager;
 mod vcpu;
 
+#[cfg(all(feature = "pvclock", target_arch = "aarch64"))]
+use std::arch::asm;
 use std::cmp::max;
 use std::collections::BTreeMap;
 use std::collections::BTreeSet;
@@ -75,7 +77,7 @@ use cros_async::Executor;
 use device_helpers::*;
 use devices::create_devices_worker_thread;
 use devices::serial_device::SerialHardware;
-#[cfg(feature = "pvclock")]
+#[cfg(all(feature = "pvclock", target_arch = "x86_64"))]
 use devices::tsc::get_tsc_sync_mitigations;
 use devices::vfio::VfioCommonSetup;
 use devices::vfio::VfioCommonTrait;
@@ -215,7 +217,7 @@ static GUNYAH_PATH: &str = "/dev/gunyah";
 
 fn create_virtio_devices(
     cfg: &Config,
-    vm: &mut impl Vm,
+    vm: &mut impl VmArch,
     resources: &mut SystemAllocator,
     #[cfg_attr(not(feature = "gpu"), allow(unused_variables))] vm_evt_wrtube: &SendTube,
     #[cfg(feature = "balloon")] balloon_device_tube: Option<Tube>,
@@ -407,22 +409,45 @@ fn create_virtio_devices(
 
     #[cfg(feature = "pvclock")]
     if let Some(suspend_tube) = pvclock_device_tube {
-        let tsc_state = devices::tsc::tsc_state()?;
-        let tsc_sync_mitigations =
-            get_tsc_sync_mitigations(&tsc_state, cfg.vcpu_count.unwrap_or(1));
-        if tsc_state.core_grouping.size() > 1 {
-            // Host TSCs are not in sync. Log what mitigations are applied.
-            warn!(
-                "Host TSCs are not in sync, applying the following mitigations: {:?}",
-                tsc_sync_mitigations
-            );
+        let frequency: u64;
+        #[cfg(target_arch = "x86_64")]
+        {
+            let tsc_state = devices::tsc::tsc_state()?;
+            let tsc_sync_mitigations =
+                get_tsc_sync_mitigations(&tsc_state, cfg.vcpu_count.unwrap_or(1));
+            if tsc_state.core_grouping.size() > 1 {
+                // Host TSCs are not in sync. Log what mitigations are applied.
+                warn!(
+                    "Host TSCs are not in sync, applying the following mitigations: {:?}",
+                    tsc_sync_mitigations
+                );
+            }
+            frequency = tsc_state.frequency;
         }
-        devs.push(create_pvclock_device(
+        #[cfg(target_arch = "aarch64")]
+        {
+            let mut x: u64;
+            // SAFETY: This instruction have no side effect apart from storing the current timestamp
+            //         frequency into the specified register.
+            unsafe {
+                asm!("mrs {x}, cntfrq_el0",
+                    x = out(reg) x,
+                );
+            }
+            frequency = x;
+
+            // If unset, KVM defaults to an offset that is calculated from VM boot time. Explicitly
+            // set it to zero on boot. When updating the offset, we always set it to the total
+            // amount of time the VM has been suspended.
+            vm.set_counter_offset(0)?;
+        }
+        let dev = create_pvclock_device(
             cfg.protection_type,
             &cfg.jail_config,
-            tsc_state.frequency,
+            frequency,
             suspend_tube,
-        )?);
+        )?;
+        devs.push(dev);
         info!("virtio-pvclock is enabled for this vm");
     }
 
@@ -720,7 +745,7 @@ fn create_virtio_devices(
 
 fn create_devices(
     cfg: &Config,
-    vm: &mut impl Vm,
+    vm: &mut impl VmArch,
     resources: &mut SystemAllocator,
     vm_evt_wrtube: &SendTube,
     iommu_attached_endpoints: &mut BTreeMap<u32, Arc<Mutex<Box<dyn MemoryMapperTrait>>>>,
@@ -2729,21 +2754,47 @@ pub fn trigger_vm_suspend_and_wait_for_entry(
 }
 
 #[cfg(feature = "pvclock")]
-fn send_pvclock_cmd(tube: &Tube, command: PvClockCommand) -> Result<()> {
+/// The action requested by the pvclock device to perform on the main thread.
+enum PvClockAction {
+    #[cfg(target_arch = "aarch64")]
+    /// Update the counter offset with VmAarch64::set_counter_offset.
+    SetCounterOffset(u64),
+}
+
+#[cfg(feature = "pvclock")]
+fn send_pvclock_cmd(tube: &Tube, command: PvClockCommand) -> Result<Option<PvClockAction>> {
     tube.send(&command)
         .with_context(|| format!("failed to send pvclock command {:?}", command))?;
     let resp = tube
         .recv::<PvClockCommandResponse>()
         .context("failed to receive pvclock command response")?;
-    if let PvClockCommandResponse::Err(e) = resp {
-        bail!("pvclock encountered error on {:?}: {}", command, e);
+    match resp {
+        PvClockCommandResponse::Err(e) => {
+            bail!("pvclock encountered error on {:?}: {}", command, e);
+        }
+        PvClockCommandResponse::DeviceInactive => {
+            warn!("Tried to send {command:?} but pvclock device was inactive");
+            Ok(None)
+        }
+        PvClockCommandResponse::Resumed {
+            total_suspended_ticks,
+        } => {
+            info!("{command:?} completed with {total_suspended_ticks} total_suspended_ticks");
+            cfg_if::cfg_if! {
+                if #[cfg(target_arch = "aarch64")] {
+                    Ok(Some(PvClockAction::SetCounterOffset(total_suspended_ticks)))
+                } else {
+                    // For non-AArch64 platforms this is handled by directly updating the offset in
+                    // shared memory in the pvclock device worker.
+                    Ok(None)
+                }
+            }
+        }
+        PvClockCommandResponse::Ok => {
+            info!("{command:?} completed with {resp:?}");
+            Ok(None)
+        }
     }
-    if let PvClockCommandResponse::DeviceInactive = resp {
-        warn!("Tried to send {command:?} but pvclock device was inactive");
-    } else {
-        info!("{command:?} completed with {resp:?}");
-    }
-    Ok(())
 }
 
 #[cfg(target_arch = "x86_64")]
@@ -3013,11 +3064,20 @@ fn process_vm_request<V: VmArch + 'static, Vcpu: VcpuArch + 'static>(
                         _ => None,
                     };
                     if let Some(cmd) = cmd {
-                        if let Err(e) = send_pvclock_cmd(pvclock_host_tube, cmd.clone()) {
-                            error!("{:?} command failed: {:#}", cmd, e);
-                        } else {
-                            info!("{:?} command successfully processed", cmd);
-                        }
+                        match send_pvclock_cmd(pvclock_host_tube, cmd.clone()) {
+                            Ok(action) => {
+                                info!("{:?} command successfully processed", cmd);
+                                if let Some(action) = action {
+                                    match action {
+                                        #[cfg(target_arch = "aarch64")]
+                                        PvClockAction::SetCounterOffset(offset) => {
+                                            state.linux.vm.set_counter_offset(offset)?;
+                                        }
+                                    }
+                                }
+                            }
+                            Err(e) => error!("{:?} command failed: {:#}", cmd, e),
+                        };
                     }
                 }
             }

vm_control/src/lib.rs
@@ -1264,6 +1264,7 @@ pub enum PvClockCommand {
 #[derive(Serialize, Deserialize, Debug)]
 pub enum PvClockCommandResponse {
     Ok,
+    Resumed { total_suspended_ticks: u64 },
     DeviceInactive,
     Err(SysError),
 }

commit acb9ecb2133945f5014c60c7e2fde6033937f874

pvclock: Fix corrupt seqlock state with non-matching resume
c2bad2ed99d (Implement virtio-pvclock for arm64) introduced a bug where
an error in self.set_suspended_time() would return while the seqlock is
"locked", i.e. having an odd value. I did not realize set_suspended_time
can return error under normal operation, when crosvm's resume command
was called twice. When this happened, the guest gets stuck as it's
unable to exit from the reading loop.

Fix this by making this use an and_then chain and returning the error at
the end. We are flipping the order between setting the stopped bit and
setting the suspend time to keep the control flow simple. This should be
fine as the guest read both variables together under a seqlock critical
section.

BUG=b:339546532
TEST=CQ

Change-Id: Ib492857bcb5fe23f1af64640890dd641cd1312f6
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5666277
Reviewed-by: Keiichi Watanabe <keiichiw@chromium.org>
Commit-Queue: Tatsuyuki Ishi <ishitatsuyuki@google.com>

devices/src/virtio/pvclock.rs
@@ -563,13 +563,13 @@ impl PvClockWorker {
         // writes to other fields.
         std::sync::atomic::fence(Ordering::SeqCst);
 
-        // Set the tsc suspended delta and guest_stopped_bit in pvclock struct. We only need to set
+        // Set the guest_stopped_bit and tsc suspended delta in pvclock struct. We only need to set
         // the bit, the guest will unset it once the guest has handled the stoppage.
         // We get the result here because we want to call increment_pvclock_seqlock regardless of
         // the result of these calls.
-        let total_suspended_ticks = self.set_suspended_time()?;
-
-        self.set_guest_stopped_bit()?;
+        let result = self
+            .set_guest_stopped_bit()
+            .and_then(|_| self.set_suspended_time());
 
         // The guest makes sure there are memory barriers in between reads of the seqlock and other
         // fields, we should make sure there are memory barriers in between writes of seqlock and
@@ -579,7 +579,7 @@ impl PvClockWorker {
         // Do a final increment once changes are done.
         self.increment_pvclock_seqlock()?;
 
-        Ok(total_suspended_ticks)
+        result
     }
 
     fn get_suspended_duration(suspend_time: &PvclockInstant) -> Duration {