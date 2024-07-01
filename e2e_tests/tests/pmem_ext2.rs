4690aabd891d92c1aadbe1266af980ac7f9342d4
Keiichi Watanabe <keiichiw@chromium.org>
2024-07-01 17:47:08 +0900

    e2e_tests: Add pmem-ext2 test
    Add e2e test for the pmem-ext2 device that shares a read-only directory containing
    text files, a directory and a symlink.
    
    BUG=b:333033395
    TEST=CQ
    
    Change-Id: Ib2844d87513e6a46b129b4caa8d1c27eb4534cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5666319
    Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
    Commit-Queue: Keiichi Watanabe <keiichiw@chromium.org>
    Reviewed-by: Takaya Saeki <takayas@chromium.org>

---
+++ e2e_tests/tests/pmem_ext2.rs
@@ -0,0 +1,73 @@
+// Copyright 2024 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Testing pmem-ext2 device.
+
+#![cfg(any(target_os = "android", target_os = "linux"))]
+
+use std::os::unix::fs::symlink;
+
+use fixture::vm::Config;
+use fixture::vm::TestVm;
+
+/// Check file contents on pmem-ext2
+#[test]
+fn pmem_ext2() -> anyhow::Result<()> {
+    // /temp_dir/
+    // ├── a.txt
+    // └── dir
+    //     ├── b.txt
+    //     └── symlink_a -> ../a.txt
+
+    const A_TXT_NAME: &str = "a.txt";
+    const A_TXT_DATA: &str = "Hello!";
+    const DIR_NAME: &str = "dir";
+    const B_TXT_NAME: &str = "b.txt";
+    const B_TXT_DATA: &str = "test test test\ntest test test";
+    const SYMLINK_A_NAME: &str = "symlink_a";
+    const SYMLINK_A_DEST: &str = "../a.txt";
+
+    let temp_dir = tempfile::tempdir()?;
+    let a_txt = temp_dir.path().join(A_TXT_NAME);
+    std::fs::write(a_txt, A_TXT_DATA)?;
+    let dir = temp_dir.path().join(DIR_NAME);
+    std::fs::create_dir(&dir)?;
+    let b_txt = dir.join(B_TXT_NAME);
+    std::fs::write(b_txt, B_TXT_DATA)?;
+    let symlink_a = dir.join(SYMLINK_A_NAME);
+    symlink(SYMLINK_A_DEST, symlink_a)?;
+
+    let config = Config::new().extra_args(vec![
+        "--pmem-ext2".to_string(),
+        temp_dir.path().to_str().unwrap().to_string(),
+    ]);
+
+    let mut vm = TestVm::new(config)?;
+    vm.exec_in_guest("mount -t ext2 /dev/pmem0 /mnt/")?;
+
+    let a_result = vm
+        .exec_in_guest_async(&format!("cat /mnt/{A_TXT_NAME}"))?
+        .with_timeout(std::time::Duration::from_secs(1))
+        .wait_ok(&mut vm)?;
+    assert_eq!(a_result.stdout.trim(), A_TXT_DATA);
+    let b_result = vm
+        .exec_in_guest_async(&format!("cat /mnt/{DIR_NAME}/{B_TXT_NAME}"))?
+        .with_timeout(std::time::Duration::from_secs(1))
+        .wait_ok(&mut vm)?;
+    assert_eq!(b_result.stdout.trim(), B_TXT_DATA);
+
+    let readlink_result = vm
+        .exec_in_guest_async(&format!("readlink /mnt/{DIR_NAME}/{SYMLINK_A_NAME}"))?
+        .with_timeout(std::time::Duration::from_secs(1))
+        .wait_ok(&mut vm)?;
+    assert_eq!(readlink_result.stdout.trim(), SYMLINK_A_DEST);
+
+    let symlink_a_result = vm
+        .exec_in_guest_async(&format!("cat /mnt/{DIR_NAME}/{SYMLINK_A_NAME}"))?
+        .with_timeout(std::time::Duration::from_secs(1))
+        .wait_ok(&mut vm)?;
+    assert_eq!(symlink_a_result.stdout.trim(), A_TXT_DATA);
+
+    Ok(())
+}
