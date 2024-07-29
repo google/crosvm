google/crosvm

commit 2fc3047b9b071af4481d216d3453a6398b74436d

hypervisor: x86_64: clarify Segment limit units
The limit value in a kvm_segment is intended to be in bytes, meaning it
should already include the scale factor from the G (granularity) field.
This is basically undocumented, but this is based on observation of the
values assigned to kvm_segment limit fields inside the kernel being
larger than 20 bits.

Rename the field in the generic Segment struct to "limit_bytes" to make
its units clear and match the KVM behavior.

I could not find documentation for either WHPX or HAXM to determine
whether their limit fields should be in units of bytes or not, so those
hypervisors currently perform the conversion to maintain the previous
behavior of using the raw limit field as encoded in the GDT entry.

This mostly does not matter for 64-bit guests, since segment limit
fields are ignored in long mode, but it does matter in 32-bit protected
mode.

BUG=b:354053941

Change-Id: I1e464d3e1b8556184c732adea034b326e5ed6a72
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5727075
Reviewed-by: Noah Gold <nkgold@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

hypervisor/src/whpx/types.rs
@@ -123,9 +123,14 @@ impl From<&Segment> for WHV_X64_SEGMENT_REGISTER {
             segment.db.into(),
             segment.g.into(),
         );
+        let limit = if segment.g == 0 {
+            segment.limit
+        } else {
+            segment.limit / 4096
+        };
         WHV_X64_SEGMENT_REGISTER {
             Base: segment.base,
-            Limit: segment.limit,
+            Limit: limit,
             Selector: segment.selector,
             __bindgen_anon_1: WHV_X64_SEGMENT_REGISTER__bindgen_ty_1 {
                 __bindgen_anon_1: WHV_X64_SEGMENT_REGISTER__bindgen_ty_1__bindgen_ty_1 {
@@ -141,9 +146,14 @@ impl From<&WHV_X64_SEGMENT_REGISTER> for Segment {
     fn from(whpx_segment: &WHV_X64_SEGMENT_REGISTER) -> Self {
         // safe because the union field can always be interpreteted as a bitfield
         let attributes = unsafe { whpx_segment.__bindgen_anon_1.__bindgen_anon_1 };
+        let limit_bytes = if attributes.Granularity() == 0 {
+            whpx_segment.Limit
+        } else {
+            (whpx_segment.Limit * 4096) + 4095
+        };
         Segment {
             base: whpx_segment.Base,
-            limit: whpx_segment.Limit,
+            limit_bytes,
             selector: whpx_segment.Selector,
             type_: attributes.SegmentType() as u8,
             present: attributes.Present() as u8,

hypervisor/src/haxm/vcpu.rs
@@ -768,9 +768,15 @@ impl From<&segment_desc_t> for Segment {
         // TODO(b/315998194): Add safety comment
         #[allow(clippy::undocumented_unsafe_blocks)]
         unsafe {
+            let g = item.__bindgen_anon_1.__bindgen_anon_1.granularity() as u8;
+            let limit_bytes = if g == 0 {
+                item.limit
+            } else {
+                (item.limit * 4096) + 4095
+            };
             Segment {
                 base: item.base,
-                limit: item.limit,
+                limit_bytes,
                 selector: item.selector,
                 type_: item.__bindgen_anon_1.__bindgen_anon_1.type_() as u8,
                 present: item.__bindgen_anon_1.__bindgen_anon_1.present() as u8,
@@ -778,7 +784,7 @@ impl From<&segment_desc_t> for Segment {
                 db: item.__bindgen_anon_1.__bindgen_anon_1.operand_size() as u8,
                 s: item.__bindgen_anon_1.__bindgen_anon_1.desc() as u8,
                 l: item.__bindgen_anon_1.__bindgen_anon_1.long_mode() as u8,
-                g: item.__bindgen_anon_1.__bindgen_anon_1.granularity() as u8,
+                g,
                 avl: item.__bindgen_anon_1.__bindgen_anon_1.available() as u8,
             }
         }
@@ -787,9 +793,14 @@ impl From<&segment_desc_t> for Segment {
 
 impl From<&Segment> for segment_desc_t {
     fn from(item: &Segment) -> Self {
+        let limit = if item.g == 0 {
+            item.limit_bytes
+        } else {
+            item.limit_bytes / 4096
+        };
         let mut segment = segment_desc_t {
             base: item.base,
-            limit: item.limit,
+            limit,
             selector: item.selector,
             ..Default::default()
         };

hypervisor/src/kvm/x86_64.rs
@@ -1598,7 +1598,7 @@ impl From<&kvm_segment> for Segment {
     fn from(s: &kvm_segment) -> Self {
         Segment {
             base: s.base,
-            limit: s.limit,
+            limit_bytes: s.limit,
             selector: s.selector,
             type_: s.type_,
             present: s.present,
@@ -1616,7 +1616,7 @@ impl From<&Segment> for kvm_segment {
     fn from(s: &Segment) -> Self {
         kvm_segment {
             base: s.base,
-            limit: s.limit,
+            limit: s.limit_bytes,
             selector: s.selector,
             type_: s.type_,
             present: s.present,

hypervisor/src/x86_64.rs
@@ -811,7 +811,8 @@ impl Default for Regs {
 #[derive(Debug, Default, Copy, Clone, Serialize, Deserialize)]
 pub struct Segment {
     pub base: u64,
-    pub limit: u32,
+    /// Limit of the segment - always in bytes, regardless of granularity (`g`) field.
+    pub limit_bytes: u32,
     pub selector: u16,
     pub type_: u8,
     pub present: u8,
@@ -871,7 +872,7 @@ impl Default for Sregs {
         // 16-bit real-mode code segment (reset vector).
         let code_seg = Segment {
             base: 0xffff0000,
-            limit: 0xffff,
+            limit_bytes: 0xffff,
             selector: 0xf000,
             type_: SEG_TYPE_CODE | SEG_TYPE_CODE_READABLE | SEG_TYPE_ACCESSED, // 11
             present: 1,
@@ -882,7 +883,7 @@ impl Default for Sregs {
         // 16-bit real-mode data segment.
         let data_seg = Segment {
             base: 0,
-            limit: 0xffff,
+            limit_bytes: 0xffff,
             selector: 0,
             type_: SEG_TYPE_DATA | SEG_TYPE_DATA_WRITABLE | SEG_TYPE_ACCESSED, // 3
             present: 1,
@@ -893,7 +894,7 @@ impl Default for Sregs {
         // 16-bit TSS segment.
         let task_seg = Segment {
             base: 0,
-            limit: 0xffff,
+            limit_bytes: 0xffff,
             selector: 0,
             type_: SEG_TYPE_CODE | SEG_TYPE_CODE_READABLE | SEG_TYPE_ACCESSED, // 11
             present: 1,
@@ -904,7 +905,7 @@ impl Default for Sregs {
         // Local descriptor table.
         let ldt = Segment {
             base: 0,
-            limit: 0xffff,
+            limit_bytes: 0xffff,
             selector: 0,
             type_: SEG_TYPE_DATA | SEG_TYPE_DATA_WRITABLE, // 2
             present: 1,

hypervisor/tests/hypervisor_virtualization.rs
@@ -301,11 +301,15 @@ const IDT_OFFSET: u64 = 0x1528;
 pub fn configure_long_mode_memory(vm: &mut dyn Vm) -> Segment {
     // Condensed version of the function in x86_64\src\gdt.rs
     pub fn segment_from_gdt(entry: u64, table_index: u8) -> Segment {
+        let g = ((entry & 0x0080000000000000) >> 55) as u8;
+        let limit =
+            ((((entry) & 0x000F000000000000) >> 32) | ((entry) & 0x000000000000FFFF)) as u32;
+        let limit_bytes = if g == 0 { limit } else { (limit * 4096) + 4095 };
         Segment {
             base: (((entry) & 0xFF00000000000000) >> 32)
                 | (((entry) & 0x000000FF00000000) >> 16)
                 | (((entry) & 0x00000000FFFF0000) >> 16),
-            limit: ((((entry) & 0x000F000000000000) >> 32) | ((entry) & 0x000000000000FFFF)) as u32,
+            limit_bytes,
             selector: (table_index * 8) as u16,
             type_: ((entry & 0x00000F0000000000) >> 40) as u8,
             present: ((entry & 0x0000800000000000) >> 47) as u8,
@@ -313,7 +317,7 @@ pub fn configure_long_mode_memory(vm: &mut dyn Vm) -> Segment {
             db: ((entry & 0x0040000000000000) >> 54) as u8,
             s: ((entry & 0x0000100000000000) >> 44) as u8,
             l: ((entry & 0x0020000000000000) >> 53) as u8,
-            g: ((entry & 0x0080000000000000) >> 55) as u8,
+            g,
             avl: ((entry & 0x0010000000000000) >> 52) as u8,
         }
     }

x86_64/src/gdt.rs
@@ -64,9 +64,19 @@ fn get_type(entry: u64) -> u8 {
 /// * `entry` - The gdt entry.
 /// * `table_index` - Index of the entry in the gdt table.
 pub fn segment_from_gdt(entry: u64, table_index: u8) -> Segment {
+    let g = get_g(entry);
+    let limit = get_limit(entry);
+    let limit_bytes = if g == 0 {
+        // 1-byte granularity
+        limit
+    } else {
+        // 4096-byte granularity
+        (limit * 4096) + 4095
+    };
+
     Segment {
         base: get_base(entry),
-        limit: get_limit(entry),
+        limit_bytes,
         selector: (table_index * 8) as u16,
         type_: get_type(entry),
         present: get_p(entry),
@@ -74,7 +84,7 @@ pub fn segment_from_gdt(entry: u64, table_index: u8) -> Segment {
         db: get_db(entry),
         s: get_s(entry),
         l: get_l(entry),
-        g: get_g(entry),
+        g,
         avl: get_avl(entry),
     }
 }
@@ -101,6 +111,6 @@ mod test {
         assert_eq!(0xB, seg.type_);
         // base and limit
         assert_eq!(0x100000, seg.base);
-        assert_eq!(0xfffff, seg.limit);
+        assert_eq!(0xffffffff, seg.limit_bytes);
     }
 }

x86_64/src/regs.rs
@@ -308,7 +308,7 @@ mod tests {
         assert_eq!(0x0, read_u64(&gm, BOOT_IDT_OFFSET));
 
         assert_eq!(0, sregs.cs.base);
-        assert_eq!(0xfffff, sregs.ds.limit);
+        assert_eq!(0xffffffff, sregs.ds.limit_bytes);
         assert_eq!(0x10, sregs.cs.selector);
         assert_eq!(0x18, sregs.ds.selector);
         assert_eq!(0x18, sregs.es.selector);
@@ -317,7 +317,7 @@ mod tests {
         assert_eq!(1, sregs.gs.g);
         assert_eq!(0, sregs.ss.avl);
         assert_eq!(0, sregs.tr.base);
-        assert_eq!(0xfffff, sregs.tr.limit);
+        assert_eq!(0xffffffff, sregs.tr.limit_bytes);
         assert_eq!(0, sregs.tr.avl);
         assert_eq!(X86_CR0_PE, sregs.cr0 & X86_CR0_PE);
         assert_eq!(EFER_LME, sregs.efer);

commit 89d641d2a5d7873ca68c2509823d2ccce9a5f64e

hypervisor: whpx: fix segment limits
It seems that WHPX also wants its segment Limit value to be in bytes
(unaffected by the Granularity flag), similar to KVM.

This also fixes a compilation failure (using "limit" rather than
"limit_bytes").

Tested with a 32-bit bzImage kernel on Windows 10 host.

Fixes: 2fc3047b9b07 ("hypervisor: x86_64: clarify Segment limit units")
Change-Id: Ifa013b3687a5febd4d36b3a830c9c6995cc2bdb9
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5743006
Reviewed-by: Noah Gold <nkgold@google.com>
Auto-Submit: Daniel Verkamp <dverkamp@chromium.org>
Commit-Queue: Noah Gold <nkgold@google.com>

hypervisor/src/whpx/types.rs
@@ -123,14 +123,9 @@ impl From<&Segment> for WHV_X64_SEGMENT_REGISTER {
             segment.db.into(),
             segment.g.into(),
         );
-        let limit = if segment.g == 0 {
-            segment.limit
-        } else {
-            segment.limit / 4096
-        };
         WHV_X64_SEGMENT_REGISTER {
             Base: segment.base,
-            Limit: limit,
+            Limit: segment.limit_bytes,
             Selector: segment.selector,
             __bindgen_anon_1: WHV_X64_SEGMENT_REGISTER__bindgen_ty_1 {
                 __bindgen_anon_1: WHV_X64_SEGMENT_REGISTER__bindgen_ty_1__bindgen_ty_1 {
@@ -146,14 +141,9 @@ impl From<&WHV_X64_SEGMENT_REGISTER> for Segment {
     fn from(whpx_segment: &WHV_X64_SEGMENT_REGISTER) -> Self {
         // safe because the union field can always be interpreteted as a bitfield
         let attributes = unsafe { whpx_segment.__bindgen_anon_1.__bindgen_anon_1 };
-        let limit_bytes = if attributes.Granularity() == 0 {
-            whpx_segment.Limit
-        } else {
-            (whpx_segment.Limit * 4096) + 4095
-        };
         Segment {
             base: whpx_segment.Base,
-            limit_bytes,
+            limit_bytes: whpx_segment.Limit,
             selector: whpx_segment.Selector,
             type_: attributes.SegmentType() as u8,
             present: attributes.Present() as u8,