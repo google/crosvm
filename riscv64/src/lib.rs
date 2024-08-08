google/crosvm

commit 49a6c3cf2ddb4cb87d9da1ffefd1ab48ae663a04

aarch64: add cmdline param to control FDT address
On aarch64, we load BIOS payloads at offset 0x200000 because
cuttlefish's u-boot BIOS requires the FDT at offset zero. However, in a
separate use case, we are loading BIOS payloads that are OK with dynamic
FDT addresses, but might use the Gunyah hypervisor, which currently
requires the payload to be loaded at offset zero. One algorithm can't
satisfy both.

Both the u-boot setup and Gunyah ought to be made more flexible, but it
seems useful to make crosvm's behavior configurable. It lets us adjust
those use cases eventually without causing an immediate backwards
incompatability scramble.

A new "after-payload" mode is included that matches crosvm's RISCV FDT
address behavior and potentially helps with the TODO for >4GB VMs.

BUG=b:353758405

Change-Id: I886f47cf8e0a6b02218794615215a0e972a4ace2
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5740018
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
Commit-Queue: Frederick Mayle <fmayle@google.com>

riscv64/src/lib.rs
@@ -196,6 +196,7 @@ impl arch::LinuxArch for Riscv64 {
         #[cfg(feature = "swap")] swap_controller: &mut Option<swap::SwapController>,
         _guest_suspended_cvar: Option<Arc<(Mutex<bool>, Condvar)>>,
         device_tree_overlays: Vec<DtbOverlay>,
+        fdt_position: Option<FdtPosition>,
     ) -> std::result::Result<RunnableLinuxVm<V, Vcpu>, Self::Error>
     where
         V: VmRiscv64,
@@ -366,6 +367,10 @@ impl arch::LinuxArch for Riscv64 {
             })
             .collect();
 
+        assert!(
+            matches!(None | Some(FdtPosition::AfterPayload)),
+            "fdt_position={fdt_position:?} not supported"
+        );
         let fdt_offset = (kernel_initrd_end + (RISCV64_FDT_ALIGN - 1)) & !(RISCV64_FDT_ALIGN - 1);
 
         let timebase_freq: u32 = vcpus[0]

aarch64/src/lib.rs
@@ -16,6 +16,7 @@ use std::sync::Arc;
 use arch::get_serial_cmdline;
 use arch::CpuSet;
 use arch::DtbOverlay;
+use arch::FdtPosition;
 use arch::GetSerialCmdlineError;
 use arch::RunnableLinuxVm;
 use arch::VcpuAffinity;
@@ -86,9 +87,8 @@ use vm_memory::MemoryRegionPurpose;
 
 mod fdt;
 
-// We place the kernel at the very beginning of physical memory.
-const AARCH64_KERNEL_OFFSET: u64 = 0;
 const AARCH64_FDT_MAX_SIZE: u64 = 0x200000;
+const AARCH64_FDT_ALIGN: u64 = 0x200000;
 const AARCH64_INITRD_ALIGN: u64 = 0x1000000;
 
 // Maximum Linux arm64 kernel command line size (arch/arm64/include/uapi/asm/setup.h).
@@ -102,11 +102,6 @@ const AARCH64_GIC_CPUI_SIZE: u64 = 0x20000;
 const AARCH64_PHYS_MEM_START: u64 = 0x80000000;
 const AARCH64_AXI_BASE: u64 = 0x40000000;
 const AARCH64_PLATFORM_MMIO_SIZE: u64 = 0x800000;
-
-// FDT is placed at the front of RAM when booting in BIOS mode.
-const AARCH64_FDT_OFFSET_IN_BIOS_MODE: u64 = 0x0;
-// Therefore, the BIOS is placed after the FDT in memory.
-const AARCH64_BIOS_OFFSET: u64 = AARCH64_FDT_MAX_SIZE;
 
 const AARCH64_PROTECTED_VM_FW_MAX_SIZE: u64 = 0x400000;
 const AARCH64_PROTECTED_VM_FW_START: u64 =
@@ -157,14 +152,6 @@ impl PayloadType {
             Self::Kernel(k) => k.size,
         }
     }
-}
-
-fn get_kernel_addr() -> GuestAddress {
-    GuestAddress(AARCH64_PHYS_MEM_START + AARCH64_KERNEL_OFFSET)
-}
-
-fn get_bios_addr() -> GuestAddress {
-    GuestAddress(AARCH64_PHYS_MEM_START + AARCH64_BIOS_OFFSET)
 }
 
 // When static swiotlb allocation is required, returns the address it should be allocated at.
@@ -334,25 +321,6 @@ pub enum Error {
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-/// Returns the address in guest memory at which the FDT should be located.
-fn fdt_address(memory_end: GuestAddress, has_bios: bool) -> GuestAddress {
-    // TODO(rammuthiah) make kernel and BIOS startup use FDT from the same location. ARCVM startup
-    // currently expects the kernel at 0x80080000 and the FDT at the end of RAM for unknown reasons.
-    // Root cause and figure out how to fold these code paths together.
-    if has_bios {
-        GuestAddress(AARCH64_PHYS_MEM_START + AARCH64_FDT_OFFSET_IN_BIOS_MODE)
-    } else {
-        // Put fdt up near the top of memory
-        // TODO(sonnyrao): will have to handle this differently if there's
-        // > 4GB memory
-        memory_end
-            .checked_sub(AARCH64_FDT_MAX_SIZE)
-            .expect("Not enough memory for FDT")
-            .checked_sub(0x10000)
-            .expect("Not enough memory for FDT")
-    }
-}
-
 fn load_kernel(
     guest_mem: &GuestMemory,
     kernel_start: GuestAddress,
@@ -465,6 +433,7 @@ impl arch::LinuxArch for AArch64 {
         #[cfg(feature = "swap")] swap_controller: &mut Option<swap::SwapController>,
         _guest_suspended_cvar: Option<Arc<(Mutex<bool>, Condvar)>>,
         device_tree_overlays: Vec<DtbOverlay>,
+        fdt_position: Option<FdtPosition>,
     ) -> std::result::Result<RunnableLinuxVm<V, Vcpu>, Self::Error>
     where
         V: VmAArch64,
@@ -473,21 +442,39 @@ impl arch::LinuxArch for AArch64 {
         let has_bios = matches!(components.vm_image, VmImage::Bios(_));
         let mem = vm.get_memory().clone();
 
+        let fdt_position = fdt_position.unwrap_or(if has_bios {
+            FdtPosition::Start
+        } else {
+            FdtPosition::End
+        });
+        let payload_address = match fdt_position {
+            // If FDT is at the start RAM, the payload needs to go somewhere after it.
+            FdtPosition::Start => GuestAddress(AARCH64_PHYS_MEM_START + AARCH64_FDT_MAX_SIZE),
+            // Otherwise, put the payload at the start of RAM.
+            FdtPosition::End | FdtPosition::AfterPayload => GuestAddress(AARCH64_PHYS_MEM_START),
+        };
+
         // separate out image loading from other setup to get a specific error for
         // image loading
         let mut initrd = None;
-        let payload = match components.vm_image {
+        let (payload, payload_end_address) = match components.vm_image {
             VmImage::Bios(ref mut bios) => {
-                let image_size = arch::load_image(&mem, bios, get_bios_addr(), u64::MAX)
+                let image_size = arch::load_image(&mem, bios, payload_address, u64::MAX)
                     .map_err(Error::BiosLoadFailure)?;
-                PayloadType::Bios {
-                    entry: get_bios_addr(),
-                    image_size: image_size as u64,
-                }
+                (
+                    PayloadType::Bios {
+                        entry: payload_address,
+                        image_size: image_size as u64,
+                    },
+                    payload_address
+                        .checked_add(image_size.try_into().unwrap())
+                        .unwrap(),
+                )
             }
             VmImage::Kernel(ref mut kernel_image) => {
-                let loaded_kernel = load_kernel(&mem, get_kernel_addr(), kernel_image)?;
+                let loaded_kernel = load_kernel(&mem, payload_address, kernel_image)?;
                 let kernel_end = loaded_kernel.address_range.end;
+                let mut payload_end = GuestAddress(kernel_end);
                 initrd = match components.initrd_image {
                     Some(initrd_file) => {
                         let mut initrd_file = initrd_file;
@@ -499,16 +486,33 @@ impl arch::LinuxArch for AArch64 {
                         let initrd_size =
                             arch::load_image(&mem, &mut initrd_file, initrd_addr, initrd_max_size)
                                 .map_err(Error::InitrdLoadFailure)?;
+                        payload_end = initrd_addr
+                            .checked_add(initrd_size.try_into().unwrap())
+                            .unwrap();
                         Some((initrd_addr, initrd_size))
                     }
                     None => None,
                 };
-                PayloadType::Kernel(loaded_kernel)
+                (PayloadType::Kernel(loaded_kernel), payload_end)
             }
         };
 
         let memory_end = GuestAddress(AARCH64_PHYS_MEM_START + components.memory_size);
-        let fdt_offset = fdt_address(memory_end, has_bios);
+
+        let fdt_address = match fdt_position {
+            FdtPosition::Start => GuestAddress(AARCH64_PHYS_MEM_START),
+            FdtPosition::End => {
+                let addr = memory_end
+                    .checked_sub(AARCH64_FDT_MAX_SIZE)
+                    .expect("Not enough memory for FDT")
+                    .align_down(AARCH64_FDT_ALIGN);
+                assert!(addr >= payload_end_address, "Not enough memory for FDT");
+                addr
+            }
+            FdtPosition::AfterPayload => payload_end_address
+                .align(AARCH64_FDT_ALIGN)
+                .expect("Not enough memory for FDT"),
+        };
 
         let mut use_pmu = vm
             .get_hypervisor()
@@ -533,7 +537,7 @@ impl arch::LinuxArch for AArch64 {
                 Self::vcpu_init(
                     vcpu_id,
                     &payload,
-                    fdt_offset,
+                    fdt_address,
                     components.hv_cfg.protection_type,
                     components.boot_cpu,
                 )
@@ -810,7 +814,7 @@ impl arch::LinuxArch for AArch64 {
             components.cpu_clusters,
             components.cpu_capacity,
             components.cpu_frequencies,
-            fdt_offset,
+            fdt_address,
             cmdline.as_str(),
             (payload.entry(), payload.size() as usize),
             initrd,
@@ -836,7 +840,7 @@ impl arch::LinuxArch for AArch64 {
 
         vm.init_arch(
             payload.entry(),
-            fdt_offset,
+            fdt_address,
             AARCH64_FDT_MAX_SIZE.try_into().unwrap(),
         )
         .map_err(Error::InitVmError)?;

arch/src/lib.rs
@@ -138,6 +138,17 @@ pub struct Pstore {
     pub size: u32,
 }
 
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, FromKeyValues)]
+#[serde(deny_unknown_fields, rename_all = "kebab-case")]
+pub enum FdtPosition {
+    /// At the start of RAM.
+    Start,
+    /// Near the end of RAM.
+    End,
+    /// After the payload, with some padding for alignment.
+    AfterPayload,
+}
+
 /// Set of CPU cores.
 #[derive(Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub struct CpuSet(Vec<usize>);
@@ -487,6 +498,7 @@ pub trait LinuxArch {
         #[cfg(feature = "swap")] swap_controller: &mut Option<swap::SwapController>,
         guest_suspended_cvar: Option<Arc<(Mutex<bool>, Condvar)>>,
         device_tree_overlays: Vec<DtbOverlay>,
+        fdt_position: Option<FdtPosition>,
     ) -> std::result::Result<RunnableLinuxVm<V, Vcpu>, Self::Error>
     where
         V: VmArch,

x86_64/src/lib.rs
@@ -55,6 +55,7 @@ use arch::get_serial_cmdline;
 use arch::serial::SerialDeviceInfo;
 use arch::CpuSet;
 use arch::DtbOverlay;
+use arch::FdtPosition;
 use arch::GetSerialCmdlineError;
 use arch::RunnableLinuxVm;
 use arch::VmComponents;
@@ -725,6 +726,7 @@ impl arch::LinuxArch for X8664arch {
         #[cfg(feature = "swap")] swap_controller: &mut Option<swap::SwapController>,
         guest_suspended_cvar: Option<Arc<(Mutex<bool>, Condvar)>>,
         device_tree_overlays: Vec<DtbOverlay>,
+        _fdt_position: Option<FdtPosition>,
     ) -> std::result::Result<RunnableLinuxVm<V, Vcpu>, Self::Error>
     where
         V: VmX86_64,

src/crosvm/cmdline.rs
@@ -23,6 +23,7 @@ use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering;
 
 use arch::CpuSet;
+use arch::FdtPosition;
 use arch::Pstore;
 #[cfg(target_arch = "x86_64")]
 use arch::SmbiosOptions;
@@ -1292,6 +1293,18 @@ pub struct RunCommand {
     #[merge(strategy = overwrite_option)]
     /// gather and display statistics on Vm Exits and Bus Reads/Writes.
     pub exit_stats: Option<bool>,
+
+    #[argh(option)]
+    #[serde(skip)]
+    #[merge(strategy = overwrite)]
+    /// where the FDT is placed in memory.
+    ///
+    /// On x86_64, no effect.
+    ///
+    /// On aarch64, defaults to `end` for kernel payloads and to `start` for BIOS payloads.
+    ///
+    /// On riscv64, defaults to `after-payload`.
+    pub fdt_position: Option<FdtPosition>,
 
     #[argh(
         option,
@@ -3569,6 +3582,8 @@ impl TryFrom<RunCommand> for super::config::Config {
         }
 
         cfg.stub_pci_devices = cmd.stub_pci_device;
+
+        cfg.fdt_position = cmd.fdt_position;
 
         cfg.file_backed_mappings = cmd.file_backed_mapping;
 

src/crosvm/config.rs
@@ -13,6 +13,7 @@ use std::time::Duration;
 
 use arch::set_default_serial_parameters;
 use arch::CpuSet;
+use arch::FdtPosition;
 use arch::Pstore;
 #[cfg(target_arch = "x86_64")]
 use arch::SmbiosOptions;
@@ -772,6 +773,7 @@ pub struct Config {
     pub executable_path: Option<Executable>,
     #[cfg(windows)]
     pub exit_stats: bool,
+    pub fdt_position: Option<FdtPosition>,
     pub file_backed_mappings: Vec<FileBackedMappingParameters>,
     pub force_calibrated_tsc_leaf: bool,
     pub force_s2idle: bool,
@@ -987,6 +989,7 @@ impl Default for Config {
             executable_path: None,
             #[cfg(windows)]
             exit_stats: false,
+            fdt_position: None,
             file_backed_mappings: Vec::new(),
             force_calibrated_tsc_leaf: false,
             force_s2idle: false,

src/crosvm/sys/linux.rs
@@ -2165,6 +2165,7 @@ where
         &mut swap_controller,
         guest_suspended_cvar.clone(),
         dt_overlays,
+        cfg.fdt_position,
     )
     .context("the architecture failed to build the vm")?;
 

src/sys/windows.rs
@@ -2646,6 +2646,7 @@ where
         None,
         /* guest_suspended_cvar= */ None,
         dt_overlays,
+        cfg.fdt_position,
     )
     .exit_context(Exit::BuildVm, "the architecture failed to build the vm")?;
 

commit fd98c741f54cdac72e6a99dedcc35aa01bbe18c6

riscv64: fix fdt_position assert
The matches!() expression was missing the fdt_postion parameter that it
was intended to be checking.

BUG=b:358112181

Fixes: 49a6c3cf2ddb ("aarch64: add cmdline param to control FDT address")
Change-Id: I53d29ea6c31142d79384c1e312e8d2bdf16b324b
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5772345
Commit-Queue: Frederick Mayle <fmayle@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Frederick Mayle <fmayle@google.com>

riscv64/src/lib.rs
@@ -369,7 +369,7 @@ impl arch::LinuxArch for Riscv64 {
             .collect();
 
         assert!(
-            matches!(None | Some(FdtPosition::AfterPayload)),
+            matches!(fdt_position, None | Some(FdtPosition::AfterPayload)),
             "fdt_position={fdt_position:?} not supported"
         );
         let fdt_offset = (kernel_initrd_end + (RISCV64_FDT_ALIGN - 1)) & !(RISCV64_FDT_ALIGN - 1);