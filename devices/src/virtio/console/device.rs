google/crosvm

commit 2e277ed5bbba6a5dc55a87efc5c9c73c05ee53ae

devices: virtio-console: refactor and consolidate
This replaces the console backend for the virtio and vhost-user console
devices with a shared implementation.

There is one main worker thread per console device, which handles all
virtio queues for that device, and one thread per port to read from the
input source in a blocking fashion.

BUG=b:354677018
BUG=b:298289666

Change-Id: I376180e26497ed6391a9f49b7d9a5c4d5aafa64f
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5739275
Reviewed-by: Noah Gold <nkgold@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Frederick Mayle <fmayle@google.com>

devices/src/virtio/console/device.rs
@@ -1,0 +1,172 @@
+// Copyright 2024 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! virtio-console and vhost-user-console device shared backend implementation
+
+use base::RawDescriptor;
+use data_model::Le32;
+use hypervisor::ProtectionType;
+use serde::Deserialize;
+use serde::Serialize;
+use zerocopy::AsBytes;
+
+use crate::virtio::base_features;
+use crate::virtio::console::port::ConsolePort;
+use crate::virtio::console::port::ConsolePortSnapshot;
+use crate::virtio::console::worker::WorkerHandle;
+use crate::virtio::console::worker::WorkerPort;
+use crate::virtio::copy_config;
+use crate::virtio::device_constants::console::virtio_console_config;
+use crate::virtio::device_constants::console::VIRTIO_CONSOLE_F_MULTIPORT;
+use crate::virtio::Interrupt;
+use crate::virtio::Queue;
+
+pub struct ConsoleDevice {
+    avail_features: u64,
+    pub(crate) ports: Vec<ConsolePort>,
+    worker: Option<WorkerHandle>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ConsoleSnapshot {
+    avail_features: u64,
+    ports: Vec<ConsolePortSnapshot>,
+}
+
+impl ConsoleDevice {
+    /// Create a console device that does not support the multiport feature.
+    pub fn new_single_port(protection_type: ProtectionType, port: ConsolePort) -> ConsoleDevice {
+        ConsoleDevice {
+            avail_features: base_features(protection_type),
+            ports: vec![port],
+            worker: None,
+        }
+    }
+
+    /// Create a console device with the multiport feature enabled.
+    pub fn new_multi_port(
+        protection_type: ProtectionType,
+        ports: Vec<ConsolePort>,
+    ) -> ConsoleDevice {
+        // Port 0 must always exist.
+        assert!(!ports.is_empty());
+
+        let avail_features = base_features(protection_type) | (1 << VIRTIO_CONSOLE_F_MULTIPORT);
+
+        ConsoleDevice {
+            avail_features,
+            ports,
+            worker: None,
+        }
+    }
+
+    pub fn features(&self) -> u64 {
+        self.avail_features
+    }
+
+    pub fn max_ports(&self) -> usize {
+        self.ports.len()
+    }
+
+    /// Returns the maximum number of queues supported by this device.
+    pub fn max_queues(&self) -> usize {
+        // The port 0 receive and transmit queues always exist;
+        // other queues only exist if VIRTIO_CONSOLE_F_MULTIPORT is set.
+        let num_queues = self.ports.len().max(1);
+        if self.avail_features & (1 << VIRTIO_CONSOLE_F_MULTIPORT) != 0 {
+            // Each port has two queues (tx & rx), plus 2 for control receiveq and transmitq.
+            num_queues * 2 + 2
+        } else {
+            // port0 receiveq + transmitq
+            2
+        }
+    }
+
+    pub fn read_config(&self, offset: u64, data: &mut [u8]) {
+        let max_nr_ports = self.max_ports();
+        let config = virtio_console_config {
+            max_nr_ports: Le32::from(max_nr_ports as u32),
+            ..Default::default()
+        };
+        copy_config(data, 0, config.as_bytes(), offset);
+    }
+
+    pub fn keep_rds(&self) -> Vec<RawDescriptor> {
+        self.ports.iter().flat_map(ConsolePort::keep_rds).collect()
+    }
+
+    fn ensure_worker_started(&mut self, interrupt: Interrupt) -> &mut WorkerHandle {
+        self.worker.get_or_insert_with(|| {
+            let ports = self
+                .ports
+                .iter_mut()
+                .map(WorkerPort::from_console_port)
+                .collect();
+            WorkerHandle::new(interrupt, ports).expect("failed to create console worker")
+        })
+    }
+
+    pub fn start_queue(
+        &mut self,
+        idx: usize,
+        queue: Queue,
+        interrupt: Interrupt,
+    ) -> anyhow::Result<()> {
+        let worker = self.ensure_worker_started(interrupt);
+        worker.start_queue(idx, queue)
+    }
+
+    pub fn stop_queue(&mut self, idx: usize) -> anyhow::Result<Option<Queue>> {
+        match self.worker.as_mut() {
+            Some(worker) => worker.stop_queue(idx),
+            None => Ok(None),
+        }
+    }
+
+    pub fn reset(&mut self) -> anyhow::Result<()> {
+        for idx in 0..self.max_queues() {
+            let _ = self.stop_queue(idx);
+        }
+        Ok(())
+    }
+
+    pub fn start_input_threads(&mut self) {
+        for port in self.ports.iter_mut() {
+            port.start_input_thread();
+        }
+    }
+
+    pub fn stop_input_threads(&mut self) {
+        for port in self.ports.iter_mut() {
+            port.stop_input_thread();
+        }
+    }
+
+    pub fn snapshot(&mut self) -> anyhow::Result<ConsoleSnapshot> {
+        let mut ports = Vec::new();
+        for port in &mut self.ports {
+            ports.push(port.snapshot());
+        }
+
+        Ok(ConsoleSnapshot {
+            avail_features: self.avail_features,
+            ports,
+        })
+    }
+
+    pub fn restore(&mut self, snap: &ConsoleSnapshot) -> anyhow::Result<()> {
+        anyhow::ensure!(
+            self.avail_features == snap.avail_features,
+            "Virtio console incorrect features for restore: Expected: {}, Actual: {}",
+            self.avail_features,
+            snap.avail_features,
+        );
+
+        for (port, port_snap) in self.ports.iter_mut().zip(snap.ports.iter()) {
+            port.restore(port_snap);
+        }
+
+        Ok(())
+    }
+}

devices/src/virtio/console/worker.rs
@@ -1,0 +1,442 @@
+// Copyright 2024 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Virtio console device worker thread.
+
+use std::collections::BTreeMap;
+use std::collections::VecDeque;
+use std::sync::mpsc;
+use std::sync::Arc;
+
+use anyhow::anyhow;
+use anyhow::Context;
+use base::error;
+use base::Event;
+use base::EventToken;
+use base::WaitContext;
+use base::WorkerThread;
+use sync::Mutex;
+
+use crate::virtio::console::control::process_control_receive_queue;
+use crate::virtio::console::control::process_control_transmit_queue;
+use crate::virtio::console::control::ControlMsgBytes;
+use crate::virtio::console::input::process_receive_queue;
+use crate::virtio::console::output::process_transmit_queue;
+use crate::virtio::console::port::ConsolePort;
+use crate::virtio::console::port::ConsolePortInfo;
+use crate::virtio::Interrupt;
+use crate::virtio::Queue;
+
+const PORT0_RECEIVEQ_IDX: usize = 0;
+const PORT0_TRANSMITQ_IDX: usize = 1;
+const CONTROL_RECEIVEQ_IDX: usize = 2;
+const CONTROL_TRANSMITQ_IDX: usize = 3;
+const PORT1_RECEIVEQ_IDX: usize = 4;
+const PORT1_TRANSMITQ_IDX: usize = 5;
+
+pub struct WorkerPort {
+    info: Option<ConsolePortInfo>,
+
+    in_avail_evt: Event,
+    input_buffer: Arc<Mutex<VecDeque<u8>>>,
+    output: Box<dyn std::io::Write + Send>,
+}
+
+impl WorkerPort {
+    pub fn from_console_port(port: &mut ConsolePort) -> WorkerPort {
+        let in_avail_evt = port.clone_in_avail_evt().unwrap();
+        let input_buffer = port.clone_input_buffer();
+        let output = port
+            .take_output()
+            .unwrap_or_else(|| Box::new(std::io::sink()));
+        let info = port.port_info().cloned();
+        WorkerPort {
+            info,
+            in_avail_evt,
+            input_buffer,
+            output,
+        }
+    }
+
+    pub fn is_console(&self) -> bool {
+        self.info
+            .as_ref()
+            .map(|info| info.console)
+            .unwrap_or_default()
+    }
+
+    pub fn name(&self) -> Option<&str> {
+        self.info.as_ref().and_then(ConsolePortInfo::name)
+    }
+}
+
+#[derive(EventToken)]
+enum Token {
+    ReceiveQueueAvailable(u32),
+    TransmitQueueAvailable(u32),
+    InputAvailable(u32),
+    ControlReceiveQueueAvailable,
+    ControlTransmitQueueAvailable,
+    InterruptResample,
+    WorkerRequest,
+    Kill,
+}
+
+pub enum WorkerRequest {
+    StartQueue {
+        idx: usize,
+        queue: Queue,
+        response_sender: mpsc::SyncSender<anyhow::Result<()>>,
+    },
+    StopQueue {
+        idx: usize,
+        response_sender: mpsc::SyncSender<Option<Queue>>,
+    },
+}
+
+pub struct Worker {
+    wait_ctx: WaitContext<Token>,
+    interrupt: Interrupt,
+
+    // Currently running queues.
+    queues: BTreeMap<usize, Queue>,
+
+    // Console ports indexed by port ID. At least port 0 will exist, and other ports may be
+    // available if `VIRTIO_CONSOLE_F_MULTIPORT` is enabled.
+    ports: Vec<WorkerPort>,
+
+    // Device-to-driver messages to be received by the driver via the control receiveq.
+    pending_receive_control_msgs: VecDeque<ControlMsgBytes>,
+
+    worker_receiver: mpsc::Receiver<WorkerRequest>,
+    worker_event: Event,
+}
+
+impl Worker {
+    pub fn new(
+        interrupt: Interrupt,
+        ports: Vec<WorkerPort>,
+        worker_receiver: mpsc::Receiver<WorkerRequest>,
+        worker_event: Event,
+    ) -> anyhow::Result<Self> {
+        let wait_ctx = WaitContext::new().context("WaitContext::new() failed")?;
+
+        wait_ctx.add(&worker_event, Token::WorkerRequest)?;
+
+        for (index, port) in ports.iter().enumerate() {
+            let port_id = index as u32;
+            wait_ctx.add(&port.in_avail_evt, Token::InputAvailable(port_id))?;
+        }
+
+        if let Some(resample_evt) = interrupt.get_resample_evt() {
+            wait_ctx.add(resample_evt, Token::InterruptResample)?;
+        }
+
+        Ok(Worker {
+            wait_ctx,
+            interrupt,
+            queues: BTreeMap::new(),
+            ports,
+            pending_receive_control_msgs: VecDeque::new(),
+            worker_receiver,
+            worker_event,
+        })
+    }
+
+    pub fn run(&mut self, kill_evt: &Event) -> anyhow::Result<()> {
+        self.wait_ctx.add(kill_evt, Token::Kill)?;
+        let res = self.run_loop();
+        self.wait_ctx.delete(kill_evt)?;
+        res
+    }
+
+    fn run_loop(&mut self) -> anyhow::Result<()> {
+        let mut running = true;
+        while running {
+            let events = self.wait_ctx.wait()?;
+
+            for event in events.iter().filter(|e| e.is_readable) {
+                match event.token {
+                    Token::TransmitQueueAvailable(port_id) => {
+                        if let (Some(port), Some(transmitq)) = (
+                            self.ports.get_mut(port_id as usize),
+                            transmitq_idx(port_id).and_then(|idx| self.queues.get_mut(&idx)),
+                        ) {
+                            transmitq
+                                .event()
+                                .wait()
+                                .context("failed reading transmit queue Event")?;
+                            process_transmit_queue(&self.interrupt, transmitq, &mut port.output);
+                        }
+                    }
+                    Token::ReceiveQueueAvailable(port_id) | Token::InputAvailable(port_id) => {
+                        let port = self.ports.get_mut(port_id as usize);
+                        let receiveq =
+                            receiveq_idx(port_id).and_then(|idx| self.queues.get_mut(&idx));
+
+                        let event = if matches!(event.token, Token::ReceiveQueueAvailable(..)) {
+                            receiveq.as_ref().map(|q| q.event())
+                        } else {
+                            port.as_ref().map(|p| &p.in_avail_evt)
+                        };
+                        if let Some(event) = event {
+                            event.wait().context("failed to clear receive event")?;
+                        }
+
+                        if let (Some(port), Some(receiveq)) = (port, receiveq) {
+                            let mut input_buffer = port.input_buffer.lock();
+                            process_receive_queue(&self.interrupt, &mut input_buffer, receiveq);
+                        }
+                    }
+                    Token::ControlReceiveQueueAvailable => {
+                        if let Some(ctrl_receiveq) = self.queues.get_mut(&CONTROL_RECEIVEQ_IDX) {
+                            ctrl_receiveq
+                                .event()
+                                .wait()
+                                .context("failed waiting on control event")?;
+                            process_control_receive_queue(
+                                ctrl_receiveq,
+                                &self.interrupt,
+                                &mut self.pending_receive_control_msgs,
+                            );
+                        }
+                    }
+                    Token::ControlTransmitQueueAvailable => {
+                        if let Some(ctrl_transmitq) = self.queues.get_mut(&CONTROL_TRANSMITQ_IDX) {
+                            ctrl_transmitq
+                                .event()
+                                .wait()
+                                .context("failed waiting on control event")?;
+                            process_control_transmit_queue(
+                                ctrl_transmitq,
+                                &self.interrupt,
+                                &self.ports,
+                                &mut self.pending_receive_control_msgs,
+                            );
+                        }
+
+                        // Attempt to send any new replies if there is space in the receiveq.
+                        if let Some(ctrl_receiveq) = self.queues.get_mut(&CONTROL_RECEIVEQ_IDX) {
+                            process_control_receive_queue(
+                                ctrl_receiveq,
+                                &self.interrupt,
+                                &mut self.pending_receive_control_msgs,
+                            )
+                        }
+                    }
+                    Token::InterruptResample => {
+                        self.interrupt.interrupt_resample();
+                    }
+                    Token::WorkerRequest => {
+                        self.worker_event.wait()?;
+                        self.process_worker_requests();
+                    }
+                    Token::Kill => running = false,
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn process_worker_requests(&mut self) {
+        while let Ok(request) = self.worker_receiver.try_recv() {
+            match request {
+                WorkerRequest::StartQueue {
+                    idx,
+                    queue,
+                    response_sender,
+                } => {
+                    let res = self.start_queue(idx, queue);
+                    let _ = response_sender.send(res);
+                }
+                WorkerRequest::StopQueue {
+                    idx,
+                    response_sender,
+                } => {
+                    let res = self.stop_queue(idx);
+                    let _ = response_sender.send(res);
+                }
+            }
+        }
+    }
+
+    fn start_queue(&mut self, idx: usize, queue: Queue) -> anyhow::Result<()> {
+        if let Some(port_id) = receiveq_port_id(idx) {
+            self.wait_ctx
+                .add(queue.event(), Token::ReceiveQueueAvailable(port_id))?;
+        } else if let Some(port_id) = transmitq_port_id(idx) {
+            self.wait_ctx
+                .add(queue.event(), Token::TransmitQueueAvailable(port_id))?;
+        } else if idx == CONTROL_RECEIVEQ_IDX {
+            self.wait_ctx
+                .add(queue.event(), Token::ControlReceiveQueueAvailable)?;
+        } else if idx == CONTROL_TRANSMITQ_IDX {
+            self.wait_ctx
+                .add(queue.event(), Token::ControlTransmitQueueAvailable)?;
+        } else {
+            return Err(anyhow!("unhandled queue idx {idx}"));
+        }
+
+        let prev = self.queues.insert(idx, queue);
+        assert!(prev.is_none());
+        Ok(())
+    }
+
+    fn stop_queue(&mut self, idx: usize) -> Option<Queue> {
+        if let Some(queue) = self.queues.remove(&idx) {
+            let _ = self.wait_ctx.delete(queue.event());
+            Some(queue)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct WorkerHandle {
+    worker_thread: WorkerThread<()>,
+    worker_sender: mpsc::Sender<WorkerRequest>,
+    worker_event: Event,
+}
+
+impl WorkerHandle {
+    pub fn new(interrupt: Interrupt, ports: Vec<WorkerPort>) -> anyhow::Result<Self> {
+        let worker_event = Event::new().context("Event::new")?;
+        let worker_event_clone = worker_event.try_clone().context("Event::try_clone")?;
+        let (worker_sender, worker_receiver) = mpsc::channel();
+        let worker_thread = WorkerThread::start("v_console", move |kill_evt| {
+            let mut worker = Worker::new(interrupt, ports, worker_receiver, worker_event_clone)
+                .expect("console Worker::new() failed");
+            if let Err(e) = worker.run(&kill_evt) {
+                error!("console worker failed: {:#}", e);
+            }
+        });
+        Ok(WorkerHandle {
+            worker_thread,
+            worker_sender,
+            worker_event,
+        })
+    }
+
+    pub fn start_queue(&mut self, idx: usize, queue: Queue) -> anyhow::Result<()> {
+        let (response_sender, response_receiver) = mpsc::sync_channel(0);
+        self.worker_sender
+            .send(WorkerRequest::StartQueue {
+                idx,
+                queue,
+                response_sender,
+            })
+            .context("mpsc::Sender::send")?;
+        self.worker_event.signal().context("Event::signal")?;
+        response_receiver.recv().context("mpsc::Receiver::recv")?
+    }
+
+    pub fn stop_queue(&mut self, idx: usize) -> anyhow::Result<Option<Queue>> {
+        let (response_sender, response_receiver) = mpsc::sync_channel(0);
+        self.worker_sender
+            .send(WorkerRequest::StopQueue {
+                idx,
+                response_sender,
+            })
+            .context("mpsc::Sender::send")?;
+        self.worker_event.signal().context("Event::signal")?;
+        response_receiver.recv().context("mpsc::Receiver::recv")
+    }
+}
+
+impl Drop for WorkerHandle {
+    fn drop(&mut self) {
+        let _ = self.worker_thread.signal();
+    }
+}
+
+fn receiveq_idx(port_id: u32) -> Option<usize> {
+    if port_id == 0 {
+        Some(PORT0_RECEIVEQ_IDX)
+    } else {
+        PORT1_RECEIVEQ_IDX.checked_add((port_id - 1).checked_mul(2)?.try_into().ok()?)
+    }
+}
+
+fn transmitq_idx(port_id: u32) -> Option<usize> {
+    if port_id == 0 {
+        Some(PORT0_TRANSMITQ_IDX)
+    } else {
+        PORT1_TRANSMITQ_IDX.checked_add((port_id - 1).checked_mul(2)?.try_into().ok()?)
+    }
+}
+
+fn receiveq_port_id(queue_idx: usize) -> Option<u32> {
+    if queue_idx == PORT0_RECEIVEQ_IDX {
+        Some(0)
+    } else if queue_idx >= PORT1_RECEIVEQ_IDX && (queue_idx & 1) == 0 {
+        ((queue_idx - PORT1_RECEIVEQ_IDX) / 2)
+            .checked_add(1)?
+            .try_into()
+            .ok()
+    } else {
+        None
+    }
+}
+
+fn transmitq_port_id(queue_idx: usize) -> Option<u32> {
+    if queue_idx == PORT0_TRANSMITQ_IDX {
+        Some(0)
+    } else if queue_idx >= PORT1_TRANSMITQ_IDX && (queue_idx & 1) == 1 {
+        ((queue_idx - PORT1_TRANSMITQ_IDX) / 2)
+            .checked_add(1)?
+            .try_into()
+            .ok()
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_receiveq_idx() {
+        assert_eq!(receiveq_idx(0), Some(0));
+        assert_eq!(receiveq_idx(1), Some(4));
+        assert_eq!(receiveq_idx(2), Some(6));
+        assert_eq!(receiveq_idx(3), Some(8));
+    }
+
+    #[test]
+    fn test_transmitq_idx() {
+        assert_eq!(transmitq_idx(0), Some(1));
+        assert_eq!(transmitq_idx(1), Some(5));
+        assert_eq!(transmitq_idx(2), Some(7));
+        assert_eq!(transmitq_idx(3), Some(9));
+    }
+
+    #[test]
+    fn test_receiveq_port_id() {
+        assert_eq!(receiveq_port_id(0), Some(0));
+        assert_eq!(receiveq_port_id(1), None); // port0 transmitq
+        assert_eq!(receiveq_port_id(2), None); // ctrl receiveq
+        assert_eq!(receiveq_port_id(3), None); // ctrl transmitq
+        assert_eq!(receiveq_port_id(4), Some(1));
+        assert_eq!(receiveq_port_id(5), None);
+        assert_eq!(receiveq_port_id(6), Some(2));
+        assert_eq!(receiveq_port_id(7), None);
+        assert_eq!(receiveq_port_id(8), Some(3));
+        assert_eq!(receiveq_port_id(9), None);
+    }
+
+    #[test]
+    fn test_transmitq_port_id() {
+        assert_eq!(transmitq_port_id(0), None); // port0 receiveq
+        assert_eq!(transmitq_port_id(1), Some(0));
+        assert_eq!(transmitq_port_id(2), None); // ctrl receiveq
+        assert_eq!(transmitq_port_id(3), None); // ctrl transmitq
+        assert_eq!(transmitq_port_id(4), None); // port1 receiveq
+        assert_eq!(transmitq_port_id(5), Some(1));
+        assert_eq!(transmitq_port_id(6), None);
+        assert_eq!(transmitq_port_id(7), Some(2));
+        assert_eq!(transmitq_port_id(8), None);
+        assert_eq!(transmitq_port_id(9), Some(3));
+    }
+}

devices/src/serial_device.rs
@@ -105,9 +105,9 @@ impl Display for SerialType {
 #[serde(rename_all = "kebab-case")]
 pub enum SerialHardware {
     Serial,              // Standard PC-style (8250/16550 compatible) UART
-    VirtioConsole,       // virtio-console device (AsyncConsole)
+    VirtioConsole,       // virtio-console device
     Debugcon,            // Bochs style debug port
-    LegacyVirtioConsole, // legacy virtio-console device (Console)
+    LegacyVirtioConsole, // legacy virtio-console device (alias for VirtioConsole)
 }
 
 impl Default for SerialHardware {

devices/src/virtio/async_device.rs
@@ -1,117 +1,0 @@
-// Copyright 2022 The ChromiumOS Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-//! Provides helpers that make it easier to process virtio queues on an async executor.
-
-#![cfg_attr(windows, allow(dead_code))]
-
-use anyhow::bail;
-use anyhow::Context;
-use base::warn;
-use cros_async::AsyncResult;
-use cros_async::Executor;
-use cros_async::TaskHandle;
-use futures::future::AbortHandle;
-use futures::future::Abortable;
-use futures::future::Pending;
-use futures::Future;
-
-/// A queue for which processing can be started on an async executor.
-///
-/// `T` is the resource type of the queue, i.e. the device-specific data it needs in order to run.
-/// For instance, a block device will likely need a file to provide its data.
-pub enum AsyncQueueState<T: 'static> {
-    /// Queue is currently stopped.
-    Stopped(T),
-    /// Queue is being processed as a `Task` on an `Executor`, and can be stopped by aborting the
-    /// `AbortHandle`.
-    Running((TaskHandle<T>, Executor, AbortHandle)),
-    /// Something terrible happened and this queue is in a non-recoverable state.
-    Broken,
-}
-
-impl<T: 'static> AsyncQueueState<T> {
-    /// Start processing of the queue on `ex`, or stop and restart it with the new parameters if
-    /// it was already running.
-    ///
-    /// `fut_provider` is a closure that is passed the resource of the queue, as well as a
-    /// `Abortable` future. It must return a `Future` that takes ownership of the device's resource
-    /// and processes the queue for as long as possible, but immediately quits and returns the
-    /// device resource when the `Abortable` is signaled.
-    ///
-    /// If `fut_provider` or the `Future` it returns end with an error, the queue is considered
-    /// broken and cannot be used anymore.
-    ///
-    /// The task is only scheduled and no processing actually starts in this method. The task is
-    /// scheduled locally, which implies that `ex` must be run on the current thread.
-    pub fn start<
-        U: Future<Output = T> + 'static,
-        F: FnOnce(T, Abortable<Pending<()>>) -> anyhow::Result<U>,
-    >(
-        &mut self,
-        ex: &Executor,
-        fut_provider: F,
-    ) -> anyhow::Result<()> {
-        if matches!(self, AsyncQueueState::Running(_)) {
-            warn!("queue is already running, stopping it first");
-            self.stop().context("while trying to restart queue")?;
-        }
-
-        let resource = match std::mem::replace(self, AsyncQueueState::Broken) {
-            AsyncQueueState::Stopped(resource) => resource,
-            _ => bail!("queue is in a bad state and cannot be started"),
-        };
-
-        let (wait_fut, abort_handle) = futures::future::abortable(futures::future::pending::<()>());
-        let queue_future = fut_provider(resource, wait_fut)?;
-        let task = ex.spawn_local(queue_future);
-
-        *self = AsyncQueueState::Running((task, ex.clone(), abort_handle));
-        Ok(())
-    }
-
-    /// Stops a previously started queue.
-    ///
-    /// The executor on which the task has been started will be run if needed in order to retrieve
-    /// the queue's resource.
-    ///
-    /// Returns `true` if the queue was running, `false` if it wasn't.
-    pub fn stop(&mut self) -> AsyncResult<bool> {
-        // TODO: schuffelen - All callers should use stop_async instead.
-        match std::mem::replace(self, AsyncQueueState::Broken) {
-            AsyncQueueState::Running((task, ex, handle)) => {
-                // Abort the task and run it to completion to retrieve the queue's resource.
-                handle.abort();
-                let resource = ex.run_until(task)?;
-                *self = AsyncQueueState::Stopped(resource);
-                Ok(true)
-            }
-            state => {
-                *self = state;
-                Ok(false)
-            }
-        }
-    }
-    /// Stops a previously started queue.
-    ///
-    /// The executor on which the task has been started will be run if needed in order to retrieve
-    /// the queue's resource.
-    ///
-    /// Returns `true` if the queue was running, `false` if it wasn't.
-    pub async fn stop_async(&mut self) -> AsyncResult<bool> {
-        match std::mem::replace(self, AsyncQueueState::Broken) {
-            AsyncQueueState::Running((task, _, handle)) => {
-                // Abort the task and run it to completion to retrieve the queue's resource.
-                handle.abort();
-                let resource = task.await;
-                *self = AsyncQueueState::Stopped(resource);
-                Ok(true)
-            }
-            state => {
-                *self = state;
-                Ok(false)
-            }
-        }
-    }
-}

commit 6aee3efa609ed33dc751874201ee2ffbfbb32cbb

devices: virtio-console: stop worker thread on reset
After stopping all queues in the worker thread, also stop the worker
thread itself so that the Interrupt it contains is dropped.

Previously, the worker thread would hold onto the old (no longer used)
Interrupt instance, even when a new Interrupt was provided by the next
call to activate(). With the change, the worker thread with the old
Interrupt will be cleaned up so the next activate() will cause
ensure_worker_started() to be called again and create a new worker with
the new Interrupt.

BUG=b:360926085

Fixes: 2e277ed5bbba ("devices: virtio-console: refactor and consolidate")
Change-Id: Ib57bbb768ab4c211b7e6f408f2c75beee8f08bd8
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5798107
Reviewed-by: Frederick Mayle <fmayle@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

devices/src/virtio/console/device.rs
@@ -107,6 +107,12 @@ impl ConsoleDevice {
         })
     }
 
+    fn ensure_worker_stopped(&mut self) {
+        if let Some(mut worker) = self.worker.take() {
+            worker.stop();
+        }
+    }
+
     pub fn start_queue(
         &mut self,
         idx: usize,
@@ -128,6 +134,7 @@ impl ConsoleDevice {
         for idx in 0..self.max_queues() {
             let _ = self.stop_queue(idx);
         }
+        self.ensure_worker_stopped();
         Ok(())
     }
 

devices/src/virtio/console/worker.rs
@@ -294,7 +294,7 @@ impl Worker {
 }
 
 pub struct WorkerHandle {
-    worker_thread: WorkerThread<()>,
+    worker_thread: Option<WorkerThread<()>>,
     worker_sender: mpsc::Sender<WorkerRequest>,
     worker_event: Event,
 }
@@ -312,7 +312,7 @@ impl WorkerHandle {
             }
         });
         Ok(WorkerHandle {
-            worker_thread,
+            worker_thread: Some(worker_thread),
             worker_sender,
             worker_event,
         })
@@ -342,11 +342,11 @@ impl WorkerHandle {
         self.worker_event.signal().context("Event::signal")?;
         response_receiver.recv().context("mpsc::Receiver::recv")
     }
-}
 
-impl Drop for WorkerHandle {
-    fn drop(&mut self) {
-        let _ = self.worker_thread.signal();
+    pub fn stop(&mut self) {
+        if let Some(worker_thread) = self.worker_thread.take() {
+            worker_thread.stop();
+        }
     }
 }
 