b94ed073cd8e83bc11401be05b469cc22d2a27b5
Frederick Mayle <fmayle@google.com>
2024-08-28 15:16:21 -0700

    devices: serial: windows: fix bad timer usage
    Waiting on a `WaitContext` isn't enough, the timer must be ack'd. This
    technically isn't needed on Windows, but that is an implementaiton
    detail and also people often copy existing usages of APIs and might not
    realize this detail.
    
    Change-Id: Ic29151b4871bebacd14c19c6dcb9e35779175c79
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5825225
    Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
    Commit-Queue: Frederick Mayle <fmayle@google.com>

--- devices/src/serial/sys/windows.rs
+++ devices/src/serial/sys/windows.rs
@@ -204,6 +204,7 @@ impl SyncWorker {
             for event in events.iter().filter(|e| e.is_readable) {
                 match event.token {
                     Token::Sync => {
+                        timer.mark_waited().unwrap();
                         if let Err(e) = self.file.fsync() {
                             error!("failed to fsync serial device, stopping sync thread: {}", e);
                             return;
