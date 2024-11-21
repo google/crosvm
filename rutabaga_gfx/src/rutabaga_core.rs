google/crosvm

commit fbe3f46963debc35a23d22a4a19705643cec3c86

devices: gpu: fallback to 2D backend on failed virgl backend init
If virglrenderer as the default GPU backend failed initialization, fall
back to 2D mode to prevent crashing the VM. This configuration is still
able to support software-rendered workflows with ChromeOS' usual
virtio-wl supported windowing.

BUG=b:329522487
TEST=Crostini software rendered glxgears still works with failing
virglrenderer init.

Change-Id: I931b0a8c5662ce9726a85aaaa05565b901b727cf
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5468982
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
Commit-Queue: Ryan Neph <ryanneph@google.com>

rutabaga_gfx/src/rutabaga_core.rs
@@ -1177,23 +1177,26 @@ impl RutabagaBuilder {
             ));
         }
 
-        if self.default_component == RutabagaComponentType::Rutabaga2D {
-            let rutabaga_2d = Rutabaga2D::init(fence_handler.clone())?;
-            rutabaga_components.insert(RutabagaComponentType::Rutabaga2D, rutabaga_2d);
-        } else {
+        #[allow(unused_mut)]
+        let mut fallback_2d = false;
+        if self.default_component != RutabagaComponentType::Rutabaga2D {
             #[cfg(feature = "virgl_renderer")]
             if self.default_component == RutabagaComponentType::VirglRenderer {
-                let virgl = VirglRenderer::init(
+                if let Ok(virgl) = VirglRenderer::init(
                     self.virglrenderer_flags,
                     fence_handler.clone(),
                     rutabaga_server_descriptor,
-                )?;
-                rutabaga_components.insert(RutabagaComponentType::VirglRenderer, virgl);
+                ) {
+                    rutabaga_components.insert(RutabagaComponentType::VirglRenderer, virgl);
 
-                push_capset(RUTABAGA_CAPSET_VIRGL);
-                push_capset(RUTABAGA_CAPSET_VIRGL2);
-                push_capset(RUTABAGA_CAPSET_VENUS);
-                push_capset(RUTABAGA_CAPSET_DRM);
+                    push_capset(RUTABAGA_CAPSET_VIRGL);
+                    push_capset(RUTABAGA_CAPSET_VIRGL2);
+                    push_capset(RUTABAGA_CAPSET_VENUS);
+                    push_capset(RUTABAGA_CAPSET_DRM);
+                } else {
+                    log::warn!("error initializing gpu backend=virglrenderer, falling back to 2d.");
+                    fallback_2d = true;
+                };
             }
 
             #[cfg(feature = "gfxstream")]
@@ -1218,6 +1221,11 @@ impl RutabagaBuilder {
             let cross_domain = CrossDomain::init(self.channels, fence_handler.clone())?;
             rutabaga_components.insert(RutabagaComponentType::CrossDomain, cross_domain);
             push_capset(RUTABAGA_CAPSET_CROSS_DOMAIN);
+        }
+
+        if self.default_component == RutabagaComponentType::Rutabaga2D || fallback_2d {
+            let rutabaga_2d = Rutabaga2D::init(fence_handler.clone())?;
+            rutabaga_components.insert(RutabagaComponentType::Rutabaga2D, rutabaga_2d);
         }
 
         Ok(Rutabaga {

devices/src/virtio/gpu/virtio_gpu.rs
@@ -859,12 +859,25 @@ impl VirtioGpu {
 
     /// Gets rutabaga's capset information associated with `index`.
     pub fn get_capset_info(&self, index: u32) -> VirtioGpuResult {
-        let (capset_id, version, size) = self.rutabaga.get_capset_info(index)?;
-        Ok(OkCapsetInfo {
-            capset_id,
-            version,
-            size,
-        })
+        if let Ok((capset_id, version, size)) = self.rutabaga.get_capset_info(index) {
+            Ok(OkCapsetInfo {
+                capset_id,
+                version,
+                size,
+            })
+        } else {
+            // Any capset_id > 63 is invalid according to the virtio-gpu spec, so we can
+            // intentionally poison the capset without stalling the guest kernel driver.
+            base::warn!(
+                "virtio-gpu get_capset_info(index={}) failed. intentionally poisoning response",
+                index
+            );
+            Ok(OkCapsetInfo {
+                capset_id: u32::max_value(),
+                version: 0,
+                size: 0,
+            })
+        }
     }
 
     /// Gets a capset from rutabaga.

commit 1f15edcd22eb10d68465d54c8eda5f5e0cfcb277

devices: gpu: fix suspend/resume for 2D fallback
When falling back to GPU 2D backend, the default_component remains set
to VirglRenderer. When Rutabaga suspend/resume feature is invoked later,
the default_component is checked and a RutabagaError::InvalidComponent
is thrown because it is not supported by the VirglRenderer backend.

This change sets the default_component to 2D whenever the fallback is
taken.

BUG=b:380180766
TEST=cargo build --features gpu,virgl_renderer

Fixes: fbe3f4696 ("devices: gpu: fallback to 2D backend on failed virgl backend init")
Change-Id: I7858fab5da05f439c74b5b6145683478d0c81433
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/6039874
Auto-Submit: Ryan Neph <ryanneph@google.com>
Commit-Queue: Ryan Neph <ryanneph@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>

rutabaga_gfx/src/rutabaga_core.rs
@@ -1305,8 +1305,6 @@ impl RutabagaBuilder {
             ));
         }
 
-        #[allow(unused_mut)]
-        let mut fallback_2d = false;
         if self.default_component != RutabagaComponentType::Rutabaga2D {
             #[cfg(feature = "virgl_renderer")]
             if self.default_component == RutabagaComponentType::VirglRenderer {
@@ -1323,7 +1321,7 @@ impl RutabagaBuilder {
                     push_capset(RUTABAGA_CAPSET_DRM);
                 } else {
                     log::warn!("error initializing gpu backend=virglrenderer, falling back to 2d.");
-                    fallback_2d = true;
+                    self.default_component = RutabagaComponentType::Rutabaga2D;
                 };
             }
 
@@ -1351,7 +1349,7 @@ impl RutabagaBuilder {
             push_capset(RUTABAGA_CAPSET_CROSS_DOMAIN);
         }
 
-        if self.default_component == RutabagaComponentType::Rutabaga2D || fallback_2d {
+        if self.default_component == RutabagaComponentType::Rutabaga2D {
             let rutabaga_2d = Rutabaga2D::init(fence_handler.clone())?;
             rutabaga_components.insert(RutabagaComponentType::Rutabaga2D, rutabaga_2d);
         }