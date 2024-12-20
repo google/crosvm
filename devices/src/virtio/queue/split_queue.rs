google/crosvm

commit fcf9a630e9668bfd7e8dec228e6ecf9887e946bf

devices: vhost-user: align sleep/wake implementation with spec
See the diffs in snapshot_extensions.md for an overview of the changes.

Originally, crosvm's virtio and vhost-user frameworks diverged in their
snapshot-restore implementations. Virtio devices returned ownership of
their queues back to VirtioPciDevice, but vhost-user devices kept
ownership in the backend process. Replacing SLEEP with GET_VRING_BASE
has a side effect of bringing the two into alignment. If you look at
this commit in isolation, you might wonder why we ever went with the
other approach. The reason is that there were a large number of misc
bugs and code organization issue in the way that have been fixed since,
for example, it wasn't possible to restart the vhost-user frontend
worker to rewire the IPC for interrupts, instead we rewired them inside
the backend. Another example: the frontend and backend were not
correctly communicating the current vring base to each other, they
always passed zero.

There is no vhost-user feature bit to indicate if the backend supports
the "suspended device state", so, for devices without support, we no
longer can fail when putting the device to sleep. Instead, we only fail
when we try to snapshot or restore.

Bug: 301269927
Change-Id: I5aaac864e36a2dffe078d7c8f8abff29365a02c0
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5394172
Commit-Queue: Frederick Mayle <fmayle@google.com>
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>

devices/src/virtio/queue/split_queue.rs
@@ -126,6 +126,29 @@ impl SplitQueue {
         })
     }
 
+    pub fn vhost_user_reclaim(&mut self, vring_base: u16) {
+        self.next_avail = Wrapping(vring_base);
+        // The vhost-user spec says:
+        //
+        //     For the Used Ring, the device only needs the next descriptor index at which to put
+        //     new descriptors, which is the value in the vring structure in memory, so this value
+        //     is not covered by this message.
+        //
+        // So, we read the value from guest memory.
+        let used_index_addr = self.used_ring.unchecked_add(2);
+        self.next_used = self
+            .mem
+            .read_obj_from_addr_volatile(used_index_addr)
+            .unwrap();
+        // We assume the vhost-user backend sent interrupts for any descriptors it marked used
+        // before it stopped processing the queue, so `last_used == next_used`.
+        self.last_used = self.next_used;
+    }
+
+    pub fn next_avail_to_process(&self) -> u16 {
+        self.next_avail.0
+    }
+
     /// Return the actual size of the queue, as the driver may not set up a
     /// queue as big as the device allows.
     pub fn size(&self) -> u16 {

devices/src/virtio/queue.rs
@@ -457,6 +457,29 @@ impl Queue {
         }
     }
 
+    /// "Reclaim" a queue that was given to a vhost-user backend and is now being taken back using
+    /// VHOST_USER_GET_VRING_BASE.
+    ///
+    /// The `Queue` will have stale fields if the vhost-user backend fulfilled any virtqueue
+    /// requests. This function updates the `Queue` to pick up where the backend left off.
+    pub fn vhost_user_reclaim(&mut self, vring_base: u16) {
+        match self {
+            Queue::SplitVirtQueue(q) => q.vhost_user_reclaim(vring_base),
+            Queue::PackedVirtQueue(q) => q.vhost_user_reclaim(vring_base),
+        }
+    }
+
+    /// Getter for the next index of the available ring that device will process.
+    ///
+    /// Not to be confused with the available ring's index field, which is the next index for the
+    /// driver to fill.
+    pub fn next_avail_to_process(&self) -> u16 {
+        match self {
+            Queue::SplitVirtQueue(q) => q.next_avail_to_process(),
+            Queue::PackedVirtQueue(q) => q.next_avail_to_process(),
+        }
+    }
+
     define_queue_method!(
         /// Getter for vector field
         vector,

devices/src/virtio/queue/packed_queue.rs
@@ -166,6 +166,15 @@ impl PackedQueue {
         })
     }
 
+    pub fn vhost_user_reclaim(&mut self, _vring_base: u16) {
+        // TODO: b/331466964 - Need more than `vring_base` to reclaim a packed virtqueue.
+        unimplemented!()
+    }
+
+    pub fn next_avail_to_process(&self) -> u16 {
+        self.avail_index.index.0
+    }
+
     /// Return the actual size of the queue, as the driver may not set up a
     /// queue as big as the device allows.
     pub fn size(&self) -> u16 {

devices/src/virtio/vhost/user/device/block.rs
@@ -87,11 +87,12 @@ impl VhostUserDevice for BlockBackend {
         self.inner.stop_queue(idx)
     }
 
-    fn stop_non_queue_workers(&mut self) -> anyhow::Result<()> {
+    fn enter_suspended_state(&mut self) -> anyhow::Result<bool> {
         // TODO: This assumes that `reset` only stops workers which might not be true in the
         // future. Consider moving the `reset` code into a `stop_all_workers` method or, maybe,
         // make `stop_queue` implicitly stop a worker thread when there is no active queue.
-        self.inner.reset()
+        self.inner.reset()?;
+        Ok(true)
     }
 
     fn snapshot(&self) -> anyhow::Result<Vec<u8>> {

devices/src/virtio/vhost/user/device/gpu.rs
@@ -93,6 +93,15 @@ struct GpuBackend {
     shmem_mapper: Arc<Mutex<Option<Box<dyn SharedMemoryMapper>>>>,
 }
 
+impl GpuBackend {
+    fn stop_non_queue_workers(&mut self) -> anyhow::Result<()> {
+        for handle in self.platform_workers.borrow_mut().drain(..) {
+            let _ = self.ex.run_until(handle.cancel());
+        }
+        Ok(())
+    }
+}
+
 impl VhostUserDevice for GpuBackend {
     fn max_queue_num(&self) -> usize {
         MAX_QUEUE_NUM
@@ -231,11 +240,9 @@ impl VhostUserDevice for GpuBackend {
         }
     }
 
-    fn stop_non_queue_workers(&mut self) -> anyhow::Result<()> {
-        for handle in self.platform_workers.borrow_mut().drain(..) {
-            let _ = self.ex.run_until(handle.cancel());
-        }
-        Ok(())
+    fn enter_suspended_state(&mut self) -> anyhow::Result<bool> {
+        self.stop_non_queue_workers()?;
+        Ok(true)
     }
 
     fn reset(&mut self) {

devices/src/virtio/vhost/user/device/handler.rs
@@ -184,12 +184,26 @@ pub trait VhostUserDevice {
     /// negotiated.
     fn set_backend_req_connection(&mut self, _conn: Arc<VhostBackendReqConnection>) {}
 
-    /// Used to stop non queue workers that `VhostUserDevice::stop_queue` can't stop. May or may
-    /// not also stop all queue workers.
-    fn stop_non_queue_workers(&mut self) -> anyhow::Result<()> {
-        error!("sleep not implemented for vhost user device");
-        // TODO(rizhang): Return error once basic devices support this.
-        Ok(())
+    /// Enter the "suspended device state" described in the vhost-user spec. See the spec for
+    /// requirements.
+    ///
+    /// One reasonably foolproof way to satisfy the requirements is to stop all worker threads.
+    ///
+    /// Called after a `stop_queue` call if there are no running queues left. Also called soon
+    /// after device creation to detect support (if supported, it is assumed the device is acting
+    /// suspended immediately on construction).
+    ///
+    /// The next `start_queue` call implicitly exits the "suspend device state".
+    ///
+    /// `Ok(false)` is returned to signal that this state isn't supported. In that case, the
+    /// framework will block some features, e.g. snapshot/restore and
+    /// `VHOST_USER_SET_DEVICE_STATE_FD`.
+    ///
+    /// * Ok(true)  => device successfully suspended
+    /// * Ok(false) => "suspended device state" not supported
+    /// * Err(_)    => unrecoverable error
+    fn enter_suspended_state(&mut self) -> anyhow::Result<bool> {
+        Ok(false)
     }
 
     /// Snapshot device and return serialized bytes.
@@ -212,17 +226,6 @@ struct Vring {
     queue: QueueConfig,
     doorbell: Option<Interrupt>,
     enabled: bool,
-    // Active queue that is only `Some` when the device is sleeping.
-    paused_queue: Option<Queue>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct VringSnapshot {
-    // Snapshot of queue config.
-    queue: serde_json::Value,
-    // Snapshot of the activated queue state.
-    paused_queue: Option<serde_json::Value>,
-    enabled: bool,
 }
 
 impl Vring {
@@ -231,7 +234,6 @@ impl Vring {
             queue: QueueConfig::new(max_size, features),
             doorbell: None,
             enabled: false,
-            paused_queue: None,
         }
     }
 
@@ -239,41 +241,6 @@ impl Vring {
         self.queue.reset();
         self.doorbell = None;
         self.enabled = false;
-        self.paused_queue = None;
-    }
-
-    fn snapshot(&self) -> anyhow::Result<VringSnapshot> {
-        Ok(VringSnapshot {
-            queue: self.queue.snapshot()?,
-            enabled: self.enabled,
-            paused_queue: self
-                .paused_queue
-                .as_ref()
-                .map(Queue::snapshot)
-                .transpose()?,
-        })
-    }
-
-    fn restore(
-        &mut self,
-        vring_snapshot: VringSnapshot,
-        mem: &GuestMemory,
-        event: Option<Event>,
-    ) -> anyhow::Result<()> {
-        self.queue.restore(vring_snapshot.queue)?;
-        self.enabled = vring_snapshot.enabled;
-        self.paused_queue = vring_snapshot
-            .paused_queue
-            .map(|value| {
-                Queue::restore(
-                    &self.queue,
-                    value,
-                    mem,
-                    event.context("missing queue event")?,
-                )
-            })
-            .transpose()?;
-        Ok(())
     }
 }
 
@@ -355,6 +322,8 @@ impl VhostUserRegularOps {
 pub struct DeviceRequestHandler<T: VhostUserDevice> {
     vrings: Vec<Vring>,
     owned: bool,
+    // true iff all queues are stopped and `enter_suspended_state` succeeded.
+    suspended: bool,
     vmm_maps: Option<Vec<MappingInfo>>,
     mem: Option<GuestMemory>,
     acked_features: u64,
@@ -367,21 +336,25 @@ pub struct DeviceRequestHandler<T: VhostUserDevice> {
 pub struct DeviceRequestHandlerSnapshot {
     acked_features: u64,
     acked_protocol_features: u64,
-    vrings: Vec<VringSnapshot>,
     backend: Vec<u8>,
 }
 
 impl<T: VhostUserDevice> DeviceRequestHandler<T> {
     /// Creates a vhost-user handler instance for `backend`.
-    pub(crate) fn new(backend: T) -> Self {
+    pub(crate) fn new(mut backend: T) -> Self {
         let mut vrings = Vec::with_capacity(backend.max_queue_num());
         for _ in 0..backend.max_queue_num() {
             vrings.push(Vring::new(Queue::MAX_SIZE, backend.features()));
         }
 
+        let suspended = backend
+            .enter_suspended_state()
+            .expect("enter_suspended_state failed on device init");
+
         DeviceRequestHandler {
             vrings,
             owned: false,
+            suspended,
             vmm_maps: None,
             mem: None,
             acked_features: 0,
@@ -552,18 +525,30 @@ impl<T: VhostUserDevice> vmm_vhost::Backend for DeviceRequestHandler<T> {
         // "The back-end must [...] stop ring upon receiving VHOST_USER_GET_VRING_BASE."
         // We only call `queue.set_ready()` when starting the queue, so if the queue is ready, that
         // means it is started and should be stopped.
-        if vring.queue.ready() {
-            if let Err(e) = self.backend.stop_queue(index as usize) {
-                error!("Failed to stop queue in get_vring_base: {:#}", e);
-            }
+        let vring_base = if vring.queue.ready() {
+            let queue = match self.backend.stop_queue(index as usize) {
+                Ok(q) => q,
+                Err(e) => {
+                    error!("Failed to stop queue in get_vring_base: {:#}", e);
+                    return Err(VhostError::BackendInternalError);
+                }
+            };
 
             vring.reset();
+
+            queue.next_avail_to_process()
+        } else {
+            0
+        };
+
+        if self.vrings.iter().all(|vring| !vring.queue.ready()) {
+            self.suspended = self
+                .backend
+                .enter_suspended_state()
+                .map_err(VhostError::EnterSuspendedState)?;
         }
 
-        Ok(VhostUserVringState::new(
-            index,
-            vring.queue.next_avail().0 as u32,
-        ))
+        Ok(VhostUserVringState::new(index, vring_base.into()))
     }
 
     fn set_vring_kick(&mut self, index: u8, file: Option<File>) -> VhostResult<()> {
@@ -606,6 +591,8 @@ impl<T: VhostUserDevice> vmm_vhost::Backend for DeviceRequestHandler<T> {
             error!("Failed to start queue {}: {}", index, e);
             return Err(VhostError::BackendInternalError);
         }
+
+        self.suspended = false;
 
         Ok(())
     }
@@ -732,51 +719,15 @@ impl<T: VhostUserDevice> vmm_vhost::Backend for DeviceRequestHandler<T> {
         })
     }
 
-    fn sleep(&mut self) -> VhostResult<()> {
-        for (index, vring) in self
-            .vrings
-            .iter_mut()
-            .enumerate()
-            .filter(|(_index, vring)| vring.queue.ready())
-        {
-            match self.backend.stop_queue(index) {
-                Ok(queue) => vring.paused_queue = Some(queue),
-                Err(e) => {
-                    error!("failed to stop queue index {}: {:#}", index, e);
-                    return Err(VhostError::StopQueueError(e));
-                }
-            }
-        }
-        self.backend
-            .stop_non_queue_workers()
-            .map_err(VhostError::SleepError)
-    }
-
-    fn wake(&mut self) -> VhostResult<()> {
-        for (index, vring) in self.vrings.iter_mut().enumerate() {
-            if let Some(queue) = vring.paused_queue.take() {
-                let mem = self.mem.clone().ok_or(VhostError::BackendInternalError)?;
-                let doorbell = vring.doorbell.clone().expect("Failed to clone doorbell");
-
-                if let Err(e) = self.backend.start_queue(index, queue, mem, doorbell) {
-                    error!("Failed to start queue {}: {}", index, e);
-                    return Err(VhostError::BackendInternalError);
-                }
-            }
-        }
-        Ok(())
-    }
-
     fn snapshot(&mut self) -> VhostResult<Vec<u8>> {
+        if !self.suspended {
+            return Err(VhostError::InvalidOperation);
+        }
         match serde_json::to_vec(&DeviceRequestHandlerSnapshot {
             acked_features: self.acked_features,
             acked_protocol_features: self.acked_protocol_features.bits(),
-            vrings: self
-                .vrings
-                .iter()
-                .map(|vring| vring.snapshot())
-                .collect::<anyhow::Result<Vec<VringSnapshot>>>()
-                .map_err(VhostError::SnapshotError)?,
+            // TODO: The backend's snapshot is getting double serialized. Consider serializing just
+            // the other fields and then passing along the backend's snapshot as is.
             backend: self.backend.snapshot().map_err(VhostError::SnapshotError)?,
         }) {
             Ok(serialized_json) => Ok(serialized_json),
@@ -787,7 +738,10 @@ impl<T: VhostUserDevice> vmm_vhost::Backend for DeviceRequestHandler<T> {
         }
     }
 
-    fn restore(&mut self, data_bytes: &[u8], queue_evts: Vec<File>) -> VhostResult<()> {
+    fn restore(&mut self, data_bytes: &[u8]) -> VhostResult<()> {
+        if !self.suspended {
+            return Err(VhostError::InvalidOperation);
+        }
         let device_request_handler_snapshot: DeviceRequestHandlerSnapshot =
             serde_json::from_slice(data_bytes).map_err(|e| {
                 error!("Failed to deserialize DeviceRequestHandlerSnapshot: {}", e);
@@ -805,43 +759,6 @@ impl<T: VhostUserDevice> vmm_vhost::Backend for DeviceRequestHandler<T> {
             )
         })
         .map_err(VhostError::RestoreError)?;
-
-        let mem = self.mem.as_ref().ok_or(VhostError::InvalidOperation)?;
-
-        let snapshotted_vrings = device_request_handler_snapshot.vrings;
-        assert_eq!(snapshotted_vrings.len(), self.vrings.len());
-
-        let mut queue_evts_iter = if queue_evts.is_empty() {
-            None
-        } else {
-            Some(queue_evts.into_iter())
-        };
-
-        for (index, (vring, snapshotted_vring)) in self
-            .vrings
-            .iter_mut()
-            .zip(snapshotted_vrings.into_iter())
-            .enumerate()
-        {
-            let queue_evt = if let Some(queue_evts_iter) = &mut queue_evts_iter {
-                // TODO(b/288596005): It is assumed that the index of `queue_evts` should map to the
-                // index of `self.vrings`. However, this assumption may break in the future, so a
-                // Map of indexes to queue_evt should be used to support sparse activated queues.
-                let queue_evt_file = queue_evts_iter
-                    .next()
-                    .ok_or(VhostError::VringIndexNotFound(index))?;
-                Some(VhostUserRegularOps::set_vring_kick(
-                    index as u8,
-                    Some(queue_evt_file),
-                )?)
-            } else {
-                None
-            };
-
-            vring
-                .restore(snapshotted_vring, mem, queue_evt)
-                .map_err(VhostError::RestoreError)?;
-        }
 
         self.backend
             .restore(device_request_handler_snapshot.backend)
@@ -1195,10 +1112,17 @@ mod tests {
             activate(&mut vmm_device);
 
             println!("virtio_sleep");
-            vmm_device.virtio_sleep().unwrap();
+            let queues = vmm_device
+                .virtio_sleep()
+                .unwrap()
+                .expect("virtio_sleep unexpectedly returned None");
 
             println!("virtio_wake");
-            vmm_device.virtio_wake(None).unwrap();
+            let mem = GuestMemory::new(&[(GuestAddress(0x0), 0x10000)]).unwrap();
+            let interrupt = Interrupt::new_for_test_with_msix();
+            vmm_device
+                .virtio_wake(Some((mem, interrupt, queues)))
+                .unwrap();
 
             println!("wait for shutdown signal");
             shutdown_rx.recv().unwrap();
@@ -1273,10 +1197,21 @@ mod tests {
         }
 
         // VhostUserFrontend::virtio_sleep()
-        handle_request(&mut req_handler, FrontendReq::SLEEP).unwrap();
+        for _ in 0..QUEUES_NUM {
+            handle_request(&mut req_handler, FrontendReq::SET_VRING_ENABLE).unwrap();
+            handle_request(&mut req_handler, FrontendReq::GET_VRING_BASE).unwrap();
+        }
 
         // VhostUserFrontend::virtio_wake()
-        handle_request(&mut req_handler, FrontendReq::WAKE).unwrap();
+        handle_request(&mut req_handler, FrontendReq::SET_MEM_TABLE).unwrap();
+        for _ in 0..QUEUES_NUM {
+            handle_request(&mut req_handler, FrontendReq::SET_VRING_NUM).unwrap();
+            handle_request(&mut req_handler, FrontendReq::SET_VRING_ADDR).unwrap();
+            handle_request(&mut req_handler, FrontendReq::SET_VRING_BASE).unwrap();
+            handle_request(&mut req_handler, FrontendReq::SET_VRING_CALL).unwrap();
+            handle_request(&mut req_handler, FrontendReq::SET_VRING_KICK).unwrap();
+            handle_request(&mut req_handler, FrontendReq::SET_VRING_ENABLE).unwrap();
+        }
 
         if allow_backend_req {
             // Make sure the connection still works even after sleep/wake.

devices/src/virtio/vhost/user/device/net.rs
@@ -203,6 +203,11 @@ where
         }
     }
 
+    fn enter_suspended_state(&mut self) -> anyhow::Result<bool> {
+        // No non-queue workers.
+        Ok(true)
+    }
+
     fn snapshot(&self) -> anyhow::Result<Vec<u8>> {
         serde_json::to_vec(&NetBackendSnapshot {
             acked_feature: self.acked_features,

devices/src/virtio/vhost/user/device/snd.rs
@@ -397,8 +397,8 @@ impl VhostUserDevice for SndBackend {
         Ok(())
     }
 
-    fn stop_non_queue_workers(&mut self) -> anyhow::Result<()> {
+    fn enter_suspended_state(&mut self) -> anyhow::Result<bool> {
         // This device has no non-queue workers to stop.
-        Ok(())
+        Ok(true)
     }
 }

commit 078ef31b7a8c04529dcdfcb1badefea6f5245d9e

devices: virtio: fix snapshot interrupt supression
When the snapshot & restore of virtqueues was split between frontend &
backend as part of fcf9a630e9668bfd7e8dec228e6ecf9887e946bf, a subtle
& unintentional change was introduced in how VIRTIO_RING_F_EVENT_IDX
operated. On restore, if interrupts were suppressed for a given
virtqueue, they would not only remain supressed, but the index tracking
when the last interrupt was sent would jump to the current used queue
head. This would stall the device because:
* The device was not sending interrupts.
* The driver, which could tell the device to send interrupts, was asleep
  and waiting for an interrupt from the device.

Thus circular waiting is established, and the device stalls.

This CL modifies initialization of split virtqueues to *always* start
with the "last index an interrupt was sent for" aka `last_used` set to
zero. This will generate at most one spurious interrupt per queue when
restoring a snapshot, which is safe and much simpler than plumbing the
actual `last_used` value through the vhost-user protocol.

NOTE: this was a regression which was introduced in
fcf9a630e9668bfd7e8dec228e6ecf9887e946bf.

BUG=b:363280532

TEST=verified that disabling VIRTIO_RING_F_EVENT_IDX fixed the
regression with no code changes. Verified that the regression goes away
once last_used is initialized to zero and VIRTIO_RING_F_EVENT_IDX is
enabled.

Change-Id: If9cacaeed40d7ba1496bd071cc4ffd765f4866de
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/6112791
Commit-Queue: Noah Gold <nkgold@google.com>
Reviewed-by: Frederick Mayle <fmayle@google.com>

devices/src/virtio/queue/split_queue.rs
@@ -129,7 +129,14 @@ impl SplitQueue {
             features: config.acked_features(),
             next_avail: config.next_avail(),
             next_used: config.next_used(),
-            last_used: config.next_used(),
+
+            // WARNING: last_used controls interrupt suppression
+            // (VIRTIO_RING_F_EVENT_IDX). The only safe value initial value is
+            // zero (unless restoring a snapshot and the value that was stored
+            // on the device is known; however we do not bother with that in our
+            // snapshot system since it is much simpler to just use the zero
+            // value and send a potentially spurious interrupt on restore).
+            last_used: Wrapping(0),
         })
     }
 
@@ -147,9 +154,30 @@ impl SplitQueue {
             .mem
             .read_obj_from_addr_volatile(used_index_addr)
             .unwrap();
-        // We assume the vhost-user backend sent interrupts for any descriptors it marked used
-        // before it stopped processing the queue, so `last_used == next_used`.
-        self.last_used = self.next_used;
+
+        // Since the backend has not told us what its actual last_used value
+        // was, we have to assume that an interrupt must be sent when next
+        // available descriptor is used, so we set this to zero.
+        //
+        // But wait, one might ask, why can't we just assume the vhost-user
+        // backend has already sent interrupts for any descriptors it marked
+        // used before it stopped processing the queue? Then we could just
+        // initialize last_used as `last_used == next_used`, which would skip
+        // spurious interrupts and be more efficient. Right?
+        //
+        // If VIRTIO_RING_F_EVENT_IDX is enabled, then no. The reason is the
+        // device could be in an interrupt suppressed state and so it may indeed
+        // have marked some descriptors used, but not yet sent an interrupt for
+        // them. Once we set last_used = next_used, no interrupts will be sent
+        // to the driver until the driver updates next_used (see
+        // queue_wants_interrupt for details), but the driver will
+        // never wake up the device isn't sending any interrupts. Thus, the
+        // device stalls.
+        //
+        // NOTE: this value is not used by the snapshot/restore process, but we
+        // still want to pick a reasonable value here in case it is used in the
+        // future.
+        self.last_used = Wrapping(0);
     }
 
     pub fn next_avail_to_process(&self) -> u16 {