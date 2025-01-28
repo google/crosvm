google/crosvm

commit b339d3d723043cda0d34c6c598fb1bf9ebaf87d0

rutabaga_gfx/ffi: Add rutabaga_resource_import
Call into stream_renderer_import_resource, if available based on gfxstream_unstable API inclusion.

Depends on: https://android-review.googlesource.com/c/platform/hardware/google/gfxstream/+/3407864

Change-Id: I1e0c1d4e0109ca252c16f13c3876b48d699ec1b3
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/6108318
Commit-Queue: Gurchetan Singh <gurchetansingh@chromium.org>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

rutabaga_gfx/ffi/src/lib.rs
@@ -169,6 +169,9 @@ pub struct rutabaga_command {
 }
 
 #[allow(non_camel_case_types)]
+type rutabaga_import_data = RutabagaImportData;
+
+#[allow(non_camel_case_types)]
 pub type rutabaga_fence_callback = extern "C" fn(user_data: u64, fence: &rutabaga_fence);
 
 #[allow(non_camel_case_types)]
@@ -425,6 +428,27 @@ pub extern "C" fn rutabaga_resource_create_3d(
 ) -> i32 {
     catch_unwind(AssertUnwindSafe(|| {
         let result = ptr.resource_create_3d(resource_id, *create_3d);
+        return_result(result)
+    }))
+    .unwrap_or(-ESRCH)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn rutabaga_resource_import(
+    ptr: &mut rutabaga,
+    resource_id: u32,
+    import_handle: &rutabaga_handle,
+    import_data: &rutabaga_import_data,
+) -> i32 {
+    catch_unwind(AssertUnwindSafe(|| {
+        let internal_handle = RutabagaHandle {
+            os_handle: RutabagaDescriptor::from_raw_descriptor(
+                (*import_handle).os_handle.try_into().unwrap(),
+            ),
+            handle_type: (*import_handle).handle_type,
+        };
+
+        let result = ptr.resource_import(resource_id, internal_handle, *import_data);
         return_result(result)
     }))
     .unwrap_or(-ESRCH)

rutabaga_gfx/ffi/src/include/rutabaga_gfx_ffi.h
@@ -107,6 +107,11 @@ extern "C" {
 #define RUTABAGA_DEBUG_WARN 0x2
 #define RUTABAGA_DEBUG_INFO 0x3
 
+/**
+ * Rutabaga resource import flags
+ */
+#define RUTABAGA_IMPORT_FLAG_3D_INFO (1 << 0)
+
 struct rutabaga;
 
 struct rutabaga_create_blob {
@@ -127,6 +132,19 @@ struct rutabaga_create_3d {
     uint32_t last_level;
     uint32_t nr_samples;
     uint32_t flags;
+};
+
+struct rutabaga_import_data {
+    uint32_t flags;
+    struct {
+        uint32_t width;
+        uint32_t height;
+        uint32_t drm_fourcc;
+        uint32_t strides[4];
+        uint32_t offsets[4];
+        uint64_t modifier;
+        bool guest_cpu_mappable;
+    } info_3d;
 };
 
 struct rutabaga_transfer {
@@ -365,6 +383,10 @@ int32_t rutabaga_snapshot(struct rutabaga *ptr, const char *dir);
 int32_t rutabaga_restore(struct rutabaga *ptr, const char *dir);
 
 int32_t rutabaga_resource_wait_sync(struct rutabaga *ptr, uint32_t resource_id);
+
+int32_t rutabaga_resource_import(struct rutabaga *ptr, uint32_t resource_id,
+                                 const struct rutabaga_handle *import_handle,
+                                 const struct rutabaga_import_data *import_data);
 
 #ifdef __cplusplus
 }

rutabaga_gfx/src/gfxstream.rs
@@ -44,6 +44,9 @@ const STREAM_RENDERER_PARAM_WIN0_HEIGHT: u64 = 5;
 const STREAM_RENDERER_PARAM_DEBUG_CALLBACK: u64 = 6;
 const STREAM_RENDERER_PARAM_RENDERER_FEATURES: u64 = 11;
 
+#[cfg(gfxstream_unstable)]
+const STREAM_RENDERER_IMPORT_FLAG_3D_INFO: u32 = 1 << 0;
+
 #[repr(C)]
 #[derive(Clone, Copy, Debug)]
 pub struct stream_renderer_param {
@@ -89,6 +92,25 @@ pub type stream_renderer_fence = RutabagaFence;
 
 #[allow(non_camel_case_types)]
 pub type stream_renderer_debug = RutabagaDebug;
+
+#[cfg(gfxstream_unstable)]
+#[repr(C)]
+pub struct stream_renderer_3d_info {
+    pub width: u32,
+    pub height: u32,
+    pub drm_fourcc: u32,
+    pub strides: [u32; 4],
+    pub offsets: [u32; 4],
+    pub modifier: u64,
+}
+
+#[cfg(gfxstream_unstable)]
+#[repr(C)]
+pub struct stream_renderer_import_data {
+    pub flags: u32,
+    pub info_3d: stream_renderer_3d_info,
+    pub info_vulkan: stream_renderer_vulkan_info,
+}
 
 extern "C" {
     // Entry point for the stream renderer.
@@ -198,6 +220,13 @@ extern "C" {
 
     #[cfg(gfxstream_unstable)]
     fn stream_renderer_wait_sync_resource(res_handle: u32) -> c_int;
+
+    #[cfg(gfxstream_unstable)]
+    fn stream_renderer_import_resource(
+        res_handle: u32,
+        import_handle: *const stream_renderer_handle,
+        import_data: *const stream_renderer_import_data,
+    ) -> c_int;
 }
 
 /// The virtio-gpu backend state tracker which supports accelerated rendering.
@@ -529,6 +558,59 @@ impl RutabagaComponent for Gfxstream {
         // Safe because gfxstream is initialized by now, and the return value is checked before
         // returning a new resource. The backing buffers are not supplied with this call.
         let ret = unsafe { stream_renderer_resource_create(&mut args, null_mut(), 0) };
+        ret_to_res(ret)?;
+
+        Ok(RutabagaResource {
+            resource_id,
+            handle: None,
+            blob: false,
+            blob_mem: 0,
+            blob_flags: 0,
+            map_info: None,
+            info_2d: None,
+            info_3d: None,
+            vulkan_info: None,
+            backing_iovecs: None,
+            component_mask: 1 << (RutabagaComponentType::Gfxstream as u8),
+            size: 0,
+            mapping: None,
+        })
+    }
+
+    #[cfg(gfxstream_unstable)]
+    fn import(
+        &self,
+        resource_id: u32,
+        import_handle: RutabagaHandle,
+        import_data: RutabagaImportData,
+    ) -> RutabagaResult<RutabagaResource> {
+        let stream_handle = stream_renderer_handle {
+            os_handle: import_handle.os_handle.into_raw_descriptor() as i64,
+            handle_type: import_handle.handle_type,
+        };
+
+        // When importing and creating a new resource, 3D_INFO flag must be set. This flag should
+        // be the only flag set in the gfxstream call
+        assert!(0 != import_data.flags & STREAM_RENDERER_IMPORT_FLAG_3D_INFO);
+        let stream_import_data = stream_renderer_import_data {
+            flags: STREAM_RENDERER_IMPORT_FLAG_3D_INFO,
+            info_3d: stream_renderer_3d_info {
+                width: import_data.info_3d.width,
+                height: import_data.info_3d.height,
+                drm_fourcc: import_data.info_3d.drm_fourcc,
+                strides: import_data.info_3d.strides,
+                offsets: import_data.info_3d.offsets,
+                modifier: import_data.info_3d.modifier,
+            },
+            info_vulkan: Default::default(),
+        };
+
+        // SAFETY:
+        // Safe because gfxstream is initialized by now, and the return value is checked before
+        // returning a new resource. The backing buffers are not supplied with this call.
+        let ret = unsafe {
+            stream_renderer_import_resource(resource_id, &stream_handle, &stream_import_data)
+        };
         ret_to_res(ret)?;
 
         Ok(RutabagaResource {

rutabaga_gfx/src/rutabaga_core.rs
@@ -173,6 +173,29 @@ pub trait RutabagaComponent {
         })
     }
 
+    fn import(
+        &self,
+        resource_id: u32,
+        _import_handle: RutabagaHandle,
+        _import_data: RutabagaImportData,
+    ) -> RutabagaResult<RutabagaResource> {
+        Ok(RutabagaResource {
+            resource_id,
+            handle: None,
+            blob: false,
+            blob_mem: 0,
+            blob_flags: 0,
+            map_info: None,
+            info_2d: None,
+            info_3d: None,
+            vulkan_info: None,
+            backing_iovecs: None,
+            component_mask: 0,
+            size: 0,
+            mapping: None,
+        })
+    }
+
     /// Implementations must attach `vecs` to the resource.
     fn attach_backing(
         &self,
@@ -652,6 +675,28 @@ impl Rutabaga {
         }
 
         let resource = component.create_3d(resource_id, resource_create_3d)?;
+        self.resources.insert(resource_id, resource);
+        Ok(())
+    }
+
+    /// Creates and imports to a resource with the external `import_handle` and the `import_data`
+    /// metadata.
+    pub fn resource_import(
+        &mut self,
+        resource_id: u32,
+        import_handle: RutabagaHandle,
+        import_data: RutabagaImportData,
+    ) -> RutabagaResult<()> {
+        let component = self
+            .components
+            .get_mut(&self.default_component)
+            .ok_or(RutabagaError::InvalidComponent)?;
+
+        if self.resources.contains_key(&resource_id) {
+            return Err(RutabagaError::InvalidResourceId);
+        }
+
+        let resource = component.import(resource_id, import_handle, import_data)?;
         self.resources.insert(resource_id, resource);
         Ok(())
     }

rutabaga_gfx/src/rutabaga_utils.rs
@@ -98,6 +98,7 @@ pub struct RutabagaMapping {
 }
 
 /// Metadata associated with a swapchain, video or camera image.
+#[repr(C)]
 #[derive(Default, Copy, Clone, Debug)]
 pub struct Resource3DInfo {
     pub width: u32,
@@ -181,6 +182,14 @@ pub const RUTABAGA_DEBUG_INFO: u32 = 0x03;
 pub struct RutabagaDebug {
     pub debug_type: u32,
     pub message: *const c_char,
+}
+
+/// Import Data for resource_import
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct RutabagaImportData {
+    pub flags: u32,
+    pub info_3d: Resource3DInfo,
 }
 
 // SAFETY:

commit efe88a43b69bd6b3d058348cbd80133749abcfdb

rutabaga_gfx: ffi: add missing RutabagaImportData import
Fixes "cannot find type `RutabagaImportData` in this scope" when
compiling rutabaga_gfx/ffi.

TEST=cd rutabaga_gfx/ffi && cargo build

Fixes: b339d3d72304 ("rutabaga_gfx/ffi: Add rutabaga_resource_import")
Change-Id: I3424ec19657f3018cbfc18fe81b4c9bbc174157b
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/6208375
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

rutabaga_gfx/ffi/src/lib.rs
@@ -40,6 +40,7 @@ use rutabaga_gfx::RutabagaFence;
 use rutabaga_gfx::RutabagaFenceHandler;
 use rutabaga_gfx::RutabagaFromRawDescriptor;
 use rutabaga_gfx::RutabagaHandle;
+use rutabaga_gfx::RutabagaImportData;
 use rutabaga_gfx::RutabagaIntoRawDescriptor;
 use rutabaga_gfx::RutabagaIovec;
 use rutabaga_gfx::RutabagaResult;