google/crosvm

commit 2aca097c140d0082acfdb0b8fa620fdd29999b03

vm_memory: return the backing object from `Guest::offset_from_base`
Callers shouldn't assume there is only a single backing object.

virtio-video is the only user. Its code is designed around the
assumption there is only one backing object, so, for now, we just
validate that is the case for the regions it maps.

BUG=b:368143426,b:381972137

Change-Id: I4afb128979770b632244217c569f5a2be70e11d3
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/6240845
Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>
Commit-Queue: Frederick Mayle <fmayle@google.com>

devices/src/virtio/video/resource.rs
@@ -8,6 +8,7 @@ use std::convert::TryInto;
 use std::fmt;
 
 use base::linux::MemoryMappingBuilderUnix;
+use base::AsRawDescriptor;
 use base::FromRawDescriptor;
 use base::IntoRawDescriptor;
 use base::MemoryMappingArena;
@@ -198,6 +199,8 @@ pub enum GuestMemResourceCreationError {
     CantGetShmOffset(GuestMemoryError),
     #[error("error while cloning shm region descriptor: {0}")]
     DescriptorCloneError(base::Error),
+    #[error("guest memory with multiple shm objects not supported")]
+    MultipleShmObjects,
 }
 
 #[derive(Debug, ThisError)]
@@ -242,17 +245,20 @@ impl GuestResource {
             .map(|entry| {
                 let addr: u64 = entry.addr.into();
                 let length: u32 = entry.length.into();
-                let region_offset = mem
+                let (backing_obj, region_offset) = mem
                     .offset_from_base(GuestAddress(addr))
                     .map_err(GuestMemResourceCreationError::CantGetShmOffset)
                     .unwrap();
+                if region_desc.as_raw_descriptor() != backing_obj.as_raw_descriptor() {
+                    return Err(GuestMemResourceCreationError::MultipleShmObjects);
+                }
 
-                GuestMemArea {
+                Ok(GuestMemArea {
                     offset: region_offset,
                     length: length as usize,
-                }
+                })
             })
-            .collect();
+            .collect::<Result<_, _>>()?;
 
         let handle = GuestResourceHandle::GuestPages(GuestMemHandle {
             desc: region_desc,

vm_memory/src/guest_memory.rs
@@ -891,7 +891,7 @@ impl GuestMemory {
     ///
     /// (i) the memory mapping associated with the target region.
     /// (ii) the relative offset from the start of the target region to `guest_addr`.
-    /// (iii) the absolute offset from the start of the memory mapping to the target region.
+    /// (iii) the absolute offset from the start of the backing object to the target region.
     ///
     /// If no target region is found, an error is returned.
     pub fn find_region(&self, guest_addr: GuestAddress) -> Result<(&MemoryMapping, usize, u64)> {
@@ -910,6 +910,10 @@ impl GuestMemory {
 
     /// Convert a GuestAddress into an offset within the associated shm region.
     ///
+    /// A `GuestMemory` may have multiple backing objects and the offset is
+    /// only meaningful in relation to the associated backing object, so a
+    /// reference to it is included in the return value.
+    ///
     /// Due to potential gaps within GuestMemory, it is helpful to know the
     /// offset within the shm where a given address is found. This offset
     /// can then be passed to another process mapping the shm to read data
@@ -927,16 +931,24 @@ impl GuestMemory {
     /// let mut gm = GuestMemory::new(&vec![
     ///     (addr_a, 0x20000),
     ///     (addr_b, 0x30000)]).expect("failed to create GuestMemory");
-    /// let offset = gm.offset_from_base(GuestAddress(0x95000))
+    /// let (_backing_object, offset) = gm.offset_from_base(GuestAddress(0x95000))
     ///                .expect("failed to get offset");
     /// assert_eq!(offset, 0x35000);
     /// ```
-    pub fn offset_from_base(&self, guest_addr: GuestAddress) -> Result<u64> {
+    pub fn offset_from_base(
+        &self,
+        guest_addr: GuestAddress,
+    ) -> Result<(&(dyn AsRawDescriptor + Send + Sync), u64)> {
         self.regions
             .iter()
             .find(|region| region.contains(guest_addr))
             .ok_or(Error::InvalidGuestAddress(guest_addr))
-            .map(|region| region.obj_offset + guest_addr.offset_from(region.start()))
+            .map(|region| {
+                (
+                    region.shared_obj.as_ref(),
+                    region.obj_offset + guest_addr.offset_from(region.start()),
+                )
+            })
     }
 
     /// Copy all guest memory into `w`.

commit 2194474e96ebf1e882dd5113d63ef23eef80c2b4

devices: virtio-video: fix guest region comparison
After the modification in commit 2aca097, the virtio-video device was no
longer functional; it would attempt to compare the backing object of
each memory region with the backing object of the first region in the
list to ensure they were all the same, but the first region's backing
object was a duplicated file handle, so its .as_raw_descriptor() result
was not identical to the original backing object's .as_raw_descriptor().

Fix this by deferring the descriptor clone to the end, after the backing
object comparisons.

Fixes: 2aca097c140d ("vm_memory: return the backing object from `Guest::offset_from_base`")
Change-Id: Ic5b83b98bae75f719ec0ee9f5ad276acd1692c43
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/6471315
Reviewed-by: Frederick Mayle <fmayle@google.com>
Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>

devices/src/virtio/video/resource.rs
@@ -8,7 +8,6 @@ use std::convert::TryInto;
 use std::fmt;
 
 use base::linux::MemoryMappingBuilderUnix;
-use base::AsRawDescriptor;
 use base::FromRawDescriptor;
 use base::IntoRawDescriptor;
 use base::MemoryMappingArena;
@@ -225,11 +224,8 @@ impl GuestResource {
             Some(entry) => {
                 let addr: u64 = entry.addr.into();
 
-                let guest_region = mem
-                    .shm_region(GuestAddress(addr))
-                    .map_err(GuestMemResourceCreationError::CantGetShmRegion)?;
-                base::clone_descriptor(guest_region)
-                    .map_err(GuestMemResourceCreationError::DescriptorCloneError)?
+                mem.shm_region(GuestAddress(addr))
+                    .map_err(GuestMemResourceCreationError::CantGetShmRegion)?
             }
         };
 
@@ -254,7 +250,8 @@ impl GuestResource {
             .collect::<Result<_, _>>()?;
 
         let handle = GuestResourceHandle::GuestPages(GuestMemHandle {
-            desc: region_desc,
+            desc: base::clone_descriptor(region_desc)
+                .map_err(GuestMemResourceCreationError::DescriptorCloneError)?,
             mem_areas,
         });
 