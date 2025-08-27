6bda8e92446f92ddb516e03d3756d51e75f63e4e
Gurchetan Singh <gurchetansingh@google.com>
2025-08-27 08:07:44 -0700

    rutabaga_gfx: rework rutabaga_gfx <--> crosvm CI integration
    This CL reworks CI integration.  In the past, the rutabaga_gfx
    build.rs script relied on "../third_party/virglrenderer" +
    "../third_party/minigbm" being present.  For gfxstream, a
    "gfxstream_stub" feature was used.  Ideally, we would get the
    dependencies in the CI/CD process, rather than relying
    on in-tree checkouts.  This is exactly what the upstream
    rutabaga_gfx CI does:
    
    https://github.com/magma-gpu/rutabaga_gfx/pull/2
    
    Is it completely necessary to test these features in crosvm?
    Maybe, maybe not.
    
    The features "virglrenderer" and "gfxstream" are completely
    internal to rutabaga_gfx, and those are already build tested in
    upstream rutabaga_gfx.  And it's already standard operating procedure
    to test on hardware-enabled downstream CI's before updating rutabaga
    in crosvm.  Both the Android CI and ChromeOS CI are more capable
    than both the upstream rutabaga_gfx CI and the crosvm CI.
    For example:
    
    https://chromium-review.googlesource.com/q/topic:%22delete-rutabaga-in-chromeos%22
    
    caught issues in the ChromeOS CI, even though it passed the crosvm
    CI.
    
    There is the "minigbm" a.k.a "wl-dmabuf" feature though.  That
    actually does leak through the rutabaga interface into crosvm,
    most notably:
    
    devices/src/virtio/wl.rs
    
    But even THAT could be use rutabaga_gralloc's system gralloc with
    some refactoring.  Every CrOS board enables "wl-dmabuf" except Grunt,
    but the issue there is nobody is going to test with a Grunt.  Plus,
    one can probably replace virtio-wl with virtio-gpu entirely now.
    
    Either way, with the proviso that we COULD probably optimize/clean-up
    further, this just adds aarch64/x86_64 builds of gfxstream, virglrenderer
    and minigbm to the dev_container.  The files are copied over to the VMs
    from the container when necessary.
    
    The "gfxstream_stub" feature is removed since it was used to accomodate
    the crosvm CI anyways.
    
    dev_container version bumper to 0053.
    
    BUG=438557794
    TEST=
         tools/presubmit
         tools/dev_container cargo build --features=gfxstream,virgl_renderer,wl-dmabuf
         tools/dev_container cargo build --target=aarch64-unknown-linux-gnu \
         --features=gfxstream,virgl_renderer,wl-dmabuf
    
    Change-Id: Id7fd84a8123fb2b9fa6c255a1102019456eb2607
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/6886676
    Reviewed-by: Frederick Mayle <fmayle@google.com>
    Commit-Queue: Gurchetan Singh <gurchetansingh@chromium.org>

--- rutabaga_gfx/build.rs
+++ rutabaga_gfx/build.rs
@@ -3,247 +3,27 @@
 // found in the LICENSE file.
 
 use std::env;
-use std::fs;
-use std::fs::File;
-use std::io::Write;
-use std::path::Path;
-use std::path::PathBuf;
-use std::process::Command;
 
-use anyhow::anyhow;
-use anyhow::bail;
-use anyhow::Context;
-use anyhow::Result;
+use pkg_config::Error;
 
-fn is_native_build() -> bool {
-    env::var("HOST").unwrap() == env::var("TARGET").unwrap()
-}
+pub type PkgConfigResult<T> = std::result::Result<T, Error>;
 
-fn use_system_minigbm() -> bool {
-    println!("cargo:rerun-if-env-changed=CROSVM_BUILD_VARIANT");
-    println!("cargo:rerun-if-env-changed=CROSVM_USE_SYSTEM_MINIGBM");
-    env::var("CROSVM_BUILD_VARIANT").unwrap_or_default() == "chromeos"
-        || env::var("CROSVM_USE_SYSTEM_MINIGBM").unwrap_or_else(|_| "0".to_string()) != "0"
-}
-
-fn use_system_virglrenderer() -> bool {
-    println!("cargo:rerun-if-env-changed=CROSVM_BUILD_VARIANT");
-    println!("cargo:rerun-if-env-changed=CROSVM_USE_SYSTEM_VIRGLRENDERER");
-    env::var("CROSVM_BUILD_VARIANT").unwrap_or_default() == "chromeos"
-        || env::var("CROSVM_USE_SYSTEM_VIRGLRENDERER").unwrap_or_else(|_| "0".to_string()) != "0"
-}
-
-/// Returns the target triplet prefix for gcc commands. No prefix is required
-/// for native builds.
-fn get_cross_compile_prefix() -> String {
-    if is_native_build() {
-        return String::from("");
-    }
-
-    let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
-    let env = env::var("CARGO_CFG_TARGET_ENV").unwrap();
-    format!("{}-{}-{}-", arch, os, env)
-}
-
-/// For cross-compilation with meson, we need to pick a cross-file, which
-/// live in /usr/local/share/meson/cross.
-fn get_meson_cross_args() -> Vec<String> {
-    if is_native_build() {
-        Vec::new()
-    } else {
-        vec![
-            "--cross-file".to_string(),
-            env::var("CARGO_CFG_TARGET_ARCH").unwrap(),
-        ]
-    }
-}
-
-fn env_prepend_pkg_config_path(new_path: &Path) -> Result<()> {
-    const KEY: &str = "PKG_CONFIG_PATH";
-    let new_path_string = new_path
-        .to_str()
-        .ok_or(anyhow!("failed to convert path to string"))?;
-    if let Ok(original_value) = env::var(KEY) {
-        env::set_var(KEY, format!("{}:{}", new_path_string, original_value));
-    } else {
-        env::set_var(KEY, new_path);
-    };
+fn minigbm() -> PkgConfigResult<()> {
+    pkg_config::probe_library("gbm")?;
     Ok(())
 }
 
-/// Builds from pinned commit as static library and probes the generated pkgconfig file to emit
-/// cargo linking metadata
-fn build_and_probe_minigbm(out_dir: &Path) -> Result<()> {
-    const SOURCE_DIR: &str = "../third_party/minigbm";
-    let pkgconfig_file = out_dir.join("gbm.pc");
-
-    println!("cargo:rerun-if-changed={}", SOURCE_DIR);
-
-    if !Path::new(SOURCE_DIR).join(".git").exists() {
-        bail!(
-            "{} source does not exist, did you forget to \
-            `git submodule update --init`?",
-            SOURCE_DIR
-        );
-    }
-
-    // build static library
-    let make_flags = env::var("CARGO_MAKEFLAGS").unwrap();
-    let status = Command::new("make")
-        .env("MAKEFLAGS", make_flags)
-        .env("VERBOSE", "1")
-        .env("CROSS_COMPILE", get_cross_compile_prefix())
-        .env("PKG_CONFIG", "pkg-config")
-        .arg(format!("OUT={}", out_dir.display()))
-        .arg("CC_STATIC_LIBRARY(libminigbm.pie.a)")
-        .current_dir(SOURCE_DIR)
-        .status()?;
-    if !status.success() {
-        bail!("make failed with status: {}", status);
-    }
-
-    // copy headers to build output
-    let src_dir = Path::new(SOURCE_DIR);
-    fs::copy(src_dir.join("gbm.h"), out_dir.join("gbm.h"))?;
-    fs::copy(
-        src_dir.join("minigbm_helpers.h"),
-        out_dir.join("minigbm_helpers.h"),
-    )?;
-
-    // minigbm will be linked using the name gbm, make sure it can be found.
-    fs::copy(out_dir.join("libminigbm.pie.a"), out_dir.join("libgbm.a"))?;
-
-    // write out a custom pkgconfig
-    let mut conf = File::create(pkgconfig_file)?;
-    let contents = format!(
-        r#"prefix={install_dir}
-includedir=${{prefix}}
-libdir=${{prefix}}
-
-Name: libgbm
-Description: A small gbm implementation
-Version: 18.0.0
-Cflags: -I${{includedir}}
-Libs: -L${{libdir}} -lgbm
-Requires.private: libdrm >= 2.4.50
-"#,
-        install_dir = out_dir.display()
-    );
-    conf.write_all(contents.as_bytes())?;
-
-    // let pkg_config crate configure the cargo link metadata according to the custom pkgconfig
-    // above
-    env_prepend_pkg_config_path(out_dir)?;
-    let mut config = pkg_config::Config::new();
-    config.statik(true).probe("gbm")?;
-    Ok(())
-}
-
-fn minigbm() -> Result<()> {
-    if use_system_minigbm() {
-        pkg_config::probe_library("gbm").context("pkgconfig failed to find gbm")?;
-    } else {
-        // Otherwise build from source and emit cargo build metadata
-        let out_dir = PathBuf::from(env::var("OUT_DIR")?).join("minigbm");
-        build_and_probe_minigbm(&out_dir).context("failed building minigbm")?;
-    };
-    Ok(())
-}
-
-/// Builds from pinned commit as static library and probes the generated pkgconfig file to emit
-/// cargo linking metadata
-fn build_and_probe_virglrenderer(out_dir: &Path) -> Result<()> {
-    const SOURCE_DIR: &str = "../third_party/virglrenderer";
-    let install_prefix = out_dir.join("installed");
-
-    println!("cargo:rerun-if-changed={}", SOURCE_DIR);
-
-    if !Path::new(SOURCE_DIR).join(".git").exists() {
-        bail!(
-            "{} source does not exist, did you forget to \
-                `git submodule update --init`?",
-            SOURCE_DIR
-        );
-    }
-
-    let mut platforms = vec!["egl"];
-    if env::var("CARGO_FEATURE_X").is_ok() {
-        platforms.push("glx");
-    }
-
-    // Ensures minigbm is available and that it's pkgconfig is locatable
-    minigbm()?;
-
-    let mut setup = Command::new("meson");
-    setup
-        .arg("setup")
-        .current_dir(SOURCE_DIR)
-        .arg("--prefix")
-        .arg(install_prefix.as_os_str())
-        .arg("--libdir")
-        .arg("lib")
-        .args(get_meson_cross_args())
-        .arg(format!("-Dplatforms={}", platforms.join(",")))
-        .arg("-Ddefault_library=static")
-        .arg(out_dir.as_os_str());
-
-    let setup_status = setup.status()?;
-    if !setup_status.success() {
-        bail!("meson setup failed with status: {}", setup_status);
-    }
-
-    let mut compile = Command::new("meson");
-    compile
-        .arg("compile")
-        .arg("src/virglrenderer")
-        .current_dir(out_dir);
-    let compile_status = compile.status()?;
-    if !compile_status.success() {
-        bail!("meson compile failed with status: {}", compile_status);
-    }
-
-    let mut install = Command::new("meson");
-    install.arg("install").current_dir(out_dir);
-    let install_status = install.status()?;
-    if !install_status.success() {
-        bail!("meson install failed with status: {}", install_status);
-    }
-
-    let pkg_config_path = install_prefix.join("lib/pkgconfig");
-    assert!(pkg_config_path.join("virglrenderer.pc").exists());
-
-    // let pkg_config crate configure the cargo link metadata according to the generated pkgconfig
-    env_prepend_pkg_config_path(pkg_config_path.as_path())?;
-    let mut config = pkg_config::Config::new();
-    config.statik(true).probe("virglrenderer")?;
-
-    Ok(())
-}
-
-fn virglrenderer() -> Result<()> {
-    if use_system_virglrenderer() && !use_system_minigbm() {
-        bail!("Must use system minigbm if using system virglrenderer (try setting CROSVM_USE_SYSTEM_MINIGBM=1)");
-    }
-
-    // Use virglrenderer package from pkgconfig on ChromeOS builds
-    if use_system_virglrenderer() {
-        let lib = pkg_config::Config::new()
-            .atleast_version("1.0.0")
-            .probe("virglrenderer")
-            .context("pkgconfig failed to find virglrenderer")?;
-        if lib.defines.contains_key("VIRGL_RENDERER_UNSTABLE_APIS") {
-            println!("cargo:rustc-cfg=virgl_renderer_unstable");
-        }
-    } else {
-        // Otherwise build from source.
-        let out_dir = PathBuf::from(env::var("OUT_DIR")?).join("virglrenderer");
-        build_and_probe_virglrenderer(&out_dir)?;
+fn virglrenderer() -> PkgConfigResult<()> {
+    let lib = pkg_config::Config::new()
+        .atleast_version("1.0.0")
+        .probe("virglrenderer")?;
+    if lib.defines.contains_key("VIRGL_RENDERER_UNSTABLE_APIS") {
+        println!("cargo:rustc-cfg=virgl_renderer_unstable");
     }
     Ok(())
 }
 
-fn gfxstream() -> Result<()> {
+fn gfxstream() -> PkgConfigResult<()> {
     let mut gfxstream_path_env_override =
         // We use the unrecommended PROFILE environment variable here, because the Windows
         // downstream can set debug = true for the release profile to keep the symbol files.
@@ -297,7 +77,7 @@ fn gfxstream() -> Result<()> {
     }
 }
 
-fn main() -> Result<()> {
+fn main() -> PkgConfigResult<()> {
     println!("cargo:rustc-check-cfg=cfg(fence_passing_option1)");
     println!("cargo:rustc-check-cfg=cfg(gfxstream_unstable)");
     println!("cargo:rustc-check-cfg=cfg(virgl_renderer_unstable)");
@@ -308,13 +88,13 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
-    if env::var("CARGO_FEATURE_MINIGBM").is_ok() {
-        minigbm()?;
-    }
-
     if env::var("CARGO_FEATURE_VIRGL_RENDERER").is_ok() {
         virglrenderer()?;
         use_fence_passing_option1 = false;
+    }
+
+    if env::var("CARGO_FEATURE_MINIGBM").is_ok() {
+        minigbm()?;
     }
 
     if env::var("CARGO_FEATURE_GFXSTREAM").is_ok()
