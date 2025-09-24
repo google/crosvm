494b0d0ab143a2d15d1af0448cee01c2a386167c
Rob Mohr <mohrr@google.com>
2025-09-24 07:39:26 -0700

    Add API and TEST_API variables
    Eventually these will be required.
    
    Bug: b/446916327
    Change-Id: I2142a147fd22b21da1051bb054172e8bdfaa09ae
    Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/6978213
    Commit-Queue: David Stevens <stevensd@chromium.org>
    Auto-Submit: Rob Mohr <mohrr@google.com>
    Reviewed-by: David Stevens <stevensd@chromium.org>

--- infra/recipe_modules/crosvm/__init__.py
+++ infra/recipe_modules/crosvm/__init__.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from . import api as _api
+
 PYTHON_VERSION_COMPATIBILITY = "PY3"
 
 DEPS = [
@@ -17,3 +19,5 @@ DEPS = [
     "recipe_engine/raw_io",
     "recipe_engine/step",
 ]
+
+API = _api.CrosvmApi
