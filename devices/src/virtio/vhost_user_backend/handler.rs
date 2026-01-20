google/crosvm

commit f04b57a1c5da6fc19d0c38b49e54ce19f871c52f

devices: vhost_user_backend: advertise REPLY_ACK feature
Since this is supported by the framework, advertise it for all of our
vhost-user backend devices.

Note that crosvm's vhost-user frontend doesn't negotiate REPLY_ACK yet,
so, by itself, this commit is a no-op for users of crosvm as a VMM.

Change-Id: I42dfc53b20e5ccc2a6e31f999a6ed575e7680e8e
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/7417305
Commit-Queue: Frederick Mayle <fmayle@google.com>
Reviewed-by: Takaya Saeki <takayas@chromium.org>

devices/src/virtio/vhost_user_backend/handler.rs
@@ -427,7 +427,7 @@ impl<T: VhostUserDevice> vmm_vhost::Backend for DeviceRequestHandler<T> {
     }
 
     fn get_protocol_features(&mut self) -> VhostResult<VhostUserProtocolFeatures> {
-        Ok(self.backend.protocol_features())
+        Ok(self.backend.protocol_features() | VhostUserProtocolFeatures::REPLY_ACK)
     }
 
     fn set_protocol_features(&mut self, features: u64) -> VhostResult<()> {

commit ea45ab7bf157ca5d9faf779a558c76e48ba6ddbb

devices: vhost_user: fix REPLY_ACK backend impl
The VhostUserProtocolFeatures::REPLY_ACK bit is always set in
get_protocol_features(), so it's always advertised to frontends, but
when the frontend actually tries to enable that feature, it's not
included in `supported`, and so doesn't actually get acked.

To fix this, reuse get_protocol_features() as part of
set_protocol_features() so any adjustments made to the device's list
of supported features by get_protocol_features() are also applied in
set_protocol_features().

TEST=Run a crosvm GPU backend against a rust-vmm vhost frontend.

Fixes: f04b57a1c ("devices: vhost_user_backend: advertise REPLY_ACK feature")
Change-Id: I356a87b548d75ad2ceb9c3565fea2676e05d9667
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/7484697
Commit-Queue: Frederick Mayle <fmayle@google.com>
Reviewed-by: Takaya Saeki <takayas@chromium.org>
Reviewed-by: Frederick Mayle <fmayle@google.com>
Commit-Queue: Zihan Chen <zihanchen@google.com>

devices/src/virtio/vhost_user_backend/handler.rs
@@ -441,7 +441,7 @@ impl<T: VhostUserDevice> vmm_vhost::Backend for DeviceRequestHandler<T> {
                 return Err(VhostError::InvalidOperation);
             }
         };
-        let supported = self.backend.protocol_features();
+        let supported = self.get_protocol_features()?;
         self.acked_protocol_features = features & supported;
         Ok(())
     }