google/crosvm

commit 729f98cd5abbd09a2fafae527d6502594df827b0

devices: vhost-user: Update GET_SHMEM_CONFIG messages
Update protocol messages associated with GET_SHMEM_CONFIG request following the standard
definitions: https://lore.kernel.org/all/20251111091058.879669-1-aesteve@redhat.com

BUG=b:445229097
TEST=./tools/dev_container ./tools/presubmit

Change-Id: I9113d4fa7430af44714a54ead38f56c067148296
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/7270969
Reviewed-by: Frederick Mayle <fmayle@google.com>
Commit-Queue: Frederick Mayle <fmayle@google.com>

devices/src/virtio/vhost_user_backend/handler.rs
@@ -79,7 +79,6 @@ use vm_control::VmMemorySource;
 use vm_memory::GuestAddress;
 use vm_memory::GuestMemory;
 use vm_memory::MemoryRegion;
-use vmm_vhost::message::VhostSharedMemoryRegion;
 use vmm_vhost::message::VhostUserConfigFlags;
 use vmm_vhost::message::VhostUserExternalMapMsg;
 use vmm_vhost::message::VhostUserGpuMapMsg;
@@ -87,6 +86,7 @@ use vmm_vhost::message::VhostUserInflight;
 use vmm_vhost::message::VhostUserMemoryRegion;
 use vmm_vhost::message::VhostUserMigrationPhase;
 use vmm_vhost::message::VhostUserProtocolFeatures;
+use vmm_vhost::message::VhostUserShMemConfigHeader;
 use vmm_vhost::message::VhostUserShmemMapMsg;
 use vmm_vhost::message::VhostUserShmemMapMsgFlags;
 use vmm_vhost::message::VhostUserShmemUnmapMsg;
@@ -828,11 +828,11 @@ impl<T: VhostUserDevice> vmm_vhost::Backend for DeviceRequestHandler<T> {
         }
     }
 
-    fn get_shared_memory_regions(&mut self) -> VhostResult<Vec<VhostSharedMemoryRegion>> {
+    fn get_shmem_config(&mut self) -> VhostResult<(VhostUserShMemConfigHeader, Vec<u64>)> {
         Ok(if let Some(r) = self.backend.get_shared_memory_region() {
-            vec![VhostSharedMemoryRegion::new(r.id, r.length)]
+            (VhostUserShMemConfigHeader::new(1), vec![r.length])
         } else {
-            Vec::new()
+            (VhostUserShMemConfigHeader::new(0), Vec::new())
         })
     }
 }

devices/src/virtio/vhost_user_backend/vsock.rs
@@ -24,12 +24,12 @@ use vhost::Vsock;
 use vm_memory::GuestMemory;
 use vmm_vhost::connection::Connection;
 use vmm_vhost::message::BackendReq;
-use vmm_vhost::message::VhostSharedMemoryRegion;
 use vmm_vhost::message::VhostUserConfigFlags;
 use vmm_vhost::message::VhostUserInflight;
 use vmm_vhost::message::VhostUserMemoryRegion;
 use vmm_vhost::message::VhostUserMigrationPhase;
 use vmm_vhost::message::VhostUserProtocolFeatures;
+use vmm_vhost::message::VhostUserShMemConfigHeader;
 use vmm_vhost::message::VhostUserSingleMemoryRegion;
 use vmm_vhost::message::VhostUserTransferDirection;
 use vmm_vhost::message::VhostUserVringAddrFlags;
@@ -440,8 +440,8 @@ impl vmm_vhost::Backend for VsockBackend {
         Err(Error::InvalidOperation)
     }
 
-    fn get_shared_memory_regions(&mut self) -> Result<Vec<VhostSharedMemoryRegion>> {
-        Ok(vec![])
+    fn get_shmem_config(&mut self) -> Result<(VhostUserShMemConfigHeader, Vec<u64>)> {
+        Ok((VhostUserShMemConfigHeader::new(0), Vec::new()))
     }
 }
 

devices/src/virtio/vhost_user_frontend/mod.rs
@@ -521,32 +521,31 @@ impl VirtioDevice for VhostUserFrontend {
         if let Some(r) = self.shmem_region.borrow().as_ref() {
             return r.clone();
         }
-        let regions = match self
+        let (config_hdr, sizes) = match self
             .backend_client
-            .get_shared_memory_regions()
+            .get_shmem_config()
             .map_err(Error::ShmemRegions)
         {
             Ok(x) => x,
             Err(e) => {
-                error!("Failed to get shared memory regions {}", e);
+                error!("Failed to get shared memory config {}", e);
                 return None;
             }
         };
-        let region = match regions.len() {
+        let region = match config_hdr.nregions {
             0 => None,
             1 => Some(SharedMemoryRegion {
-                id: regions[0].id,
-                length: regions[0].length,
+                id: 0,
+                length: sizes[0],
             }),
             n => {
                 error!(
-                    "Failed to get shared memory regions {}",
-                    Error::TooManyShmemRegions(n)
+                    "Failed to get shared memory region {}",
+                    Error::TooManyShmemRegions(n as usize)
                 );
                 return None;
             }
         };
-
         *self.shmem_region.borrow_mut() = Some(region.clone());
         region
     }

third_party/vmm_vhost/src/backend_client.rs
@@ -368,27 +368,20 @@ impl BackendClient {
         self.wait_for_ack(&hdr)
     }
 
-    /// Gets the shared memory regions used by the device.
-    pub fn get_shared_memory_regions(&self) -> Result<Vec<VhostSharedMemoryRegion>> {
-        let hdr = self.send_request_header(FrontendReq::GET_SHARED_MEMORY_REGIONS, None)?;
-        let (body_reply, buf_reply, rfds) = self.recv_reply_with_payload::<VhostUserU64>(&hdr)?;
-        let struct_size = mem::size_of::<VhostSharedMemoryRegion>();
-        if !rfds.is_empty() || buf_reply.len() != body_reply.value as usize * struct_size {
-            return Err(VhostUserError::InvalidMessage);
+    /// Get the shared memory configuration.
+    pub fn get_shmem_config(&self) -> Result<(VhostUserShMemConfigHeader, Vec<u64>)> {
+        let hdr = self.send_request_header(FrontendReq::GET_SHMEM_CONFIG, None)?;
+        let (body_reply, buf_reply, _rfds) =
+            self.recv_reply_with_payload::<VhostUserShMemConfigHeader>(&hdr)?;
+        let mut memory_sizes = Vec::new();
+        for i in 0..body_reply.nregions {
+            const U64_SIZE: usize = mem::size_of::<u64>();
+            let offset = (i as usize) * U64_SIZE;
+            let value =
+                VhostUserU64::read_from_bytes(&buf_reply[offset..(offset + U64_SIZE)]).unwrap();
+            memory_sizes.push(value.value);
         }
-        let mut regions = Vec::new();
-        let mut offset = 0;
-        for _ in 0..body_reply.value {
-            regions.push(
-                // Can't fail because the input is the correct size.
-                VhostSharedMemoryRegion::read_from_bytes(
-                    &buf_reply[offset..(offset + struct_size)],
-                )
-                .unwrap(),
-            );
-            offset += struct_size;
-        }
-        Ok(regions)
+        Ok((body_reply, memory_sizes))
     }
 
     fn send_request_header(

third_party/vmm_vhost/src/backend_server.rs
@@ -75,7 +75,7 @@ pub trait Backend {
         fd: File,
     ) -> Result<Option<File>>;
     fn check_device_state(&mut self) -> Result<()>;
-    fn get_shared_memory_regions(&mut self) -> Result<Vec<VhostSharedMemoryRegion>>;
+    fn get_shmem_config(&mut self) -> Result<(VhostUserShMemConfigHeader, Vec<u64>)>;
 }
 
 impl<T> Backend for T
@@ -209,8 +209,8 @@ where
         self.as_mut().check_device_state()
     }
 
-    fn get_shared_memory_regions(&mut self) -> Result<Vec<VhostSharedMemoryRegion>> {
-        self.as_mut().get_shared_memory_regions()
+    fn get_shmem_config(&mut self) -> Result<(VhostUserShMemConfigHeader, Vec<u64>)> {
+        self.as_mut().get_shmem_config()
     }
 }
 
@@ -655,12 +655,11 @@ impl<S: Backend> BackendServer<S> {
                 self.send_reply_message(&hdr, &msg)?;
                 res?;
             }
-            Ok(FrontendReq::GET_SHARED_MEMORY_REGIONS) => {
-                let regions = self.backend.get_shared_memory_regions()?;
+            Ok(FrontendReq::GET_SHMEM_CONFIG) => {
+                let (msg, sizes) = self.backend.get_shmem_config()?;
                 let mut buf = Vec::new();
-                let msg = VhostUserU64::new(regions.len() as u64);
-                for r in regions {
-                    buf.extend_from_slice(r.as_bytes())
+                for e in sizes {
+                    buf.extend_from_slice(e.as_bytes())
                 }
                 self.send_reply_with_payload(&hdr, &msg, buf.as_slice())?;
             }

third_party/vmm_vhost/src/test_backend.rs
@@ -277,7 +277,7 @@ impl Backend for TestBackend {
         Ok(())
     }
 
-    fn get_shared_memory_regions(&mut self) -> Result<Vec<VhostSharedMemoryRegion>> {
-        Ok(Vec::new())
+    fn get_shmem_config(&mut self) -> Result<(VhostUserShMemConfigHeader, Vec<u64>)> {
+        Ok((VhostUserShMemConfigHeader::new(0), Vec::new()))
     }
 }

third_party/vmm_vhost/src/message.rs
@@ -151,10 +151,8 @@ pub enum FrontendReq {
     /// After transferring the back-end’s internal state during migration, check whether the
     /// back-end was able to successfully fully process the state.
     CHECK_DEVICE_STATE = 43,
-
-    // Non-standard message types.
-    /// Get a list of the device's shared memory regions.
-    GET_SHARED_MEMORY_REGIONS = 1004,
+    /// Retrieve Shared Memory Regions configuration.
+    GET_SHMEM_CONFIG = 44,
 }
 
 impl From<FrontendReq> for u32 {
@@ -1195,25 +1193,28 @@ impl QueueRegionPacked {
     }
 }
 
-/// Virtio shared memory descriptor.
 #[repr(C)]
-#[derive(Default, Copy, Clone, FromBytes, Immutable, IntoBytes, KnownLayout)]
-pub struct VhostSharedMemoryRegion {
-    /// The shared memory region's shmid.
-    pub id: u8,
-    /// Padding
-    padding: [u8; 7],
-    /// The length of the shared memory region.
-    pub length: u64,
+#[derive(Debug, Default, Copy, Clone, FromBytes, Immutable, IntoBytes, KnownLayout)]
+pub struct VhostUserShMemConfigHeader {
+    /// Total number of shared memory regions
+    pub nregions: u32,
+    /// Padding for correct alignment
+    padding: u32,
 }
 
-impl VhostSharedMemoryRegion {
-    /// New instance of VhostSharedMemoryRegion struct
-    pub fn new(id: u8, length: u64) -> Self {
-        VhostSharedMemoryRegion {
-            id,
-            padding: [0; 7],
-            length,
+impl VhostUserMsgValidator for VhostUserShMemConfigHeader {
+    #[allow(clippy::if_same_then_else)]
+    fn is_valid(&self) -> bool {
+        true
+    }
+}
+
+impl VhostUserShMemConfigHeader {
+    /// Create a new instance
+    pub fn new(nregions: u32) -> Self {
+        Self {
+            nregions,
+            padding: 0,
         }
     }
 }

commit c7904eede809ffe68787f0a8cf59a85537dd294c

devices: vhost-user: fix transmitting shm ID
The new GET_SHMEM_CONFIG encodes VIRTIO shmids as indexes in the sizes
array in the message body.  Unused shmids are represented with a 0
entry in the size array.  crosvm was not implementing this correctly —
the single shared memory region was always transmitted as the sole
element in the array, meaning id 0, and the actual shmid was
discarded.  This meant that running crosvm against its own vhost-user
GPU backend didn't work, since the GPU shared memory region has shmid
1.

TEST=Run crosvm with a crosvm vhost-user GPU device.

Fixes: 729f98cd5 ("devices: vhost-user: Update GET_SHMEM_CONFIG messages")
Change-Id: I32db8d0acc8c633f9c61d9d6b2055e2bf448c335
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/7486920
Reviewed-by: Sergio Rodriguez <sorama@google.com>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>
Reviewed-by: Frederick Mayle <fmayle@google.com>

devices/src/virtio/mod.rs
@@ -43,6 +43,8 @@ pub mod vhost_user_backend;
 pub mod vhost_user_frontend;
 pub mod vsock;
 
+pub use vmm_vhost::SharedMemoryRegion;
+
 #[cfg(feature = "balloon")]
 pub use self::balloon::Balloon;
 #[cfg(feature = "balloon")]
@@ -102,7 +104,6 @@ pub use self::vhost_user_frontend::VhostUserFrontend;
 pub use self::video::VideoDevice;
 pub use self::virtio_device::SharedMemoryMapper;
 pub use self::virtio_device::SharedMemoryPrepareType;
-pub use self::virtio_device::SharedMemoryRegion;
 pub use self::virtio_device::VirtioDevice;
 pub use self::virtio_mmio_device::VirtioMmioDevice;
 pub use self::virtio_pci_device::PciCapabilityType;

devices/src/virtio/vhost_user_backend/handler.rs
@@ -830,12 +830,12 @@ impl<T: VhostUserDevice> vmm_vhost::Backend for DeviceRequestHandler<T> {
         }
     }
 
-    fn get_shmem_config(&mut self) -> VhostResult<Vec<u64>> {
-        Ok(if let Some(r) = self.backend.get_shared_memory_region() {
-            vec![r.length]
-        } else {
-            Vec::new()
-        })
+    fn get_shmem_config(&mut self) -> VhostResult<Vec<SharedMemoryRegion>> {
+        Ok(self
+            .backend
+            .get_shared_memory_region()
+            .into_iter()
+            .collect())
     }
 }
 

devices/src/virtio/vhost_user_backend/vsock.rs
@@ -35,6 +35,7 @@ use vmm_vhost::message::VhostUserVringAddrFlags;
 use vmm_vhost::message::VhostUserVringState;
 use vmm_vhost::Error;
 use vmm_vhost::Result;
+use vmm_vhost::SharedMemoryRegion;
 use vmm_vhost::VHOST_USER_F_PROTOCOL_FEATURES;
 use zerocopy::IntoBytes;
 
@@ -439,7 +440,7 @@ impl vmm_vhost::Backend for VsockBackend {
         Err(Error::InvalidOperation)
     }
 
-    fn get_shmem_config(&mut self) -> Result<Vec<u64>> {
+    fn get_shmem_config(&mut self) -> Result<Vec<SharedMemoryRegion>> {
         Ok(Vec::new())
     }
 }

devices/src/virtio/vhost_user_frontend/mod.rs
@@ -524,7 +524,7 @@ impl VirtioDevice for VhostUserFrontend {
         if let Some(r) = self.shmem_region.borrow().as_ref() {
             return r.clone();
         }
-        let sizes = match self
+        let regions = match self
             .backend_client
             .get_shmem_config()
             .map_err(Error::ShmemRegions)
@@ -535,12 +535,9 @@ impl VirtioDevice for VhostUserFrontend {
                 return None;
             }
         };
-        let region = match sizes.len() {
+        let region = match regions.len() {
             0 => None,
-            1 => Some(SharedMemoryRegion {
-                id: 0,
-                length: sizes[0],
-            }),
+            1 => Some(regions[0]),
             n => {
                 error!(
                     "Failed to get shared memory region {}",

devices/src/virtio/virtio_device.rs
@@ -31,14 +31,6 @@ pub enum SharedMemoryPrepareType {
     DynamicPerMapping,
 }
 
-#[derive(Clone)]
-pub struct SharedMemoryRegion {
-    /// The id of the shared memory region. A device may have multiple regions, but each
-    /// must have a unique id. The meaning of a particular region is device-specific.
-    pub id: u8,
-    pub length: u64,
-}
-
 /// Trait for mapping memory into the device's shared memory region.
 pub trait SharedMemoryMapper: Send {
     /// Maps the given |source| into the shared memory region at |offset|.

third_party/vmm_vhost/src/backend_client.rs
@@ -23,6 +23,7 @@ use crate::Error as VhostUserError;
 use crate::FrontendReq;
 use crate::Result as VhostUserResult;
 use crate::Result;
+use crate::SharedMemoryRegion;
 
 /// Client for a vhost-user device. The API is a thin abstraction over the vhost-user protocol.
 pub struct BackendClient {
@@ -368,16 +369,23 @@ impl BackendClient {
     }
 
     /// Get the shared memory configuration.
-    pub fn get_shmem_config(&self) -> Result<Vec<u64>> {
+    pub fn get_shmem_config(&self) -> Result<Vec<SharedMemoryRegion>> {
         let hdr = self.send_request_header(FrontendReq::GET_SHMEM_CONFIG, None)?;
         let (body_reply, buf_reply, _rfds) =
             self.recv_reply_with_payload::<VhostUserShMemConfigHeader>(&hdr)?;
-        let memory_sizes = <[u64]>::ref_from_bytes_with_elems(
-            buf_reply.as_slice(),
-            body_reply.nregions.try_into().unwrap(),
-        )
-        .map_err(|_| VhostUserError::InvalidMessage)?;
-        Ok(memory_sizes.to_vec())
+        let memory_sizes =
+            <[u64]>::ref_from_bytes(&buf_reply).map_err(|_| VhostUserError::InvalidMessage)?;
+
+        let shared_memory_regions = memory_sizes
+            .iter()
+            .enumerate()
+            .filter(|&(_, &n)| n != 0)
+            .take(body_reply.nregions.try_into().unwrap())
+            .map(|(id, &length)| id.try_into().map(|id| SharedMemoryRegion { id, length }))
+            .collect::<std::result::Result<_, _>>()
+            .map_err(|_| VhostUserError::OversizedMsg)?;
+
+        Ok(shared_memory_regions)
     }
 
     fn send_request_header(

third_party/vmm_vhost/src/backend_server.rs
@@ -1,6 +1,7 @@
 // Copyright (C) 2019 Alibaba Cloud Computing. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::cmp::max;
 use std::fs::File;
 use std::mem;
 
@@ -19,6 +20,7 @@ use crate::Connection;
 use crate::Error;
 use crate::FrontendReq;
 use crate::Result;
+use crate::SharedMemoryRegion;
 
 /// Trait for vhost-user backends.
 ///
@@ -75,7 +77,7 @@ pub trait Backend {
         fd: File,
     ) -> Result<Option<File>>;
     fn check_device_state(&mut self) -> Result<()>;
-    fn get_shmem_config(&mut self) -> Result<Vec<u64>>;
+    fn get_shmem_config(&mut self) -> Result<Vec<SharedMemoryRegion>>;
 }
 
 impl<T> Backend for T
@@ -209,7 +211,7 @@ where
         self.as_mut().check_device_state()
     }
 
-    fn get_shmem_config(&mut self) -> Result<Vec<u64>> {
+    fn get_shmem_config(&mut self) -> Result<Vec<SharedMemoryRegion>> {
         self.as_mut().get_shmem_config()
     }
 }
@@ -656,9 +658,14 @@ impl<S: Backend> BackendServer<S> {
                 res?;
             }
             Ok(FrontendReq::GET_SHMEM_CONFIG) => {
-                let sizes = self.backend.get_shmem_config()?;
-                let msg = VhostUserShMemConfigHeader::new(sizes.len().try_into().unwrap());
-                self.send_reply_with_payload(&hdr, &msg, sizes.as_slice().as_bytes())?;
+                let regions = self.backend.get_shmem_config()?;
+                let msg = VhostUserShMemConfigHeader::new(regions.len().try_into().unwrap());
+                let mut buf = Vec::new();
+                for region in regions {
+                    buf.resize(max(buf.len(), usize::from(region.id) + 1), 0);
+                    buf[usize::from(region.id)] = region.length;
+                }
+                self.send_reply_with_payload(&hdr, &msg, buf.as_bytes())?;
             }
             _ => {
                 return Err(Error::InvalidMessage);

third_party/vmm_vhost/src/lib.rs
@@ -137,6 +137,14 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// Result of request handler.
 pub type HandlerResult<T> = std::result::Result<T, IOError>;
 
+#[derive(Copy, Clone)]
+pub struct SharedMemoryRegion {
+    /// The id of the shared memory region. A device may have multiple regions, but each
+    /// must have a unique id. The meaning of a particular region is device-specific.
+    pub id: u8,
+    pub length: u64,
+}
+
 /// Utility function to convert a vector of files into a single file.
 /// Returns `None` if the vector contains no files or more than one file.
 pub(crate) fn into_single_file(mut files: Vec<File>) -> Option<File> {

third_party/vmm_vhost/src/test_backend.rs
@@ -7,6 +7,7 @@ use crate::message::*;
 use crate::Backend;
 use crate::Error;
 use crate::Result;
+use crate::SharedMemoryRegion;
 
 pub const MAX_QUEUE_NUM: usize = 2;
 pub const MAX_VRING_NUM: usize = 256;
@@ -277,7 +278,7 @@ impl Backend for TestBackend {
         Ok(())
     }
 
-    fn get_shmem_config(&mut self) -> Result<Vec<u64>> {
+    fn get_shmem_config(&mut self) -> Result<Vec<SharedMemoryRegion>> {
         Ok(Vec::new())
     }
 }